bf8c397b3159fbbf9cf379136b7c0c76f7328ef6
NIH\wangh3 <NIH\wangh3@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-05-25 21:11:56 +0000

    -- Added comments to the functions -- Added the response to the key board event.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@489 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/srb/JDialogSetupPipeline.java
+++ trunk/mipav/src/gov/nih/mipav/view/srb/JDialogSetupPipeline.java
@@ -14,9 +14,9 @@ import javax.swing.*;
 
 
 /**
- * DOCUMENT ME!
+ * This dialog is used to set up the parameters of NDARPipeline.
  */
-public class JDialogSetupPipeline extends JDialog implements ActionListener, KeyListener {
+public class JDialogSetupPipeline extends JDialog implements ActionListener {
 
     //~ Instance fields ------------------------------------------------------------------------------------------------
 
@@ -63,32 +63,15 @@ public class JDialogSetupPipeline extends JDialog implements ActionListener, Key
     /**
      * Action event listener.
      *
-     * @param  e  DOCUMENT ME!
+     * @param  e  the action event.
      */
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
 
         if (command.equals("OK")) {
-
-            /**
-             * First check every text field, make sure it has right input.
-             */
-            if (scriptFileNameField.getText().length() == 0) {
-                scriptFileNameField.requestFocus();
-
-                return;
-            }
-
-            if (targetSRBDirField.getText().length() == 0) {
-                targetSRBDirField.requestFocus();
-
-                return;
-            }
-            this.dispose();
-            cancelled = false;
+            approve();
         } else if (command.equals("Cancel")) {
-            this.dispose();
-            cancelled = true;
+            cancel();
         } else if(command.equals("scriptFileNameBrowse")){
             /**
              * Uses the JFileChooser to retrieve the file that the user wants to open.
@@ -160,37 +143,9 @@ public class JDialogSetupPipeline extends JDialog implements ActionListener, Key
         }
     }
 
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  e  DOCUMENT ME!
-     */
-    public void keyPressed(KeyEvent e) { }
 
     /**
-     * DOCUMENT ME!
-     *
-     * @param  e  DOCUMENT ME!
-     */
-    public void keyReleased(KeyEvent e) { }
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param  e  DOCUMENT ME!
-     */
-    public void keyTyped(KeyEvent e) {
-        int keyChar = e.getKeyChar();
-
-        if (keyChar == KeyEvent.VK_ENTER) {
-            actionPerformed(new ActionEvent(this, 10, "OK"));
-        } else if (keyChar == KeyEvent.VK_ESCAPE) {
-            actionPerformed(new ActionEvent(this, 11, "Cancel"));
-        }
-    }
-
-    /**
-     * DOCUMENT ME!
+     * Initializes the GUI of this pipeline.
      */
     private void init() {
         PanelManager manager = new PanelManager();
@@ -202,14 +157,16 @@ public class JDialogSetupPipeline extends JDialog implements ActionListener, Key
         manager.add(scriptFileNameLabel);
 
         // Sets up the script file name field.
-        scriptFileNameField = WidgetFactory.buildTextField("");
+        String scriptFile = Preferences.getScriptFile();
+        if(scriptFile == null){
+            scriptFile = "";
+        }
+        scriptFileNameField = WidgetFactory.buildTextField(scriptFile);
         scriptFileNameField.setColumns(COLUMN_COUNT);
-        scriptFileNameField.addKeyListener(this);
         manager.add(scriptFileNameField);
 
         scriptFileNameBrowseButton = WidgetFactory.buildTextButton("Browse", "Choose the script file", "scriptFileNameBrowse", this);
         scriptFileNameBrowseButton.setPreferredSize(new Dimension(90, 30));
-        scriptFileNameBrowseButton.addKeyListener(this);
         manager.add(scriptFileNameBrowseButton);
 
         // Sets up the srb target directory label.
@@ -220,12 +177,10 @@ public class JDialogSetupPipeline extends JDialog implements ActionListener, Key
         // Sets up the script file name field.
         targetSRBDirField = WidgetFactory.buildTextField("");
         targetSRBDirField.setColumns(COLUMN_COUNT);
-        targetSRBDirField.addKeyListener(this);
         manager.add(targetSRBDirField);
 
         targetSRBDirBrowseButton = WidgetFactory.buildTextButton("Browse", "Choose the target srb directory", "targetSRBDirBrowse", this);
         targetSRBDirBrowseButton.setPreferredSize(new Dimension(90, 30));
-        targetSRBDirBrowseButton.addKeyListener(this);
         manager.add(targetSRBDirBrowseButton);
 
         this.getContentPane().setLayout(new BorderLayout());
@@ -234,15 +189,35 @@ public class JDialogSetupPipeline extends JDialog implements ActionListener, Key
         JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         okButton = WidgetFactory.buildTextButton("  OK  ", "Set up the parameters for the pipeline", "OK", this);
         okButton.setPreferredSize(new Dimension(90, 30));
-        okButton.addKeyListener(this);
         bottomPanel.add(okButton);
 
         cancelButton = WidgetFactory.buildTextButton("Cancel", "Cancel the parameters for the pipeline", "Cancel", this);
         cancelButton.setPreferredSize(new Dimension(90, 30));
         bottomPanel.add(cancelButton);
         this.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+        
         this.pack();
 
+        // Added the responses to the key board.
+        okButton.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false), "Approve");
+        okButton.getActionMap().put("Approve", new AbstractAction("Approve"){
+            public void actionPerformed(ActionEvent e){
+                approve();
+            }
+        });
+        cancelButton.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0, false), "Cancel");
+        
+        cancelButton.getActionMap().put("Cancel", new AbstractAction("Cancel"){
+            public void actionPerformed(ActionEvent e){
+                cancel();
+            }
+        });
+        okButton.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false), "Cancel");
+        okButton.getActionMap().put("Cancel", new AbstractAction("Cancel"){
+            public void actionPerformed(ActionEvent e){
+                cancel();
+            }
+        });
         /**
          * You have to bring these two statement before the setVisible(true), otherwise doesn't work.
          */
@@ -250,15 +225,64 @@ public class JDialogSetupPipeline extends JDialog implements ActionListener, Key
         this.setVisible(true);
     }
     
+    /**
+     * Returns the script file name.
+     * @return the script file name.
+     */
     public String getScriptFileName(){
         return scriptFileNameField.getText();
     }
     
+    /**
+     * Returns the destination directory on the srb server.
+     * @return the destination directory on the srb server.
+     */
     public String getTargetSRBDir(){
         return targetSRBDirField.getText();
     }
     
+    /**
+     * Return true if this dialog was cancelled.
+     * @return true if this dialog was cancelled.
+     */
     public boolean isCancelled(){
         return cancelled;
     }
+    
+    /**
+     * The action need be performed when this dialog was approved.
+     */
+    private void approve(){
+
+        /**
+         * First check every text field, make sure it has right input.
+         */
+        if (scriptFileNameField.getText().length() == 0) {
+            scriptFileNameField.requestFocus();
+
+            return;
+        }
+
+        if (targetSRBDirField.getText().length() == 0) {
+            targetSRBDirField.requestFocus();
+
+            return;
+        }
+        String scriptFileName = scriptFileNameField.getText();
+        Preferences.setScriptFile(scriptFileName);
+        int index = scriptFileName.lastIndexOf(File.separator);
+        Preferences.setScriptsDirectory(scriptFileName.substring(0, index));
+        this.dispose();
+        cancelled = false;
+    }
+    
+    /**
+     * The action need be performed when this dialog was cancelled.
+     *
+     */
+    private void cancel(){
+        this.dispose();
+        cancelled = true;
+    }
 }
+
