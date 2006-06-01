8bee671932a86d307c3d5749d6516b740ace3136
nih\ilb <nih\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-06-01 19:10:09 +0000

    Added call to JDialogDENCLUE.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@520 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewJFrameImage.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewJFrameImage.java
@@ -1732,6 +1732,8 @@ public class ViewJFrameImage extends ViewJFrameBase implements KeyListener, Mous
             }
         } else if (command.equals("histTwoDim")) {
             new JDialogHistogram2Dim(this, getActiveImage());
+        } else if (command.equals("DENCLUE")) {
+            new JDialogDENCLUE(this, getActiveImage());
         } else if (command.equals("colocDENCLUE")) {
             new JDialogColocalizationDENCLUE(this, getActiveImage());
         } else if (command.equals("colocEM")) {
