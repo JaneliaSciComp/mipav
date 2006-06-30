9d67719399670964f85c26b83fecbaf648253139
NIH\wangh3 <NIH\wangh3@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-06-30 16:10:45 +0000

    -- removed the showProgressBar variable from the constructor
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@672 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileNIFTI.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileNIFTI.java
@@ -2053,7 +2053,7 @@ public class FileNIFTI extends FileBase {
 
         if (options.isMultiFile()) {
             FileRaw rawFile;
-            rawFile = new FileRaw(image.getFileInfo(0), true);
+            rawFile = new FileRaw(image.getFileInfo(0));
             flipTopBottom(image);
 
             if (oneFile) {
