d1c6594ff4e683b4e26ae3b72de5f789adf53ac2
NIH\pollackn <NIH\pollackn@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-07-20 15:14:48 +0000

    Updated to new scripting design, extending Scriping Dialog and overriding storeParamsFromGUI() , setGUIFromParams(), and doPostAlgorithmActions()
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@791 ba61647d-9d00-f842-95cd-605cb4296b96

--- branches/scripting/mipav/src/gov/nih/mipav/view/dialogs/JDialogEdgeNMSuppression.java
+++ branches/scripting/mipav/src/gov/nih/mipav/view/dialogs/JDialogEdgeNMSuppression.java
@@ -3,6 +3,8 @@ package gov.nih.mipav.view.dialogs;
 
 import gov.nih.mipav.model.algorithms.*;
 import gov.nih.mipav.model.file.*;
+import gov.nih.mipav.model.scripting.ParserException;
+import gov.nih.mipav.model.scripting.parameters.ParameterFactory;
 import gov.nih.mipav.model.structures.*;
 
 import gov.nih.mipav.view.*;
@@ -20,7 +22,7 @@ import javax.swing.*;
  *
  * @see  AlgorithmGaussianBlur
  */
-public class JDialogEdgeNMSuppression extends JDialogBase implements AlgorithmInterface, ScriptableInterface {
+public class JDialogEdgeNMSuppression extends JDialogScriptableBase implements AlgorithmInterface{
 
     //~ Static fields/initializers -------------------------------------------------------------------------------------
 
@@ -135,26 +137,44 @@ public class JDialogEdgeNMSuppression extends JDialogBase implements AlgorithmIn
     public JDialogEdgeNMSuppression(Frame theParentFrame, ModelImage im) {
         super(theParentFrame, true);
         image = im;
-        userInterface = ((ViewJFrameBase) (parentFrame)).getUserInterface();
+        userInterface = ViewUserInterface.getReference();
         init();
     }
 
-    /**
-     * Used primarily for the script to store variables and run the algorithm. No actual dialog will appear but the set
-     * up info and result image will be stored here.
-     *
-     * @param  UI  The user interface, needed to create the image frame.
-     * @param  im  Source image.
-     */
-    public JDialogEdgeNMSuppression(ViewUserInterface UI, ModelImage im) {
-        super();
-        userInterface = UI;
-        image = im;
-        parentFrame = image.getParentFrame();
-    }
+   
 
     //~ Methods --------------------------------------------------------------------------------------------------------
-
+    /**
+     * Record the parameters just used to run this algorithm in a script.
+     * 
+     * @throws  ParserException  If there is a problem creating/recording the new parameters.
+     */
+    protected void storeParamsFromGUI() throws ParserException{
+        scriptParameters.storeInputImage(image);
+        scriptParameters.storeOutputImageParams(resultImage, (displayLoc == NEW));
+        
+        scriptParameters.getParams().put(ParameterFactory.newParameter("regionFlag",regionFlag));
+        scriptParameters.getParams().put(ParameterFactory.newParameter("image25D",image25D));
+    }
+    
+    /**
+     * Set the dialog GUI using the script parameters while running this algorithm as part of a script.
+     */
+    protected void setGUIFromParams(){
+        regionFlag = scriptParameters.getParams().getBoolean("regionFlag");
+        image25D = scriptParameters.getParams().getBoolean("image25D");
+   }
+    
+    /**
+     * Used to perform actions after the execution of the algorithm is completed (e.g., put the result image in the image table).
+     * Defaults to no action, override to actually have it do something.
+     */
+    protected void doPostAlgorithmActions() {
+        if (displayLoc == NEW) {
+            AlgorithmParameters.storeImageInRunner(getResultImage());
+        }
+    }
+    
     /**
      * Closes dialog box when the OK button is pressed and calls the algorithm.
      *
@@ -215,7 +235,7 @@ public class JDialogEdgeNMSuppression extends JDialogBase implements AlgorithmIn
             }
         }
 
-        insertScriptLine(algorithm);
+        insertScriptLine();
 
         nmSupAlgo.finalize();
         nmSupAlgo = null;
@@ -255,44 +275,7 @@ public class JDialogEdgeNMSuppression extends JDialogBase implements AlgorithmIn
         return resultImage;
     }
 
-    /**
-     * If a script is being recorded and the algorithm is done, add an entry for this algorithm.
-     *
-     * @param  algo  the algorithm to make an entry for
-     */
-    public void insertScriptLine(AlgorithmBase algo) {
-
-        if (algo.isCompleted()) {
-
-            if (userInterface.isScriptRecording()) {
-
-                // check to see if the match image is already in the ImgTable
-                if (userInterface.getScriptDialog().getImgTableVar(image.getImageName()) == null) {
-
-                    if (userInterface.getScriptDialog().getActiveImgTableVar(image.getImageName()) == null) {
-                        userInterface.getScriptDialog().putActiveVar(image.getImageName());
-                    }
-                }
-
-                userInterface.getScriptDialog().append("EdgeNMSuppression " +
-                                                       userInterface.getScriptDialog().getVar(image.getImageName()) +
-                                                       " ");
-
-                if (displayLoc == NEW) {
-                    userInterface.getScriptDialog().putVar(resultImage.getImageName());
-                    userInterface.getScriptDialog().append(userInterface.getScriptDialog().getVar(resultImage.getImageName()) +
-                                                           " " + regionFlag + " " + image25D + " " + scaleX + " " +
-                                                           scaleY + " " + scaleZ);
-                } else {
-                    userInterface.getScriptDialog().append(userInterface.getScriptDialog().getVar(image.getImageName()) +
-                                                           " " + regionFlag + " " + image25D + " " + scaleX + " " +
-                                                           scaleY + " " + scaleZ);
-                }
-
-                userInterface.getScriptDialog().append("\n");
-            }
-        }
-    }
+ 
 
     // *******************************************************************
     // ************************* Item Events ****************************
@@ -331,59 +314,6 @@ public class JDialogEdgeNMSuppression extends JDialogBase implements AlgorithmIn
         }
     }
 
-    /**
-     * Run this algorithm from a script.
-     *
-     * @param   parser  the script parser we get the state from
-     *
-     * @throws  IllegalArgumentException  if there is something wrong with the arguments in the script
-     */
-    public void scriptRun(AlgorithmScriptParser parser) throws IllegalArgumentException {
-        String srcImageKey = null;
-        String destImageKey = null;
-
-        try {
-            srcImageKey = parser.getNextString();
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-        ModelImage im = parser.getImage(srcImageKey);
-
-        image = im;
-        userInterface = image.getUserInterface();
-        parentFrame = image.getParentFrame();
-
-        // the result image
-        try {
-            destImageKey = parser.getNextString();
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-        if (srcImageKey.equals(destImageKey)) {
-            this.setDisplayLocReplace();
-        } else {
-            this.setDisplayLocNew();
-        }
-
-        try {
-            setRegionFlag(parser.getNextBoolean());
-            setImage25D(parser.getNextBoolean());
-            setScaleX(parser.getNextFloat());
-            setScaleY(parser.getNextFloat());
-            setScaleZ(parser.getNextFloat());
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-        setSeparateThread(false);
-        callAlgorithm();
-
-        if (!srcImageKey.equals(destImageKey)) {
-            parser.putVariable(destImageKey, getResultImage().getImageName());
-        }
-    }
 
     /**
      * Accessor that sets the display loc variable to new, so that a new image is created once the algorithm completes.
