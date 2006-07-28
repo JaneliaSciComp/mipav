78f29da46b3976dfa954bc34ba9bf184a918559c
nih\ilb <nih\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-07-28 22:13:47 +0000

    Started work.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@828 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileInfoNRRD.java
@@ -0,0 +1,130 @@
+package gov.nih.mipav.model.file;
+
+
+import gov.nih.mipav.model.structures.*;
+
+import gov.nih.mipav.view.*;
+import gov.nih.mipav.view.dialogs.*;
+
+import java.util.*;
+
+
+/**
+ *
+ * @see  FileNRRD
+ */
+
+public class FileInfoNRRD extends FileInfoBase {
+
+    //~ Static fields/initializers -------------------------------------------------------------------------------------
+
+    /** Use serialVersionUID for interoperability. */
+    //private static final long serialVersionUID = -5506021019885109431L;
+
+    
+    //~ Instance fields ------------------------------------------------------------------------------------------------
+
+    
+    /** Transformation matrix. */
+    private TransMatrix matrix = new TransMatrix(4);
+    
+    private int versionNumber = -1;
+
+    
+    //~ Constructors ---------------------------------------------------------------------------------------------------
+
+    /**
+     * file info storage constructor.
+     *
+     * @param  name       file name
+     * @param  directory  directory
+     * @param  format     file format
+     */
+    public FileInfoNRRD(String name, String directory, int format) {
+        super(name, directory, format);
+    }
+
+    //~ Methods --------------------------------------------------------------------------------------------------------
+
+    /**
+     * Displays the file information.
+     *
+     * @param  dlog    dialog box that is written to
+     * @param  matrix  transformation matrix
+     */
+    public void displayAboutInfo(JDialogBase dlog, TransMatrix matrix) {
+        JDialogFileInfo dialog = (JDialogFileInfo) dlog;
+        int[] extents;
+        int i;
+        int[] editorChoice = new int[1];
+        editorChoice[0] = JDialogEditor.STRING;
+
+        dialog.displayAboutInfo(this); // setup layout in the dialog
+
+        extents = super.getExtents();
+
+        for (i = 0; i < extents.length; i++) {
+            dialog.appendPrimaryData("Dimension " + i, Integer.toString(extents[i]));
+        }
+
+        dialog.appendPrimaryData("Type", ModelStorageBase.getBufferTypeStr(getDataType()));
+
+        if (ModelImage.isColorImage(getDataType())) {
+            dialog.appendPrimaryData("Min red", Double.toString(getMinR()));
+            dialog.appendPrimaryData("Max red", Double.toString(getMaxR()));
+            dialog.appendPrimaryData("Min green", Double.toString(getMinG()));
+            dialog.appendPrimaryData("Max green", Double.toString(getMaxG()));
+            dialog.appendPrimaryData("Min blue", Double.toString(getMinB()));
+            dialog.appendPrimaryData("Max blue", Double.toString(getMaxB()));
+
+        } else {
+            dialog.appendPrimaryData("Min", Double.toString(getMin()));
+            dialog.appendPrimaryData("Max", Double.toString(getMax()));
+        }
+
+        dialog.appendPrimaryData("Modality", FileInfoBase.getModalityStr(getModality()));
+
+        dialog.appendPrimaryData("Orientation", getImageOrientationStr(getImageOrientation()));
+
+        float[] resolutions; // = new float[5];
+        resolutions = getResolutions();
+
+        int[] measure; // = new int[5];
+        measure = getUnitsOfMeasure();
+
+        for (i = 0; i < extents.length; i++) {
+
+            if (resolutions[i] > 0.0) {
+                String pixelRes = "Pixel resolution " + i;
+                dialog.appendPrimaryData(pixelRes,
+                                         Float.toString(resolutions[i]) + " " + getUnitsOfMeasureStr(measure[i]));
+            } // end of if (resolutions[i] > 0.0)
+        } // for (i=0; i < 5; i++)
+
+        if (getEndianess() == FileBase.LITTLE_ENDIAN) {
+            dialog.appendPrimaryData("Endianess", "Little Endian");
+        } else {
+            dialog.appendPrimaryData("Endianess", "Big Endian");
+        }
+
+        if (matrix != null) {
+
+            // when using displayAboutInfo(dialog) this doesn't appear
+            // calling prg might use an editing panel to adjust this matrix
+            dialog.appendPrimaryData("Matrix", matrix.matrixToString(10, 4));
+        }
+        
+        if (versionNumber != -1) {
+            dialog.appendSecondaryData("File format version", String.valueOf(versionNumber));
+        }
+    }
+    
+    /**
+     * 
+     * @param versionNumber
+     */
+    public void setVersionNumber(int versionNumber) {
+        this.versionNumber = versionNumber;
+    }
+  
+}
