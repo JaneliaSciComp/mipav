447e51a80034d127511db473855713c8c94ca4c0
NIH\linkb <NIH\linkb@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-07-31 15:51:27 +0000

    updated for new scripting
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@831 ba61647d-9d00-f842-95cd-605cb4296b96

--- branches/scripting/mipav/src/gov/nih/mipav/view/dialogs/JDialogWaveletThreshold.java
+++ branches/scripting/mipav/src/gov/nih/mipav/view/dialogs/JDialogWaveletThreshold.java
@@ -3,6 +3,8 @@ package gov.nih.mipav.view.dialogs;
 
 import gov.nih.mipav.model.algorithms.*;
 import gov.nih.mipav.model.algorithms.filters.*;
+import gov.nih.mipav.model.scripting.ParserException;
+import gov.nih.mipav.model.scripting.parameters.ParameterFactory;
 import gov.nih.mipav.model.structures.*;
 
 import gov.nih.mipav.view.*;
@@ -18,7 +20,7 @@ import javax.swing.*;
 /**
  * DOCUMENT ME!
  */
-public class JDialogWaveletThreshold extends JDialogBase implements AlgorithmInterface, ScriptableInterface {
+public class JDialogWaveletThreshold extends JDialogScriptableBase implements AlgorithmInterface {
 
     //~ Static fields/initializers -------------------------------------------------------------------------------------
 
@@ -219,7 +221,10 @@ public class JDialogWaveletThreshold extends JDialogBase implements AlgorithmInt
         // Update frame
         // ((ViewJFrameBase)parentFrame).updateImages(true);
 
-        insertScriptLine(algorithm);
+        if (algorithm.isCompleted()) {
+            insertScriptLine();
+        }
+        
 
         waveletAlgo.finalize();
         waveletAlgo = null;
@@ -234,45 +239,51 @@ public class JDialogWaveletThreshold extends JDialogBase implements AlgorithmInt
     public ModelImage getResultImage() {
         return resultImage;
     }
-
+    
     /**
-     * If a script is being recorded and the algorithm is done, add an entry for this algorithm.
-     *
-     * @param  algo  the algorithm to make an entry for
+     * {@inheritDoc}
      */
-    public void insertScriptLine(AlgorithmBase algo) {
-
-        if (algo.isCompleted()) {
-
-            if (userInterface.isScriptRecording()) {
-
-                // check to see if the match image is already in the ImgTable
-                if (userInterface.getScriptDialog().getImgTableVar(image.getImageName()) == null) {
-
-                    if (userInterface.getScriptDialog().getActiveImgTableVar(image.getImageName()) == null) {
-                        userInterface.getScriptDialog().putActiveVar(image.getImageName());
-                    }
-                }
-
-                userInterface.getScriptDialog().append("WaveletThreshold " +
-                                                       userInterface.getScriptDialog().getVar(image.getImageName()) +
-                                                       " ");
-
-                if (displayLoc == NEW) {
-                    userInterface.getScriptDialog().putVar(resultImage.getImageName());
-                    userInterface.getScriptDialog().append(userInterface.getScriptDialog().getVar(resultImage.getImageName()) +
-                                                           " " + cNum + " " + thresholdType + " " + threshold + " " +
-                                                           doWaveletImage + "\n");
-                } else {
-                    userInterface.getScriptDialog().append(userInterface.getScriptDialog().getVar(image.getImageName()) +
-                                                           " " + cNum + " " + thresholdType + " " + threshold + " " +
-                                                           doWaveletImage + "\n");
-
-                }
-            }
+    protected void storeParamsFromGUI() throws ParserException {
+        scriptParameters.storeInputImage(image);
+        scriptParameters.storeOutputImageParams(resultImage, (displayLoc == NEW));
+        
+        scriptParameters.getParams().put(ParameterFactory.newParameter("cNum",cNum));
+        scriptParameters.getParams().put(ParameterFactory.newParameter("thresholdType",thresholdType));
+        scriptParameters.getParams().put(ParameterFactory.newParameter("threshold",threshold));
+        scriptParameters.getParams().put(ParameterFactory.newParameter("doWaveletImage",doWaveletImage));
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected void setGUIFromParams() {
+        image = scriptParameters.retrieveInputImage();
+        
+        if (scriptParameters.getParams().getBoolean(AlgorithmParameters.DO_OUTPUT_NEW_IMAGE)) {
+            this.setDisplayLocNew();
+        } else {
+            this.setDisplayLocReplace();
+        }
+        
+        userInterface = image.getUserInterface();
+        parentFrame = image.getParentFrame();
+        
+        cNum = scriptParameters.getParams().getInt("cNum");
+        thresholdType = scriptParameters.getParams().getInt("thresholdType");
+        threshold = scriptParameters.getParams().getFloat("threshold");
+        doWaveletImage = scriptParameters.getParams().getBoolean("doWaveletImage");
+        
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    protected void doPostAlgorithmActions() {
+        if (displayLoc == NEW) {
+            AlgorithmParameters.storeImageInRunner(resultImage);
         }
     }
-
+    
     // *******************************************************************
     // ************************* Item Events ****************************
     // *******************************************************************
@@ -286,59 +297,6 @@ public class JDialogWaveletThreshold extends JDialogBase implements AlgorithmInt
         // Object source = event.getSource();
         // float tempNum;
 
-    }
-
-    /**
-     * Run this algorithm from a script.
-     *
-     * @param   parser  the script parser we get the state from
-     *
-     * @throws  IllegalArgumentException  if there is something wrong with the arguments in the script
-     */
-    public void scriptRun(AlgorithmScriptParser parser) throws IllegalArgumentException {
-        String srcImageKey = null;
-        String destImageKey = null;
-
-        try {
-            srcImageKey = parser.getNextString();
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-        ModelImage im = parser.getImage(srcImageKey);
-
-        image = im;
-        userInterface = image.getUserInterface();
-        parentFrame = image.getParentFrame();
-
-        // the result image
-        try {
-            destImageKey = parser.getNextString();
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-        if (srcImageKey.equals(destImageKey)) {
-            this.setDisplayLocReplace();
-        } else {
-            this.setDisplayLocNew();
-        }
-
-        try {
-            setCNum(parser.getNextInteger());
-            setThresholdType(parser.getNextInteger());
-            setThreshold(parser.getNextFloat());
-            setDoWaveletImage(parser.getNextBoolean());
-        } catch (Exception e) {
-            throw new IllegalArgumentException();
-        }
-
-        setSeparateThread(false);
-        callAlgorithm();
-
-        if (!srcImageKey.equals(destImageKey)) {
-            parser.putVariable(destImageKey, getResultImage().getImageName());
-        }
     }
 
     /**
