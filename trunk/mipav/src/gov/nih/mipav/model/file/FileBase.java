d5d3f3fb1c0a101eefdfbb0cb5def6068505a9e6
nih\ilb <nih\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-08-08 17:50:13 +0000

    Added GE Signa 4x file format.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@873 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileBase.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileBase.java
@@ -154,6 +154,9 @@ public abstract class FileBase{
     
     /** Nearly raw raster data */
     public static final int NRRD = 42;
+    
+    /** GE Signa 4.x */
+    public static final int GE_SIGNA4X = 43;
 
     /** Arrary of strings describing the file formats. */
     private static String[] fileFormatStr = {
@@ -161,7 +164,7 @@ public abstract class FileBase{
         "MINC", "Avi", "Analyze multifile", "QT", "Chesire", "Chesire Overlay", "Afni", "FITS", "STK",
         "Magnetom Vision", "GE Genesis", "MRC", "Interfile", "Raw multifile", "Micro CAT", "LSM", "Bio-Rad", "COR",
         "BRUKER", "XML", "XML multifile", "SPM", "Project", "NIFTI", "NIFTI multifile", "ICS", "LSM multifile", "TMG",
-        "OSM", "Surface XML", "DM3", "NRRD"
+        "OSM", "Surface XML", "DM3", "NRRD", "GE Signa4x"
     };
 
     /** Byte order. Rightmost byte is most significant. */
