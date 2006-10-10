b7a2eb00231a77c1c729c229a696188055ba0137
NIH\mccreedy <NIH\mccreedy@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-10-10 20:27:52 +0000

    - Refactored the ImageRegistryMonitor thread to a separate class so that it can be used in places other than ViewJFrameRegisteredImages.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@1097 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewJFrameRegisteredImages.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewJFrameRegisteredImages.java
@@ -1,8 +1,8 @@
 package gov.nih.mipav.view;
 
 
-import gov.nih.mipav.model.scripting.ScriptRecorder;
-import gov.nih.mipav.model.scripting.actions.ActionCollectGarbage;
+import gov.nih.mipav.model.scripting.*;
+import gov.nih.mipav.model.scripting.actions.*;
 import gov.nih.mipav.model.structures.*;
 
 import java.awt.*;
@@ -206,10 +206,10 @@ public class ViewJFrameRegisteredImages extends JFrame
         buttonPan.add(callToFrontbutton);
         buttonPan.add(callDeletebutton);
         buttonPan.add(callGCbutton);
-        
+
         this.getContentPane().add(buttonPan, BorderLayout.SOUTH);
 
-        surf = new ImageRegistryMonitor(UI);
+        surf = new ImageRegistryMonitor();
         surf.addImageRegistryChangeListener(this);
         sampleRate.addFocusListener(surf);
 
@@ -356,18 +356,10 @@ public class ViewJFrameRegisteredImages extends JFrame
      * @param  event  DOCUMENT ME!
      */
     public void valueChanged(ListSelectionEvent event) {
-        //        if (event.getValueIsAdjusting()) {
-        //            System.out.println("...is adjusting....");
-        //            return;
-        //        }
-        //        JList list = (JList)event.getSource();
-        //        System.out.println("value changed.");
-        //        if (list.isSelectionEmpty()) {
-        //            return;
-        //        }
-        //        String selectedName = (String) list.getSelectedValue();
-        //        imageToFront(selectedName);
-        //        System.out.println("ValueChanged");
+        //        if (event.getValueIsAdjusting()) {            System.out.println("...is adjusting...."); return;
+        //  }        JList list = (JList)event.getSource();        System.out.println("value changed.");      if
+        // (list.isSelectionEmpty()) {            return;        }        String selectedName = (String)
+        // list.getSelectedValue();        imageToFront(selectedName);        System.out.println("ValueChanged");
     }
 
 
@@ -430,170 +422,6 @@ public class ViewJFrameRegisteredImages extends JFrame
     }
 
     //~ Inner Classes --------------------------------------------------------------------------------------------------
-
-    /**
-     * Tracks registered images, and notifies anybody who is interested in finding out about them.
-     */
-    public class ImageRegistryMonitor implements Runnable, FocusListener {
-
-        /** DOCUMENT ME! */
-        private ChangeEvent changeEvent;
-
-        /** DOCUMENT ME! */
-        private EventListenerList listenerList;
-
-        /** DOCUMENT ME! */
-        private Vector registeredNames = new Vector();
-
-        /** DOCUMENT ME! */
-        private long sleepAmount = 5000;
-
-        /** DOCUMENT ME! */
-        private volatile Thread thread;
-
-        /** DOCUMENT ME! */
-        private ViewUserInterface UI;
-
-        /**
-         * Constructor. Creates the list for listeners
-         *
-         * @param  ui  DOCUMENT ME!
-         */
-        public ImageRegistryMonitor(ViewUserInterface ui) {
-            this.UI = ui;
-            listenerList = new EventListenerList();
-        }
-
-
-        // ****************** change listener support
-        /**
-         * add a memory change listener.
-         *
-         * @param  l  DOCUMENT ME!
-         */
-        public void addImageRegistryChangeListener(ChangeListener l) {
-            (listenerList).add(ChangeListener.class, l);
-        }
-
-
-        // **************
-        // FOCUS LISTENER
-        /**
-         * focus gained.
-         *
-         * @param  fe  DOCUMENT ME!
-         */
-        public void focusGained(FocusEvent fe) { }
-
-        /**
-         * when focus is lost the source is assumed to be a JTextField, and it sets the sample period.
-         *
-         * @param  fe  DOCUMENT ME!
-         */
-        public void focusLost(FocusEvent fe) {
-            Object source = fe.getSource();
-
-            if (!fe.isTemporary()) {
-
-                if (source instanceof JTextField) {
-                    sleepAmount = Long.parseLong(((JTextField) source).getText());
-                }
-            }
-        }
-
-        /**
-         * The name of images currently registered.
-         *
-         * @return  Vector of image names
-         */
-        public Vector getRegisteredNames() {
-            return (Vector) (registeredNames);
-        }
-
-        /**
-         * removes the change listener.
-         *
-         * @param  l  DOCUMENT ME!
-         */
-        public void removeImageRegistryChangeListener(ChangeListener l) {
-            (listenerList).remove(ChangeListener.class, l);
-        }
-
-
-        /**
-         * when the thread wakes up, it collects information from the runtime on current memory status. It then notifies
-         * all listeners.
-         */
-        public void run() {
-            Thread me = Thread.currentThread();
-
-            while (thread == me) {
-                registeredNames.clear();
-
-                Enumeration e = UI.getRegisteredImageNames();
-
-                while (e.hasMoreElements()) {
-                    registeredNames.addElement(e.nextElement());
-                }
-
-                fireImageRegistryChanged(); // too many elements and we waste
-                                            // time here...and be thread-selfish
-
-                try {
-                    thread.sleep(sleepAmount);
-                } catch (InterruptedException ie) {
-                    break;
-                }
-            }
-
-            thread = null;
-        }
-
-        /**
-         * Start the thread as a minimum priority thread.
-         */
-        public void start() {
-            thread = new Thread(this);
-            thread.setPriority(Thread.MIN_PRIORITY);
-            thread.setName("ImageRegistryMonitor");
-            thread.start();
-        }
-
-
-        /**
-         * kill the thread.
-         */
-        public void stop() {
-            thread = null;
-        }
-
-        /**
-         * Notify all listeners that have registered interest for notification on this event type. The event instance is
-         * lazily created using the parameters passed into the fire method.
-         */
-        protected void fireImageRegistryChanged() {
-
-            // Guaranteed to return a non-null array
-            Object[] listeners = (listenerList).getListenerList();
-
-            // Process the listeners last to first, notifying
-            // those that are interested in this event
-            for (int i = listeners.length - 2; i >= 0; i -= 2) {
-
-                if (listeners[i] == ChangeListener.class) {
-
-                    // Lazily create the event:
-                    if (changeEvent == null) {
-                        changeEvent = new ChangeEvent(this);
-                    }
-
-                    ((ChangeListener) listeners[i + 1]).stateChanged(changeEvent);
-
-                }
-            }
-        }
-
-    } // end class ImageRegistryMonitor
 
     /**
      * Identifies components that can be used as "rubber stamps" to paint the cells in a JList.
