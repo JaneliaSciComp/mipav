62f6b6f11d805b097a09eb5b80d2aff059565e88
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-10-17 16:09:51 +0000

    changed the position of the advanced paint icon
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@1130 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewToolBarBuilder.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewToolBarBuilder.java
@@ -398,11 +398,12 @@ public class ViewToolBarBuilder implements ItemListener {
         paintToolBar.add(makeSeparator());
 
         paintToolBar.add(buildToggleButton("PaintBrush", "Draw using a brush.", "brush", VOIGroup));
+        paintToolBar.add(buildButton("AdvancedPaint", "Load advanced paint tools", "advancedpaint"));
         paintToolBar.add(buildToggleButton("Dropper", "Picks up a color from the image.", "dropper", VOIGroup));
         paintToolBar.add(buildToggleButton("PaintCan", "Fills an area with desired color.", "paintcan", VOIGroup));
         paintToolBar.add(buildToggleButton("Eraser", "Erases paint.", "eraser", VOIGroup));
         //using a diff icon until an icon is ready for me to use
-        paintToolBar.add(buildButton("AdvancedPaint", "Load advanced paint tools", "advancedpaint"));
+        
         if (nDim > 2) {
             paintToolBar.add(buildButton("EraseCurrent", "Erase paint from current frame", "clearcurrent"));
         }
