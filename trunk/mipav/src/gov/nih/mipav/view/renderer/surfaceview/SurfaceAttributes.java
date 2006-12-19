e633b36ed46245b3d6f5388e66c971988f0ca89c
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2006-12-19 17:37:37 +0000

    comments added.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@1392 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/surfaceview/SurfaceAttributes.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/surfaceview/SurfaceAttributes.java
@@ -8,16 +8,19 @@ import javax.vecmath.*;
 
 
 /**
- * Class that holds the information about each surface; the BranchGroup which holds the surface subtree, the name of the
- * surface in the dialog, the color of the surface, the shininess of the surface, the level of detail (for clod meshes),
- * the number of triangles (changes with level of detail), the polygon mode (fill, line, or point), and a flag
- * indicating if this is a clod mesh.
+ * Class that holds the information about each surface; the BranchGroup which
+ * holds the surface subtree, the name of the surface in the dialog, the color
+ * of the surface, the shininess of the surface, the level of detail (for clod
+ * meshes), the number of triangles (changes with level of detail), the
+ * polygon mode (fill, line, or point), and a flag indicating if this is a
+ * clod mesh.
  */
 public class SurfaceAttributes {
 
     //~ Instance fields ------------------------------------------------------------------------------------------------
 
-    /** Area of triangle mesh displayed associated with the surface. Changes with level of detail. */
+    /** Area of triangle mesh displayed associated with the surface. Changes
+     * with level of detail. */
     private float mArea;
 
     /** Surface center. */
@@ -33,8 +36,9 @@ public class SurfaceAttributes {
     private Shape3D[] mSurfaceShape;
 
     /**
-     * Color of surface. The element type is Color4f, although currently the alpha channel does not appear to be
-     * supported by Java3D materials (even though the Material API allows the alpha channel to be set for diffuse
+     * Color of surface. The element type is Color4f, although currently the
+     * alpha channel does not appear to be supported by Java3D materials (even
+     * though the Material API allows the alpha channel to be set for diffuse
      * colors).
      */
     private Color4f mColor;
@@ -46,8 +50,9 @@ public class SurfaceAttributes {
     private String mFullPath;
 
     /**
-     * <code>true</code> indicates that this surface is a clod mesh, <code>false</code> that it is a standard triangle
-     * mesh; clod meshes can change level of detail, triangle meshes cannot.
+     * <code>true</code> indicates that this surface is a clod mesh,
+     * <code>false</code> that it is a standard triangle mesh; clod meshes can
+     * change level of detail, triangle meshes cannot.
      */
     private boolean mIsClodMesh;
 
@@ -61,32 +66,47 @@ public class SurfaceAttributes {
     private float mOpacity;
 
     /**
-     * Polygon mode of surface, one of PolygonAttribute.POLYGON_FILL, PolygonAttribute.POLYGON_LINE, or
-     * PolygonAttribute.POLYGON_POINT.
+     * Polygon mode of surface, one of PolygonAttribute.POLYGON_FILL,
+     * PolygonAttribute.POLYGON_LINE, or PolygonAttribute.POLYGON_POINT.
      */
     private int mPolygonMode;
 
     /** Shininess of surface. */
     private int mShininess;
 
-    /** Surface subtree, holds all the Shape3D objects that make up the surface. */
+    /** Surface subtree, holds all the Shape3D objects that make up the
+     * surface. */
     private BranchGroup mSurfaceBranchGroup;
 
     /** Surface volume bit set mask. */
     private BitSet mSurfaceMask;
 
-    /** Number of triangles displayed associated with the surface. Changes with level of detail. */
+    /** Number of triangles displayed associated with the surface. Changes
+     * with level of detail. */
     private int mNumberTriangles;
 
-    /** Volume of triangle mesh displayed associated with the surface. Changes with level of detail. */
+    /** Volume of triangle mesh displayed associated with the surface. Changes
+     * with level of detail. */
     private float mVolume;
 
+    /** Surface Material object. */
     private Material mMaterial;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
 
-    public SurfaceAttributes( ModelTriangleMesh[] mesh, String fullPath, String name, int triangles, float volume, float area, Point3f center )
+    /** Constructor. Most common usage of SurfaceAttributes. 
+     * @param mesh, array of ModelTriangleMesh objects the SurfaceAttributes describes
+     * @param fullPath, directory/file name containing the triangle mesh
+     * @param name, file name
+     * @param triangles, mesh triangle count 
+     * @param volume, volume enclosed in mesh
+     * @param area, mesh surface area
+     * @param center, 3D center point of mesh.
+     */
+    public SurfaceAttributes( ModelTriangleMesh[] mesh,
+                              String fullPath, String name,
+                              int triangles, float volume, float area, Point3f center )
     {
         this.mTriangleMesh = mesh;
         this.mClodMesh = null;
@@ -104,31 +124,53 @@ public class SurfaceAttributes {
         this.mIsClodMesh = false;
         this.mOpacity = 0.5f;
         this.mSurfaceMask = null;
-        
+        /* sets the default color */
         this.setColor( new Color4f( 1, 0, 0, 1 ) ); 
     }
 
+    // Access functions: 
+
+    /**
+     * sets the array of ModelTriangleMesh objects:
+     * @param mesh, array of ModelTriangleMesh objects.
+     */
     public void setMesh( ModelTriangleMesh[] mesh )
     {
         mTriangleMesh = mesh;
     }
 
-
+    /**
+     * returns the array of ModelTriangleMesh objects.
+     * @return mTriangleMesh, the array of ModelTriangleMesh objects.
+    */
     public ModelTriangleMesh[] getMesh()
     {
         return mTriangleMesh;
     }
 
+    /**
+     * sets the array of Shape3D objects:
+     * @param surfaceShape, array of Shape3D objects.
+     */
     public void setShape( Shape3D[] surfaceShape )
     {
         mSurfaceShape = surfaceShape;
     }
 
+    /**
+     * returns the array of Shape3D objects.
+     * @return mSurfaceShape, the array of Shape3D objects.
+     */
     public Shape3D[] getShape()
     {
         return mSurfaceShape;
     }
 
+    /**
+     * sets the surface Material. the Material diffuse color is used to set
+     * the color value as well.
+     * @param material, new surface Material.
+     */
     public void setMaterial( Material material )
     {
         mMaterial = material;
@@ -139,77 +181,140 @@ public class SurfaceAttributes {
         mColor.z = diffuse.z;
     }
 
+    /**
+     * returns the Material.
+     * @return mMaterial, the surface Material.
+     */
     public Material getMaterial()
     {
         return mMaterial;
     }
 
+    /**
+     * sets the surface Opacity. 
+     * @param opacity, new opacity value.
+     */
     public void setOpacity( float opacity )
     {
         mOpacity = opacity;
     }
 
+    /**
+     * returns the surface opacity
+     * @return mOpacity, surface opacity.
+     */
     public float getOpacity()
     {
         return mOpacity;
     }
 
+    /**
+     * sets the level of detail for the ClodMesh
+     * @param levelDetail, the level of detail
+     */
     public void setLevelDetail( int levelDetail )
     {
         mLevelDetail = levelDetail;
     }
 
+    /**
+     * returns the level of detail
+     * @return mLevelDetail, the level of detail
+     */
     public int getLevelDetail()
     {
         return mLevelDetail;
     }
 
-
+    /**
+     * sets the BranchGroup in the scene graph that renders the surface.
+     * @param branch, BranchGroup in the scenegraph
+     */
     public void setBranch( BranchGroup branch )
     {
         mSurfaceBranchGroup = branch;
     }
     
+    /**
+     * returns the BranchGroup from the scene graph
+     * @return mSurfaceBranchGroup, the BranchGroup containing the mesh
+     */
     public BranchGroup getBranch() 
     {
         return mSurfaceBranchGroup;
     }
 
+    /**
+     * Sets the volume mask data -- the intersection of the triangle mesh with
+     * the volume data.
+     * @param mask, the BitSet mask data
+     */
     public void setMask( BitSet mask )
     {
         mSurfaceMask = mask;
     }
 
+    /**
+     * returns the surface mask data
+     * @return mSurfaceMask, intersection of the triangle mesh with the volume data.
+     */
     public BitSet getMask()
     {
         return mSurfaceMask;
     }
 
+    /**
+     * Returns true is the mesh described in the SurfaceAttributes is a VOI.
+     * @return mIsVOIPt, true when the mesh is a VOI, false otherwise.
+     */
     public boolean getIsVOIPt()
     {
         return mIsVOIPt;
     }
 
+    /**
+     * Sets the clod mesh flag.
+     * @param isClod, when true the mesh is of type ClodMesh, when false the
+     * mesh is ModelTriangleMesh
+     */
     public void setIsClodMesh( boolean isClod )
     {
         mIsClodMesh = isClod;
     }
 
+    /**
+     * Returns if the mesh is type ClodMesh or ModelTriangleMesh.
+     * @return mIsClodMesh, true when the mesh is type ClodMesh, false when
+     * the mesh is type ModelTriangleMesh
+     */
     public boolean getIsClodMesh()
     {
         return mIsClodMesh;
     }
 
+    /**
+     * Returns the file name of the surface, if the surface was loaded from file.
+     * @return mName, the surface file name, or surface name.
+     */
     public String getName()
     {
         return mName;
     }
     
+    /**
+     * Returns the file pathname of the surface, if the surface was loaded from file.
+     * @return mFullPath, the surface pathname, or null.
+     */
     public String getFullPath()
     {
         return mFullPath;
     }
 
+    /**
+     * Sets the surface color. Also sets the Material diffuse, specular, and
+     * ambient colors.
+     * @param color, the new surface Color.
+     */
     public void setColor( Color4f color )
     {
         mColor = color;
@@ -225,51 +330,93 @@ public class SurfaceAttributes {
         mMaterial.setAmbientColor( mColor.x, mColor.y, mColor.z);
     }
 
+    /**
+     * Gets the surface color.
+     * @return mColor, the surface color. 
+     */
     public Color4f getColor()
     {
         return mColor;
     }
 
+    /**
+     * Sets the surface polygon mode. PolygonAttributes.POLYGON_FILL,
+     * PolygonAttributes.POLYGON_LINE, or PolygonAttributes.POLYGON_POINT
+     * @param mode, PolygonAttributes mode
+     */
     public void setPolygonMode( int mode )
     {
         mPolygonMode = mode;
     }
 
+    /**
+     * Returns the polgyon mode.
+     * @return mPolygonMode PolygonAttributes.POLYGON_FILL,
+     * PolygonAttributes.POLYGON_LINE, or PolygonAttributes.POLYGON_POINT
+     */
     public int getPolygonMode()
     {
         return mPolygonMode;
     }
 
+    /**
+     * Sets the value of the surface volume.
+     * @param volume, the surface volume contained in the ModelTriangleMesh objects.
+     */
     public void setVolume( float volume )
     {
         mVolume = volume;
     }
 
+    /**
+     * Gets the surface volume.
+     * @return the volume contained within the ModelTriangleMesh objects.
+     */
     public float getVolume()
     {
         return mVolume;
     }
 
+    /**
+     * Sets the value of the surface area of the ModelTriangleMesh objects.
+     * @param area, the surface area of the ModelTriangleMesh objects.
+     */
     public void setArea( float area )
     {
         mArea = area;
     }
 
+    /**
+     * Return the surface area of the ModelTriangleMesh objects.
+     * @return area, the surface area of the ModelTriangleMesh objects.
+     */
     public float getArea()
     {
         return mArea;
     }
 
+    /**
+     * Sets the number of triangles of the ModelTriangleMesh objects.
+     * @param nTriangles, the number of triangles of the ModelTriangleMesh objects.
+     */
     public void setNumberTriangles( int nTriangles )
     {
         mNumberTriangles = nTriangles;
     }
 
+    /**
+     * Gets the number of triangles of the ModelTriangleMesh objects.
+     * @return nNumberTriangles, the number of triangles of the ModelTriangleMesh objects.
+     */
     public int getNumberTriangles()
     {
         return mNumberTriangles;
     }
 
+    /**
+     * Gets the center of the ModelTriangleMesh objects.
+     * @return mCenter, the center of the ModelTriangleMesh objects.
+     */
     public Point3f getCenter()
     {
         return mCenter;
