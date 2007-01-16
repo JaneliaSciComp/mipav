f5329820163c6e89abd980ba454024a3ef4385df
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2007-01-16 18:26:01 +0000

    Edited file for Advanced Paint...users can change mask numbers
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@1453 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogChangeMaskNumber.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogChangeMaskNumber.java
@@ -3,6 +3,8 @@ package gov.nih.mipav.view.dialogs;
 
 
 
+import gov.nih.mipav.model.structures.ModelImage;
+import gov.nih.mipav.model.structures.ModelLUT;
 import gov.nih.mipav.view.MipavUtil;
 
 import java.awt.*;
@@ -44,6 +46,17 @@ public class JDialogChangeMaskNumber extends JDialogBase {
     /** this represents the current index of the btArrayList */
     private int currentMaskNumberIndex;
     
+    /** re f to Color array **/
+    private Color[] color;
+    
+    /** lutB **/
+    private ModelLUT lutB;
+    
+    /** ref to image **/
+    private ModelImage image;
+    
+
+    
   
 
 	public void actionPerformed(ActionEvent e) {
@@ -52,16 +65,13 @@ public class JDialogChangeMaskNumber extends JDialogBase {
 			for(Iterator iter = btArrayList.iterator();iter.hasNext();) {
 				Integer num = (Integer)iter.next();
 				int numCheck = num.intValue();
-				if(((Integer.valueOf(srcButton.getText())).intValue()) == numCheck) {
-					currentMaskNumberIndex =btArrayList.indexOf(num);
-				}
 				try {
 					if(((Integer.valueOf(numberField.getText())).intValue()) == numCheck) {
 						MipavUtil.displayError("Must enter a unique numeric value less than 255");
 						return;
 					}
 					if (((Integer.valueOf(numberField.getText())).intValue()) > 255) {
-						MipavUtil.displayError("Must enter a unique numeric value less than 255");
+						MipavUtil.displayError("Must enter a numeric value less than 255");
 						return;
 					}
 				}
@@ -70,9 +80,20 @@ public class JDialogChangeMaskNumber extends JDialogBase {
 					return;
 				}
 			}
+
+			int newValue = (new Integer(numberField.getText())).intValue();
+			//since the color array starts at 1
+			color[currentMaskNumberIndex] = lutB.getColor(newValue);
 			srcButton.setText(numberField.getText());
 			correspButton.setText(numberField.getText());
-			btArrayList.set(currentMaskNumberIndex,new Integer(numberField.getText()));
+			srcButton.setBackground(color[currentMaskNumberIndex]);
+			correspButton.setBackground(color[currentMaskNumberIndex]);
+			btArrayList.set(currentMaskNumberIndex - 1,new Integer(numberField.getText()));
+			image.getParentFrame().getComponentImage().setIntensityDropper((float) newValue);
+            image.getParentFrame().getControls().getTools().setPaintColor(color[currentMaskNumberIndex]);
+            //retrieve the mask
+            BitSet obj = image.getParentFrame().getComponentImage().getPaintMask();
+            refreshImagePaint(image, obj);
 			dispose();
 		}
 
@@ -80,14 +101,18 @@ public class JDialogChangeMaskNumber extends JDialogBase {
 	
 
 	
-	public JDialogChangeMaskNumber(JButton theSrcButton, JButton theCorrespButton, ArrayList buttonTextArrayList) {
-		init(theSrcButton,theCorrespButton,buttonTextArrayList);
+	public JDialogChangeMaskNumber(JButton theSrcButton, JButton theCorrespButton, ArrayList buttonTextArrayList, Color[] color, ModelLUT lutB, ModelImage image, int selected) {
+		init(theSrcButton,theCorrespButton,buttonTextArrayList,color,lutB,image,selected);
 	}
 	
-	public void init(JButton theSrcButton, JButton theCorrespButton, ArrayList buttonTextArrayList) {
+	public void init(JButton theSrcButton, JButton theCorrespButton, ArrayList buttonTextArrayList, Color[] color, ModelLUT lutB, ModelImage image, int selected) {
 		
 		srcButton = theSrcButton;
 		correspButton = theCorrespButton;
+		this.color = color;
+		this.lutB = lutB;
+		this.image = image;
+		currentMaskNumberIndex = selected;
 		
 		setTitle("Change Mask Number");
 		
@@ -110,8 +135,34 @@ public class JDialogChangeMaskNumber extends JDialogBase {
 		getContentPane().add(numberField);
 		getContentPane().add(changeButton);
 		
+
+		
 		
 	}
+	
+	
+	/**
+     * Refreshes the displayed paint mask.
+     *
+     * @param  img  the image
+     * @param  obj  mask
+     */
+    private void refreshImagePaint(ModelImage img, BitSet obj) {
+
+        // replace it by previous
+        img.getParentFrame().getComponentImage().setPaintMask(obj);
+        img.setMask(obj);
+
+        // show result
+        img.getParentFrame().updateImages(true);
+
+        if (img.getTriImageFrame() != null) {
+            img.getTriImageFrame().getTriImage(0).setPaintMask(obj);
+            img.getTriImageFrame().getTriImage(1).setPaintMask(obj);
+            img.getTriImageFrame().getTriImage(2).setPaintMask(obj);
+            img.getTriImageFrame().updateImages(true);
+        }
+    }
 	
 	
 
