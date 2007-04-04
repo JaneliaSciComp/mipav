deb2db2b2bc3d723f7de247d5e92c0c17a3c4072
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2007-04-04 19:33:47 +0000

    added sort capability to left side panel....also added new Comparator to TableSorter to handle String comparisons with numbers in front
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@1723 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewOpenImageSequence.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewOpenImageSequence.java
@@ -1100,13 +1100,10 @@ public class ViewOpenImageSequence extends JFrame
         filenameTableSorter = new TableSorter(filenameTableModel);
         filenameTable = new JTable(filenameTableSorter);
         filenameTable.addKeyListener(this);
-        filenameTableSorter.setColumnComparator(new Integer(0).getClass(), TableSorter.COMPARABLE_COMPARATOR);
-        filenameTableSorter.setColumnComparator(new Double("0.0").getClass(), TableSorter.COMPARABLE_COMPARATOR);
-        filenameTableSorter.setColumnComparator(new Float("0.0").getClass(), TableSorter.COMPARABLE_COMPARATOR);
-        filenameTableSorter.setColumnComparator(new String().getClass(), TableSorter.LEXICAL_COMPARATOR);
-        filenameTableSorter.setTableHeader(filenameTable.getTableHeader());
-        //MouseListener tableListener = new TableListener();
         
+
+        filenameTableSorter.setColumnComparator(new String().getClass(), TableSorter.LEXICAL_NUMS_COMPARATOR);
+        filenameTableSorter.setTableHeader(filenameTable.getTableHeader());
         filenameTable.addMouseListener(this);
         filenameTableModel.addColumn("File Name");
         filenameTable.setDefaultRenderer(new String().getClass(), new MIPAVTableCellRenderer());
