036235e4cc648b8c5f3bd1ed889622d933e34156
NIH\senseneyj <NIH\senseneyj@ba61647d-9d00-f842-95cd-605cb4296b96>
2007-06-15 19:34:35 +0000

    generic plugin template
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@2013 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/plugins/PlugInAlgorithmNewGeneric.java
@@ -0,0 +1,77 @@
+import gov.nih.mipav.model.algorithms.AlgorithmBase;
+import gov.nih.mipav.model.structures.ModelImage;
+
+
+
+public class PlugInAlgorithmNewGeneric extends AlgorithmBase {
+    
+    /** X dimension of the CT image */
+    private int xDim;
+
+    /** Y dimension of the CT image */
+    private int yDim;
+
+    /** Slice size for xDim*yDim */
+    private int sliceSize;
+    
+    /**
+     * Constructor.
+     *
+     * @param  resultImage  Result image model
+     * @param  srcImg       Source image model.
+     */
+    public PlugInAlgorithmNewGeneric(ModelImage resultImage, ModelImage srcImg) {
+        super(resultImage, srcImg);
+    }
+    
+    /**
+     * Starts the algorithm.
+     */
+    public void runAlgorithm() {
+
+        if (srcImage == null) {
+            displayError("Source Image is null");
+
+            return;
+        }
+
+        constructLog();
+
+        if (srcImage.getNDims() == 2) {
+            calc2D();
+        } else if (srcImage.getNDims() > 2) {
+            calc3D();
+        }
+    } // end runAlgorithm()
+    
+//  ~ Methods --------------------------------------------------------------------------------------------------------
+
+    /**
+     * Prepares this class for destruction.
+     */
+    public void finalize() {
+        destImage = null;
+        srcImage = null;
+        super.finalize();
+    }
+    
+    /**
+     * Constructs a string of the contruction parameters and outputs the string to the messsage frame if the logging
+     * procedure is turned on.
+     */
+    private void constructLog() {
+        historyString = new String("KidneySegmentation(" + ")\n");
+    }
+    
+    private void calc2D() {
+        
+    }
+    
+    private void calc3D() {
+        xDim = srcImage.getExtents()[0];
+        yDim = srcImage.getExtents()[1];
+        sliceSize = xDim * yDim;
+
+        int zDim = srcImage.getExtents()[2];
+    }
+}
