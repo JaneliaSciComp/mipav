773d68db9b4c4f6e6a6359ca47929852776a6be3
NIH\hemlerp <NIH\hemlerp@ba61647d-9d00-f842-95cd-605cb4296b96>
2007-07-31 14:19:39 +0000

    A plugin I made to allow me to quickly output various values of an image with or without a paint mask and also to display a processed image.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@2216 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/plugins/PlugInImageStatistics.java
@@ -0,0 +1,44 @@
+import gov.nih.mipav.model.structures.*;
+
+import gov.nih.mipav.plugins.*; // needed to load PlugInAlgorithm / PlugInView / PlugInFile interface
+
+import gov.nih.mipav.view.*;
+
+import java.awt.*;
+
+
+/**
+ * This is simple plugin computes and displays various statistics of an image
+ *
+ * @see  PlugInAlgorithm
+ */
+
+// This is a Algorithm type of PlugIn, and therefore must implement PlugInAlgorithm
+// Implementing the PlugInAlgorithm requires this class to implement the run method
+// with the correct parameters.
+
+public class PlugInImageStatistics implements PlugInAlgorithm {
+
+    //~ Methods --------------------------------------------------------------------------------------------------------
+
+    /**
+     * Defines body of run method, which was declared in the interface. 
+     *
+     * @param  UI           User Interface
+     * @param  parentFrame  parent frame
+     * @param  image        current ModelImage - this is an image already loaded into MIPAV. Can be null.
+     *
+     * @see    ViewUserInterface
+     * @see    ModelImage
+     * @see    ViewJFrameImage
+     */
+    public void run(ViewUserInterface UI, Frame parentFrame, ModelImage image) {
+
+        if (parentFrame instanceof ViewJFrameImage) {
+            new PlugInDialogImageStatistics(parentFrame, image);
+        } else {
+            MipavUtil.displayError("PlugInNewGeneric only runs on an image frame.");
+        }
+    }
+
+}
