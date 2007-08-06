6d649cf97a8af71b867b4ba6e78884507a1e556f
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2007-08-06 17:17:59 +0000

    Fixed comments.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@2249 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/WildMagic/LibFoundation/Mathematics/Mathf.java
+++ trunk/mipav/src/gov/nih/mipav/view/WildMagic/LibFoundation/Mathematics/Mathf.java
@@ -1,14 +1,20 @@
-// Geometric Tools, Inc.
+// Wild Magic Source Code
+// David Eberly
 // http://www.geometrictools.com
-// Copyright (c) 1998-2006.  All Rights Reserved
+// Copyright (c) 1998-2007
 //
-// The Wild Magic Version 4 Foundation Library source code is supplied
-// under the terms of the license agreement
-//     http://www.geometrictools.com/License/Wm4FoundationLicense.pdf
-// and may not be copied or disclosed except in accordance with the terms
-// of that agreement.
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or (at
+// your option) any later version.  The license is available for reading at
+// either of the locations:
+//     http://www.gnu.org/copyleft/lgpl.html
+//     http://www.geometrictools.com/License/WildMagicLicense.pdf
 //
-// Version: 4.0.1 (2006/08/22)
+// Version: 4.0.0 (2006/06/28)
+//
+// Ported to Java by Alexandra Bokinsky, PhD, Geometric Tools, Inc. (July 2007)
+//
 
 package gov.nih.mipav.view.WildMagic.LibFoundation.Mathematics;
 
