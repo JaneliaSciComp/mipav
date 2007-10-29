fe7345cdd6568a7842d3976301426e03d164f3d2
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2007-10-29 19:56:58 +0000

    removed the method for opening up a 4d dataset for raw images
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@2641 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmDTIOpenListFile.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmDTIOpenListFile.java
@@ -10,9 +10,17 @@ import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
 
 import gov.nih.mipav.model.algorithms.AlgorithmBase;
+import gov.nih.mipav.model.file.FileBase;
+import gov.nih.mipav.model.file.FileDicom;
 import gov.nih.mipav.model.file.FileIO;
 import gov.nih.mipav.model.file.FileInfoBase;
+import gov.nih.mipav.model.file.FileInfoDicom;
+import gov.nih.mipav.model.file.FileInfoImageXML;
+import gov.nih.mipav.model.file.FileRaw;
+import gov.nih.mipav.model.file.FileUtility;
+import gov.nih.mipav.model.file.RawImageInfo;
 import gov.nih.mipav.model.structures.ModelImage;
+import gov.nih.mipav.model.structures.ModelStorageBase;
 
 
 /**
@@ -80,6 +88,9 @@ public class PlugInAlgorithmDTIOpenListFile extends AlgorithmBase {
 	/** extents for dest image **/
 	private int[] extents;
 	
+	
+	ArrayList pathsAL;
+	
 
 	
 	
@@ -108,9 +119,12 @@ public class PlugInAlgorithmDTIOpenListFile extends AlgorithmBase {
 	 */
 	public void runAlgorithm() {
 		fireProgressStateChanged("Reading list file");
-        fireProgressStateChanged(10);
+        fireProgressStateChanged(5);
 		boolean success = false;
 		success = readListFile();
+		if(success) {
+			success = readPathFile();
+		}
 		if (success) {
 			if(format.equalsIgnoreCase("dicom")) {
 				fireProgressStateChanged("Reading dicom files");
@@ -128,7 +142,7 @@ public class PlugInAlgorithmDTIOpenListFile extends AlgorithmBase {
 	 */
 	public boolean readListFile() {
 		File listFile = new File(listFileAbsPath);
-		fireProgressStateChanged(20);
+		fireProgressStateChanged(10);
 		try {
             bReader = new BufferedReader(new FileReader(listFile));
             String lineString = null;
@@ -177,9 +191,9 @@ public class PlugInAlgorithmDTIOpenListFile extends AlgorithmBase {
 			return false;
 		}
 	}
+
 	
-	
-	
+
 	
 	/**
 	 * reads dicom and exports to 4d dataest
@@ -187,14 +201,92 @@ public class PlugInAlgorithmDTIOpenListFile extends AlgorithmBase {
 	 */
 	public boolean openDicom() {
 		long begTime = System.currentTimeMillis();
-		fireProgressStateChanged(30);
+		fireProgressStateChanged(15);
+		int length;
+		short[] buffer;	
+		length = xDim * yDim;	
+		buffer = new short[length];
+		String dir;
+		String filename;
+		FileInfoBase fileInfo = null;
+		float[] resols = new float[4];
+        int[] units = new int[4];
+        float[] startLocs = new float[4];
+		int j=0;
+		FileDicom fileDicom = null;
+		FileInfoDicom refFileInfo = null;
+		fireProgressStateChanged(20);
+		
+		try {
+			for(int i=0;i<tDim;i++) {
+				int currVol = i + 1;
+				int totVols = tDim;
+				fireProgressStateChanged("Reading dicom files...Vol " + currVol + "/" + totVols);
+				fireProgressStateChanged(20 + (i * 75 /tDim));
+				for(int k=i;k<pathsAL.size();k=k+tDim) {
+					String path = (String)pathsAL.get(k);
+					dir = path.substring(0,path.lastIndexOf(File.separator)) + File.separator;
+					filename = path.substring(path.lastIndexOf(File.separator) + 1, path.length());
+					if(j == 0) {
+						fileDicom = new FileDicom(filename, dir);
+						fileDicom.setQuiet(true);
+						fileDicom.readHeader(true);
+						refFileInfo = (FileInfoDicom) fileDicom.getFileInfo();
+						fileInfo = fileDicom.getFileInfo();
+					}else {
+						
+						fileDicom.setFileName(filename, dir, refFileInfo);
+						fileDicom.readHeader(true);
+						fileInfo = fileDicom.getFileInfo();
+					}
+					fileDicom.readImage(buffer, ModelStorageBase.SHORT, 0);
+					if(j == 0) {
+						destImage = new ModelImage(ModelStorageBase.SHORT, extents, studyName);
+					}
+					destImage.importData(length * j, buffer, false);
+					destImage.setFileInfo((FileInfoBase) (fileInfo.clone()), j);
+					for (int m = 0; m< 3; m++) {
+			            resols[m] = fileInfo.getResolutions()[m];
+			            units[m] = fileInfo.getUnitsOfMeasure()[m];
+			            startLocs[m] = fileInfo.getOrigin(m);
+			        }
+					resols[3] = 0;
+			        units[3] = FileInfoBase.MILLIMETERS;
+			        startLocs[3] = 0;
+			        
+			        destImage.getFileInfo(j).setResolutions(resols);
+	                destImage.getFileInfo(j).setUnitsOfMeasure(units);
+	                destImage.getFileInfo(j).setExtents(extents);
+	                destImage.getFileInfo(j).setOrigin(startLocs);
+
+					j++;
+					
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		destImage.calcMinMax();
+		long endTime = System.currentTimeMillis();
+        long diffTime = endTime - begTime;
+        float seconds = ((float) diffTime) / 1000;
+		fireProgressStateChanged(95);
+		System.out.println("** Algorithm took " + seconds + " seconds \n");
+		
+		return true;
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean readPathFile() {
 		File pathFile = new File(pathFileAbsPath);
 		String[] paths = new String[zDim * tDim];
-		
-		FileIO fileIO = new FileIO();
-		fileIO.setQuiet(true);
-		
-		
 		try {
             bReader = new BufferedReader(new FileReader(pathFile));
             String lineString = null;
@@ -211,149 +303,7 @@ public class PlugInAlgorithmDTIOpenListFile extends AlgorithmBase {
 		}
 
 		
-		ArrayList pathsAL = new ArrayList(Arrays.asList(paths));
-		
-		int length;
-		float[] buffer;	
-		length = xDim * yDim;	
-		buffer = new float[length];
-		ModelImage volume;
-		String dir;
-		String filename;
-		FileInfoBase[] fileInfo;
-		float[] resols = new float[4];
-        int[] units = new int[4];
-        float[] startLocs = new float[4];
-		int j=0;
-		fireProgressStateChanged(35);
-		try {
-			for(int i=0;i<tDim;i++) {
-				long begVolTime = System.currentTimeMillis();
-				int currVol = i + 1;
-				int totVols = tDim;
-				fireProgressStateChanged("Reading dicom files...Vol " + currVol + "/" + totVols);
-				fireProgressStateChanged(35 + (i * 60 /tDim));
-				System.out.println("i is " + i);
-				for(int k=i;k<pathsAL.size();k=k+tDim) {
-					//fireProgressStateChanged(35 + (i * 60 /t));
-					System.out.println("k is  " + k);
-					String path = (String)pathsAL.get(k);
-					dir = path.substring(0,path.lastIndexOf(File.separator)) + File.separator;
-					filename = path.substring(path.lastIndexOf(File.separator) + 1, path.length());
-						long begVolReadTime = System.currentTimeMillis();
-					volume = fileIO.readImage(filename,dir,false,null);
-						long endVolReadTime = System.currentTimeMillis();
-						long diffVolReadTime = endVolReadTime - begVolReadTime;
-						float volReadSeconds = ((float) diffVolReadTime) / 1000;
-						System.out.println("******** reading in image took " + volReadSeconds + " seconds \n");
-
-						
-						long begVolExportTime = System.currentTimeMillis();
-					volume.exportData(0, length, buffer);
-						long endVolExportTime = System.currentTimeMillis();
-						long diffVolExportTime = endVolExportTime - begVolExportTime;
-						float volExportSeconds = ((float) diffVolExportTime) / 1000;
-						System.out.println("******** exporting data buffer took " + volExportSeconds + " seconds \n");
-					if(j == 0) {
-						destImage = new ModelImage(volume.getType(), extents, studyName);
-					}
-					fileInfo = volume.getFileInfo();
-						long begVolImportTime = System.currentTimeMillis();
-					destImage.importData(length * j, buffer, true);
-						long endVolImportTime = System.currentTimeMillis();
-						long diffVolImportTime = endVolImportTime - begVolImportTime;
-						float volImportSeconds = ((float) diffVolImportTime) / 1000;
-						System.out.println("******** importing data buffer took " + volImportSeconds + " seconds \n");
-					destImage.setFileInfo((FileInfoBase) (fileInfo[0].clone()), j);
-					for (int m = 0; m< 3; m++) {
-			            resols[m] = fileInfo[0].getResolutions()[m];
-			            units[m] = fileInfo[0].getUnitsOfMeasure()[m];
-			            startLocs[m] = fileInfo[0].getOrigin(m);
-			        }
-					resols[3] = 0;
-			        units[3] = FileInfoBase.MILLIMETERS;
-			        startLocs[3] = 0;
-			        
-			        destImage.getFileInfo(j).setResolutions(resols);
-	                destImage.getFileInfo(j).setUnitsOfMeasure(units);
-	                destImage.getFileInfo(j).setExtents(extents);
-	                destImage.getFileInfo(j).setOrigin(startLocs);
-	                volume.disposeLocal();
-					volume = null;
-					j++;
-					
-				}
-				long endVolTime = System.currentTimeMillis();
-		        long diffVolTime = endVolTime - begVolTime;
-		        float volSeconds = ((float) diffVolTime) / 1000;
-		        System.out.println("******** volume took " + volSeconds + " seconds \n");
-			}
-		}catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		
-		
-	
-		//delete MODEL IMAGES!
-		
-		
-		
-		
-		
-		
-		
-		//fireProgressStateChanged(95);
-		
-		
-
-
-		
-		//import the data to 4d datset and copy over file info
-		/*
-		fireProgressStateChanged("Importing data to 4D dataset");
-		
-		for(int i=0;i<volumes.length;i++) {
-			fireProgressStateChanged(65 + (i * 30 /volumes.length));
-			try {
-				fileInfo = volumes[i].getFileInfo();
-				volumes[i].exportData(0, length, buffer);
-				destImage.importData(length * i, buffer, true);
-				for(int k=0;k<fileInfo.length;k++) {
-					destImage.setFileInfo((FileInfoBase) (fileInfo[k].clone()), slice);
-					
-					for (int m = 0; m< 3; m++) {
-			            resols[m] = fileInfo[k].getResolutions()[m];
-			            units[m] = fileInfo[k].getUnitsOfMeasure()[m];
-			            startLocs[m] = fileInfo[k].getOrigin(m);
-			        }
-					resols[3] = 0;
-			        units[3] = FileInfoBase.MILLIMETERS;
-			        startLocs[3] = 0;
-			        
-			        destImage.getFileInfo(slice).setResolutions(resols);
-	                destImage.getFileInfo(slice).setUnitsOfMeasure(units);
-	                destImage.getFileInfo(slice).setExtents(extents);
-	                destImage.getFileInfo(slice).setOrigin(startLocs);
-
-					slice++;
-				}
-				volumes[i].disposeLocal();
-				volumes[i] = null;
-			}catch(Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		}
-		
-		*/
-		
-		long endTime = System.currentTimeMillis();
-        long diffTime = endTime - begTime;
-        float seconds = ((float) diffTime) / 1000;
-        
-		fireProgressStateChanged(95);
-		System.out.println("** Algorithm took " + seconds + " seconds \n");
+		pathsAL = new ArrayList(Arrays.asList(paths));
 		
 		return true;
 	}
