00a4901904b586385bc0bddac2a32e0f47d8f394
NIH\linkb <NIH\linkb@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-02-22 16:46:22 +0000

    changed icon for voi properties
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@3090 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/CustomUIBuilder.java
+++ trunk/mipav/src/gov/nih/mipav/view/CustomUIBuilder.java
@@ -42,7 +42,7 @@ public class CustomUIBuilder {
 	public static final UIParams PARAM_VOI_POINT_ADD = new UIParams("Add point", "addPoint", UIParams.INVALID_MNEMONIC, "Add point", "pointROI", false);
 	
 	/** VOI Actions/Properties */
-	public static final UIParams PARAM_VOI_PROPERTIES = new UIParams("VOIProperties", "View VOI Properties", "voiproperties");
+	public static final UIParams PARAM_VOI_PROPERTIES = new UIParams("VOIProperties", "View VOI Properties", "voiquestion");
 	public static final UIParams PARAM_VOI_COLOR = new UIParams("VOIColor", "Current/change VOI Color", "transparent");
 	public static final UIParams PARAM_VOI_NEW = new UIParams("NewVOI", "Initiate new VOI", "newvoi");
 	public static final UIParams PARAM_VOI_UNDO = new UIParams("undoVOI", "Undo last VOI change (Ctrl-Z)", "undopaint");
