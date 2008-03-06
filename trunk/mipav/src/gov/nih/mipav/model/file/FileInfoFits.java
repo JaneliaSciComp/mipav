64c8a2f35b1a1f52341032e2dad1a758777b85d4
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-03-06 22:54:29 +0000

    origin, dateAcquired, timeAcquired, dateWritten, timeWritten, jobName, object, and observer added.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@3182 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileInfoFits.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileInfoFits.java
@@ -15,6 +15,15 @@ public class FileInfoFits extends FileInfoBase {
 
     /** Use serialVersionUID for interoperability. */
     private static final long serialVersionUID = 8285638648507573285L;
+    
+    private String origin = null;
+    private String dateAcquired = null;
+    private String timeAcquired = null;
+    private String dateWritten = null;
+    private String timeWritten = null;
+    private String jobName = null;
+    private String object = null;
+    private String observer = null;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
@@ -40,5 +49,72 @@ public class FileInfoFits extends FileInfoBase {
     public void displayAboutInfo(JDialogBase dlog, TransMatrix matrix) {
         JDialogText dialog = (JDialogText) dlog;
         displayPrimaryInfo(dialog, matrix);
+        
+        dialog.append("\n\n                Other information\n\n");
+        
+        if (origin != null) {
+            dialog.append("ORIGIN, installation where file is written = " + origin + "\n");
+        }
+        
+        if (dateAcquired != null) {
+            dialog.append("DATE data acquired = " + dateAcquired + "\n");
+        }
+        
+        if (timeAcquired != null) {
+            dialog.append("TIME data acquired = " + timeAcquired + "\n");
+        }
+        
+        if (dateWritten != null) {
+            dialog.append("DATE file written = " + dateWritten + "\n");
+        }
+        
+        if (timeWritten != null) {
+            dialog.append("TIME file written = " + timeWritten + "\n");
+        }
+        
+        if (jobName != null) {
+            dialog.append("JOB NAME = " + jobName + "\n");
+        }
+        
+        if (object != null) {
+            dialog.append("OBJECT observed = " + object + "\n");
+        }
+        
+        if (observer != null) {
+            dialog.append("OBSERVER = " + observer + "\n");
+        }
+        
+    }
+    
+    public void setDateAcquired(String dateAcquired) {
+        this.dateAcquired = dateAcquired;
+    }
+    
+    public void setTimeAcquired(String timeAcquired) {
+        this.timeAcquired = timeAcquired;
+    }
+    
+    public void setDateWritten(String dateWritten) {
+        this.dateWritten = dateWritten;
+    }
+    
+    public void setTimeWritten(String timeWritten) {
+        this.timeWritten = timeWritten;
+    }
+    
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+    
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+    
+    public void setObject(String object) {
+        this.object = object;
+    }
+    
+    public void setObserver(String observer) {
+        this.observer = observer;
     }
 }
