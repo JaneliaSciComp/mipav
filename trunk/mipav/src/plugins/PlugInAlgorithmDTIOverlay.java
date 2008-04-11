f3a8fb867a726191c90d2a16007d7ec347b6a49f
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-04-11 15:12:07 +0000

    p;ugin now handles color images
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@3382 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmDTIOverlay.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmDTIOverlay.java
@@ -99,89 +99,158 @@ public class PlugInAlgorithmDTIOverlay extends AlgorithmBase {
 			return;
 		}
 
-		
-		
-		//ok now create a blank image b based on fiber array
-		int bufferSize = xdim * ydim * zdim;
-		int[] extents = new int[3];
-		extents[0] = xdim;
-		extents[1] = ydim;
-		extents[2] = zdim;
-        imageB = new ModelImage(ModelStorageBase.SHORT, extents, frecImage.getImageName() + "_imageB");
-        
-        //set up lutB
-        int[] dimExtentsLUT = new int[2];
-        dimExtentsLUT[0] = 4;
-        dimExtentsLUT[1] = 256;
-        lutb = new ModelLUT(ModelLUT.STRIPED, 256, dimExtentsLUT);
+		if(!frecImage.isColorImage()) {
+			
+			//ok now create a blank image b based on fiber array
+			int bufferSize = xdim * ydim * zdim;
+			int[] extents = new int[3];
+			extents[0] = xdim;
+			extents[1] = ydim;
+			extents[2] = zdim;
+	        imageB = new ModelImage(ModelStorageBase.SHORT, extents, frecImage.getImageName() + "_imageB");
+	        
+	        //set up lutB
+	        int[] dimExtentsLUT = new int[2];
+	        dimExtentsLUT[0] = 4;
+	        dimExtentsLUT[1] = 256;
+	        lutb = new ModelLUT(ModelLUT.STRIPED, 256, dimExtentsLUT);
+	
+	        
+	        //set up the data buffer
+			short[] buffer = new short[bufferSize];
+			int index=0;
+			int k;
+			boolean match = false;
+			for(int z=0;z<zdim;z++) {
+				for(int y=0;y<ydim;y++) {
+					for(int x=0;x<xdim;x++) {
+						short r = (short)fiberArray[x][y][z][0];
+						short g = (short)fiberArray[x][y][z][1];
+						short b = (short)fiberArray[x][y][z][2];
 
-        
-        //set up the data buffer
-		short[] buffer = new short[bufferSize];
-		int index=0;
-		int k;
-		boolean match = false;
-		for(int z=0;z<zdim;z++) {
-			for(int y=0;y<ydim;y++) {
-				for(int x=0;x<xdim;x++) {
-					short r = (short)fiberArray[x][y][z][0];
-					short g = (short)fiberArray[x][y][z][1];
-					short b = (short)fiberArray[x][y][z][2];
-					if(r != 0 || g != 0 || b != 0) {
-						Color maskColor = new Color(r,g,b);
-						loop: for (k = 1; k < 256; k++) {
-								Color currColor = lutb.getColor(k);
-								if(currColor.getRed() == maskColor.getRed() && currColor.getGreen() == maskColor.getGreen() && currColor.getBlue() == maskColor.getBlue()) {
-									buffer[index] = (short)k;
-									break loop;
-								}
-								else {
-									for(int i=0;i<addedColors.size();i++) {
-										if(((Color)addedColors.get(i)).equals(currColor)) {
-											continue loop;
-										}
+						if(r != 0 || g != 0 || b != 0) {
+							Color maskColor = new Color(r,g,b);
+							loop: for (k = 1; k < 256; k++) {
+									Color currColor = lutb.getColor(k);
+									if(currColor.getRed() == maskColor.getRed() && currColor.getGreen() == maskColor.getGreen() && currColor.getBlue() == maskColor.getBlue()) {
+										buffer[index] = (short)k;
+										break loop;
 									}
-									lutb.setColor(k, maskColor);
-									addedColors.add(maskColor);
-									buffer[index] = (short)k;
-									break loop;
-								}
-	                    	}	
+									else {
+										for(int i=0;i<addedColors.size();i++) {
+											if(((Color)addedColors.get(i)).equals(currColor)) {
+												continue loop;
+											}
+										}
+										lutb.setColor(k, maskColor);
+										addedColors.add(maskColor);
+										buffer[index] = (short)k;
+										break loop;
+									}
+		                    	}	
+						}
+						else {
+							buffer[index] = 0;
+						}
+						
+						++ index;
 					}
-					else {
-						buffer[index] = 0;
-					}
-					
-					++ index;
 				}
 			}
+			
+			
+			//import buffer into image b
+			try {
+	            imageB.importData(0, buffer, false);
+	        } catch (IOException error) {
+	            error.printStackTrace();
+	
+	            return;
+	        }
+	
+	        
+	        FileInfoBase[] fileInfoBases = new FileInfoBase[imageB.getExtents()[2]];
+	        for (int i=0;i<fileInfoBases.length;i++) {
+	
+	        	 fileInfoBases[i] = new FileInfoImageXML(frecImage.getImageName() + "_imageB", null, FileUtility.XML);
+	        }
+	        
+	        
+	        FileInfoBase.copyCoreInfo(frecImage.getFileInfo(), fileInfoBases);
+	        for (int i=0;i<fileInfoBases.length;i++) {
+	       	 	fileInfoBases[i].setDataType(ModelStorageBase.SHORT); 
+	       	 	fileInfoBases[i].setModality(FileInfoBase.UNKNOWN_MODALITY);
+	        }
+	
+	        imageB.setFileInfo(fileInfoBases);
+		}else {
+			//color src image
+			//ok now create a blank image b based on fiber array
+			int bufferSize = xdim * ydim * zdim * 4;
+			int[] extents = new int[3];
+			extents[0] = xdim;
+			extents[1] = ydim;
+			extents[2] = zdim;
+	        imageB = new ModelImage(ModelStorageBase.ARGB_FLOAT, extents, frecImage.getImageName() + "_imageB");
+	        float[] rgbBuffer;
+	        rgbBuffer = new float[bufferSize];
+	        
+			
+			
+	        //set up the data buffer
+	        int index=0;
+	        for(int z=0;z<zdim;z++) {
+				for(int y=0;y<ydim;y++) {
+					for(int x=0;x<xdim;x++) {
+						float r = (float)fiberArray[x][y][z][0];
+						float g = (float)fiberArray[x][y][z][1];
+						float b = (float)fiberArray[x][y][z][2];
+						
+						rgbBuffer[index+1] = r;
+						rgbBuffer[index+2] = g;
+						rgbBuffer[index+3] = b;
+						
+						index = index + 4;
+					}
+				}
+	        }
+			
+			
+			
+	        //import buffer into image b
+			try {
+	            imageB.importData(0, rgbBuffer, false);
+	        } catch (IOException error) {
+	            error.printStackTrace();
+	
+	            return;
+	        }
+	
+	        
+	        FileInfoBase[] fileInfoBases = new FileInfoBase[imageB.getExtents()[2]];
+	        for (int i=0;i<fileInfoBases.length;i++) {
+	
+	        	 fileInfoBases[i] = new FileInfoImageXML(frecImage.getImageName() + "_imageB", null, FileUtility.XML);
+	        }
+	        
+	        
+	        FileInfoBase.copyCoreInfo(frecImage.getFileInfo(), fileInfoBases);
+	        for (int i=0;i<fileInfoBases.length;i++) {
+	       	 	fileInfoBases[i].setDataType(ModelStorageBase.ARGB_FLOAT); 
+	       	 	fileInfoBases[i].setModality(FileInfoBase.UNKNOWN_MODALITY);
+	        }
+	
+	        imageB.setFileInfo(fileInfoBases);
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		}
-		
-		
-		//import buffer into image b
-		try {
-            imageB.importData(0, buffer, false);
-        } catch (IOException error) {
-            errorCleanUp("Algorithm Gaussian Blur: Image(s) locked", false);
-
-            return;
-        }
-
-        
-        FileInfoBase[] fileInfoBases = new FileInfoBase[imageB.getExtents()[2]];
-        for (int i=0;i<fileInfoBases.length;i++) {
-
-        	 fileInfoBases[i] = new FileInfoImageXML(frecImage.getImageName() + "_imageB", null, FileUtility.XML);
-        }
-        
-        
-        FileInfoBase.copyCoreInfo(frecImage.getFileInfo(), fileInfoBases);
-        for (int i=0;i<fileInfoBases.length;i++) {
-       	 	fileInfoBases[i].setDataType(ModelStorageBase.SHORT); 
-       	 	fileInfoBases[i].setModality(FileInfoBase.UNKNOWN_MODALITY);
-        }
-
-        imageB.setFileInfo(fileInfoBases);
         setCompleted(true);	
 
 	}
