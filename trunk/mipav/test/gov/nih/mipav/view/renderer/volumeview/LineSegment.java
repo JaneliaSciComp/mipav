12d9d81bc31dd0966661fe8d2515b708680a8460
NIH\wangh3 <NIH\wangh3@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-05-01 14:27:51 +0000

    Parallelized RayCasting algorithm.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@3473 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/test/gov/nih/mipav/view/renderer/volumeview/LineSegment.java
@@ -0,0 +1,10 @@
+package gov.nih.mipav.view.renderer.volumeview;
+
+public class LineSegment {
+	public float t0;
+	public float t1;
+	public LineSegment(float t0, float t1){
+		this.t0 = t0;
+		this.t1 = t1;
+	}
+}
