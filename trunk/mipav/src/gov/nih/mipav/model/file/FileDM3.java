d918838449dff3bf7dcd5d929f7a36845e87e72b
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-06-30 17:53:47 +0000

    Added image = null to finalize();
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@3848 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileDM3.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileDM3.java
@@ -240,6 +240,7 @@ public class FileDM3 extends FileBase {
         fileName = null;
         fileDir = null;
         fileInfo = null;
+        image = null;
         try {
             super.finalize();
         } catch (Throwable er) { }
