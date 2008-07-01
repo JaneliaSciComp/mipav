d2b74784cb19c898e7dbb170b5491057555e54fb
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-07-01 19:41:21 +0000

    Added finalize().
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@3873 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileLIFF.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileLIFF.java
@@ -181,6 +181,25 @@ public class FileLIFF extends FileBase {
     //~ Methods --------------------------------------------------------------------------------------------------------
 
     /**
+     * Prepares this class for cleanup. Calls the <code>finalize</code> method for existing elements, closes any open
+     * files and sets other elements to <code>null</code>.
+     */
+    public void finalize() {
+        fileName = null;
+        fileDir = null;
+        fileInfo = null;
+        image = null;
+        imageExtents = null;
+        imgBuffer = null;
+        imgResols = null;
+        LUT = null;
+        
+        try {
+            super.finalize();
+        } catch (Throwable er) { }
+    }
+    
+    /**
      * Accessor that returns the file info.
      *
      * @return  FileInfoBase containing the file info
