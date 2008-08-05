f45914c2e0c28b89336c04b2090b38153db8949e
NIH\morseaj <NIH\morseaj@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-08-05 19:18:51 +0000

    finishing up the PlugIn
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@4078 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/plugins/PlugInAlgorithmNEIRetinalRegistration2.java
@@ -0,0 +1,163 @@
+import gov.nih.mipav.model.algorithms.AlgorithmBase;
+import gov.nih.mipav.model.algorithms.AlgorithmCenterOfMass;
+import gov.nih.mipav.model.file.FileIO;
+import gov.nih.mipav.model.file.FileVOI;
+import gov.nih.mipav.model.structures.ModelImage;
+import gov.nih.mipav.model.structures.VOI;
+
+import gov.nih.mipav.view.MipavUtil;
+
+import java.awt.Point;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.BitSet;
+
+import javax.swing.JTextArea;
+
+
+public class PlugInAlgorithmNEIRetinalRegistration2 extends AlgorithmBase {
+
+    /** mpMap File **/
+    private File mpMapFile;
+    
+    /** VOI File **/
+    private File voiFile;
+    
+    /** dots per pixel value **/
+    private int dPerp;
+    
+    /** output box **/
+    private JTextArea outputbox;
+    
+    /** IO **/
+    private FileIO fileIO = new FileIO();
+    
+    /** center of mass **/
+    private Point cm;
+    
+    /** finds center of mass **/
+    private AlgorithmCenterOfMass algoFindCM;
+    
+    /** sees if value has been used in calculating data already **/
+    private BitSet check;
+    
+    public  PlugInAlgorithmNEIRetinalRegistration2(){
+        
+    }
+    
+    public PlugInAlgorithmNEIRetinalRegistration2(String mpMapLoc, String voiLoc, int dPerp, JTextArea outbox2){//, float deltaR){
+        
+        //save user input
+        mpMapFile = new File(mpMapLoc);
+        voiFile = new File(voiLoc);
+        this.outputbox = outbox2;
+        this.dPerp = dPerp;
+        //this.deltaR = deltaR;
+        
+    }
+    @Override
+    public void runAlgorithm() {
+
+        outputbox.append("*Beginning MPmap Analysis* \n");
+        fileIO.setQuiet(true);
+        
+        //read in mpMap and VOI
+        ModelImage image = fileIO.readImage(mpMapFile.getAbsolutePath());
+        FileVOI voiIO;
+        try {
+            voiIO = new FileVOI(voiFile.getName(), voiFile.getParent() + voiFile.separator, image);
+
+        outputbox.append("****Loading image w/ fovea VOI \n");
+        VOI voi[] = voiIO.readVOI(false);
+        
+        for (int i = 0; i < voi.length; i++) {
+            image.registerVOI(voi[i]);
+        }
+        
+        } catch (IOException e) {
+            MipavUtil.displayError("Error loading VOI to MPmap!");
+            setCompleted(true);
+            this.notifyListeners(this);
+            return;
+        }
+        
+        //calculate center of mass of VOI
+        float threshold[] = new float[2];
+        threshold[0] = (float)image.getMin();
+        threshold[1] = (float)image.getMax();
+        outputbox.append("****Calculating Center of Mass \n");
+        algoFindCM = new AlgorithmCenterOfMass(image, threshold, false);
+        algoFindCM.run();
+        
+        //save center
+        cm = new Point((int)((float)algoFindCM.getCenterOfMass()[0]/image.getResolutions(0)[0]),(int)((float)algoFindCM.getCenterOfMass()[1]/image.getResolutions(0)[1]));
+        Point current;
+        
+        
+        float[] buffer = new float[image.getExtents()[0] * image.getExtents()[1]];
+        int xDim = image.getExtents()[0], yDim = image.getExtents()[1], pos = 0;
+        check = new BitSet(image.getExtents()[0] * image.getExtents()[1]);
+        
+        //get image data
+        try {
+            image.exportData(0, image.getExtents()[0] * image.getExtents()[1], buffer);
+        } catch (IOException e) {
+        }
+        
+        float total = 0, denom = 0;
+        File data = new File(mpMapFile.getParent() + mpMapFile.separatorChar + "data.txt");
+        try {
+            data.createNewFile();
+            FileWriter out = new FileWriter(data);
+            
+            outputbox.append("****Calculating Azimuthal Average \n");
+            
+            
+                for (int r = 1; r <=475; r++)  {
+                    check.clear();
+                        total = 0;
+                    denom = 0;
+                for(float theta = 0; theta < 360; theta=theta + (float).1){
+                    
+                    //get point in relation to center of mass
+                    current = new Point((int)(r*Math.sin(theta)), (int)(r*Math.cos(theta)));
+                    current.translate((int)cm.getX(), (int)cm.getY());
+                    
+                    pos = (int)(current.getY()*xDim + current.getX());
+                    
+                    //if NaN dont add it
+                    if (!check.get(pos) && !Float.isNaN(buffer[pos])){
+                        total = total + buffer[pos];
+                        denom++;
+                        check.set(pos, true);
+                    }
+    
+                }
+                //divide by total values added
+                total = total / denom;
+                
+                //print to file
+                out.write((r*dPerp) + "," + total + "\r\n");
+                    
+        
+        }
+        outputbox.append("****Saving data in " + data.getPath() + " \n");
+        out.close();
+        
+        if (image != null){
+            image.disposeLocal();
+            image = null;
+        }
+        
+    } catch (IOException e) {
+        MipavUtil.displayError("FileIO Error!");
+    }
+        outputbox.append("*End MPmap Analysis* \n");
+        setCompleted(true);
+        return;
+        
+    }
+    
+
+}
