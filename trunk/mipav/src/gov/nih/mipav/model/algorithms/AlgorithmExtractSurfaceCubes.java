0637bdb5c216c5212b7fd9cf87b5a2000081270f
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-08-12 16:27:46 +0000

    Fixed surface extraction -- wasn't detecting duplicate vertices so too many vertices in the triangle mesh.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@4110 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmExtractSurfaceCubes.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmExtractSurfaceCubes.java
@@ -317,14 +317,18 @@ public class AlgorithmExtractSurfaceCubes extends AlgorithmBase {
             fireProgressStateChanged("Starting surface extraction");
 
             TriMesh kMesh = kExtractor.getLevelSurface(level);
+            // Get the adjacent triangles:
+            VETMesh kVETMesh = new VETMesh( 2* kMesh.VBuffer.GetVertexQuantity(), .9f,
+                    2 * kMesh.IBuffer.GetIndexQuantity(), .9f,
+                    2 * kMesh.GetTriangleQuantity(), .9f,
+                    kMesh.IBuffer.GetData() );
+            kMesh.IBuffer = new IndexBuffer( kVETMesh.GetTriangles() );
 
             buffer = null;
             System.gc();
             fireProgressStateChanged(50);
 
-            if (triangleConsistencyMode == ADJ_MODE) {
-                //kMesh.getConsistentComponents();
-            } else if (triangleConsistencyMode == SMOOTH_MODE) {
+            if (triangleConsistencyMode == SMOOTH_MODE) {
                 //kMesh.smoothTwo(2, 0.03f, true, 0.01f, false);
             }
 
