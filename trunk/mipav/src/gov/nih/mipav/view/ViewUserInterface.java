ee360493fe60a912d9c2950938d3a8dbf7e45f0b
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-09-04 19:16:05 +0000

    if script runs in -hide mode, usage error dialog will not pop up if there is an issue....there will still be a System.out.println of the usage though
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@4256 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewUserInterface.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewUserInterface.java
@@ -3871,10 +3871,12 @@ public class ViewUserInterface implements ActionListener, WindowListener, KeyLis
 
         // print the usage help to a dialog.
         // maybe later we can make this an option...
-        JTextArea helpArea = new JTextArea(helpInfo);
-        helpArea.setFont(MipavUtil.courier12);
-        helpArea.setEditable(false);
-        JOptionPane.showMessageDialog(null, helpArea, "Command line help", JOptionPane.INFORMATION_MESSAGE);
+        if(!isAppFrameVisible) {
+	        JTextArea helpArea = new JTextArea(helpInfo);
+	        helpArea.setFont(MipavUtil.courier12);
+	        helpArea.setEditable(false);
+	        JOptionPane.showMessageDialog(null, helpArea, "Command line help", JOptionPane.INFORMATION_MESSAGE);
+        }
 
         System.exit(0);
     }
