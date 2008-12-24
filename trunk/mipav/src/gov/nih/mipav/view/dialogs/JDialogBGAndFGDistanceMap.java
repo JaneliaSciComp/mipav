c1150e5ae400592f380b81d0258b92765383d1ec
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2008-12-24 18:54:53 +0000

    added 3d version of BG+FG distance map
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@4785 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogBGAndFGDistanceMap.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogBGAndFGDistanceMap.java
@@ -15,6 +15,7 @@ import javax.swing.JPanel;
 import gov.nih.mipav.model.algorithms.AlgorithmBase;
 import gov.nih.mipav.model.algorithms.AlgorithmInterface;
 import gov.nih.mipav.model.algorithms.AlgorithmMorphology2D;
+import gov.nih.mipav.model.algorithms.AlgorithmMorphology3D;
 import gov.nih.mipav.model.scripting.ParserException;
 import gov.nih.mipav.model.structures.ModelImage;
 import gov.nih.mipav.view.MipavUtil;
@@ -34,7 +35,9 @@ public class JDialogBGAndFGDistanceMap extends JDialogScriptableBase implements 
     
     private JPanelAlgorithmOutputOptions outputPanel;
     
-    private AlgorithmMorphology2D distanceMapAlgo1;
+    private AlgorithmMorphology2D distanceMapAlgo2D;
+    
+    private AlgorithmMorphology3D distanceMapAlgo3D = null;
     
     /** DOCUMENT ME! */
     private String[] titles;
@@ -47,9 +50,9 @@ public class JDialogBGAndFGDistanceMap extends JDialogScriptableBase implements 
 	
 	public JDialogBGAndFGDistanceMap(Frame theParentFrame, ModelImage im) {
 		
-		if ((im.getNDims() != 2) && (im.getType() != ModelImage.BOOLEAN) && (im.getType() != ModelImage.UBYTE) &&
+		if ((im.getType() != ModelImage.BOOLEAN) && (im.getType() != ModelImage.UBYTE) &&
                 (im.getType() != ModelImage.USHORT)) {
-            MipavUtil.displayError("Source Image must be a 2D BOOLEAN, UNSIGNED BYTE or UNSIGNED SHORT");
+            MipavUtil.displayError("Source Image must be BOOLEAN, UNSIGNED BYTE or UNSIGNED SHORT");
             dispose();
 
             return;
@@ -102,97 +105,197 @@ public class JDialogBGAndFGDistanceMap extends JDialogScriptableBase implements 
 
 	protected void callAlgorithm() {
 		String name = makeImageName(image.getImageName(), "_BGAndFGDistance");
-		
-		if (outputPanel.isOutputNewImageSet()) {
+		if (image.getNDims() == 2) { // source image is 2D
+			if (outputPanel.isOutputNewImageSet()) {
+	
+	            try {
+	
+	                // Make result image of float type
+	                resultImage = (ModelImage) image.clone();
+	                resultImage.setImageName(name);
+	                
+	                distanceMapAlgo2D = new AlgorithmMorphology2D(resultImage, 0, 0, AlgorithmMorphology2D.DISTANCE_MAP_FOR_SHAPE_INTERPOLATION, 0, 0, 0, 0, outputPanel.isProcessWholeImageSet());
+	                
+	                
+	                // This is very important. Adding this object as a listener allows the algorithm to
+	                // notify this object when it has completed of failed. See algorithm performed event.
+	                // This is made possible by implementing AlgorithmedPerformed interface
+	                distanceMapAlgo2D.addListener(this);
+	
+	                createProgressBar(resultImage.getImageName(), distanceMapAlgo2D);
+	                
+	                if (outputPanel.isProcessWholeImageSet() == false) {
+	                	distanceMapAlgo2D.setMask(resultImage.generateVOIMask());
+	                }
+	
+	                // Hide dialog
+	                setVisible(false);
+	
+	                if (isRunInSeparateThread()) {
+	                    // Start the thread as a low priority because we wish to still have user interface work fast.
+	                    if (distanceMapAlgo2D.startMethod(Thread.MIN_PRIORITY) == false) {
+	                        MipavUtil.displayError("A thread is already running on this object");
+	                    }
+	                } else {
+	                	//to maintain proper script execution, directly calling run
+	                	distanceMapAlgo2D.run();
+	                }
+	            }catch (OutOfMemoryError x) {
+	                MipavUtil.displayError("Dialog bg and fg distance map: unable to allocate enough memory");
+	
+	                return;
+	            }
+			}else {
+				try {
+	
+	                // No need to make new image space because the user has choosen to replace the source image
+	                // Make the algorithm class
+	                distanceMapAlgo2D = new AlgorithmMorphology2D(image, 0, 0, AlgorithmMorphology2D.DISTANCE_MAP_FOR_SHAPE_INTERPOLATION, 0, 0, 0, 0, outputPanel.isProcessWholeImageSet());
+	
+	                // This is very important. Adding this object as a listener allows the algorithm to
+	                // notify this object when it has completed of failed. See algorithm performed event.
+	                // This is made possible by implementing AlgorithmedPerformed interface
+	                distanceMapAlgo2D.addListener(this);
+	                
+	                createProgressBar(image.getImageName(), distanceMapAlgo2D);
+	                
+	
+	                if (outputPanel.isProcessWholeImageSet() == false) {
+	                	distanceMapAlgo2D.setMask(image.generateVOIMask());
+	                }
+	
+	                // Hide the dialog since the algorithm is about to run.
+	                setVisible(false);
+	
+	                // These next lines set the titles in all frames where the source image is displayed to
+	                // "locked - " image name so as to indicate that the image is now read/write locked!
+	                // The image frames are disabled and then unregisted from the userinterface until the
+	                // algorithm has completed.
+	                Vector imageFrames = image.getImageFrameVector();
+	                titles = new String[imageFrames.size()];
+	
+	                for (int i = 0; i < imageFrames.size(); i++) {
+	                    titles[i] = ((Frame) (imageFrames.elementAt(i))).getTitle();
+	                    ((Frame) (imageFrames.elementAt(i))).setTitle("Locked: " + titles[i]);
+	                    ((Frame) (imageFrames.elementAt(i))).setEnabled(false);
+	                    userInterface.unregisterFrame((Frame) (imageFrames.elementAt(i)));
+	                }
+	
+	                if (isRunInSeparateThread()) {
+	                    // Start the thread as a low priority because we wish to still have user interface work fast.
+	                    if (distanceMapAlgo2D.startMethod(Thread.MIN_PRIORITY) == false) {
+	                        MipavUtil.displayError("A thread is already running on this object");
+	                    }
+	                } else {
+	                	//to maintain proper script execution, directly calling run
+	                	distanceMapAlgo2D.run();
+	                }
+	            } catch (OutOfMemoryError x) {
+	                MipavUtil.displayError("Dialog distance map: unable to allocate enough memory");
+	
+	                return;
+	            }
+				
+			}
+		}else if (image.getNDims() == 3) {
+			if (outputPanel.isOutputNewImageSet()) {
 
-            try {
+                try {
 
-                // Make result image of float type
-                resultImage = (ModelImage) image.clone();
-                resultImage.setImageName(name);
-                
-                distanceMapAlgo1 = new AlgorithmMorphology2D(resultImage, 0, 0, AlgorithmMorphology2D.DISTANCE_MAP_FOR_SHAPE_INTERPOLATION, 0, 0, 0, 0, outputPanel.isProcessWholeImageSet());
-                
-                
-                // This is very important. Adding this object as a listener allows the algorithm to
-                // notify this object when it has completed of failed. See algorithm performed event.
-                // This is made possible by implementing AlgorithmedPerformed interface
-                distanceMapAlgo1.addListener(this);
+                    // Make result image of float type
+                    resultImage = (ModelImage) image.clone();
+                    resultImage.setImageName(name);
 
-                createProgressBar(resultImage.getImageName(), distanceMapAlgo1);
-                
-                if (outputPanel.isProcessWholeImageSet() == false) {
-                	distanceMapAlgo1.setMask(resultImage.generateVOIMask());
-                }
+                    // Make algorithm
+                    distanceMapAlgo3D = new AlgorithmMorphology3D(resultImage, 0, 0,
+                                                                  AlgorithmMorphology3D.DISTANCE_MAP_FOR_SHAPE_INTERPOLATION, 0, 0, 0, 0,
+                                                                  outputPanel.isProcessWholeImageSet());
 
-                // Hide dialog
-                setVisible(false);
-
-                if (isRunInSeparateThread()) {
-                    // Start the thread as a low priority because we wish to still have user interface work fast.
-                    if (distanceMapAlgo1.startMethod(Thread.MIN_PRIORITY) == false) {
-                        MipavUtil.displayError("A thread is already running on this object");
+                    if (outputPanel.isProcessWholeImageSet() == false) {
+                        distanceMapAlgo3D.setMask(image.generateVOIMask());
                     }
-                } else {
-                	//to maintain proper script execution, directly calling run
-                	distanceMapAlgo1.run();
-                }
-            }catch (OutOfMemoryError x) {
-                MipavUtil.displayError("Dialog bg and fg distance map: unable to allocate enough memory");
 
-                return;
-            }
-		}else {
-			try {
+                    // This is very important. Adding this object as a listener allows the algorithm to
+                    // notify this object when it has completed of failed. See algorithm performed event.
+                    // This is made possible by implementing AlgorithmedPerformed interface
+                    distanceMapAlgo3D.addListener(this);
 
-                // No need to make new image space because the user has choosen to replace the source image
-                // Make the algorithm class
-                distanceMapAlgo1 = new AlgorithmMorphology2D(image, 0, 0, AlgorithmMorphology2D.DISTANCE_MAP_FOR_SHAPE_INTERPOLATION, 0, 0, 0, 0, outputPanel.isProcessWholeImageSet());
+                    createProgressBar(image.getImageName(), distanceMapAlgo3D);
+                    
+                    // Hide dialog
+                    setVisible(false);
 
-                // This is very important. Adding this object as a listener allows the algorithm to
-                // notify this object when it has completed of failed. See algorithm performed event.
-                // This is made possible by implementing AlgorithmedPerformed interface
-                distanceMapAlgo1.addListener(this);
-                
-                createProgressBar(image.getImageName(), distanceMapAlgo1);
-                
-
-                if (outputPanel.isProcessWholeImageSet() == false) {
-                	distanceMapAlgo1.setMask(image.generateVOIMask());
-                }
-
-                // Hide the dialog since the algorithm is about to run.
-                setVisible(false);
-
-                // These next lines set the titles in all frames where the source image is displayed to
-                // "locked - " image name so as to indicate that the image is now read/write locked!
-                // The image frames are disabled and then unregisted from the userinterface until the
-                // algorithm has completed.
-                Vector imageFrames = image.getImageFrameVector();
-                titles = new String[imageFrames.size()];
-
-                for (int i = 0; i < imageFrames.size(); i++) {
-                    titles[i] = ((Frame) (imageFrames.elementAt(i))).getTitle();
-                    ((Frame) (imageFrames.elementAt(i))).setTitle("Locked: " + titles[i]);
-                    ((Frame) (imageFrames.elementAt(i))).setEnabled(false);
-                    userInterface.unregisterFrame((Frame) (imageFrames.elementAt(i)));
-                }
-
-                if (isRunInSeparateThread()) {
-                    // Start the thread as a low priority because we wish to still have user interface work fast.
-                    if (distanceMapAlgo1.startMethod(Thread.MIN_PRIORITY) == false) {
-                        MipavUtil.displayError("A thread is already running on this object");
+                    if (isRunInSeparateThread()) {
+	                    // Start the thread as a low priority because we wish to still have user interface work fast.
+	                    if (distanceMapAlgo3D.startMethod(Thread.MIN_PRIORITY) == false) {
+	                        MipavUtil.displayError("A thread is already running on this object");
+	                    }
+                    } else {
+                    	//to maintain proper script execution, directly calling run
+                    	distanceMapAlgo3D.run();
                     }
-                } else {
-                	//to maintain proper script execution, directly calling run
-                	distanceMapAlgo1.run();
-                }
-            } catch (OutOfMemoryError x) {
-                MipavUtil.displayError("Dialog distance map: unable to allocate enough memory");
+                } catch (OutOfMemoryError x) {
+                    MipavUtil.displayError("Dialog distance map: unable to allocate enough memory");
 
-                return;
+                    if (resultImage != null) {
+                        resultImage.disposeLocal(); // Clean up image memory
+                        resultImage = null;
+                    }
+
+                    return;
+                }
+            } else {
+
+                try {
+
+                    // Make algorithm
+                    distanceMapAlgo3D = new AlgorithmMorphology3D(image, 0, 0,
+                                                                  AlgorithmMorphology3D.DISTANCE_MAP_FOR_SHAPE_INTERPOLATION, 0, 0, 0, 0,
+                                                                  outputPanel.isProcessWholeImageSet());
+
+                    if (outputPanel.isProcessWholeImageSet() == false) {
+                        distanceMapAlgo3D.setMask(image.generateVOIMask());
+                    }
+
+                    // This is very important. Adding this object as a listener allows the algorithm to
+                    // notify this object when it has completed of failed. See algorithm performed event.
+                    // This is made possible by implementing AlgorithmedPerformed interface
+                    distanceMapAlgo3D.addListener(this);
+
+                    createProgressBar(image.getImageName(), distanceMapAlgo3D);
+                    
+                    // Hide dialog
+                    setVisible(false);
+
+                    // These next lines set the titles in all frames where the source image is displayed to
+                    // "locked - " image name so as to indicate that the image is now read/write locked!
+                    // The image frames are disabled and then unregisted from the userinterface until the
+                    // algorithm has completed.
+                    Vector imageFrames = image.getImageFrameVector();
+                    titles = new String[imageFrames.size()];
+
+                    for (int i = 0; i < imageFrames.size(); i++) {
+                        titles[i] = ((Frame) (imageFrames.elementAt(i))).getTitle();
+                        ((Frame) (imageFrames.elementAt(i))).setTitle("Locked: " + titles[i]);
+                        ((Frame) (imageFrames.elementAt(i))).setEnabled(false);
+                        userInterface.unregisterFrame((Frame) (imageFrames.elementAt(i)));
+                    }
+
+                    if (isRunInSeparateThread()) {
+	                    // Start the thread as a low priority because we wish to still have user interface work fast.
+	                    if (distanceMapAlgo3D.startMethod(Thread.MIN_PRIORITY) == false) {
+	                        MipavUtil.displayError("A thread is already running on this object");
+	                    }
+                    } else {
+                    	//to maintain proper script execution, directly calling run
+                    	distanceMapAlgo3D.run();
+                    }
+                } catch (OutOfMemoryError x) {
+                    MipavUtil.displayError("Dialog distance map: unable to allocate enough memory");
+
+                    return;
+                }
             }
-			
 		}
 
 	}
@@ -210,7 +313,7 @@ public class JDialogBGAndFGDistanceMap extends JDialogScriptableBase implements 
 	public void algorithmPerformed(AlgorithmBase algorithm) {
 		 if (algorithm instanceof AlgorithmMorphology2D) {
 			 image.clearMask();
-			 if ((distanceMapAlgo1.isCompleted() == true) && (resultImage != null)) {
+			 if ((distanceMapAlgo2D.isCompleted() == true) && (resultImage != null)) {
 	                updateFileInfo(image, resultImage);
 	                resultImage.clearMask();
 
@@ -249,6 +352,46 @@ public class JDialogBGAndFGDistanceMap extends JDialogScriptableBase implements 
 	                resultImage = null;
 	            }
 	            
+		 }else if(algorithm instanceof AlgorithmMorphology3D) {
+			 image.clearMask();
+			 if ((distanceMapAlgo3D.isCompleted() == true) && (resultImage != null)) {
+	                updateFileInfo(image, resultImage);
+	                resultImage.clearMask();
+
+	                // The algorithm has completed and produced a new image to be displayed.
+	                try {
+	                    resultImage.setImageName("Bg. distance map image");
+	                    new ViewJFrameImage(resultImage, null, new Dimension(610, 200));
+	                } catch (OutOfMemoryError error) {
+	                    MipavUtil.displayError("Out of memory: unable to open new frame");
+	                }
+	            } else if (resultImage == null) {
+
+	                // These next lines set the titles in all frames where the source image is displayed to
+	                // image name so as to indicate that the image is now unlocked!
+	                // The image frames are enabled and then registed to the userinterface.
+	                Vector imageFrames = image.getImageFrameVector();
+
+	                for (int i = 0; i < imageFrames.size(); i++) {
+	                    ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
+	                    ((Frame) (imageFrames.elementAt(i))).setEnabled(true);
+
+	                    if (((Frame) (imageFrames.elementAt(i))) != parentFrame) {
+	                        userInterface.registerFrame((Frame) (imageFrames.elementAt(i)));
+	                    }
+	                }
+
+	                if (parentFrame != null) {
+	                    userInterface.registerFrame(parentFrame);
+	                }
+
+	                image.notifyImageDisplayListeners(null, true);
+	            } else if (resultImage != null) {
+
+	                // algorithm failed but result image still has garbage
+	                resultImage.disposeLocal(); // clean up memory
+	                resultImage = null;
+	            }
 		 }
 
 	}
