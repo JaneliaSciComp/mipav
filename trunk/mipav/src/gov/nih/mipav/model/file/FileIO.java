86b3c96740183a5e83a0cc5f49a3fa221d431073
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-03-27 15:03:24 +0000

    coonfirm dialog for saving unsigned short as short does show if its not being scripted
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5137 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileIO.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileIO.java
@@ -8811,11 +8811,19 @@ public class FileIO {
     	 */
     	if(image.getType() == ModelStorageBase.USHORT) {
     		double max = image.getMax();
-    		if(max > 32767) {
+    		if(max <= 32767) {
+    			//this means it will fit in to short
+    			if(!quiet) {
+	    			int response = JOptionPane.showConfirmDialog(UI.getMainFrame(), "Analyze 7.5 does not support Unsigned Short. However, the data will fit into Signed Short. Do you wish to proceed?","Warning",JOptionPane.YES_NO_OPTION);
+					if (response == JOptionPane.NO_OPTION) {
+						return false;
+					}
+    			}
+    		}else {
     			if ( !quiet) {
     				MipavUtil.displayError("Analyze 7.5 does not support Unsigned Short. You must convert the data type to something other than Unsigned Short");
     			}
-    			return false;
+    				return false;
     		}
     	}
     	
