4fd12af2fa3e85a6dbd221d55ef411575137bb32
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-03-28 21:14:11 +0000

    Added DTI statistics: Trace, Relative Anisotropy (RA), Volume Ratio (VR). Images are written at the same time as the Eigen-values and functional anisotropy (FA) images.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5145 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/DiffusionTensorImaging/AlgorithmDTI2EGFA.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/DiffusionTensorImaging/AlgorithmDTI2EGFA.java
@@ -25,6 +25,12 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
     private ModelImage m_kEigenImage = null;
     /** Output Functional Anisotropy Image: */
     private ModelImage m_kFAImage = null;
+    /** Output Trace Image (before diagonalization of diffusion tensor): */
+    private ModelImage m_kTraceImage = null;
+    /** Output RA (Relative Anisotropy) Image: */
+    private ModelImage m_kRAImage = null;
+    /** Output VR (Volume Radio) Image: */
+    private ModelImage m_kVRImage = null;
 
     /** Initialize the Algorithm with the input DTI Image:
      * @param kDTIImage input DTI Image.
@@ -40,6 +46,9 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
         m_kDTIImage = null;
         m_kEigenImage = null;
         m_kFAImage = null;
+        m_kTraceImage = null;
+        m_kRAImage = null;
+        m_kVRImage = null;
     }
 
     /** Run the DTI -> EigenVector Functional Anisotropy algorithm. */
@@ -68,6 +77,30 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
         return m_kFAImage;
     }
 
+    /** Returns the Trace Image. 
+     * @return the Trace Image. 
+     */
+    public ModelImage getTraceImage()
+    {
+        return m_kTraceImage;
+    }
+
+    /** Returns the Relative Anisotropy Image. 
+     * @return the Relative Anisotropy Image. 
+     */
+    public ModelImage getRAImage()
+    {
+        return m_kRAImage;
+    }
+
+    /** Returns the Volume Ratio Image. 
+     * @return the Volume Ratio Image. 
+     */
+    public ModelImage getVRImage()
+    {
+        return m_kVRImage;
+    }
+
 
     /** 
      * Calculates the eigen vector data from the dtiImage.
@@ -82,6 +115,9 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
         int iZDim = m_kDTIImage.getExtents()[2];
         int iSliceSize = m_kDTIImage.getExtents()[0] * m_kDTIImage.getExtents()[1];
         float[] afData = new float[iLen];
+        float[] afTraceData = new float[iLen];
+        float[] afRAData = new float[iLen];
+        float[] afVRData = new float[iLen];
         float[] afDataCM = new float[iLen*9];
         float[] afTensorData = new float[6];
 
@@ -108,6 +144,9 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
                 kMatrix.Set( afTensorData[0], afTensorData[3], afTensorData[4],
                                                  afTensorData[3], afTensorData[1], afTensorData[5], 
                                                  afTensorData[4], afTensorData[5], afTensorData[2] );
+
+                
+                afTraceData[i] = kMatrix.M00 + kMatrix.M11 + kMatrix.M22;
                 
                 Matrix3f.EigenDecomposition( kMatrix, kEigenValues );
                 float fLambda1 = kEigenValues.M22;
@@ -122,10 +161,21 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
                                 (fLambda2 - fLambda3)*(fLambda2 - fLambda3) +
                                 (fLambda3 - fLambda1)*(fLambda3 - fLambda1)   ) ) /
                                 ( Math.sqrt( fLambda1*fLambda1 + fLambda2*fLambda2 + fLambda3*fLambda3 ) ) ) );
+                
+                float fLambda = (fLambda1 + fLambda2 + fLambda3)/3.0f;
+                afRAData[i] = (float)(Math.sqrt((fLambda1 - fLambda)*(fLambda1 - fLambda) + 
+                                                (fLambda2 - fLambda)*(fLambda2 - fLambda) +
+                                                (fLambda3 - fLambda)*(fLambda3 - fLambda)   ) / 
+                                                Math.sqrt( 3.0f * fLambda ));
+                afVRData[i] = (fLambda1*fLambda2*fLambda3)/(fLambda*fLambda*fLambda);
+                
             }
             else
             {
                 afData[i] = 0;
+                afTraceData[i] = 0;
+                afRAData[i] = 0;
+                afVRData[i] = 0;
             }
 
             afDataCM[i + 0*iLen] = kV1.X;
@@ -169,6 +219,36 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
             m_kFAImage = null;
         }
 
+        m_kTraceImage = new ModelImage( ModelStorageBase.FLOAT,
+                                     extentsA,
+                                     new String( m_kDTIImage.getFileInfo(0).getFileName() + "Trace") );
+        try {
+            m_kTraceImage.importData(0, afTraceData, true);
+        } catch (IOException e) {
+            m_kTraceImage.disposeLocal();
+            m_kTraceImage = null;
+        }
+
+        m_kRAImage = new ModelImage( ModelStorageBase.FLOAT,
+                                     extentsA,
+                                     new String( m_kDTIImage.getFileInfo(0).getFileName() + "RA") );
+        try {
+            m_kRAImage.importData(0, afRAData, true);
+        } catch (IOException e) {
+            m_kRAImage.disposeLocal();
+            m_kRAImage = null;
+        }
+
+        m_kVRImage = new ModelImage( ModelStorageBase.FLOAT,
+                                     extentsA,
+                                     new String( m_kDTIImage.getFileInfo(0).getFileName() + "VR") );
+        try {
+            m_kVRImage.importData(0, afVRData, true);
+        } catch (IOException e) {
+            m_kVRImage.disposeLocal();
+            m_kVRImage = null;
+        }
+
         m_kEigenImage = new ModelImage( ModelStorageBase.FLOAT,
                                         extentsEV,
                                         new String( m_kDTIImage.getFileInfo(0).getFileName() + "EG") );
@@ -181,8 +261,9 @@ public class AlgorithmDTI2EGFA extends AlgorithmBase
 
         extentsEV = null;
         extentsA = null;
-        
+
         afData = null;
+        afTraceData = null;
         afDataCM = null;
         afTensorData = null;
 
