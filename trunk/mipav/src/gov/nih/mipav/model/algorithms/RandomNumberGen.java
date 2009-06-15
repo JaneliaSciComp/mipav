8697933ca43847eeeef63f13164c70813e9a48bb
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-06-15 17:54:59 +0000

    Deleted previous Poisson code. Added PoissDecay derived from 4 routines in Data Reduction and Error Analysis for the Physical Sciences.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5497 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/RandomNumberGen.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/RandomNumberGen.java
@@ -243,136 +243,78 @@ public class RandomNumberGen extends Random {
         return ((nextDouble() * (endRange - stRange)) + stRange);
     }
     
-    /**
-     * Derived from STSDAS Help Pages Space Telescope Science Institute
-     * From 2 lines in procedure mkpnoise in t_mknoise
-     * Seen at http://stsdas.stsci.edu/cgi-bin/gethelp.cgi?mknoise.src
-     * @param inputValue
-     * @param background
+    /** This code is derived from PoissDecay.cpp, PoissonDeviate, PoissonRecur, and RandomDeviate,
+     * code accompanying Data Reduction and Error Analysis for the
+     * Physical Sciences, Third Edition, by Philip R. Bevington and D. Keith Robinson
+     * p(x, u)is the probability of observing x events where u is the average number of events observed.
+     * x must be zero or a positive integer.
+     * p(x, u) = (u**x)* exp(-u)/x!
+     * The sum of x from 0 to infinity of p(x, u) = 1.
+     * mean value of x = u
+     * variance of x = u
+     * Return gain * p(x, u) + offset
+     * @param nEvents
+     * @param mu mean of Poisson distribtuion
      * @param gain
+     * @param offset
      * @return
      */
-    public final double genPoissonRandomNum(double inputValue, double background, double gain) {
-        double v;
-        double outputValue;
-        // Random number seed
-        long seed = System.currentTimeMillis();
+    public final double[] poissDecay(int nEvents, double mu, double gain, double offset) {
+        int maxBins = 100;
+        double poissEvents[] = new double[nEvents];
+        long seed1;
+        long seed2;
+        long seed3;
+        int i;
+        int n;
+        double poiss;
+        double ptable[];
+        int jx;
+        double temp;
+        double r;
         
-        v = gain * (inputValue + background);
-        outputValue = inputValue + (poidev(v, seed) - v)/ gain;
-        return outputValue;
-    }
-    
-    /** From Numerical Recipes in C The Art of Scientific Computing Second Edition by William H. Press,
-     *  Saul A. Teukolsky, William T.Vetterling, and Brian P. Flannery, pp. 293-295 Poisson Deviates
-     *  routine poidev adapted with minor changes
-     *  Returns as a double an integer value that is a random deviate drawn from a Poisson distribution
-     *  of mean xm
-     * @param xm
-     * @param seed
-     * @return
-     */
-    public final double poidev(double xm, long seed) {
-        double g;
-        double em;
-        double t;
-        double sq;
-        double alxm;
-        double result[] = new double[1];
-        //  0 for ln(gamma(x)), 1 for gamma(x)
-        int functionCode = 0;
-        Gamma gammaln;
-        double y;
-        // smallest positive nonzero value 2**-1074
-        double smallestUniformValue = Double.MIN_VALUE;
-        double largestUniformValue = 1.0;
-        double eps = Double.MIN_VALUE;
-        while (largestUniformValue == 1.0) {
-            largestUniformValue = largestUniformValue - eps;
-            eps = 2.0 * eps;
+        // Initialize the 3 seeds to integers uniformly distributed between 1 and 30,000
+        seed1 = genUniformRandomNum(1, 30000);
+        seed2 = genUniformRandomNum(1, 30000);
+        seed3 = genUniformRandomNum(1, 30000);
+        // mean + 8 * sigma
+        n = (int)(mu + 8 * Math.sqrt(mu));
+        if (n > maxBins) {
+            MipavUtil.displayError("Overflow error in poissDecay");
+            return null;
         }
-        if (xm < 12.0) {
-            // Use direct method
-            g = Math.exp(-xm);
-            em = -1.0;
-            t = 1.0;
-            do {
-                // Instead of adding exponential deviates it is equivalent to multiply uniform deviates.
-                // We never actually have to take the log, merely compare to the pre-computed exponential.
-                ++em;
-                t *= genUniformRandomNum(smallestUniformValue, largestUniformValue);
-            } while (t > g);
-        } // if (xm < 12.0)
-        else {
-            // Use rejection method
-            sq = Math.sqrt(2.0 * xm);
-            alxm = Math.log(xm);
-            gammaln = new Gamma(xm+1.0, functionCode, result);
-            gammaln.run();
-            try {
-                gammaln.finalize();
+        ptable = new double[n+1];
+        poiss = Math.exp(-mu);
+        ptable[0] = poiss;
+        for (i = 1; i <= n-1; i++) {
+            // poiss = (mu**i)*exp(-mu)/(i!)
+            poiss = poiss * mu/i;
+            ptable[i] = ptable[i-1]+poiss;
+        } // for (i = 1; i <= n-1; i++)
+        // Assure unit probability
+        ptable[n] = 1.0;
+        for (i = 0; i < nEvents; i++) {
+            jx = -1;
+            seed1 = 171 * (seed1 % 177) - 2 * (seed1 / 177);
+            if (seed1 < 0) {
+                seed1 = seed1 + 30269;
             }
-            catch (Throwable ex) {
-                MipavUtil.displayError("Exception on Gamma.finalize()");
-                return 0;
+            seed2 = 172*(seed2 % 176) - 35 * (seed2/ 176);
+            if (seed2 < 0) {
+                seed2 = seed2 + 30307;
             }
-            g = xm * alxm - result[0];
+            seed3 = 170 * (seed3 % 178) - 63 * (seed3 / 178);
+            if (seed3 < 0) {
+                seed3 = seed3 + 30323;
+            }
+            temp = seed1/30269.0 + seed2/30307.0 + seed3/30323.0;
+            r = temp - (int)temp;
             do {
-                do {
-                    // y is a deviate from a Lorentzian comparison function
-                    y = Math.tan(Math.PI * genUniformRandomNum(smallestUniformValue, largestUniformValue)); 
-                    // em is y, shifted and scaled.
-                    em = sq*y + xm;
-                    // Reject if in regime of zero probability
-                } while (em < 0.0);
-                // The trick for integer-valued distributions.
-                em = Math.floor(em);
-                gammaln = new Gamma(em+1.0, functionCode, result);
-                gammaln.run();
-                try {
-                    gammaln.finalize();
-                }
-                catch (Throwable ex) {
-                    MipavUtil.displayError("Exception on Gamma.finalize()");
-                    return 0;
-                }
-                t = 0.9*(1.0 + y*y)*Math.exp(em*alxm - result[0] - g);
-                // The ratio of the desired distribution to the comparison function;  we accept or 
-                // reject by comparing it to another uniform deviate.  The factor of 0.9 is chosen so
-                // that t never exceeds 1.
-            } while (genUniformRandomNum(smallestUniformValue, largestUniformValue) > t);
-        } // else
-        return em;
-    }
-
-    /**
-     * Calculates the Poisson probability function.
-     *
-     * @param   numberObserved  number of items observed
-     * @param   average         mean of the Poisson distribution
-     *
-     * @return  probability of numberObserved items occurring Note that if the average rate lambda is given instead of
-     *          the mean, then the mean is found from mean = lambda * time interval The Poisson may be used to
-     *          approximate a binomial distribution if the number n is large and the probability p is small. In this
-     *          case the mean is given by n*p. Since the number of counts in a unit time interval cannot be negative,
-     *          the Poisson is undefined for numberObserved less than zero. Likewise, the average of the number of
-     *          observed items cannot be negative. The variance of a Poisson distribution is equal to its mean.
-     */
-    public final float poissonProbability(int numberObserved, float average) {
-
-        if (numberObserved < 0) {
-            MipavUtil.displayError("The number observed cannot be less than zero");
-
-            return -1.0f;
-        }
-
-        if (average < 0.0f) {
-            MipavUtil.displayError("The average number of observed items cannot be negative");
-
-            return -1.0f;
-        }
-
-        return (float) (Math.pow(average, numberObserved) / factorial(numberObserved) * Math.exp(-average));
+                jx++;
+            } while (ptable[jx] <= r);
+            poissEvents[i] = gain * jx + offset;
+        } // for (i = 1; i <= nEvents; i++)
+        return poissEvents;
     }
 
 }
