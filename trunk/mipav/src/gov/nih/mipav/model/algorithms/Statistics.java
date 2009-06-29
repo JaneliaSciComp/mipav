ae7428143257f4989d23d6bd7b87d8322ea41fe6
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-06-29 18:21:30 +0000

    GaussianProbabilityIntegral now handles x < 0.0.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5559 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/Statistics.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/Statistics.java
@@ -71,9 +71,12 @@ public class Statistics {
      */
     private void gaussianProbabilityIntegral() {
         double t;
-        t = 1.0/(1.0 + 0.2316419*x);
+        t = 1.0/(1.0 + 0.2316419*Math.abs(x));
         ansR[0] = 1.0 - (1.0/Math.sqrt(2.0*Math.PI))*Math.exp(-x*x/2.0)*t*((((1.330274429*t - 1.821255978)*t
                   + 1.781477937)*t - 0.356563782)*t + 0.31938153);
+        if (x < 0.0) {
+            ansR[0] = 1.0 - ansR[0];
+        }
         return;
     }
     
