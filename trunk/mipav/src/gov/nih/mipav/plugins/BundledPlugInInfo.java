2a926ccf13fd27dc5b178aaf829d3b421e6212b4
NIH\mccreedy <NIH\mccreedy@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-07-13 17:23:58 +0000

    Updated the PlugInBundle class to be abstract with a non-static method that returns the list of BundledPlugInInfos.  The BundledPlugInInfo was changed to be an interface at the request of Bennett.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5618 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/plugins/BundledPlugInInfo.java
+++ trunk/mipav/src/gov/nih/mipav/plugins/BundledPlugInInfo.java
@@ -1,29 +1,8 @@
 package gov.nih.mipav.plugins;
 
 
-public class BundledPlugInInfo {
-    public String category;
+public interface BundledPlugInInfo {
+    public String[] getCategory();
 
-    public String name;
-
-    public BundledPlugInInfo(final String cat, final String pluginName) {
-        category = cat;
-        name = pluginName;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setCategory(final String cat) {
-        category = cat;
-    }
-
-    public void setName(final String pluginName) {
-        name = pluginName;
-    }
+    public String getName();
 }
