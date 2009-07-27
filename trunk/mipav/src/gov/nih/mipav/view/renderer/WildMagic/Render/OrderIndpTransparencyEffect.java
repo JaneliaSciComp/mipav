b37a13822978aef79492451fcf380a174c901ef3
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-07-27 15:56:04 +0000

    Comments code cleanup for transparency task.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5678 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/Render/OrderIndpTransparencyEffect.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/Render/OrderIndpTransparencyEffect.java
@@ -1,40 +1,34 @@
 package gov.nih.mipav.view.renderer.WildMagic.Render;
-import WildMagic.LibFoundation.Mathematics.ColorRGBA;
 import WildMagic.LibGraphics.Effects.*;
 import WildMagic.LibGraphics.Rendering.*;
 import WildMagic.LibGraphics.Shaders.*;
 
+/**
+ * This class sets up the final pass in rendering multiple semi-transparent surfaces.
+ * 
+ * Rendering multiple translucent surfaces is accomplished with the following steps:
+ *   1. A FrameBuffer Object (FBO) is created with three render-to-texture targets, and a depth buffer.
+ *   2. The first render target is activated. Solid objects are rendered: writing color and depth
+ *      information to the FBO target1. The color information displays the solid objects, the depth
+ *      information enables the solid objects to properly occlude the translucent objects.
+ *   3. The second and third render targets are activated. Translucent objects are rendered: writing
+ *      accumulated color and alpha to the second target and the number of overlapping surfaces per
+ *      pixel to the third target. In this pass the depth buffer is read-only.
+ *   4. Rendering to the three texture targets is disabled.
+ *   5. A screen-space polygon is rendered, it reads the three textures generated in the previous pass.
+ *      The first texture contains the color from the solid objects, the next two textures contain the
+ *      color information and weighted-average for rendering the translucent textures.
+ */
 public class OrderIndpTransparencyEffect extends ShaderEffect
 {
     /**
-     * Creates a new MultitextureEffect with the number of Textures specified
-     * by iTextureQuantity
-     * @param iTextureQuantity the number of textures in this
-     * MultitextureEffect.
+     * Creates a new OrderIndpTransparencyEffect for the final rendering pass.
+     * @param akTextures, set of three Frame Buffer Object (FBO) textures to render into/read from.
+     * @param kBackgroundColor, background color.
      */
-    public OrderIndpTransparencyEffect (Texture[] akTextures, Texture kSolidTex, ColorRGBA kBackgroundColor)
+    public OrderIndpTransparencyEffect (Texture[] akTextures)
     {
         super(1);   
-        m_kBackgroundColor = kBackgroundColor;
-        SetVShader(0,new VertexShader("TextureV", true));
-        PixelShader kPShader = new PixelShader("OrderIndpTransparencyFinalP", true);
-        SetPShader(0,kPShader);
-
-        kPShader.SetTextureQuantity(3);
-        for ( int i = 0; i < 2; i++ )
-        {
-            kPShader.SetTexture( i, akTextures[i] );
-            kPShader.SetImageName( i, akTextures[i].GetName() );
-        }
-        kPShader.SetTexture( 2, kSolidTex );
-        kPShader.SetImageName( 2, kSolidTex.GetName() );
-    }
-    
-
-    public OrderIndpTransparencyEffect (Texture[] akTextures, ColorRGBA kBackgroundColor)
-    {
-        super(1);   
-        m_kBackgroundColor = kBackgroundColor;
         SetVShader(0,new VertexShader("TextureV", true));
         PixelShader kPShader = new PixelShader("OrderIndpTransparencyFinalP", true);
         SetPShader(0,kPShader);
@@ -46,57 +40,4 @@ public class OrderIndpTransparencyEffect extends ShaderEffect
             kPShader.SetImageName( i, akTextures[i].GetName() );
         }
     }
-    
-    public OrderIndpTransparencyEffect (final String rkBaseName, float fAlpha)
-    {
-        super(1);   
-        m_fAlpha = fAlpha;
-        m_kVShader.set(0, new VertexShader("MaterialTextureV"));
-        //m_kPShader.set(0, new PixelShader("MaterialTextureP"));
-        m_kPShader.set(0, new PixelShader("OrderIndpTransparencyInitP"));
-
-        m_kPShader.get(0).SetTextureQuantity(1);
-        m_kPShader.get(0).SetImageName(0,rkBaseName);
-    }
-    
-    /* (non-Javadoc)
-     * @see WildMagic.LibGraphics.Effects.ShaderEffect#OnLoadPrograms(int, WildMagic.LibGraphics.Shaders.Program, WildMagic.LibGraphics.Shaders.Program)
-     */
-    public void OnLoadPrograms (int iPass, Program pkVProgram, Program pkPProgram, Program pkCProgram)
-    {
-        /*
-        if ( pkProgram != null && pkProgram.GetUC("BackgroundColor") != null ) 
-        {
-            pkProgram.GetUC("BackgroundColor").GetData()[0] = m_kBackgroundColor.R;
-            pkProgram.GetUC("BackgroundColor").GetData()[1] = m_kBackgroundColor.G;
-            pkProgram.GetUC("BackgroundColor").GetData()[2] = m_kBackgroundColor.B;
-        }
-        */
-        if ( pkCProgram.GetUC("MyAlpha") != null ) 
-        {
-            pkCProgram.GetUC("MyAlpha").GetData()[0] = m_fAlpha;
-        }
-    }
-    
-
-    public void SetAlpha (float fAlpha)
-    {
-        m_fAlpha = fAlpha;
-        Program pkCProgram = GetCProgram(0);
-        /*
-        if ( pkProgram != null && pkProgram.GetUC("BackgroundColor") != null ) 
-        {
-            pkProgram.GetUC("BackgroundColor").GetData()[0] = m_kBackgroundColor.R;
-            pkProgram.GetUC("BackgroundColor").GetData()[1] = m_kBackgroundColor.G;
-            pkProgram.GetUC("BackgroundColor").GetData()[2] = m_kBackgroundColor.B;
-        }
-        */
-        if ( pkCProgram.GetUC("MyAlpha") != null ) 
-        {
-            pkCProgram.GetUC("MyAlpha").GetData()[0] = m_fAlpha;
-        }
-    }
-    
-    private float m_fAlpha = 0.5f;
-    private ColorRGBA m_kBackgroundColor;
 }
