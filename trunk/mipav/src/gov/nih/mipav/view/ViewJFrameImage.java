8336fca2d4ba547b560a6714149f2527e629b1f4
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-09-10 16:11:54 +0000

    makeTitle uses imageNameArray for 3D if imageNameArray is not null.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@5824 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewJFrameImage.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewJFrameImage.java
@@ -5234,28 +5234,42 @@ public class ViewJFrameImage extends ViewJFrameBase implements KeyListener, Mous
      */
     protected String makeTitle() {
         String str;
+        String imageNameArray[] = null;
 
         if (displayMode == ViewJFrameBase.IMAGE_A) {
+            imageNameArray = imageA.getImageNameArray();
 
             if (imageA.getNDims() == 4) { // Setup the title for 4D image
                 str = imageA.getImageName() + "  " + String.valueOf(componentImage.getSlice()) + "/" + String.valueOf(nImage - 1) + "z  "
                         + String.valueOf(componentImage.getTimeSlice()) + "/" + String.valueOf(nTImage - 1) + "t M:"
                         + makeString(componentImage.getZoomX(), 2);
             } else if (imageA.getNDims() == 3) { // Setup the title for 3D image
+                if (imageNameArray != null) {
+                    str = imageNameArray[componentImage.getSlice()] + "  " + String.valueOf(componentImage.getSlice()) + "/" + String.valueOf(nImage - 1) + " M:"
+                    + makeString(componentImage.getZoomX(), 2);    
+                }
+                else {
                 str = imageA.getImageName() + "  " + String.valueOf(componentImage.getSlice()) + "/" + String.valueOf(nImage - 1) + " M:"
                         + makeString(componentImage.getZoomX(), 2);
+                }
             } else {
                 str = imageA.getImageName() + "  M:" + makeString(componentImage.getZoomX(), 2);
             }
         } else {
-
+            imageNameArray = imageB.getImageNameArray();
             if (imageB.getNDims() == 4) { // Setup the title for 4D image of image B
                 str = imageB.getImageName() + "  " + String.valueOf(componentImage.getSlice()) + "/" + String.valueOf(nImage - 1) + "z  "
                         + String.valueOf(componentImage.getTimeSlice()) + "/" + String.valueOf(nTImage - 1) + "t M:"
                         + makeString(componentImage.getZoomX(), 2);
             } else if (imageB.getNDims() == 3) { // Setup the title
+                if (imageNameArray != null) {
+                    str = imageNameArray[componentImage.getSlice()] + "  " + String.valueOf(componentImage.getSlice()) + "/" + String.valueOf(nImage - 1) + " M:"
+                    + makeString(componentImage.getZoomX(), 2);    
+                }
+                else {
                 str = imageB.getImageName() + "  " + String.valueOf(componentImage.getSlice()) + "/" + String.valueOf(nImage - 1) + " M:"
                         + makeString(componentImage.getZoomX(), 2);
+                }
             } else {
                 str = imageB.getImageName() + "  M:" + makeString(componentImage.getZoomX(), 2);
             }
