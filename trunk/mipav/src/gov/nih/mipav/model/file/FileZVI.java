c96eb6df45064003b6be2a5f959b16d26e80798f
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-12-07 19:42:53 +0000

    Started work on.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6078 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileZVI.java
@@ -0,0 +1,164 @@
+package gov.nih.mipav.model.file;
+
+
+import gov.nih.mipav.model.structures.*;
+
+import java.io.*;
+import gov.nih.mipav.view.*;
+
+/**
+ 
+ */
+
+public class FileZVI extends FileBase {
+   
+    //~ Instance fields ------------------------------------------------------------------------------------------------
+
+    /** DOCUMENT ME! */
+    private File file;
+
+    /** DOCUMENT ME! */
+    private String fileDir;
+
+
+    /** DOCUMENT ME! */
+    private FileInfoZVI fileInfo;
+
+    /** DOCUMENT ME! */
+    private String fileName;
+
+    /** DOCUMENT ME! */
+    private ModelImage image;
+
+    /** DOCUMENT ME! */
+    private int[] imageExtents = null;
+
+    /** DOCUMENT ME! */
+    private float[] imgBuffer = null;
+
+    /** DOCUMENT ME! */
+    private float[] imgResols = new float[5];
+
+    /** DOCUMENT ME! */
+    private ModelLUT LUT = null;
+
+    //~ Constructors ---------------------------------------------------------------------------------------------------
+
+    /**
+     * ZVI reader constructor.
+     *
+     * @param      fileName  file name
+     * @param      fileDir   file directory
+     *
+     * @exception  IOException  if there is an error making the file
+     */
+    public FileZVI(String fileName, String fileDir) throws IOException {
+
+        this.fileName = fileName;
+        this.fileDir = fileDir;
+    }
+
+    //~ Methods --------------------------------------------------------------------------------------------------------
+
+    /**
+     * Prepares this class for cleanup. Calls the <code>finalize</code> method for existing elements, closes any open
+     * files and sets other elements to <code>null</code>.
+     */
+    public void finalize() {
+        fileName = null;
+        fileDir = null;
+        fileInfo = null;
+        file = null;
+        image = null;
+        imageExtents = null;
+        imgBuffer = null;
+        imgResols = null;
+        LUT = null;
+        
+        try {
+            super.finalize();
+        } catch (Throwable er) { }
+    }
+    
+    /**
+     * Accessor that returns the file info.
+     *
+     * @return  FileInfoBase containing the file info
+     */
+    public FileInfoBase getFileInfo() {
+        return fileInfo;
+    }
+
+
+    /**
+     * Accessor that returns the image buffer.
+     *
+     * @return  buffer of image.
+     */
+    public float[] getImageBuffer() {
+        return imgBuffer;
+    }
+
+    /**
+     * Rreturns LUT if defined.
+     *
+     * @return  the LUT if defined else it is null
+     */
+    public ModelLUT getModelLUT() {
+        return LUT;
+    }
+    
+    /**
+     * Reads the LIFF header which indicates endianess, the TIFF magic number, and the offset in bytes of the first IFD.
+     * It then reads all the IFDs. This method then opens a Model of an image and imports the the images one slice at a
+     * time. Image slices are separated by an IFD.
+     *
+     * @param      multiFile  <code>true</code> if a set of files each containing a separate 2D image is present <code>
+     *                        false</code> if one file with either a 2D image or a stack of 2D images
+     * @param      one        <code>true</code> if only want to read in one image of the 3D set
+     *
+     * @return     returns the image
+     *
+     * @exception  IOException  if there is an error reading the file
+     */
+    public ModelImage readImage(boolean multiFile, boolean one) throws IOException {
+        long fileLength;
+        boolean endianess;
+        int i, j, k;
+        
+        try {
+            
+            imgResols[0] = imgResols[1] = imgResols[2] = imgResols[3] = imgResols[4] = (float) 1.0;
+            file = new File(fileDir + fileName);
+            raFile = new RandomAccessFile(file, "r");
+            
+            fileLength = raFile.length();
+            
+            
+            
+            fileInfo = new FileInfoZVI(fileName, fileDir, FileUtility.ZVI); // dummy fileInfo
+            fileInfo.setEndianess(FileBase.LITTLE_ENDIAN);
+            
+            
+            
+            image.calcMinMax();
+            fireProgressStateChanged(100);
+            
+        } catch (OutOfMemoryError error) {
+
+            if (image != null) {
+                image.disposeLocal();
+                image = null;
+            }
+
+            System.gc();
+            throw error;
+        }
+
+        return image;
+    }
+    
+    
+
+    
+}
