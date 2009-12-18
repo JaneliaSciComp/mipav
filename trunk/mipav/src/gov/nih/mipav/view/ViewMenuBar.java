8db41d73f6ed05cabee5284558cf1a78262da4a0
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2009-12-18 13:51:26 +0000

    changed text to Grid overlay options
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6117 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewMenuBar.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewMenuBar.java
@@ -540,7 +540,7 @@ public class ViewMenuBar {
                 menuBuilder.buildCheckBoxMenuItem("Show slice number overlay", "ShowSliceNum", true),
                 menuBuilder.buildCheckBoxMenuItem("Show image/DICOM overlay", "ShowOverlay", showOverlay),
                 menuBuilder.buildCheckBoxMenuItem("Show overlay grid", "ShowGrid", false),
-                menuBuilder.buildMenuItem("Grid options", "GridOptions", 0, null, true),
+                menuBuilder.buildMenuItem("Grid overlay options", "GridOptions", 0, null, true),
                 menuBuilder.buildMenuItem("DICOM overlay options", "DICOMOverlayOptions", 0, null, true),
                 menuBuilder.buildMenuItem("Image overlay options", "ImageOverlayOptions", 0, null, true),});
     }
