e3000840f6f883191be8d17666e0d19491a89652
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-01-06 17:29:26 +0000

    In setComboBox changed MILS to MILS (.001 INCH).
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6192 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogRawIO.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogRawIO.java
@@ -937,7 +937,7 @@ public class JDialogRawIO extends JDialogBase {
         cBox.setBackground(Color.white);
         cBox.addItem(" UNKNOWN");
         cBox.addItem(" INCHES ");
-        cBox.addItem(" MILS ");
+        cBox.addItem(" MILS (.001 INCH) ");
         cBox.addItem(" CENTIMETERS ");
         cBox.addItem(" ANGSTROMS ");
         cBox.addItem(" NANOMETERS ");
