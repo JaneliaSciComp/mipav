03de163121e62426ec1bab05443bfaa1541b1028
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-02-17 20:03:21 +0000

    In driver corrected if ((exitStatus < 0) && outputMes) to if (exitStatus < 0) with separate if (outputMes) clause.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6343 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/NLConstrainedEngine.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/NLConstrainedEngine.java
@@ -728,8 +728,10 @@ public abstract class NLConstrainedEngine {
             // call lsunc to minimize
             lsunc();
 
-            if (exitStatus < 0  && outputMes) {
-                Preferences.debug("exitStatus after lsunc() call = " + exitStatus + "\n");
+            if (exitStatus < 0) {
+            	if (outputMes) {
+                    Preferences.debug("exitStatus after lsunc() call = " + exitStatus + "\n");
+            	}
 
                 return;
             }
