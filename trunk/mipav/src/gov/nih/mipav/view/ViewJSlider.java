a9d50064af43ef849c24350cc828c915da87a900
NIH\pandyan <NIH\pandyan@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-04-22 14:55:21 +0000

    moved the isPrime method to MipavMath and renamed it to isPrime_under1000
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6585 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewJSlider.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewJSlider.java
@@ -1,5 +1,7 @@
 package gov.nih.mipav.view;
 
+import gov.nih.mipav.util.MipavMath;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ComponentEvent;
@@ -304,7 +306,7 @@ public class ViewJSlider extends JSlider {
             return;
         }
         
-        if(isPrime(maxPlusMin)) {
+        if(MipavMath.isPrime_under1000(maxPlusMin)) {
             majTickSpacing = 0;
             minTickSpacing = getBestTickSpacing(maxPlusMin + 1,maxMinorTicks);
             setMajorTickSpacing(majTickSpacing);
@@ -327,7 +329,7 @@ public class ViewJSlider extends JSlider {
                 setSnapToTicks(true);
                 return;
             }else {
-                if(isPrime(majTickSpacing)) {
+                if(MipavMath.isPrime_under1000(majTickSpacing)) {
                     minTickSpacing = majTickSpacing;
                     setMajorTickSpacing(majTickSpacing);
                     setMinorTickSpacing(minTickSpacing);
@@ -386,27 +388,5 @@ public class ViewJSlider extends JSlider {
     
 
     
-    /**
-     * Helper function that determines if number is prime.
-     * NEEDS TO NE MOVED TO MIPAVMATH AT SOME POINT
-     * @param num Number to be determined if it is prime or not.
-     * @return boolean telling whether it is prime or not.
-     */
-    private static boolean isPrime(int num) {
-        boolean isPrime = false;
-        int[] primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,
-                113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,
-                241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,
-                383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,
-                541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,
-                683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,
-                853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
-        for(int i=0;i<primes.length;i++) {
-            if(num == primes[i]) {
-                isPrime = true;
-                break;
-            }
-        }
-        return isPrime;
-    }
+    
 }
