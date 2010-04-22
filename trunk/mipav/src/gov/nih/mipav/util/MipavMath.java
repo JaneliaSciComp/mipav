276b32e0affb97ae059fbfe339d85741b6be4875
NIH\mccreedy <NIH\mccreedy@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-04-22 17:11:21 +0000

    Fixed formatting.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6591 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/util/MipavMath.java
+++ trunk/mipav/src/gov/nih/mipav/util/MipavMath.java
@@ -9,20 +9,18 @@ import WildMagic.LibFoundation.Mathematics.*;
  */
 public class MipavMath {
 
-   
-	/**
-	 * Calculates the angle between two lines.
-	 * 
-	 * @param x X values.
-	 * @param y Y values.
-	 * @param res Voxel resolutions.
-	 * @return the angle between two lines.
-	 */
-	
-	public static final double angle(final float[] x, final float[] y, final float[] res) {
-        
-		final double theta  = (180.0 / Math.PI) * Math.atan2( ( (y[1] - y[0]) * res[1]), ( (x[1] - x[0]) * res[0]));
-              double theta2 = (180.0 / Math.PI) * Math.atan2( ( (y[2] - y[0]) * res[1]), ( (x[2] - x[0]) * res[0]));
+    /**
+     * Calculates the angle between two lines.
+     * 
+     * @param x X values.
+     * @param y Y values.
+     * @param res Voxel resolutions.
+     * @return the angle between two lines.
+     */
+    public static final double angle(final float[] x, final float[] y, final float[] res) {
+
+        final double theta = (180.0 / Math.PI) * Math.atan2( ( (y[1] - y[0]) * res[1]), ( (x[1] - x[0]) * res[0]));
+        double theta2 = (180.0 / Math.PI) * Math.atan2( ( (y[2] - y[0]) * res[1]), ( (x[2] - x[0]) * res[0]));
         theta2 = theta2 - theta;
 
         if (theta2 < -180.0) {
@@ -342,7 +340,8 @@ public class MipavMath {
     }
 
     /**
-     * Returns the minimum value found in a float array.  
+     * Returns the minimum value found in a float array.
+     * 
      * @param data the data array
      * @return minimum value in the array.
      */
@@ -357,7 +356,8 @@ public class MipavMath {
     }
 
     /**
-     * Returns the minimum value found in a short array.  
+     * Returns the minimum value found in a short array.
+     * 
      * @param data the data array
      * @return minimum value in the array.
      */
@@ -372,7 +372,8 @@ public class MipavMath {
     }
 
     /**
-     * Returns the maximum value found in a float array.  
+     * Returns the maximum value found in a float array.
+     * 
      * @param data the data array
      * @return minimum value in the array.
      */
@@ -385,9 +386,10 @@ public class MipavMath {
         }
         return max;
     }
-    
+
     /**
-     * Returns the maximum value found in a short array.  
+     * Returns the maximum value found in a short array.
+     * 
      * @param data the data array
      * @return minimum value in the array.
      */
@@ -421,24 +423,26 @@ public class MipavMath {
 
         return r;
     }
-    
+
     /**
      * Function that determines if number is prime from a set of primes that is under 1000.
      * 
      * @param num Number to be determined if it is prime or not.
      * @return boolean indicating whether it is prime or not.
      */
-    public static boolean isPrimeUnder1000(int num) {
+    public static boolean isPrimeUnder1000(final int num) {
         boolean isPrime = false;
-        int[] primes = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,
-                113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,
-                241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,
-                383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,
-                541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,
-                683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,
-                853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997};
-        for(int i=0;i<primes.length;i++) {
-            if(num == primes[i]) {
+        final int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+                89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+                197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
+                313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433,
+                439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569,
+                571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683,
+                691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827,
+                829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971,
+                977, 983, 991, 997};
+        for (final int element : primes) {
+            if (num == element) {
                 isPrime = true;
                 break;
             }
