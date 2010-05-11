7f6ef754b7292c1df767a85eadb25a5cef3af4bb
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-05-11 19:37:56 +0000

    Added useNativeFormat parameter to AlgorithmChangeType constructor.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6669 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/JPanelVolOpacityRGB.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/JPanelVolOpacityRGB.java
@@ -806,9 +806,10 @@ public class JPanelVolOpacityRGB extends JPanelVolOpacityBase {
                 rendererProgressBar.setValue(20);
                 rendererProgressBar.update(rendererProgressBar.getGraphics());
 
+                boolean useNativeFormat = false;
                 AlgorithmChangeType changeTypeAlgo_A = new AlgorithmChangeType(gradMagRescale_A, gradMag_A,
                                                                                gradMag_A.getMin(), gradMag_A.getMax(),
-                                                                               0, 255, false);
+                                                                               0, 255, false, useNativeFormat);
 
                 changeTypeAlgo_A.setRunningInSeparateThread(isActiveImage);
                 changeTypeAlgo_A.run();
@@ -884,9 +885,10 @@ public class JPanelVolOpacityRGB extends JPanelVolOpacityBase {
                 rendererProgressBar.setValue(70);
                 rendererProgressBar.update(rendererProgressBar.getGraphics());
 
+                boolean useNativeFormat = false;
                 AlgorithmChangeType changeTypeAlgo_B = new AlgorithmChangeType(gradMagRescale_B, gradMag_B,
                                                                                gradMag_B.getMin(), gradMag_B.getMax(),
-                                                                               0, 255, false);
+                                                                               0, 255, false, useNativeFormat);
 
                 changeTypeAlgo_B.setRunningInSeparateThread(isActiveImage);
                 changeTypeAlgo_B.run();
