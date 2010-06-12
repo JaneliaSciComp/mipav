37cf741241125592471a63c18d69568656b122a7
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-06-12 21:28:27 +0000

    Moved the retraceContour to VOIManager. Reactivated the retraceContour functionality.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@6872 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/VOI/VOIManager.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/VOI/VOIManager.java
@@ -345,6 +345,7 @@ public class VOIManager implements ActionListener, KeyListener, MouseListener, M
     private static final int RECTANGLE3D = 10;
     private static final int SPLITLINE = 11;
     private static final int LUT = 12;
+    private static final int RETRACE = 13;
     private int m_iDrawType;
 
     private VOIBase m_kCurrentVOI = null;
@@ -1242,6 +1243,11 @@ public class VOIManager implements ActionListener, KeyListener, MouseListener, M
                 anchorPolyline( kEvent.getX(), kEvent.getY(), false );
             }
             return;
+        }
+        else if ( m_iDrawType == RETRACE && m_kCurrentVOI != null )
+        {
+            m_kCurrentVOI.trimPoints(Preferences.getTrim(), Preferences.getTrimAdjacient());
+            resetStart();
         }
 
 
@@ -3641,7 +3647,12 @@ public class VOIManager implements ActionListener, KeyListener, MouseListener, M
         } 
         else if ( m_kParent.getPointerButton().isSelected() &&  !m_bDrawVOI )
         {
-            if ( m_iNearStatus == NearPoint )
+            if ( kEvent.isAltDown() && m_kCurrentVOI != null )
+            {
+                //resetStart();
+                retraceContour( m_kCurrentVOI, kEvent.getX(), kEvent.getY() );
+            }
+            else if ( m_iNearStatus == NearPoint )
             {
                 if ( m_bFirstDrag && ((m_fMouseX != kEvent.getX()) || (m_fMouseY != kEvent.getY())) )
                 {
@@ -4709,8 +4720,227 @@ public class VOIManager implements ActionListener, KeyListener, MouseListener, M
         return null;
     }
     
+
+    private int indexRetrace = -99;
+    private VOIContour oldContour = null;
+    private int lastX = -1;
+    private int lastY = -1;
+    private int lastZ = -1;
+    private boolean knowDirection = false;
+    private boolean isFirst = true;
+    private boolean resetStart = true;
     
-    
+
+    /**
+     * Redraws contour. Must move cursor in clockwise motion to work best
+     * 
+     * @param zoomX
+     *            zoom for the x coordinate
+     * @param zoomY
+     *            zoom for the y coordinate
+     * @param resolutionX
+     *            resolution of the pixel in the X direction (aspect ratio)
+     * @param resolutionY
+     *            resolution of the pixel in the Y direction (aspect ratio)
+     * @param resols
+     *            array of pixel resolutions
+     * @param x1
+     *            x coordinate of new point to add
+     * @param y1
+     *            y coordinateof new point to add
+     * @param g
+     *            graphics to paint in
+     */
+    public void retraceContour(VOIBase kVOIIn, int x1, int y1 ) {
+        if ( !(kVOIIn instanceof VOIContour) )
+        {
+            return;
+        }
+        m_iDrawType = RETRACE;
+        VOIContour kVOI = (VOIContour)kVOIIn;
+        Vector3f ptRetrace = null;
+        double minDistance, dist;
+
+        try {
+            kVOI.makeCounterClockwise();
+            if (indexRetrace == -99) {
+                oldContour = new VOIContour( kVOI );
+            }
+
+            // Return if trying to add the same point.
+            Vector3f kScreen = new Vector3f( x1, y1, m_iSlice );
+            Vector3f kCurrent = m_kDrawingContext.screenToFile( kScreen );
+
+            if (kVOI.contains(kCurrent)) {
+                return;
+            }
+
+            kVOI.setActive(false);
+
+            // Find nearest point in old contour
+            minDistance = 9999999;
+            int end = oldContour.size();
+
+            for (int i = 0; i < end; i++) {
+                dist = MipavMath.distance(kCurrent, oldContour.elementAt(i));
+
+                if (dist < minDistance) {
+                    ptRetrace = oldContour.elementAt(i);
+                    minDistance = dist;
+                }
+            }
+
+            if (resetStart) {
+                kVOI.makeCounterClockwise( kVOI.indexOf(ptRetrace) );
+                resetIndex();
+                resetStart = false;
+            } else {
+
+                int index, indexold;
+                Vector3f newer = new Vector3f( kCurrent );
+
+                if (lastX == -1) { // if first time
+
+                    lastX = (int) ptRetrace.X;
+                    lastY = (int) ptRetrace.Y;
+                    lastZ = (int) ptRetrace.Z;
+                    indexRetrace = oldContour.indexOf(ptRetrace);
+                    kVOI.insertElementAt(newer,
+                            kVOI.indexOf(oldContour.get(indexRetrace)));
+                } else if (!(lastX == (int) ptRetrace.X && lastY == (int) ptRetrace.Y)) {
+
+                    index = oldContour.indexOf(ptRetrace);
+                    Vector3f old = new Vector3f(lastX, lastY, lastZ);
+                    indexold = indexRetrace;
+                    if (index == 0 || index - indexold == 1) { // if
+                                                                // countclockwise
+                                                                // and no jumps
+                        knowDirection = true;
+                        lastX = (int) ptRetrace.X;
+                        lastY = (int) ptRetrace.Y;
+                        lastZ = (int) ptRetrace.Z;
+                        kVOI.insertElementAt(newer, kVOI.indexOf(old));
+                        kVOI.removeElement(old);
+                        indexRetrace = index;
+                    } else if (isFirst && indexold >= kVOI.size() - 3
+                            && indexold - index != 1) {
+                        if (((indexold - index) < (oldContour.size() / 2))) {
+                            for (; index != indexold; indexold--) {
+                                indexRetrace = kVOI.indexOf(old) - 1;
+                                oldContour.removeElementAt(indexRetrace);
+                                kVOI.removeElementAt(indexRetrace);
+                                old = kVOI.get(indexRetrace);
+                            }
+                            lastX = (int) old.X;
+                            lastY = (int) old.Y;
+                            lastZ = (int) old.Z;
+                            kVOI.insertElementAt(newer, kVOI.indexOf(old) + 1);
+                        } else {
+                            indexRetrace = kVOI.indexOf(old) - 1;
+                            oldContour.removeElementAt(indexRetrace);
+                            kVOI.removeElementAt(indexRetrace);
+                            for (int size = kVOI.size() - 1, i = 0; i != size
+                                    - indexold; i++) {
+                                oldContour.remove(0);
+                                kVOI.remove(0);
+                            }
+                            lastX = (int) old.X;
+                            lastY = (int) old.Y;
+                            lastZ = (int) old.Z;
+                            kVOI.insertElementAt(newer, 0);
+                            indexRetrace = 0;
+                        }
+
+                    } else if ((indexold - index) >= 1) { // if clockwise in
+                                                            // general
+                        knowDirection = true;
+                        if (!isFirst) {
+                            if (!(index - indexold > oldContour.size() / 2 || index == 0)) {
+                                for (; index != indexold; index++) {
+                                    indexRetrace = kVOI.indexOf(old);
+                                    oldContour.removeElementAt(oldContour
+                                            .indexOf(old));
+                                    kVOI.removeElementAt(indexRetrace);
+                                    old = kVOI.get(indexRetrace - 1);
+                                }
+                                lastX = (int) old.X;
+                                lastY = (int) old.Y;
+                                lastZ = (int) old.Z;
+                                kVOI.insertElementAt(newer, kVOI.indexOf(old) + 1);
+                            }
+
+                        } else {
+                            isFirst = false;
+                            if (indexold - index == 1) {
+                                knowDirection = true;
+                                lastX = (int) ptRetrace.X;
+                                lastY = (int) ptRetrace.Y;
+                                lastZ = (int) ptRetrace.Z;
+
+                                if (isFirst) {
+                                    kVOI.insertElementAt(newer, kVOI.indexOf(old) - 1);
+                                }
+                                kVOI.removeElement(old);
+                                indexRetrace = index;
+                            }
+                        }
+                    } else { // if counterclockwise and with jumps
+                        Vector3f right = new Vector3f(oldContour.get(index + 1));
+                        for (; index != indexold - 1; indexold++) {
+                            if (oldContour.indexOf(indexold) != -1) {
+                                oldContour.removeElementAt(indexold);
+                            }
+                            kVOI.removeElement(oldContour.elementAt(indexold));
+                        }
+
+                        kVOI.insertElementAt(newer, kVOI.indexOf(right));
+                        ptRetrace = right;
+                        lastX = (int) ptRetrace.X;
+                        lastY = (int) ptRetrace.Y;
+                        lastZ = (int) ptRetrace.Z;
+
+                        indexRetrace = oldContour.indexOf(right) - 1;
+                    }
+                } else if (minDistance > 5 && knowDirection) {
+                    if (isFirst)
+                        kVOI.insertElementAt(newer, kVOI.indexOf(ptRetrace));
+                    else {
+                        kVOI.insertElementAt(newer, kVOI.indexOf(ptRetrace) + 1);
+                    }
+                }
+            }
+            kVOI.setActive(true);
+            m_kParent.updateDisplay();
+        } catch (OutOfMemoryError error) {
+            System.gc();
+        } catch (ArrayIndexOutOfBoundsException error) {
+            resetStart();
+        } catch (NullPointerException error) {
+            resetStart();
+        }
+    }
+
+    /* Resets the index and flag used in the retraceContour mode. It should be
+     * called after retracing a Contour.
+     */
+    public void resetIndex() {
+        indexRetrace = -99;
+        oldContour = null;
+        lastX = -1;
+        knowDirection = false;
+        isFirst = true;
+    }
+
+
+
+    /**
+     * Resets all retrace variables and allows it to start anewA
+     */
+    public void resetStart() {
+        resetStart = true;
+        resetIndex();
+
+    }
     
     
 }
