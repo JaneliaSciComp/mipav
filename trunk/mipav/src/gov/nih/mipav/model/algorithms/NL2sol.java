0f4475e03afd5ac3873f8a2e2b07491c98d97b89
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-07-26 16:42:00 +0000

    Debugging.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@7063 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/NL2sol.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/NL2sol.java
@@ -7754,7 +7754,7 @@ private boolean testMode = false;
 	//
 	//  Print iteration summary, check iteration limit.
     //
-	loop1: while (!do700) {
+	loop1: while ((!do700) && (!do710) && (!do730)) {
      if (do150) {
 	     do150 = false;
 
@@ -8589,7 +8589,7 @@ private boolean testMode = false;
 	      iv[1] = 2;
 	      do150 = true;
 	 } // if (do570)
-	} // loop1: while (!do700)
+	} // loop1: while ((!do700) && (!do710) && (!do730))
 	//
 	//  Bad parameters to ASSESS.
 	//
