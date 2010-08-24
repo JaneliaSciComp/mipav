bd3a1769256ff1802d867046bd3854d64ec4326d
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-08-24 14:09:38 +0000

    Fixed mask error: was causing the 'median' statistic to be incorrect, and the counter statistic, etc.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@7154 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/structures/VOI.java
+++ trunk/mipav/src/gov/nih/mipav/model/structures/VOI.java
@@ -2010,7 +2010,7 @@ public class VOI extends ModelSerialCloneable {
      */
     public void setActive(boolean act) {
         this.active = act;
-        fireVOIselection();
+        //fireVOIselection();
     }
 
     /**
