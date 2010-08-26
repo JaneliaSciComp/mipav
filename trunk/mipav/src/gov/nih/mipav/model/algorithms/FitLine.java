07c99a3ccf0ab569ee08b6df88c6c39b40f30d04
NIH\ilb <NIH\ilb@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-08-26 19:31:40 +0000

    Now works with NLConstrainedEngine instead of NLEngine. caclulateFittedY filled in.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@7178 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/FitLine.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/FitLine.java
@@ -9,7 +9,8 @@ import gov.nih.mipav.view.*;
  * y = mx + b;.
  */
 public class FitLine extends NLFittedFunction {
-
+	private double xSeries[];
+	private double ySeries[];
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
 	/**
@@ -19,6 +20,19 @@ public class FitLine extends NLFittedFunction {
 
         // 10 points, 2 parameters, and function type
         super(10, 2);
+        
+        bounds = 0; // bounds = 0 means unconstrained
+        // bounds = 1 means same lower and upper bounds for
+        // all parameters
+        // bounds = 2 means different lower and upper bounds
+        // for all parameters
+        
+        // The default is internalScaling = false
+        // To make internalScaling = true and have the columns of the
+        // Jacobian scaled to have unit length include the following line.
+        // internalScaling = true;
+        // Suppress diagnostic messages
+        outputMes = false;
 
         setupData();
     }
@@ -34,28 +48,34 @@ public class FitLine extends NLFittedFunction {
 
         // nPoints data points, 3 coefficients, and exponential fitting
         super(nPoints, 2);
-
+        
+        xSeries = new double[xData.length];
+        ySeries = new double[yData.length];
         int i;
-
-        // ia[0] equals 0 for fixed c1; ia[0] is nonzero for fitting c1
-        ia[0] = 1;
-
-        // ia[1] equals 0 for fixed c2; ia[1] is nonzero for fitting c2
-        ia[1] = 1;
-
-        for (i = 0; i < nPoints; i++) {
-            xseries[i] = (double) xData[i];
-            yseries[i] = (double) yData[i];
+        for (i = 0; i < xData.length; i++) {
+        	xSeries[i] = xData[i];
         }
+        for (i = 0; i < yData.length; i++) {
+        	ySeries[i] = yData[i];
+        }
+        
+        bounds = 0; // bounds = 0 means unconstrained
+        // bounds = 1 means same lower and upper bounds for
+        // all parameters
+        // bounds = 2 means different lower and upper bounds
+        // for all parameters
+        
+        // The default is internalScaling = false
+        // To make internalScaling = true and have the columns of the
+        // Jacobian scaled to have unit length include the following line.
+        // internalScaling = true;
+        // Suppress diagnostic messages
+        outputMes = false;      
 
         gues[0] = 0; // y intercept
         gues[1] = -1; // slope
 
-        stdv = 1;
-
-        for (i = 0; i < nPoints; i++) {
-            sig[i] = stdv;
-        }
+        
     }
 
     //~ Methods --------------------------------------------------------------------------------------------------------
@@ -66,98 +86,101 @@ public class FitLine extends NLFittedFunction {
     public void displayResults() {
         Preferences.debug(" ******* Dump FitLine ********* \n");
 
-        Preferences.debug("Number of iterations: " + String.valueOf(kk) + "\n");
-        Preferences.debug("Chi-squared: " + String.valueOf(chisq) + "\n");
-        Preferences.debug("Final lamda: " + String.valueOf(flamda) + "\n");
-        Preferences.debug("a0 " + String.valueOf(a[0]) + " +/- " + String.valueOf(Math.sqrt(covar[0][0])) + "\n");
-        Preferences.debug("a1 " + String.valueOf(a[1]) + " +/- " + String.valueOf(Math.sqrt(covar[1][1])) + "\n\n");
+        Preferences.debug("Number of iterations: " + String.valueOf(iters) + "\n");
+        Preferences.debug("Chi-squared: " + String.valueOf(getChiSquared()) + "\n");
+        Preferences.debug("a0 " + String.valueOf(a[0]) + " +/- " + String.valueOf(Math.sqrt(covarMat[0][0])) + "\n");
+        Preferences.debug("a1 " + String.valueOf(a[1]) + " +/- " + String.valueOf(Math.sqrt(covarMat[1][1])) + "\n\n");
 
     }
-
-    /**
-     * Fits line to function.
-     *
-     * @param   x1    The x value of the data point.
-     * @param   atry  The best guess parameter values.
-     * @param   dyda  The derivative values of y with respect to fitting parameters.
-     *
-     * @return  The calculated y value.
+    
+    /** 
+     * Fit line to function.
+     * @param a The best guess parameter values.
+     * @param residuals ymodel - yData.
+     * @param covarMat The derivative values of y with respect to fitting parameters.
      */
-    public double fitToFunction(double x1, double[] atry, double[] dyda) {
-
-        // called by mrqcof
-        // mrqcof supplies x1 and atry[]
-        // function returns partial derivatives dyda[] and the calculated ymod
-        double fac, ymod = 0;
+    public void fitToFunction(final double[] a, final double[] residuals, final double[][] covarMat) {
+        int ctrl;
+        int j;
+        double ymod = 0;
 
         try {
-            fac = atry[1] * x1;
-            ymod = atry[0] + fac;
-            dyda[0] = 1; // a0 partial derivative (y intecept)
-            dyda[1] = x1; // a1 partial derivative (slope)
-        } catch (Exception e) {
-            Preferences.debug("function error: " + e.getMessage());
+            ctrl = ctrlMat[0];
+            // Preferences.debug("ctrl = " + ctrl + " a[0] = " + a[0] + " a[1] = " + a[1] + "\n");
+            if ( (ctrl == -1) || (ctrl == 1)) {
+                
+                // evaluate the residuals[j] = ymod - yData[j]
+                for (j = 0; j < nPts; j++) {
+                	ymod = a[1] * xSeries[j] + a[0];
+                    residuals[j] = ymod - ySeries[j];
+                    // Preferences.debug("residuals["+ j + "] = " + residuals[j] + "\n");
+                }
+            } // if ((ctrl == -1) || (ctrl == 1))
+            else if (ctrl == 2) {
+                // Calculate the Jacobian analytically
+                for (j = 0; j < nPts; j++) {
+                	covarMat[j][0] = 1; // a0 partial derivative
+                	covarMat[j][1] = xSeries[j]; // a1 partial derivative
+                }
+            }
+            // Calculate the Jacobian numerically
+            // else if (ctrl == 2) {
+            // ctrlMat[0] = 0;
+            // }
+        } catch (final Exception exc) {
+            Preferences.debug("function error: " + exc.getMessage() + "\n");
         }
 
-        return ymod;
+        return;
     }
 
     /**
      * Sets up data to test fitting of exponential.
      */
     private void setupData() {
-        int i;
-
-        stdv = 1;
-
-        // ia[0] = 0 for fixed offset parameter; ia[0] is nonzero for fitting offset parameter
-        ia[0] = 1;
-
-        // ia[1] = 0 for fixed slope parameter; ia[1] is nonzero for fitting slope parameter
-        ia[1] = 1;
+        
         gues[0] = 1;
         gues[1] = 1;
 
-        xseries[0] = 0;
-        yseries[0] = 0;
+        xSeries[0] = 0;
+        ySeries[0] = 0;
 
-        xseries[1] = 1;
-        yseries[1] = 1.5;
+        xSeries[1] = 1;
+        ySeries[1] = 1.5;
 
-        xseries[2] = 2;
-        yseries[2] = 1.75;
+        xSeries[2] = 2;
+        ySeries[2] = 1.75;
 
-        xseries[3] = 3;
-        yseries[3] = 3.25;
+        xSeries[3] = 3;
+        ySeries[3] = 3.25;
 
-        xseries[4] = 4;
-        yseries[4] = 4;
+        xSeries[4] = 4;
+        ySeries[4] = 4;
 
-        xseries[5] = 5;
-        yseries[5] = 5.5;
+        xSeries[5] = 5;
+        ySeries[5] = 5.5;
 
-        xseries[6] = 6;
-        yseries[6] = 5.75;
+        xSeries[6] = 6;
+        ySeries[6] = 5.75;
 
-        xseries[7] = 7;
-        yseries[7] = 8;
+        xSeries[7] = 7;
+        ySeries[7] = 8;
 
-        xseries[8] = 8;
-        yseries[8] = 8;
+        xSeries[8] = 8;
+        ySeries[8] = 8;
 
-        xseries[9] = 9;
-        yseries[9] = 8.5;
-
-        // Equal standard deviations are assigned to all 10 points
-        for (i = 0; i < 10; i++) {
-            sig[i] = stdv;
-        }
+        xSeries[9] = 9;
+        ySeries[9] = 8.5;
 
     }
 
 	@Override
 	protected void calculateFittedY() {
-		// TODO Auto-generated method stub
+		int j;
+		yDataFitted = new double[nPts];
+		for (j = 0; j < nPts; j++) {
+			yDataFitted[j] = a[1] * xSeries[j] + a[0];
+		}
 		
 	}
 
