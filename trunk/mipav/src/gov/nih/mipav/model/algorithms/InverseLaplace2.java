149920e6a55cb4ef0d80c02218e5e3756f1a5844
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2010-11-19 17:24:52 +0000

    maxBound increased from 5000 to 20000.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@7504 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/InverseLaplace2.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/InverseLaplace2.java
@@ -107,7 +107,7 @@ public abstract class InverseLaplace2 {
     private int it;
 
     /** maxBound is a bound on the number of terms used in the Durbin formula. */
-    private final int maxBound = 5000;
+    private final int maxBound = 20000;
 
     /** DOCUMENT ME! */
     private int nex;
