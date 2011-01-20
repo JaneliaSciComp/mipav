b11a7d74782da6cf72d78b0d6cc6af2a15a5a649
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-01-20 20:51:09 +0000

    simplified method for accessing VOI statistics properties from hashtable (user of method no longer needs to know structure of key, and is no longer dependent on it to retrieve statistics).
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@7741 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/structures/VOIStatisticalProperties.java
+++ trunk/mipav/src/gov/nih/mipav/model/structures/VOIStatisticalProperties.java
@@ -12,8 +12,12 @@ public class VOIStatisticalProperties implements VOIStatisticList {
 
     //~ Instance fields ------------------------------------------------------------------------------------------------
 
-    /** DOCUMENT ME! */
-    private Hashtable data;
+    public static final char DELIM = ';';
+    
+    public static final String TOTAL = "Total";
+    
+    /** The hashtable of statistics. */
+    private Hashtable<String, Object> data;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
@@ -21,29 +25,132 @@ public class VOIStatisticalProperties implements VOIStatisticList {
      * Creates a new VOIStatisticalProperties object.
      */
     public VOIStatisticalProperties() {
-        data = new Hashtable();
+        data = new Hashtable<String, Object>();
     }
 
     //~ Methods --------------------------------------------------------------------------------------------------------
 
     /**
-     * DOCUMENT ME!
-     *
-     * @param   key  String
-     *
-     * @return  String
+     * Gets the total statistics for all elements of the VOIs that are included in this properties object. 
+     * For example, if statistics for every slice have been calculated, this method should be used to 
+     * store the total of this statistic on all slices.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @return The calculated statistic
      */
-    public String getProperty(String key) {
-        return (String) data.get(key);
+    public Object getTotalStatistic(String statistic) {
+        return data.get(statistic+TOTAL);
+    }
+    
+    /**
+     * Stores the total statistics for all elements of the VOIs that are included in this properties object. 
+     * For example, if statistics for every slice have been calculated, this method should be used to 
+     * store the total of this statistic on all slices.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param statisticValue The value of the calculated statistic
+     */
+    public void setTotalStatistic(String statistic, Object statisticValue) {
+        data.put(statistic+TOTAL, statisticValue);
+    }
+    
+    /**
+     * Gets the statistic for all the contours of a VOI that exist on a particular 
+     * slice.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param slice The slice that the contours exist on 
+     * @return The value of the calculated statistic
+     */
+    public Object getSliceStatistic(String statistic, int slice) {
+        return data.get(statistic+slice+DELIM);
+    }
+    
+    /**
+     * Stores the statistic for all the contours of a VOI that exist on a particular 
+     * slice.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param slice The slice that the contours exist on 
+     * @param statisticValue The value of the calculated statistic
+     */
+    public void setSliceStatistic(String statistic, int slice, Object statisticValue) {
+        data.put(statistic+slice+DELIM, statisticValue);
     }
 
     /**
-     * DOCUMENT ME!
-     *
-     * @param  key  String
-     * @param  obj  Object
+     * Gets the statistic for a particular contour of a VOI without reference to the slice
+     * a contour may exist on.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param contourLabel The contour that this statistic relates to 
+     * @return The value of the calculated statistic
      */
-    public void setProperty(String key, Object obj) {
-        data.put(key, obj);
+    public Object getContourStatistic(String statistic, String contourLabel) {
+        return data.get(statistic+contourLabel);
+    }
+    
+    /**
+     * Stores the statistic for a particular contour of a VOI without reference to the slice
+     * a contour may exist on.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param contourLabel The contour that this statistic relates to 
+     * @param statisticValue The value of the calculated statistic
+     */
+    public void setContourStatistic(String statistic, String contourLabel, Object statisticValue) {
+        data.put(statistic+contourLabel, statisticValue);
+    }
+    
+    /**
+     * Gets the statistic for a particular contour on a particular slice of a VOI.  
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param slice The image slice that this statistic relate to
+     * @param contourLabel The contour that this statistic relates to
+     * @return The value of the calculated statistic
+     */
+    public Object getSliceContourStatistic(String statistic, int slice, String contourLabel) {
+        return data.get(statistic+slice+DELIM+contourLabel);
+    }
+    
+    /**
+     * Stores the statistic for a particular contour on a particular slice of a VOI.  
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param slice The image slice that this statistic relate to
+     * @param contourLabel The contour that this statistic relates to
+     * @param statisticValue The value of the calculated statistic
+     */
+    public void setSliceContourStatistic(String statistic, int slice, String contourLabel, Object statisticValue) {
+        data.put(statistic+slice+DELIM+contourLabel, statisticValue);
+    }
+    
+    /**
+     * Gets the statistic for the whole VOI.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @return The value of the calculated statistic
+     */
+    public Object getVOIStatistic(String statistic) {
+        return data.get(statistic);
+    }
+    
+    /**
+     * Stores the statistic for the whole VOI.
+     * 
+     * @param statistic The statistic that has been calculated
+     * @param statisticValue The value of the calculated statistic
+     */
+    public void setVOIStatistic(String statistic, Object statisticValue) {
+        data.put(statistic, statisticValue);
+    }
+
+    public void setProperty(String string, Object value) {
+        data.put(string, value);
+    }
+    
+    public String getProperty(Object key) {
+        return data.get(key).toString();
     }
 }
