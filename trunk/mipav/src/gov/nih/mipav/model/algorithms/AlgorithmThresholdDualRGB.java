27c29919e4a9f1f7ce21816d3a99fa3a9d73e17d
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-17 18:13:53 +0000

    Removed warning.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@7986 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmThresholdDualRGB.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmThresholdDualRGB.java
@@ -1,7 +1,5 @@
 package gov.nih.mipav.model.algorithms;
 
-
-import gov.nih.mipav.model.file.FileInfoBase;
 import gov.nih.mipav.model.file.FileInfoBase.Unit;
 import gov.nih.mipav.model.structures.*;
 import gov.nih.mipav.view.ViewUserInterface;
