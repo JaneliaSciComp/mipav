abcbe554756324df3e3b113434d6944ead92690e
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-18 20:45:45 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8085 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogExtractBrain.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogExtractBrain.java
@@ -275,8 +275,8 @@ public class JDialogExtractBrain extends JDialogScriptableBase implements Algori
             // array with the smaller number of voxels represents the scalp
             // voxels in the upper-half of the head.  The comparison of counts
             // is based on empirical studies.
-            Vector kLess = new Vector();
-            Vector kGreater = new Vector();
+            Vector<Vector3f> kLess = new Vector<Vector3f>();
+            Vector<Vector3f> kGreater = new Vector<Vector3f>();
             int iHalfYBound = yDim / 2;
             int iHalfZBound = zDim / 2;
 
@@ -510,7 +510,7 @@ public class JDialogExtractBrain extends JDialogScriptableBase implements Algori
             // These next lines set the titles in all frames where the source image is displayed to
             // image name so as to indicate that the image is now unlocked!
             // The image frames are enabled and then registed to the userinterface.
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
             for (int i = 0; i < imageFrames.size(); i++) {
                 ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -846,7 +846,7 @@ public class JDialogExtractBrain extends JDialogScriptableBase implements Algori
             // "locked - " image name so as to indicate that the image is now read/write locked!
             // The image frames are disabled and then unregisted from the userinterface until the
             // algorithm has completed.
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
             titles = new String[imageFrames.size()];
 
