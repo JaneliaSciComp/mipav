622a6f6c90b2c8cf340d893466f21b4816f75342
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-18 23:02:19 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8096 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogFlip.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogFlip.java
@@ -149,7 +149,7 @@ public class JDialogFlip extends JDialogScriptableBase implements AlgorithmInter
             // These next lines set the titles in all frames where the source image is displayed to
             // image name so as to indicate that the image is now unlocked!
             // The image frames are enabled and then registered to the userinterface.
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
             for (int i = 0; i < imageFrames.size(); i++) {
                 ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -202,7 +202,7 @@ public class JDialogFlip extends JDialogScriptableBase implements AlgorithmInter
             // The image frames are disabled and then unregisted from the userinterface until the
             // algorithm has completed.
             if (image.getImageFrameVector() != null){
-	            Vector imageFrames = image.getImageFrameVector();
+	            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 	            titles = new String[imageFrames.size()];
 	
 	            for (int i = 0; i < imageFrames.size(); i++) {
