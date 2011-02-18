989300bce962eb4046f848e10b10c5698b671a4b
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-18 17:03:47 +0000

    Warnings removed.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8032 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogAutoCorrelation.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogAutoCorrelation.java
@@ -185,7 +185,7 @@ public class JDialogAutoCorrelation extends JDialogScriptableBase implements Alg
                     // These next lines set the titles in all frames where the source image is displayed to
                     // image name so as to indicate that the image is now unlocked!
                     // The image frames are enabled and then registered to the userinterface.
-                    Vector imageFrames = image.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
                     for (int i = 0; i < imageFrames.size(); i++) {
                         ((ViewJFrameBase) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -243,7 +243,7 @@ public class JDialogAutoCorrelation extends JDialogScriptableBase implements Alg
                     // These next lines set the titles in all frames where the source image is displayed to
                     // image name so as to indicate that the image is now unlocked!
                     // The image frames are enabled and then registered to the userinterface.
-                    Vector imageFrames = image.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
                     for (int i = 0; i < imageFrames.size(); i++) {
                         ((ViewJFrameBase) (imageFrames.elementAt(i))).setTitle(titles[i]);
