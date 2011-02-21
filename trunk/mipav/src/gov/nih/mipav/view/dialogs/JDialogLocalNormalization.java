ff2879a3344f1368f8e19551786f9c009f11800b
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-21 20:35:03 +0000

    Removed warning.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8144 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogLocalNormalization.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogLocalNormalization.java
@@ -236,7 +236,7 @@ public class JDialogLocalNormalization extends JDialogScriptableBase
                 // These next lines set the titles in all frames where the source image is displayed to
                 // image name so as to indicate that the image is now unlocked!
                 // The image frames are enabled and then registered to the userinterface.
-                Vector imageFrames = sourceImage.getImageFrameVector();
+                Vector<ViewImageUpdateInterface> imageFrames = sourceImage.getImageFrameVector();
                 titles = new String[imageFrames.size()];
 
                 for (int i = 0; i < imageFrames.size(); i++) {
