504a17787037f00474430d57e13d5788b712f62b
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-21 21:28:49 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8161 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogNLNoiseReduction.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogNLNoiseReduction.java
@@ -3,7 +3,6 @@ package gov.nih.mipav.view.dialogs;
 
 import gov.nih.mipav.model.algorithms.*;
 import gov.nih.mipav.model.algorithms.filters.*;
-import gov.nih.mipav.model.file.*;
 import gov.nih.mipav.model.file.FileInfoBase.Unit;
 import gov.nih.mipav.model.scripting.*;
 import gov.nih.mipav.model.scripting.parameters.*;
@@ -192,7 +191,7 @@ public class JDialogNLNoiseReduction extends JDialogScriptableBase implements Al
                 // These next lines set the titles in all frames where the source image is displayed to
                 // image name so as to indicate that the image is now unlocked!
                 // The image frames are enabled and then registed to the userinterface.
-                Vector imageFrames = image.getImageFrameVector();
+                Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
                 for (int i = 0; i < imageFrames.size(); i++) {
                     ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -385,7 +384,7 @@ public class JDialogNLNoiseReduction extends JDialogScriptableBase implements Al
                     // "locked - " image name so as to indicate that the image is now read/write locked!
                     // The image frames are disabled and then unregisted from the userinterface until the
                     // algorithm has completed.
-                    Vector imageFrames = image.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
                     titles = new String[imageFrames.size()];
 
                     for (int i = 0; i < imageFrames.size(); i++) {
@@ -484,7 +483,7 @@ public class JDialogNLNoiseReduction extends JDialogScriptableBase implements Al
                     // "locked - " image name so as to indicate that the image is now read/write locked!
                     // The image frames are disabled and then unregisted from the userinterface until the
                     // algorithm has completed.
-                    Vector imageFrames = image.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
                     titles = new String[imageFrames.size()];
 
                     for (int i = 0; i < imageFrames.size(); i++) {
