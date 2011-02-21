a2c7e55ce5aa27052bc075a1fb7564325ad5c9b7
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-21 19:29:02 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8132 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogItkFilter.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogItkFilter.java
@@ -167,7 +167,7 @@ public class JDialogItkFilter extends JDialogScriptableBase
             // These next lines set the titles in all frames where the source image is displayed to
             // image name so as to indicate that the image is now unlocked!
             // The image frames are enabled and then registered to the userinterface.
-            Vector imageFrames = m_srcImage.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = m_srcImage.getImageFrameVector();
 
             for (int i = 0; i < imageFrames.size(); i++) {
                 ((Frame) (imageFrames.elementAt(i))).setTitle(m_titles[i]);
@@ -391,7 +391,7 @@ public class JDialogItkFilter extends JDialogScriptableBase
                     // "locked - " image name so as to indicate that the image is now read/write locked!
                     // The image frames are disabled and then unregisted from the userinterface until the
                     // algorithm has completed.
-                    Vector imageFrames = m_srcImage.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = m_srcImage.getImageFrameVector();
 
                     m_titles = new String[imageFrames.size()];
 
@@ -524,7 +524,7 @@ public class JDialogItkFilter extends JDialogScriptableBase
                     // "locked - " image name so as to indicate that the image is now read/write locked!
                     // The image frames are disabled and then unregisted from the userinterface until the
                     // algorithm has completed.
-                    Vector imageFrames = m_srcImage.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = m_srcImage.getImageFrameVector();
 
                     m_titles = new String[imageFrames.size()];
 
