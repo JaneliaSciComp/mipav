df7d4864db6dd7589dce44a7cbbc3feb6da36610
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-21 22:13:41 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8175 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogRandomizeSliceOrder.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogRandomizeSliceOrder.java
@@ -94,7 +94,7 @@ public class JDialogRandomizeSliceOrder extends JDialogScriptableBase implements
             // These next lines set the titles in all frames where the source image is displayed to
             // image name so as to indicate that the image is now unlocked!
             // The image frames are enabled and then registered to the userinterface.
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
             for (i = 0; i < imageFrames.size(); i++) {
                 ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -176,7 +176,7 @@ public class JDialogRandomizeSliceOrder extends JDialogScriptableBase implements
             // "locked - " image name so as to indicate that the image is now read/write locked!
             // The image frames are disabled and then unregisted from the userinterface until the
             // algorithm has completed.
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
             titles = new String[imageFrames.size()];
 
             for (int i = 0; i < imageFrames.size(); i++) {
