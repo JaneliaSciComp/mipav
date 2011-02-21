8354b6fa0d99b047e09eed9e639ad1cb54a93556
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-21 22:26:48 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8180 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogRegistrationOAR25D.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogRegistrationOAR25D.java
@@ -4,6 +4,7 @@ package gov.nih.mipav.view.dialogs;
 import gov.nih.mipav.model.algorithms.*;
 import gov.nih.mipav.model.algorithms.registration.*;
 import gov.nih.mipav.model.file.*;
+import gov.nih.mipav.model.file.FileInfoBase.Unit;
 import gov.nih.mipav.model.scripting.*;
 import gov.nih.mipav.model.scripting.parameters.*;
 import gov.nih.mipav.model.structures.*;
@@ -384,7 +385,7 @@ public class JDialogRegistrationOAR25D extends JDialogScriptableBase
 
                     ViewJFrameGraph transGraph = new ViewJFrameGraph(posT, trans, "Translations", "Slice number",
                                                                      "Translations in " +
-                                                                     FileInfoBase.getUnitsOfMeasureAbbrevStr(matchImage.getFileInfo(0).getUnitsOfMeasure(0)));
+                                                                     (Unit.getUnitFromLegacyNum(matchImage.getFileInfo(0).getUnitsOfMeasure(0))).getAbbrev());
                     transGraph.makeRangeSymmetric();
                     transGraph.showXYZLegends();
                     transGraph.setDefaultDirectory(UI.getDefaultDirectory());
