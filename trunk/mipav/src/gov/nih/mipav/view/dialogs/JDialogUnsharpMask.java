8587dc866c9186c673d5b53b02a15d7bd72deb15
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-22 20:32:26 +0000

    Warnings removed.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8247 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogUnsharpMask.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogUnsharpMask.java
@@ -151,7 +151,7 @@ public class JDialogUnsharpMask extends JDialogScriptableBase implements Algorit
                 // These next lines set the titles in all frames where the source image is displayed to
                 // image name so as to indicate that the image is now unlocked!
                 // The image frames are enabled and then registed to the userinterface.
-                Vector imageFrames = image.getImageFrameVector();
+                Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
 
                 for (int i = 0; i < imageFrames.size(); i++) {
                     ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -366,7 +366,7 @@ public class JDialogUnsharpMask extends JDialogScriptableBase implements Algorit
                     // "locked - " image name so as to indicate that the image is now read/write locked!
                     // The image frames are disabled and then unregisted from the userinterface until the
                     // algorithm has completed.
-                    Vector imageFrames = image.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
                     titles = new String[imageFrames.size()];
 
                     for (int i = 0; i < imageFrames.size(); i++) {
@@ -469,7 +469,7 @@ public class JDialogUnsharpMask extends JDialogScriptableBase implements Algorit
                     // "locked - " image name so as to indicate that the image is now read/write locked!
                     // The image frames are disabled and then unregisted from the userinterface until the
                     // algorithm has completed.
-                    Vector imageFrames = image.getImageFrameVector();
+                    Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
                     titles = new String[imageFrames.size()];
 
                     for (int i = 0; i < imageFrames.size(); i++) {
