4438d4ab6e362117d0d8ad86dab30a9edec10f46
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-02-28 17:31:54 +0000

    Removed warnings.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8479 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInDialogProcessICG.java
+++ trunk/mipav/src/plugins/PlugInDialogProcessICG.java
@@ -156,7 +156,7 @@ public class PlugInDialogProcessICG extends JDialogScriptableBase implements Alg
             // These next lines set the titles in all frames where the source image is displayed to
             // image name so as to indicate that the image is now unlocked!
             // The image frames are enabled and then registed to the userinterface.
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
                       
             for (int i = 0; i < imageFrames.size(); i++) {
                 ((Frame) (imageFrames.elementAt(i))).setTitle(titles[i]);
@@ -245,7 +245,7 @@ public class PlugInDialogProcessICG extends JDialogScriptableBase implements Alg
 
             // unregister the frame holding the source image
 
-            Vector imageFrames = image.getImageFrameVector();
+            Vector<ViewImageUpdateInterface> imageFrames = image.getImageFrameVector();
             titles = new String[imageFrames.size()];
 
             for (int i = 0; i < imageFrames.size(); i++) {
