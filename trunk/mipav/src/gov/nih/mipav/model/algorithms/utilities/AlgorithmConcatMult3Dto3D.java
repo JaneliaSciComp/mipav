a5cb039b79e8a3f02757532fa10cc92a1faaffb9
pandyan@NIH.GOV <pandyan@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-05-25 17:26:57 +0000

    created a concat mult 3d to 3d utility...also changed up the menu items for concating accordingly
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8831 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/utilities/AlgorithmConcatMult3Dto3D.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/utilities/AlgorithmConcatMult3Dto3D.java
@@ -6,14 +6,8 @@ import gov.nih.mipav.model.file.FileInfoDicom;
 import gov.nih.mipav.model.file.FileInfoImageXML;
 import gov.nih.mipav.model.structures.ModelImage;
 
+public class AlgorithmConcatMult3Dto3D extends AlgorithmBase {
 
-
-/**
- * @author pandyan
- * This algorithm concats multiple 3D images of same type to a 4D image
- *
- */
-public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
 	
 	/** array of 3D images **/
 	private ModelImage[] images;
@@ -28,7 +22,7 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
 	 * @param images
 	 * @param destImage
 	 */
-	public AlgorithmConcatMult3Dto4D(ModelImage[] images, ModelImage destImage) {
+	public AlgorithmConcatMult3Dto3D(ModelImage[] images, ModelImage destImage) {
 		this.images = images;
 		this.destImage = destImage;
 		
@@ -67,18 +61,18 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
 	        
 	        destImage.calcMinMax();
 
-	        float[] resols = new float[4];
-	        float[] origins = new float[4];
+	        float[] resols = new float[3];
+	        float[] origins = new float[3];
 	        
 
 	        resols[0] = images[0].getFileInfo()[0].getResolutions()[0];
 	        resols[1] = images[0].getFileInfo()[0].getResolutions()[1];
 	        resols[2] = images[0].getFileInfo()[0].getResolutions()[2];
-	        resols[3] = 1;
+
 	        origins[0] = images[0].getFileInfo()[0].getOrigin(0);
 	        origins[1] = images[0].getFileInfo()[0].getOrigin(1);
 	        origins[2] = images[0].getFileInfo()[0].getOrigin(2);
-	        origins[3] = 0;
+
 	        
 	        
 	        boolean isFileInfoDicom = true;
@@ -93,7 +87,7 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
 	        
 
 	        if (isFileInfoDicom) {
-	            fileInfoDicom = new FileInfoDicom[destImage.getExtents()[2] * destImage.getExtents()[3]];
+	            fileInfoDicom = new FileInfoDicom[destImage.getExtents()[2]];
 	            
 	            counter = 0;
 		        for(int i=0;i<images.length;i++) {
@@ -113,7 +107,7 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
 	        } else {
 	            fileInfo = destImage.getFileInfo();
 
-	            for (int i = 0; (i < (destImage.getExtents()[2] * destImage.getExtents()[3])); i++) {
+	            for (int i = 0; (i < destImage.getExtents()[2]); i++) {
 	                fileInfo[i].setModality(images[0].getFileInfo()[0].getModality());
 	                fileInfo[i].setFileDirectory(images[0].getFileInfo()[0].getFileDirectory());
 	                fileInfo[i].setEndianess(images[0].getFileInfo()[0].getEndianess());
