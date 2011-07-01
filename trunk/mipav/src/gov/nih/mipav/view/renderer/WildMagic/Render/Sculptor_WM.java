aea9bedd152901bfcfff9bd83fc954b64ce5b379
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-07-01 21:53:55 +0000

    3 changed to Preferences.DEBUG_COMMS in Preferences.debug.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@8988 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/Render/Sculptor_WM.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/Render/Sculptor_WM.java
@@ -805,7 +805,7 @@ public class Sculptor_WM implements MouseMotionListener, MouseListener {
                     
                 } catch (OutOfMemoryError error) {
                     MipavUtil.displayError("Out of memory: ViewJFrameBase.save");
-                    Preferences.debug("Out of memory: ViewJFrameBase.save\n", 3);
+                    Preferences.debug("Out of memory: ViewJFrameBase.save\n", Preferences.DEBUG_COMMS);
 
                     return false;
                 }
