e4d06bc4a5de92cdc6ca6bf90218661dc4f9749a
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-09-12 15:58:31 +0000

    fixed 0028,0008 tag value for writing 4D images
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9439 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileDicom.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileDicom.java
@@ -737,6 +737,10 @@ public class FileDicom extends FileDicomBase {
         String name = key.toString(); // string representing the tag
         int tagVM;
 
+        if(name.equals("0028,0008")) {
+        	System.out.println("Stop");
+        }
+        
         Preferences.debug("name = " + name + " length = " +
          elementLength + "\n", Preferences.DEBUG_FILEIO);
         if ( (fileInfo.getVr_type() == VRtype.IMPLICIT) || (groupWord == 2)) {
@@ -2366,7 +2370,7 @@ public class FileDicom extends FileDicomBase {
 
         fileInfo = (FileInfoDicom) image.getFileInfo(0);
 
-        final String nFramesStr = String.valueOf(endSlice - startSlice + 1);
+        final String nFramesStr = String.valueOf((endSlice - startSlice + 1)*(endTime - startTime + 1));
         fileInfo.getTagTable().setValue("0028,0008", nFramesStr, nFramesStr.length());
 
         if (stampSecondary) {
@@ -3482,6 +3486,10 @@ public class FileDicom extends FileDicomBase {
     	VR vr = VR.UN;
 
     	Preferences.debug("Writing tag "+tag.getKey().toString()+"\n", Preferences.DEBUG_FILEIO);
+    	
+    	if(tag.getKey().toString().equals("0028,0008")) {
+    	    System.out.println("Stop");
+    	}
     	
         // System.out.println("w = " + dicomTags[i].toString());
         try {
