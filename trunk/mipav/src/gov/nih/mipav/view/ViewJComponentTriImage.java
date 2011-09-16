154d7a0f11d4448bbcf2e72e3dd5efe71857ee4d
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-09-16 18:09:37 +0000

    In getTheta changed if (protractor2.Y > protractor1Y) to if (protractor2.Y < protractor1.Y).
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9468 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewJComponentTriImage.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewJComponentTriImage.java
@@ -542,7 +542,7 @@ public class ViewJComponentTriImage extends ViewJComponentEditImage implements M
             theta = voiProtractor.getTheta(res);
             Vector2f protractor1 = getScreenCoordinates(voiProtractor.get(1));
             Vector2f protractor2 = getScreenCoordinates(voiProtractor.get(2));
-            if (protractor2.Y > protractor1.Y) {
+            if (protractor2.Y < protractor1.Y) {
             	theta = -theta;
             }
             return theta;
