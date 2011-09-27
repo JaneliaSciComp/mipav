7d972aa9ef9c6ba1edb21dd8b305ca8b09b42ebb
tyrieek@NIH.GOV <tyrieek@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-09-27 19:30:05 +0000

    Creates object to store parameters in the ModelImage acquired from DWI image file or ImageInfo DTI tab
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9524 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/file/DTIParameters.java
@@ -0,0 +1,54 @@
+package gov.nih.mipav.model.file;
+
+
+/**
+ * Object to store parameters in the ModelImage acquired from DWI image file or ImageInfo DTI tab 
+ *
+ * @version  0.1 September 27, 2011
+ * @author   Beth Tyrie
+ * @see      
+ */
+
+
+public class DTIParameters {
+
+    //~ Instance fields ------------------------------------------------------------------------------------------------
+    
+    private float[] bValues;
+    
+    private float[][] gradients;
+    
+    private int numVolumes;
+      
+    //~ Constructors ---------------------------------------------------------------------------------------------------   
+    
+    public DTIParameters(int numVolumes){
+        this.numVolumes = numVolumes;                
+    }
+    
+    //Getters and Setters
+    public float[] getbValues() {
+        return bValues;
+    }
+
+    public void setbValues(float[] bValues) {
+        this.bValues = bValues;
+    }
+
+    public float[][] getGradients() {
+        return gradients;
+    }
+
+    public void setGradients(float[][] gradients) {
+        this.gradients = gradients;
+    }
+
+    public int getNumVolumes() {
+        return numVolumes;
+    }
+
+    public void setNumVolumes(int numVolumes) {
+        this.numVolumes = numVolumes;
+    }
+        
+}
