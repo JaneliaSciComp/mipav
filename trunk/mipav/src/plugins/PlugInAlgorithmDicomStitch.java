2ce7fb3f17be612773060c44551234f8307ab9f5
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-09-29 17:11:50 +0000

    trying Vector3f
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9543 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmDicomStitch.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmDicomStitch.java
@@ -23,6 +23,7 @@ This software may NOT be used for diagnostic purposes.
 ******************************************************************/
 
 import WildMagic.LibFoundation.Mathematics.Matrix2f;
+import WildMagic.LibFoundation.Mathematics.Matrix3f;
 import WildMagic.LibFoundation.Mathematics.Matrix4f;
 import WildMagic.LibFoundation.Mathematics.Vector2f;
 import WildMagic.LibFoundation.Mathematics.Vector3f;
@@ -94,19 +95,19 @@ public class PlugInAlgorithmDicomStitch extends AlgorithmBase {
     	    FileInfoDicom infoDicomOrig = (FileInfoDicom) srcImage.getFileInfo()[z];
     	    
     	    //get necessary dicom elements for matrix multiplication
-    	    Matrix4f toPos = createSpacingMatrix(infoDicomStitch);
+    	    Matrix3f toPos = createSpacingMatrix(infoDicomStitch);
     	    
-    	    Matrix4f toImage = createSpacingMatrix(infoDicomOrig);
+    	    Matrix3f toImage = createSpacingMatrix(infoDicomOrig);
     	    toImage.Inverse();
     	    
     	    toImage.Mult(toPos);
     	    
     	    for(int i=0; i<xDim; i++) {
     	        for(int j=0; j<yDim; j++) {
-    	            Vector4f posMove = new Vector4f();
-    	            posMove.Set(i, j, 0, 1);
+    	            Vector3f posMove = new Vector3f();
+    	            posMove.Set(i, j, 1);
     	            
-    	            Vector4f posResult = new Vector4f();
+    	            Vector3f posResult = new Vector3f();
     	            toImage.MultRight(posMove, posResult);
     	            System.out.println(posMove+" changed to "+posResult.toString());
     	        }
@@ -122,35 +123,40 @@ public class PlugInAlgorithmDicomStitch extends AlgorithmBase {
         }
     }
 
-    private Matrix4f createSpacingMatrix(FileInfoDicom infoDicom) {
-        Double[] xyCos = (Double[]) infoDicom.getTagTable().get("0020,0037").getValue(false);
-        Double[] spacing = (Double[]) infoDicom.getTagTable().get("0028,0030").getValue(false);
-        Double[] position = (Double[]) infoDicom.getTagTable().get("0020,0032").getValue(false);
+    private Matrix3f createSpacingMatrix(FileInfoDicom infoDicom) {
+        Double[] xyCos = fromObjectToDouble(infoDicom.getTagTable().get("0020,0037").getValueList());
+        Double[] spacing = fromObjectToDouble(infoDicom.getTagTable().get("0028,0030").getValueList());
+        Double[] position = fromObjectToDouble(infoDicom.getTagTable().get("0020,0032").getValueList());
         
-        Matrix4f mat = new Matrix4f();
+        Matrix3f mat = new Matrix3f();
         mat.Set(0, 0, (float)(xyCos[0].doubleValue()*spacing[0].doubleValue()));
-        mat.Set(0, 1, (float)(xyCos[1].doubleValue()*spacing[0].doubleValue()));
-        mat.Set(0, 2, (float)(xyCos[2].doubleValue()*spacing[0].doubleValue()));
-        mat.Set(0, 3, 0.0f);
+        mat.Set(1, 0, (float)(xyCos[1].doubleValue()*spacing[0].doubleValue()));
+        mat.Set(2, 0, (float)(xyCos[2].doubleValue()*spacing[0].doubleValue()));
         
-        mat.Set(1, 0, (float)(xyCos[3].doubleValue()*spacing[1].doubleValue()));
+        mat.Set(0, 1, (float)(xyCos[3].doubleValue()*spacing[1].doubleValue()));
         mat.Set(1, 1, (float)(xyCos[4].doubleValue()*spacing[1].doubleValue()));
-        mat.Set(1, 2, (float)(xyCos[5].doubleValue()*spacing[1].doubleValue()));
-        mat.Set(1, 3, 0.0f);
-        
-        mat.Set(2, 0, 0.0f);
-        mat.Set(2, 1, 0.0f);
-        mat.Set(2, 2, 0.0f);
-        mat.Set(2, 3, 0.0f);
+        mat.Set(2, 1, (float)(xyCos[5].doubleValue()*spacing[1].doubleValue()));
        
-        mat.Set(3, 0, position[0].floatValue());
-        mat.Set(3, 1, position[1].floatValue());
-        mat.Set(3, 2, position[2].floatValue());
-        mat.Set(3, 3, 1.0f);
+        mat.Set(0, 3, position[0].floatValue());
+        mat.Set(1, 3, position[1].floatValue());
+        mat.Set(2, 3, position[2].floatValue());
         
         System.out.println("Created dicom position matrix:\n"+mat.toString());
         
         return mat;
+    }
+    
+    private Double[] fromObjectToDouble(Object[] ar) {
+        Double[] arDouble = new Double[ar.length];
+        try {
+            for(int i=0; i<ar.length; i++) {
+                arDouble[i] = Double.valueOf(ar[i].toString());
+            }
+        } catch(NumberFormatException nfe) {
+            return null;
+        }
+        
+        return arDouble;
     }
         
 	
