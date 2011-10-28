8727913c57aae7691fcabf49f727e64a2b8f81d5
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-10-28 17:23:37 +0000

    Added createOneElementBinaryMask3D routine to handle each curve of a VOI.CONTOUR separately.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9623 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/structures/VOI.java
+++ trunk/mipav/src/gov/nih/mipav/model/structures/VOI.java
@@ -1175,6 +1175,99 @@ public class VOI extends ModelSerialCloneable {
 			} // for (i = 0; i < curves.size(); i++)
 		}
 	}
+	
+	/**
+	 * Creates a 3D binary mask. 
+	 * @param mask mask written into
+	 * @param xDim x-dimensions of the image.
+	 * @param yDim y-dimensions of the images.
+	 * @param XOR indicates that nested VOI contours will be exclusive ORed with other contours of the VOI 
+	 * @param onlyActive Only mask regions that are active (i.e. selected )
+	 */
+	public void createOneElementBinaryMask3D(BitSet mask, int xDim, int yDim, boolean XOR, boolean onlyActive, int elementNum) {
+
+		if ((process == true) && (curveType == CONTOUR)){
+
+			if (!onlyActive || ((VOIContour) (curves.elementAt(elementNum))).isActive()) {
+
+				curves.elementAt(elementNum).setMask( mask, xDim, yDim, XOR, polarity );     
+			}
+		} else if ((process == true) && (curveType == POINT)) {
+			Vector3f pt;
+			int size = curves.size();
+
+			for (int i = 0; i < size; i++) {
+				pt = ((VOIPoint) (curves.elementAt(i))).exportPoint();
+
+				if (!onlyActive || ((VOIPoint) (curves.elementAt(i))).isActive()) {
+					int offset = MipavMath.round(pt.Z * xDim * yDim + pt.Y * xDim + pt.X);
+					if (polarity == ADDITIVE) {
+
+						if (XOR && mask.get(offset)) {
+							mask.clear(offset);
+						} else {
+							mask.set(offset);
+						}
+					} else if (polarity == SUBTRACTIVE) {
+						mask.clear(offset);
+					} else {
+						// mask[0].clear(offset + x);
+					}
+				}
+			}
+		} else if ((process == true) && (curveType == LINE)) {
+			double x0, x1, y0, y1;
+			double distance;
+			int i, j;
+			int indexX, indexY;
+			double myY, myX, yInc, xInc;
+			int len;
+			VOIBase vBase;
+			Vector3f vf0;
+			Vector3f vf1;
+			for (i = 0; i < curves.size(); i++) {
+				vBase = curves.elementAt(i);
+				vf0 = vBase.get(0);
+				vf1 = vBase.get(1);
+				x0 = vf0.X;
+				y0 = vf0.Y;
+				x1 = vf1.X;
+				y1 = vf1.Y;
+				distance = Math.sqrt(((x1 - x0) * (x1 - x0)) + ((y1 - y0) * (y1 - y0)));
+				myY = y0;
+				myX = x0;
+				xInc = (x1 - x0) / (2 * distance);
+				yInc = (y1 - y0) / (2 * distance);
+
+				len = (int) Math.round(2 * distance);
+				indexX = -1;
+				indexY = -1;
+				for (j = 0; j < len; j++) {
+
+					if ((indexX != Math.round(myX)) || (indexY != Math.round(myY))) {
+						indexY = (int) Math.round(myY);
+						indexX = (int) Math.round(myX);
+						int offset = Math.round(vf0.Z) * xDim * yDim + (indexY * xDim) + indexX;
+						if (polarity == ADDITIVE) {
+
+							if (XOR && mask.get(offset)) {
+								mask.clear(offset);
+							} else {
+								mask.set(offset);
+							}
+						} else if (polarity == SUBTRACTIVE) {
+							mask.clear(offset);
+						} else {
+							// mask[0].clear(offset + x);
+						}
+					}
+
+					myX = myX + xInc;
+					myY = myY + yInc;
+				} // for (j = 0; j < len; j++)
+			} // for (i = 0; i < curves.size(); i++)
+		}
+	}
 
 	/**
 	 * Creates a short image of the VOI. Positions within the VOI are set to the VOI's watershed ID.
