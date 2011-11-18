fd89fa46522fe0e9ad7a9f23ac18835cb291fcdf
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-11-18 20:18:09 +0000

    In calcStoreInPlace change srcImage to another data type when necessary.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9666 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmFuzzyMinimization.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmFuzzyMinimization.java
@@ -2,6 +2,7 @@ package gov.nih.mipav.model.algorithms.filters;
 
 
 import gov.nih.mipav.model.algorithms.*;
+import gov.nih.mipav.model.algorithms.utilities.AlgorithmChangeType;
 import gov.nih.mipav.model.structures.*;
 
 import java.io.*;
@@ -188,6 +189,7 @@ public class AlgorithmFuzzyMinimization extends AlgorithmBase {
         int iters;
         double alpha;
         double maxVal;
+        int newType;
 
         try {
 
@@ -257,7 +259,60 @@ public class AlgorithmFuzzyMinimization extends AlgorithmBase {
         }
         srcList.clear();
         
+        newType = srcImage.getType();
+        if (newType == ModelStorageBase.DOUBLE) {
+        	
+        }
+        else if (newType == ModelStorageBase.FLOAT) {
+        	if ((gmin < -Float.MAX_VALUE) || (gmax > Float.MAX_VALUE)) {
+        		newType = ModelStorageBase.DOUBLE;
+        	}
+        }
+        else if ((gmin < Long.MIN_VALUE) || (gmax > Long.MAX_VALUE)) {
+        	newType = ModelStorageBase.DOUBLE;
+        }
+        else if (newType == ModelStorageBase.LONG) {
+        	
+        }
+        else if (newType == ModelStorageBase.UINTEGER) {
+        	if ((gmin < 0) || (gmax > 4294967295L)) {
+        	    newType = ModelStorageBase.LONG;
+        	}
+        }
+        else if ((gmin < Integer.MIN_VALUE) || (gmax > Integer.MAX_VALUE)) {
+        	newType = ModelStorageBase.LONG;
+        }
+        else if (newType == ModelStorageBase.INTEGER) {
+        	
+        }
+        else if (newType == ModelStorageBase.USHORT) {
+        	if ((gmin < 0) || (gmax > 65535)) {
+        		newType = ModelStorageBase.INTEGER;
+        	}
+        }
+        else if ((gmin < -32768) || (gmax > 32767)) {
+        	newType = ModelStorageBase.INTEGER;
+        }
+        else if (newType == ModelStorageBase.SHORT) {
+        	
+        }
+        else if (newType == ModelStorageBase.UBYTE) {
+        	if ((gmin < 0) || (gmax > 255)) {
+        		newType = ModelStorageBase.SHORT;
+        	}
+        }
+        else if (newType == ModelStorageBase.BYTE) {
+        	if ((gmin < -128) || (gmax > 127)) {
+        		newType = ModelStorageBase.SHORT;
+        	}
+        }
         
+        if (srcImage.getType() != newType) {
+        	AlgorithmChangeType convertType = new AlgorithmChangeType(srcImage, newType, srcImage.getMin(),
+                    srcImage.getMax(), gmin, gmax, false);
+            convertType.run();
+            convertType = null;
+        }
         
         if (threadStopped) {
             finalize();
