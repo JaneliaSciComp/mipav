a99ea4117a4de6f7d85f07b7fe971077085ead53
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2011-12-22 16:52:01 +0000

    Only create an image for 2D and 3D point files, not 2D, 3D, and 4D point files, since we cannot place VOIs in 4D.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9772 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogKMeans.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogKMeans.java
@@ -715,7 +715,7 @@ public class JDialogKMeans extends JDialogScriptableBase implements AlgorithmInt
 		
 		
 
-		if (((nDims >= 2) && (nDims <= 4)  && (image == null)) || ((image != null) && (image.isColorImage()))) {
+		if (((nDims >= 2) && (nDims <= 3)  && (image == null)) || ((image != null) && (image.isColorImage()))) {
 			if ((image != null) && (image.isColorImage())) {
 				scaleMax = Math.max(scaleMax, image.getMax());
 				image = new ModelImage(ModelStorageBase.BYTE, extents, 
@@ -755,7 +755,7 @@ public class JDialogKMeans extends JDialogScriptableBase implements AlgorithmInt
 	            centroidPos = new double[nDims][numberClusters];
 			} // else 
 		    
-		} // if (((nDims >= 2) && (nDims <= 4)  && (image == null)) || ((image != null) && (image.isColorImage())))
+		} // if (((nDims >= 2) && (nDims <= 3)  && (image == null)) || ((image != null) && (image.isColorImage())))
 		else {
 			centroidPos = new double[nDims][numberClusters];
 		}
