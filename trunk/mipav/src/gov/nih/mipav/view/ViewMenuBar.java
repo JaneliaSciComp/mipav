2e2b058239f87e88486293ae421f4db1af13b88f
tyrieek@NIH.GOV <tyrieek@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2012-01-24 19:20:23 +0000

    Deleted LoadDWIDir menu option
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9863 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewMenuBar.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewMenuBar.java
@@ -410,7 +410,6 @@ public class ViewMenuBar {
         return menuBuilder.makeMenu("DICOM", true, new JComponent[] {
                 menuBuilder.buildMenuItem("DICOM browser", "BrowseDICOM", 0, null, true),
                 menuBuilder.buildMenuItem("DICOMDIR browser", "BrowseDICOMDIR", 0, null, true),
-                menuBuilder.buildMenuItem("Load DWIDIR", "LoadDWIDIR", 0, null, true),
                 menuBuilder.buildMenuItem("Anonymize DICOM directory", "AnonymizeDirectory", 0, null, true),
                 menuBuilder.buildMenuItem("DICOM database access", "QueryDatabase", 0, "database.gif", true),
                 menuBuilder.buildCheckBoxMenuItem("Activate DICOM receiver", "Dicom", Preferences
