32ff0aa4feffe5fdfc0b20a90e2a78f771846f8f
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2012-02-17 19:40:16 +0000

    added disclosure to plugins
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@9916 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmMuscleSegmentation540a.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmMuscleSegmentation540a.java
@@ -1,3 +1,26 @@
+//MIPAV is freely available from http://mipav.cit.nih.gov
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+//OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+//NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+//HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+//OR OTHER DEALINGS IN THE SOFTWARE. 
+
+/*****************************************************************
+******************************************************************
+
+The MIPAV application is intended for research use only.
+This application has NOT been approved for ANY diagnostic use 
+by the Food and Drug Administration. There is currently no 
+approval process pending. 
+
+This software may NOT be used for diagnostic purposes.
+
+******************************************************************
+******************************************************************/
 
 import gov.nih.mipav.model.algorithms.AlgorithmBase;
 import gov.nih.mipav.model.structures.*;
