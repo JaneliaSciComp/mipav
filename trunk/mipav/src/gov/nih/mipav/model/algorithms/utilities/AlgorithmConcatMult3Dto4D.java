e7aaf660c56fb1c0d79f380a375857b9b2c2e1e3
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2012-07-17 19:56:01 +0000

    utilized multiple concatenation super class
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@10560 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/utilities/AlgorithmConcatMult3Dto4D.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/utilities/AlgorithmConcatMult3Dto4D.java
@@ -10,24 +10,12 @@ import gov.nih.mipav.model.structures.ModelImage;
 
 
 /**
- * @author pandyan
+ * @author senseneyj
  * This algorithm concats multiple 3D images of same type to a 4D image
  *
  */
-public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
-	
-	/** array of 3D images **/
-	private ModelImage[] images;
-	
-	/** final image **/
-	private ModelImage destImage;
+public class AlgorithmConcatMult3Dto4D extends AlgorithmConcatMult {
 
-	/** x and y dimensions of image */
-    private int xDim, yDim;
-
-	/** Whether to copy all file information */
-    private boolean copyAllInfo = false;
-	
 	/**
 	 * constructor
 	 * @param images
@@ -114,10 +102,9 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
                for (int t = 0; t < destImage.getExtents()[3]; t++) {
                    for (int z = 0; z <destImage.getExtents()[2] ; z++) {
                        fireProgressStateChanged((100 * sliceCounter)/(destImage.getExtents()[3]));
-                       j = (t*destImage.getExtents()[2]) + z;
                        
-                       if(images[0].isDicomImage()) {
-                           copyDicomInfo(destFileInfo, resols, j, z, t, sliceCounter); 
+                       if(images[t].isDicomImage()) {
+                           copyDicomInfo(destFileInfo, resols, z, t, sliceCounter); 
                            
                        } else {
                            destFileInfo[sliceCounter] = (FileInfoBase) images[t].getFileInfo(z).clone();
@@ -126,6 +113,8 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
                        sliceCounter++; 
                    }
                }
+               
+               destImage.setFileInfo(destFileInfo);
          } else {
              fileInfo = destImage.getFileInfo();
 
@@ -133,8 +122,7 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
                  fireProgressStateChanged((100 * i)/(destImage.getExtents()[3]));
                  copyBaseInfo(fileInfo, images[0].getFileInfo()[0], resols, i);
              }
-             
-             
+
              counter = 0;
              for(int i=0;i<images.length;i++) {
                  ModelImage img = images[i];
@@ -149,9 +137,6 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
                  }
              }
          }
-             
-               
-        destImage.setFileInfo(destFileInfo);
 
         setCompleted(true);
         fileInfo = null;
@@ -165,74 +150,6 @@ public class AlgorithmConcatMult3Dto4D extends AlgorithmBase {
 
 	}
 	
-	private void copyBaseInfo(FileInfoBase[] fileInfo, FileInfoBase srcFileInfo, float[] resols, int i) {
-	    fileInfo[i].setModality(srcFileInfo.getModality());
-        fileInfo[i].setFileDirectory(srcFileInfo.getFileDirectory());
-        fileInfo[i].setEndianess(srcFileInfo.getEndianess());
-        fileInfo[i].setUnitsOfMeasure(srcFileInfo.getUnitsOfMeasure());
-        fileInfo[i].setResolutions(resols);
-        fileInfo[i].setExtents(destImage.getExtents());
-        fileInfo[i].setMax(destImage.getMax());
-        fileInfo[i].setMin(destImage.getMin());
-        fileInfo[i].setImageOrientation(srcFileInfo.getImageOrientation());
-        fileInfo[i].setPixelPadValue(srcFileInfo.getPixelPadValue());
-        fileInfo[i].setPhotometric(srcFileInfo.getPhotometric());
-        fileInfo[i].setAxisOrientation(srcFileInfo.getAxisOrientation());
-	}
-
-	/**
-	 * Most efficient way of creating DICOM tags for 4-D. Uses pointers based on srcimage dicom tags  
-	 * 
-	 * @param destFileInfo
-	 * @param resols
-	 * @param j
-	 * @param z
-	 * @param t
-	 * @param sliceCounter
-	 */
-    private void copyDicomInfo(FileInfoBase[] destFileInfo, float[] resols, int j, int z, int t, int sliceCounter) {
-        FileInfoDicom oldDicomInfo = (FileInfoDicom) images[t].getFileInfo(z);
-        if (z == 0) {
-            destFileInfo[j] = new FileInfoDicom(oldDicomInfo.getFileName(), oldDicomInfo.getFileDirectory(),
-                                            oldDicomInfo.getFileFormat());
-            ((FileInfoDicom)destFileInfo[j]).setVr_type(oldDicomInfo.getVr_type());     
-        }
-        else {
-            destFileInfo[j] = new FileInfoDicom(oldDicomInfo.getFileName(), oldDicomInfo.getFileDirectory(),
-                                            oldDicomInfo.getFileFormat(), (FileInfoDicom) destFileInfo[t*(destImage.getExtents()[2])]);
-            
-            ((FileInfoDicom)destFileInfo[j]).setVr_type(oldDicomInfo.getVr_type()); 
-
-           
-        }
-         
-        double sliceResolution = 0.0;
-        
-         FileDicomTagTable newTagTable = ((FileInfoDicom) destFileInfo[j]).getTagTable();
-         if (newTagTable.getValue("0018,0088") != null) {
-             String sliceGapString = ((String) ((FileInfoDicom) destFileInfo[j]).getTagTable().getValue("0018,0088")).trim();
-             sliceResolution = new Double(sliceGapString.trim()).doubleValue();
-         }          
-         
-         fireProgressStateChanged((((100 * (t*2)))/(destImage.getExtents()[2]+1)));
-         resols[0] = images[t].getFileInfo(z).getResolutions()[0];
-         resols[1] = images[t].getFileInfo(z).getResolutions()[1];
-         resols[2] = images[t].getFileInfo(z).getResolutions()[2];
-         resols[3] = (float)sliceResolution;
-         destFileInfo[sliceCounter].setResolutions(resols);
-         destFileInfo[sliceCounter].setExtents(destImage.getExtents());
-         destFileInfo[sliceCounter].setAxisOrientation(images[t].getFileInfo(z).getAxisOrientation()[0], 0);
-         destFileInfo[sliceCounter].setAxisOrientation(images[t].getFileInfo(z).getAxisOrientation()[1], 1);
-         destFileInfo[sliceCounter].setAxisOrientation(images[t].getFileInfo(z).getAxisOrientation()[2], 2);
-         destFileInfo[sliceCounter].setImageOrientation(images[t].getFileInfo(z).getImageOrientation());  
-         ((FileInfoDicom) destFileInfo[j]).getTagTable().importTags((FileInfoDicom) images[t].getFileInfo(z));
-         ((FileInfoDicom) destFileInfo[j]).getTagTable().setValue("0028,0011", new Short((short) xDim), 2); // columns
-         ((FileInfoDicom) destFileInfo[j]).getTagTable().setValue("0028,0010", new Short((short) yDim), 2); // rows                 
-         ((FileInfoDicom) destFileInfo[j]).getTagTable().setValue("0020,0013", Short.toString((short) (t + 1)),
-                                                  Short.toString((short) (t + 1)).length()); // instance number
-         ((FileInfoDicom) destFileInfo[j]).getTagTable().removeTag("0019,100A");// Removes NumberofImages in Mosaic Tag
-         
-        
-    }
+	
 
 }
