04f9d846e3e056ed6a80d03e925942949e008d64
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2012-09-20 16:57:39 +0000

    template for new spar file type
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@10901 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileSpar.java
@@ -0,0 +1,22 @@
+package gov.nih.mipav.model.file;
+
+import java.io.IOException;
+
+import gov.nih.mipav.model.structures.ModelImage;
+
+public class FileSpar extends FileBase {
+
+    public FileSpar(String fileName, String fileDir) {
+        this(null, fileName, fileDir);
+    }
+    
+    public FileSpar(FileInfoBase imageAInfo, String fileName, String fileDir) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public ModelImage readImage(boolean one) throws IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
