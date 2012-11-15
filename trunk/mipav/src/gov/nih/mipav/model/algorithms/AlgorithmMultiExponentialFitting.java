b334d0c432462a1e03876f0cdf1542f75f598a22
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2012-11-15 23:12:29 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11210 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmMultiExponentialFitting.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmMultiExponentialFitting.java
@@ -56,6 +56,7 @@ public class AlgorithmMultiExponentialFitting extends AlgorithmBase {
     private int iblokb[] = new int[12];
     private boolean lbloka[] = new boolean[45];
     private int mlammx = 9;
+    private double convrg = 5.0E-5;
     
     // nlammx >=1 && nlammx <= mlammx
     private int nlammx;
@@ -63,7 +64,6 @@ public class AlgorithmMultiExponentialFitting extends AlgorithmBase {
     private int iwt;
     // mtry >= 1 && mtry <= 45
     private int mtry;
-    private boolean last;
     // Read in t values if regint == false
     // Calculates t values from tstart, tend, and nt if regint == true
     private boolean regint;
@@ -93,17 +93,30 @@ public class AlgorithmMultiExponentialFitting extends AlgorithmBase {
     // sqrtw has n values
     private double sqrtw[];
     
+    private double ylyfit[] = new double[nmax];
+    // Original code has equivalence of e and gse
+    private double e[][] = new double[nmax][10];
+    private double gse[][] = new double[500][4];
+    private int ndime;
+    private double deltat[] = new double[nintmx];
+    private int ndimg;
+    private int iwtsgn;
+    private int routineCalled;
+    private int evarCalled = 1;
+    private int varfCalled = 2;
+    private boolean failed = false;
+    private double var = 0.0;
+    
     // ~ Constructors
     // ---------------------------------------------------------------------------------------------------
     
-    public AlgorithmMultiExponentialFitting(int nlammx, int iwt, int mtry, boolean last, boolean regint,
+    public AlgorithmMultiExponentialFitting(int nlammx, int iwt, int mtry, boolean regint,
             boolean nobase, boolean noneg, boolean pry, boolean prprel, boolean prfinl, boolean plotrs,
             boolean repeat, int n, double t[], int nint, double tstart[], double tend, int nt[],
             double y[], double sqrtw[]) {
         this.nlammx = nlammx;
         this.iwt = iwt;
         this.mtry = mtry;
-        this.last = last;
         this.regint = regint;
         this.nobase = nobase;
         this.noneg = noneg;
@@ -137,19 +150,10 @@ public class AlgorithmMultiExponentialFitting extends AlgorithmBase {
      * starts the algorithm.
      */
     public void runAlgorithm() {
-        // Original code has equivalence of e and gse
-        double e[][] = new double[nmax][10];
-        double gse[][] = new double[500][4];
-        double ylyfit[] = new double[nmax];
-        double deltat[] = new double[nintmx];
-        double sqrtw[] = new double[nmax];
-        int iwtsgn;
         int j;
         double dub;
         int l;
         int k;
-        int ndime;
-        int ndimg;
         
         iwtsgn = iwt;
         iwt = Math.abs(iwt);
@@ -205,14 +209,295 @@ public class AlgorithmMultiExponentialFitting extends AlgorithmBase {
         
         // weight generates crude starting values for constrained least square fits to raw data to generate
         // weights and does initial computations of quantities for later use.
+        weight(false);
+        
+        // fanlyz is for the constrained stepwise least squares analysis of the raw data and the transforms
+        // to get starting estimates for the final least squares analysis of the raw data.
+        fanlyz();
+        
+        // weight uses starting values from fanlyz for constrained least squares fits to raw data to generate
+        // weights for the final least squares fits to the raw data.
+        weight(true);
+        
+        // yanlyz is for the constrained stepwise least squares analysis of the raw data (using the starting 
+        // values from fanlyz and the weights from weight).
+        yanlyz();
+        
         setCompleted(true);
         return;
     }
     
-    private void weight(boolean finalVar, double timu[], double e[][], int ndime, double deltat[], double gse[][],
-                        int dimg) {
+    private void weight(boolean finalVar) {
+        // double timu[] in original code replaced by calling double ylyfit[]
+        // Does initial least squares fits to raw data to generate weights, evaluates data transforms, transform
+        // and exponential sums at grid points for interpolation, and other quantities for later use.
+        // If finalVar == true, uses lamf (values from fanlyz).
+        // If finalVar == false, generates its own crude starting lambdas.
         
+        // Calls routines lstsqr, etheor
+        // which in turn call evar, pivot, pivot1, anlerr
+        double fhatmx;
+        int mdima;
+        int k;
+        int j;
+        double zero[] = new double[10];
+        double tn;
+        double ddum;
+        double dum = 0.0;
+        double dddum;
+        double t1 = 0.0;
+        double t2 = 0.0;
+        double t5;
+        double tnl1;
+        double delta;
+        double lamnmx[][] = new double[2][2];
+        double t1lamx = 2.08;
+        double tnlamn[] = new double[]{0.2, 0.02};
+        int nparmx;
+        int ibase = 0;
+        double fhat[] = new double[19];
+        double num;
+        double xrange;
+        double sumwt;
+        double sumwty;
+        double varmin;
+        boolean wted;
+        double varbes;
+        double ratiol;
+        int jlam;
+        int nf;
+        boolean ffail[][] = new boolean[9][2];
+        double lamst[] = new double[9];
+        double lamf[][][] = new double[9][9][2];
+        int nlinwt = 0;
+        double wtalp[] = new double[10];
+        double palpha[] = new double[10];
+        double wtlam[] = new double[10];
+        double plam[] = new double[10];
+        double s;
+        int L = 0;
+        int i;
+        double term;
+        double errfit;
+        double dgride;
+        double rgride;
+        int jgeadd;
+        double gest;
+        double gestl;
+        int nge1;
+        int nperg2[] = new int[9];
+        int ngrid1 = 240;
+        int ngrid2[] = new int[9];
+        double gamma[] = new double[21];
+        int ng2max = 20;
+        int nbinom[][] = new int[20][9];
+        
+        if (!finalVar) {
+            fhatmx = 0.0; 
+            mdima = 2*mlammx + 1;
+            k = mlammx + 1;
+            for (j = 1; j <= k; j++) {
+                zero[j-1] = 0.0; 
+            } // for (j = 1; j <= k; j++)
+            tn = -1.0E20;
+            ddum = -1.0E20;
+            for (k = 1; k <= 5; k++) {
+                dum = 1.0E20;
+                for (j = 1; j <= n; j++) {
+                    dddum = t[j-1];
+                    if (k == 1) {
+                        tn = Math.max(tn,dddum);
+                    }
+                    if ((dddum > ddum) && (dddum < dum)) {
+                        dum = dddum;
+                    }
+                } // for (j = 1; j <= n; j++)
+                ddum = dum + 1.0E-5*Math.abs(dum);
+                if (k == 1) {
+                    t1 = dum;
+                }
+                if (k == 2) {
+                    t2 = dum;
+                }
+            } // for (k = 1; k <= 5; k++)
+            t5 = dum;
+            tnl1 = -1.0E20;
+            dum = tn - 1.0E-5 * Math.abs(tn);
+            for (j = 1; j <= n; j++) {
+                if (t[j-1] < dum) {
+                    tnl1 = Math.max(tnl1, t[j-1]);
+                }
+            } // for (j = 1; j <= n; j++)
+            dum = 0.25*(t5-t1);
+            delta = dum - t1;
+            // lamnmx[j-1][k-1] = min. (j == 1) and max. (j == 2) allowed values of lambda
+            // for least squares parameters (k == 1) and for starting values (k == 2).
+            lamnmx[1][0] = t1lamx/(t1 + delta);
+            lamnmx[1][1] = Math.min(lamnmx[1][0], 0.693/(t1 + delta));
+            lamnmx[0][1] = tnlamn[0]/(tn + delta);
+            if (!nobase) {
+                nparmx = 2*nlammx + 1;
+                ibase = 1;
+                lamnmx[0][0] = lamnmx[0][1];
+            } // if (!nobase)
+            else { // nobase == true
+                ibase = 0;
+                nparmx = 2 * nlammx;
+                fhat[nparmx] = 0.0;
+                lamnmx[0][0] = tnlamn[1]/(tn + delta);
+            } // else nobase == true
+            // If necessary, increase delta so that distance between first two points on x = log(t + delta) axis
+            // is less than a half-period of the highest frequency used in the transforms
+            while (true) {
+                num = tn + delta;
+                num = num * num;
+                xrange = Math.log(num/((t1 + delta)* (tnl1 + delta)));
+                if (Math.log((t2 + delta)/(t1 + delta)) * (2.0 * nlammx) < xrange) {
+                    break;
+                }
+                delta = delta + dum;
+            } // while (true)
+        } // if (!finalVar)
+        else { // finalVar == true
+            if ((iwt == 1) || (iwt == 4)) {
+                return;
+            }
+            for (j = 0; j < n; j++) {
+                y[j] = y[j]/sqrtw[j];  
+            } 
+        } // else finalVar == true
+        if (iwt != 4) {
+            sumwt = (double)n;
+            sumwty = 0.0;
+            dum = 0.0;
+            for (j = 0; j < n; j++) {
+                sumwty = sumwty + y[j];
+                dum = dum + y[j] * y[j];
+                sqrtw[j] = 1.0;
+            } // for (j = 0; j < n; j++)
+            varmin = dum * convrg * convrg;
+            if (iwt != 1) {
+                wted = false;
+                varbes = 1.0E20;
+                ratiol = lamnmx[1][1]/lamnmx[0][1];
+                
+                // Start of main loop for stepwise analysis
+                for (jlam = 1; jlam <= nlammx; jlam++) {
+                    nf = 2*jlam + ibase;  
+                    Preferences.debug("Preliminary analysis to determine weights\n", Preferences.DEBUG_ALGORITHM);
+                    Preferences.debug("Analysis assuming " + jlam + " components\n", Preferences.DEBUG_ALGORITHM);
+                    if (!finalVar) {
+                        ffail[jlam-1][0] = true;
+                        dum = Math.pow(ratiol, 1.0/(jlam+1.0));
+                        lamst[0] = lamnmx[0][1] * dum;
+                        if (jlam != 1) {
+                            for (j = 2; j <= jlam; j++) {
+                                lamst[j-1] = lamst[j-2] * dum;
+                            } // for (j = 2; j <= jlam; j++)
+                        } // if (jlam != 1)
+                    } // if (!finalVar)
+                    else { // finalVar == true
+                        for (j = 1; j <= jlam; j++) {
+                            lamst[j-1] = lamf[j-1][jlam-1][0];    
+                        } // for (j = 1; j <= jlam; j++)
+                    } // else finalVar == true
+                    lstsqr(lamst, jlam, true, n, (jlam + ibase), evarCalled, 1, ffail[jlam-1][0], false, prprel);
+                    if (failed || var >= varbes) {
+                        break;
+                    }
+                    varbes = var;
+                    nlinwt = jlam + ibase;
+                    for (j = 0; j < nlinwt; j++) {
+                        wtalp[j] = palpha[j];
+                        wtlam[j] = plam[j];
+                    } // for (j = 0; j < nlinwt; j++)
+                    if (iwtsgn < 0) {
+                        nlinwt = jlam;
+                    }
+                } // for (jlam = 1; jlam <= nlammx; jlam++)
+                if (varbes >= 1.0E20) {
+                    Preferences.debug("Component analysis to determine weights somehow failed\n", Preferences.DEBUG_ALGORITHM);
+                    Preferences.debug("Unit weights will be used\n", Preferences.DEBUG_ALGORITHM);
+                    iwt = 1;
+                }
+                else {// varbes < 1.0E20
+                    // Calculate weights including errfit (standard error in region of the theoretical curve where it
+                    // is closest to y = 0.0, where a weight could otherwise be disastrously large if errfit were not
+                    // accounted for).
+                    dum = 1.0E20;
+                    for (k = 1; k <= n; k++) {
+                        s = 0.0;
+                        ddum = t[k-1];
+                        for (j = 0; j < nlinwt; j++) {
+                            s = s + wtalp[j] * Math.exp(-ddum * wtlam[j]);
+                        }
+                        sqrtw[k-1] = s;
+                        if (Math.abs(s) < dum) {
+                            dum = Math.abs(s);
+                            L = k;
+                        }
+                    } // for (k = 1; k <= n; k++)
+                    k = Math.min(n, L+5);
+                    i = Math.max(1, k-9);
+                    dum = 0.0;
+                    ddum = 0.0;
+                    if ((iwtsgn < 0)  && (!nobase)) {
+                        ddum = wtalp[nlinwt];
+                    }
+                    for (j = i; j <= k; j++) {
+                        term = y[j-1] - sqrtw[j-1] - ddum;
+                        dum = dum + term * term;
+                    } // for (j = i; j <= k; j++)
+                    errfit = Math.sqrt(dum/(k - i + 1.0));
+                    for (k = 0; k < n; k++) {
+                        sqrtw[k] = 1.0/(Math.abs(sqrtw[k]) + errfit);
+                        if (iwt == 2) {
+                            sqrtw[k] = Math.sqrt(sqrtw[k]);
+                        }
+                    } // for (k = 0; k < n; k++)
+                    L = 1;
+                    if (finalVar) {
+                        L = 2;
+                    }
+                    if (L == 1) {
+                        Preferences.debug("Parameters used to generate weights for transforms\n", Preferences.DEBUG_ALGORITHM);
+                    }
+                    else if (L == 2) {
+                        Preferences.debug("Parameters used to generate weights for raw data\n", Preferences.DEBUG_ALGORITHM);    
+                    }
+                    Preferences.debug("errfit, the uncertainty term added to absolute values of theoretical curve = " + errfit + "\n",
+                                      Preferences.DEBUG_ALGORITHM);
+                    Preferences.debug("lambda = wtlam  alpha = wtalp\n", Preferences.DEBUG_ALGORITHM);
+                    for (k = 0; k < nlinwt; k++) {
+                        Preferences.debug("wtlam["+k+"] = " + wtlam[k] + "  wtalp[" + k + "] = " + wtalp[k] + "\n", 
+                                           Preferences.DEBUG_ALGORITHM);
+                    }
+                } // else varbes < 1.0E20
+            } // if (iwt != 1)
+        } // if (iwt != 4)
+        wted = (iwt != 1);
+        if (!finalVar) {
+            // Initial computation of quantities to be used later
+            dgride = Math.log(lamnmx[1][1]/lamnmx[0][1])/(ngrid1 - 1.0);
+            rgride = Math.exp(dgride);
+            //jgeadd = minter/2;
+        } // if (!finalVar)
     } // weight
+    
+    private void lstsqr(double lamst[], int jlam, boolean rawdat, int n, int nlin, int routineCalled, int itype, boolean split,
+                        boolean inter, boolean prlsq) {
+        // routineCalled substituted for external vardum
+        // routineCalled == evarCalled or routineCalled == varfCalled
+        
+    } // lstsqr
+    
+    private void fanlyz() {
+        
+    } // fanlyz
+    
+    private void yanlyz() {
+        
+    } // yanlyz
     
 
 }
