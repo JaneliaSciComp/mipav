5bc1303881a893e6ff1d4d678cd9919a88dda741
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2012-12-19 03:48:50 +0000

    Updated lighting and surface extract shaders to current GLSL shading language.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11261 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/WildMagic/Shaders/GLSL/SurfaceExtract.glsl
+++ trunk/mipav/src/WildMagic/Shaders/GLSL/SurfaceExtract.glsl
@@ -2,6 +2,9 @@ uniform vec4    StepSize;
 uniform sampler3D bVolumeImageA; 
 uniform sampler1D cColorMapA; 
 
+in vec3 varTexCoord;
+out vec4 fragColor;
+
 void p_SurfaceExtract()
 {
     // texture-coordinate steps:
@@ -12,21 +15,21 @@ void p_SurfaceExtract()
 
     // Look at the opacity values of all 8 corners of this voxel "Cube":
     vec3 cube[8];
-    cube[ 0 ] = gl_TexCoord[0].xyz; // x,y,z
-    cube[ 1 ] = gl_TexCoord[0].xyz; cube[1].x += StepSize.x; // x+1,y,z
-    cube[ 2 ] = gl_TexCoord[0].xyz; cube[2].y += StepSize.y; // x,y+1,z
-    cube[ 3 ] = gl_TexCoord[0].xyz; cube[3].x += StepSize.x; cube[3].y += StepSize.y; //x+1,y+1,z
-    cube[ 4 ] = gl_TexCoord[0].xyz; cube[4].z += StepSize.z; //x,y,z+1
-    cube[ 5 ] = gl_TexCoord[0].xyz; cube[5].x += StepSize.x; cube[5].z += StepSize.z; //x+1,y,z+1
-    cube[ 6 ] = gl_TexCoord[0].xyz; cube[6].y += StepSize.y; cube[6].z += StepSize.z; //x,y+1,z+1 
-    cube[ 7 ] = gl_TexCoord[0].xyz; cube[7].x += StepSize.x; cube[7].y += StepSize.y; cube[7].z += StepSize.z; //x+1,y+1,z+1
+    cube[ 0 ] = varTexCoord.xyz; // x,y,z
+    cube[ 1 ] = varTexCoord.xyz; cube[1].x += StepSize.x; // x+1,y,z
+    cube[ 2 ] = varTexCoord.xyz; cube[2].y += StepSize.y; // x,y+1,z
+    cube[ 3 ] = varTexCoord.xyz; cube[3].x += StepSize.x; cube[3].y += StepSize.y; //x+1,y+1,z
+    cube[ 4 ] = varTexCoord.xyz; cube[4].z += StepSize.z; //x,y,z+1
+    cube[ 5 ] = varTexCoord.xyz; cube[5].x += StepSize.x; cube[5].z += StepSize.z; //x+1,y,z+1
+    cube[ 6 ] = varTexCoord.xyz; cube[6].y += StepSize.y; cube[6].z += StepSize.z; //x,y+1,z+1 
+    cube[ 7 ] = varTexCoord.xyz; cube[7].x += StepSize.x; cube[7].y += StepSize.y; cube[7].z += StepSize.z; //x+1,y+1,z+1
 
     vec4 color[8];
     float opacity = 0.0;
     int iShow = 8;
 
-    color[0] = texture3D(bVolumeImageA,cube[0]);
-    opacity = texture1D(cColorMapA,color[0].r).a;
+    color[0] = texture(bVolumeImageA,cube[0]);
+    opacity = texture(cColorMapA,color[0].r).a;
     if ( opacity <= 0.0 )
     {
         color[0].r = 0.0;
@@ -35,8 +38,8 @@ void p_SurfaceExtract()
         iShow--;
     }
 
-    color[1] = texture3D(bVolumeImageA,cube[1]);
-    opacity = texture1D(cColorMapA,color[1].r).a;
+    color[1] = texture(bVolumeImageA,cube[1]);
+    opacity = texture(cColorMapA,color[1].r).a;
     if ( opacity <= 0.0 )
     {
         color[1].r = 0.0;
@@ -45,8 +48,8 @@ void p_SurfaceExtract()
         iShow--;
     }
 
-    color[2] = texture3D(bVolumeImageA,cube[2]);
-    opacity = texture1D(cColorMapA,color[2].r).a;
+    color[2] = texture(bVolumeImageA,cube[2]);
+    opacity = texture(cColorMapA,color[2].r).a;
     if ( opacity <= 0.0 )
     {
         color[2].r = 0.0;
@@ -55,8 +58,8 @@ void p_SurfaceExtract()
         iShow--;
     }
 
-    color[3] = texture3D(bVolumeImageA,cube[3]);
-    opacity = texture1D(cColorMapA,color[3].r).a;
+    color[3] = texture(bVolumeImageA,cube[3]);
+    opacity = texture(cColorMapA,color[3].r).a;
     if ( opacity <= 0.0 )
     {
         color[3].r = 0.0;
@@ -65,8 +68,8 @@ void p_SurfaceExtract()
         iShow--;
     }
 
-    color[4] = texture3D(bVolumeImageA,cube[4]);
-    opacity = texture1D(cColorMapA,color[4].r).a;
+    color[4] = texture(bVolumeImageA,cube[4]);
+    opacity = texture(cColorMapA,color[4].r).a;
     if ( opacity <= 0.0 )
     {
         color[4].r = 0.0;
@@ -76,8 +79,8 @@ void p_SurfaceExtract()
     }
 
 
-    color[5] = texture3D(bVolumeImageA,cube[5]);
-    opacity = texture1D(cColorMapA,color[5].r).a;
+    color[5] = texture(bVolumeImageA,cube[5]);
+    opacity = texture(cColorMapA,color[5].r).a;
     if ( opacity <= 0.0 )
     {
         color[5].r = 0.0;
@@ -86,8 +89,8 @@ void p_SurfaceExtract()
         iShow--;
     }
 
-    color[6] = texture3D(bVolumeImageA,cube[6]);
-    opacity = texture1D(cColorMapA,color[6].r).a;
+    color[6] = texture(bVolumeImageA,cube[6]);
+    opacity = texture(cColorMapA,color[6].r).a;
     if ( opacity <= 0.0 )
     {
         color[6].r = 0.0;
@@ -96,8 +99,8 @@ void p_SurfaceExtract()
         iShow--;
     }
 
-    color[7] = texture3D(bVolumeImageA,cube[7]);
-    opacity = texture1D(cColorMapA,color[7].r).a;
+    color[7] = texture(bVolumeImageA,cube[7]);
+    opacity = texture(cColorMapA,color[7].r).a;
     if ( opacity <= 0.0 )
     {
         color[7].r = 0.0;
@@ -107,14 +110,14 @@ void p_SurfaceExtract()
     }
 
 
-    gl_FragColor.r = color[0].r;
-    gl_FragColor.g = color[0].g;
-    gl_FragColor.b = color[0].b;
-    gl_FragColor.a = 0.0;
+    fragColor.r = color[0].r;
+    fragColor.g = color[0].g;
+    fragColor.b = color[0].b;
+    fragColor.a = 0.0;
     // Alpha-value determines if voxel is part of surface:
     if ( iShow != 0.0 )
     {
-        gl_FragColor.a = 1.0;
+        fragColor.a = 1.0;
     }
 }
 
--- trunk/mipav/src/WildMagic/Shaders/GLSL/SurfaceExtract_P2.glsl
+++ trunk/mipav/src/WildMagic/Shaders/GLSL/SurfaceExtract_P2.glsl
@@ -1,5 +1,8 @@
 uniform vec4    StepSize;
 uniform sampler3D VolumeExtract;
+
+in vec3 varTexCoord;
+out vec4 fragColor;
 void p_SurfaceExtract_P2()
 {
     float gaussian[27];
@@ -50,164 +53,164 @@ void p_SurfaceExtract_P2()
     float stepZ[3];    stepZ[0] = -StepSize.z;    stepZ[1] = 0.0;    stepZ[2] = StepSize.z;
 
     // z = 0:
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[0]);
     colorAcc.y += (color4.y * gaussian[0]);
     colorAcc.z += (color4.z * gaussian[0]);
     colorAcc.w += (color4.w * gaussian[0]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[1]);
     colorAcc.y += (color4.y * gaussian[1]);
     colorAcc.z += (color4.z * gaussian[1]);
     colorAcc.w += (color4.w * gaussian[1]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[2]);
     colorAcc.y += (color4.y * gaussian[2]);
     colorAcc.z += (color4.z * gaussian[2]);
     colorAcc.w += (color4.w * gaussian[2]);
 
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[3]);
     colorAcc.y += (color4.y * gaussian[3]);
     colorAcc.z += (color4.z * gaussian[3]);
     colorAcc.w += (color4.w * gaussian[3]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[4]);
     colorAcc.y += (color4.y * gaussian[4]);
     colorAcc.z += (color4.z * gaussian[4]);
     colorAcc.w += (color4.w * gaussian[4]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[5]);
     colorAcc.y += (color4.y * gaussian[5]);
     colorAcc.z += (color4.z * gaussian[5]);
     colorAcc.w += (color4.w * gaussian[5]);
 
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[6]);
     colorAcc.y += (color4.y * gaussian[6]);
     colorAcc.z += (color4.z * gaussian[6]);
     colorAcc.w += (color4.w * gaussian[6]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[7]);
     colorAcc.y += (color4.y * gaussian[7]);
     colorAcc.z += (color4.z * gaussian[7]);
     colorAcc.w += (color4.w * gaussian[8]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[0];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[0];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[8]);
     colorAcc.y += (color4.y * gaussian[8]);
     colorAcc.z += (color4.z * gaussian[8]);
     colorAcc.w += (color4.w * gaussian[8]);
 
     // z = 1:
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[9]);
     colorAcc.y += (color4.y * gaussian[9]);
     colorAcc.z += (color4.z * gaussian[9]);
     colorAcc.w += (color4.w * gaussian[9]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[10]);
     colorAcc.y += (color4.y * gaussian[10]);
     colorAcc.z += (color4.z * gaussian[10]);
     colorAcc.w += (color4.w * gaussian[10]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[11]);
     colorAcc.y += (color4.y * gaussian[11]);
     colorAcc.z += (color4.z * gaussian[11]);
     colorAcc.w += (color4.w * gaussian[11]);
 
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[12]);
     colorAcc.y += (color4.y * gaussian[12]);
     colorAcc.z += (color4.z * gaussian[12]);
     colorAcc.w += (color4.w * gaussian[12]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[13]);
     colorAcc.y += (color4.y * gaussian[13]);
     colorAcc.z += (color4.z * gaussian[13]);
     colorAcc.w += (color4.w * gaussian[13]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[14]);
     colorAcc.y += (color4.y * gaussian[14]);
     colorAcc.z += (color4.z * gaussian[14]);
     colorAcc.w += (color4.w * gaussian[14]);
 
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[15]);
     colorAcc.y += (color4.y * gaussian[15]);
     colorAcc.z += (color4.z * gaussian[15]);
     colorAcc.w += (color4.w * gaussian[15]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[16]);
     colorAcc.y += (color4.y * gaussian[16]);
     colorAcc.z += (color4.z * gaussian[16]);
     colorAcc.w += (color4.w * gaussian[16]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[1];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[1];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[17]);
     colorAcc.y += (color4.y * gaussian[17]);
     colorAcc.z += (color4.z * gaussian[17]);
@@ -216,82 +219,82 @@ void p_SurfaceExtract_P2()
 
 
     // z = 2:
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[18]);
     colorAcc.y += (color4.y * gaussian[18]);
     colorAcc.z += (color4.z * gaussian[18]);
     colorAcc.w += (color4.w * gaussian[18]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[19]);
     colorAcc.y += (color4.y * gaussian[19]);
     colorAcc.z += (color4.z * gaussian[19]);
     colorAcc.w += (color4.w * gaussian[19]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[0];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[0];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[20]);
     colorAcc.y += (color4.y * gaussian[20]);
     colorAcc.z += (color4.z * gaussian[20]);
     colorAcc.w += (color4.w * gaussian[20]);
 
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[21]);
     colorAcc.y += (color4.y * gaussian[21]);
     colorAcc.z += (color4.z * gaussian[21]);
     colorAcc.w += (color4.w * gaussian[21]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[22]);
     colorAcc.y += (color4.y * gaussian[22]);
     colorAcc.z += (color4.z * gaussian[22]);
     colorAcc.w += (color4.w * gaussian[22]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[1];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[1];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[23]);
     colorAcc.y += (color4.y * gaussian[23]);
     colorAcc.z += (color4.z * gaussian[23]);
     colorAcc.w += (color4.w * gaussian[23]);
 
-    index3.x = gl_TexCoord[0].x + stepX[0];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[0];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[24]);
     colorAcc.y += (color4.y * gaussian[24]);
     colorAcc.z += (color4.z * gaussian[24]);
     colorAcc.w += (color4.w * gaussian[24]);
 
-    index3.x = gl_TexCoord[0].x + stepX[1];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[1];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[25]);
     colorAcc.y += (color4.y * gaussian[25]);
     colorAcc.z += (color4.z * gaussian[25]);
     colorAcc.w += (color4.w * gaussian[25]);
 
-    index3.x = gl_TexCoord[0].x + stepX[2];
-    index3.y = gl_TexCoord[0].y + stepY[2];
-    index3.z = gl_TexCoord[0].z + stepZ[2];
-    color4 = texture3D( VolumeExtract, index3 );
+    index3.x = varTexCoord.x + stepX[2];
+    index3.y = varTexCoord.y + stepY[2];
+    index3.z = varTexCoord.z + stepZ[2];
+    color4 = texture( VolumeExtract, index3 );
     colorAcc.x += (color4.x * gaussian[26]);
     colorAcc.y += (color4.y * gaussian[26]);
     colorAcc.z += (color4.z * gaussian[26]);
@@ -299,9 +302,9 @@ void p_SurfaceExtract_P2()
 
 
 
-    gl_FragColor.r = colorAcc.x/norm;
-    gl_FragColor.g = colorAcc.y/norm;
-    gl_FragColor.b = colorAcc.z/norm;
-    gl_FragColor.a = colorAcc.w/norm;
+    fragColor.r = colorAcc.x/norm;
+    fragColor.g = colorAcc.y/norm;
+    fragColor.b = colorAcc.z/norm;
+    fragColor.a = colorAcc.w/norm;
 }
 //----------------------------------------------------------------------------
