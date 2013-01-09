09c75cd3eca4678f2425d0b762559fe0ec3f5184
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-01-09 17:36:35 +0000

    Testing shading language features on Mac...
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11296 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/WildMagic/Shaders/GLSL/MaterialTextureP.glsl
+++ trunk/mipav/src/WildMagic/Shaders/GLSL/MaterialTextureP.glsl
@@ -2,21 +2,18 @@ uniform sampler2D BaseSampler;
 uniform float MyAlpha;
 in vec3 varTexCoord;
 in vec4 varColor;
-#if version >= 330
+#if __VERSION__ >= 150
 layout(location = 0) out vec4     outFragData0;
+#else
+out vec4     outFragData0;
 #endif
 void p_MaterialTextureP()
 {
     // Add the material and texture colors.
     vec4 kBaseColor = texture(BaseSampler,varTexCoord.xy);
-#if version >= 330
     outFragData0.rgb = clamp(kBaseColor.rgb + varColor.rgb, 0.0, 1.0);
     
     // Multiply the material and texture alphas.
     outFragData0.a = 1.0;
-#else    
-    gl_FragData[0].rgb = clamp(kBaseColor.rgb + varColor.rgb, 0.0, 1.0);
-    gl_FragData[0].a = 1.0;
-#endif
 }
 //----------------------------------------------------------------------------
