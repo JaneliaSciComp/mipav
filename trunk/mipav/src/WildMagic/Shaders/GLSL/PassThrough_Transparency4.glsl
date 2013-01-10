a0a56f6a147c717c5d3a577c6163984ad242314b
abokinsky <abokinsky@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-01-10 16:01:11 +0000

    Updating shaders to correctly select the frag_data output.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11300 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/WildMagic/Shaders/GLSL/PassThrough_Transparency4.glsl
+++ trunk/mipav/src/WildMagic/Shaders/GLSL/PassThrough_Transparency4.glsl
@@ -1,7 +1,12 @@
 //----------------------------------------------------------------------------
 in vec4 varColor;
+#if __VERSION__ > 150
 layout(location = 0) out vec4     outFragData0;
 layout(location = 1) out vec4     outFragData1;
+#else
+out vec4     outFragData0;
+out vec4     outFragData1;
+#endif
 void p_PassThrough_Transparency4 ()
 {
     outFragData0 = vec4(varColor.rgb * varColor.a, varColor.a);
