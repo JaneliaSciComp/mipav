9b1f2ede5bf26db8f02ba11fe0db86d0146ac8e6
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-02-21 18:48:16 +0000

    Don't know why trunk directory is sometimes showing up in tags...
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11379 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/release-4.2.1/trunk/mipav/src/WildMagic/Shaders/GLSL/TextureP.glsl
+++
@@ -1,8 +0,0 @@
-//----------------------------------------------------------------------------
-uniform sampler2D BaseSampler;
-void p_TextureP ()
-{
-    // Sample the texture image.
-    gl_FragColor = texture2D(BaseSampler,gl_TexCoord[0].xy);
-}
-//----------------------------------------------------------------------------
--- tags/release-4.2.1/trunk/mipav/src/plugins/PlugInCreateXML.java
+++
@@ -1,9 +0,0 @@
-import gov.nih.mipav.plugins.PlugInGeneric;
-
-
-public class PlugInCreateXML implements PlugInGeneric {
-
-	public void run() {
-		new PlugInDialogCreateXML(false);
-	}
-}
