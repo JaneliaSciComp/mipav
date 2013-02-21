b8613a14c2d593f80e580a8f62cda71f3965c6f3
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-02-21 18:48:01 +0000

    Don't know why trunk directory is sometimes showing up in tags...
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11378 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/release-4.3.0/trunk/mipav/src/WildMagic/Shaders/GLSL/VolumePreRender.glsl
+++
@@ -1,9 +0,0 @@
-uniform mat4 WVPMatrix;
-uniform float Blend;
-void v_VolumePreRender()
-{
-    // Transform the position from model space to clip space.
-    gl_Position = WVPMatrix * gl_Vertex;
-    gl_FrontColor.rgb = gl_MultiTexCoord0.xyz;
-    gl_FrontColor.a = Blend;
-}
--- tags/release-4.3.0/trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/Poisson/Geometry/TriangleIndex.java
+++
@@ -1,5 +0,0 @@
-package gov.nih.mipav.view.renderer.WildMagic.Poisson.Geometry;
-
-public class TriangleIndex {
-	public int[] idx = new int[3];
-}
