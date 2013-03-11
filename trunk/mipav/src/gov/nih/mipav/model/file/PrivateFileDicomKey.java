0cbd31a300af140958d95e6cef776b6333d7b8e4
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-03-11 18:50:46 +0000

    created a PrivateFileDicomKey
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11469 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/file/PrivateFileDicomKey.java
@@ -0,0 +1,37 @@
+package gov.nih.mipav.model.file;
+
+/**
+ * Associates a FileDicomKey with a particular publisher to denote the source of the FileDicomKey.  This publisher
+ * is often printed in the (x, 0010) tag for any group.  When no publisher exists uses publisher NO_PUBLISHER
+ * 
+ * @author justinsenseney
+ *
+ */
+public class PrivateFileDicomKey extends FileDicomKey {
+
+	public static final String NO_PUBLISHER = "No publisher";
+	
+	/** The publisher for this tag. */
+	private String publisher;
+	
+	public PrivateFileDicomKey(String keyStr) {
+		this(NO_PUBLISHER, keyStr);
+	}
+	
+	public PrivateFileDicomKey(String publisher, String keyStr) {
+		super(keyStr);
+		this.publisher = publisher;
+	}
+
+	public PrivateFileDicomKey(int group, int element)
+			throws NumberFormatException {
+		this(NO_PUBLISHER, group, element);
+	}
+	
+	public PrivateFileDicomKey(String publisher, int group, int element)
+			throws NumberFormatException {
+		super(group, element);
+		this.publisher = publisher;
+	}
+
+}
