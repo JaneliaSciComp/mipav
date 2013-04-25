96849f9f6b6633d08f285d68175dc51a19243d41
senseneyj@NIH.GOV <senseneyj@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-04-25 12:19:00 +0000

    created input package for new GUI input types, implemented the space navigator input
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11649 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/view/input/spacenav/SpaceNavigatorPoller.java
@@ -0,0 +1,79 @@
+package gov.nih.mipav.view.input.spacenav;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import javax.swing.Timer;
+
+/**
+ * Polls the space navigator.
+ * 
+ * @author justinsenseney
+ *
+ */
+public class SpaceNavigatorPoller {
+
+	private static final int DELAY = 50;
+	private static Timer pollTimer; 
+	private static SpaceNavigatorListener[] listeners = new SpaceNavigatorListener[0];
+	
+	public static void fireSpaceNavEvent() {
+		for(int i=0; i<listeners.length; i++) {
+			listeners[i].processSpaceNavEvent();
+		}
+	}
+	
+	public static void deRegisterListener(SpaceNavigatorListener listener) {
+		int toRemove = -1;
+		for(int i=0; i<listeners.length; i++) {
+			if(listeners[i] == listener) {
+				toRemove = i;
+				break;
+			}
+		}
+		
+		if(toRemove != -1) {
+			SpaceNavigatorListener[] listenersNew = new SpaceNavigatorListener[listeners.length-1];
+			for(int i=0; i<toRemove; i++) {
+				listenersNew[i] = listeners[i];
+			}
+			
+			for(int i=toRemove+1; i<listeners.length; i++) {
+				listenersNew[i-1] = listeners[i]; 
+			}
+			
+			listeners = listenersNew;
+		}
+		
+		if(listeners.length == 0) {
+			pollTimer.stop();
+		}
+	}
+	
+	public static void registerListener(SpaceNavigatorListener listener) {
+		SpaceNavigatorListener[] listenersNew = Arrays.copyOf(listeners, listeners.length+1);
+		listenersNew[listeners.length] = listener;
+		listeners = listenersNew;
+		
+		if(pollTimer == null) {
+			ActionListener pollPerformer = new ActionListener() { 
+				public void actionPerformed (ActionEvent e) { 
+					SpaceNavigatorController.poll();
+					if(SpaceNavigatorController.getRX() != 0 || 
+							SpaceNavigatorController.getRY() != 0 || 
+							SpaceNavigatorController.getRZ() != 0 || 
+							SpaceNavigatorController.getTX() != 0 || 
+							SpaceNavigatorController.getTY() != 0 || 
+							SpaceNavigatorController.getTZ() != 0) {
+						fireSpaceNavEvent();
+					}
+				} 
+			}; 
+			pollTimer = new Timer(DELAY, pollPerformer); 
+		}
+		
+		if(pollTimer != null && !pollTimer.isRunning()) {
+			pollTimer.start(); 
+		}
+	}
+}
