bb45473a6a31d7b73437153135b63564c8e8efc2
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-05-08 20:06:11 +0000

    In public VOI(VOI kVOI) VOIBase kContour = kVOI.curves.get(j).clone(); was changed to VOIBase kContour = new VOIContour((VOIContour)kVOI.curves.get(j)); because this.curves.add(kContour) was seen to occur before the clone completed.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11700 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/structures/VOI.java
+++ trunk/mipav/src/gov/nih/mipav/model/structures/VOI.java
@@ -549,7 +549,9 @@ public class VOI extends ModelSerialCloneable {
 		this.curves = new VOIBaseVector(this);
 		for ( int j = 0; j < kVOI.curves.size(); j++ )
 		{
-			VOIBase kContour = kVOI.curves.get(j).clone();
+			//VOIBase kContour = kVOI.curves.get(j).clone();
+		    VOIBase kContour = new VOIContour((VOIContour)kVOI.curves.get(j));
+			// With clone this.curves.add( kContour) was seen to occur before the clone operation was complete
 			this.curves.add( kContour );
 		}
 
