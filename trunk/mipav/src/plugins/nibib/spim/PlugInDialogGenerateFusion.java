80322f562afba6784eaf9925a2652b8f048e897c
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-05-10 18:26:45 +0000

    Made standalone.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11723 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/nibib/spim/PlugInDialogGenerateFusion.java
+++ trunk/mipav/src/plugins/nibib/spim/PlugInDialogGenerateFusion.java
@@ -29,6 +29,7 @@ import gov.nih.mipav.model.algorithms.utilities.AlgorithmMaximumIntensityProject
 import gov.nih.mipav.model.scripting.*;
 import gov.nih.mipav.model.scripting.parameters.ParameterFactory;
 import gov.nih.mipav.model.structures.*;
+import gov.nih.mipav.plugins.JDialogStandaloneScriptablePlugin;
 
 import gov.nih.mipav.view.*;
 import gov.nih.mipav.view.dialogs.GuiBuilder;
@@ -40,6 +41,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -68,7 +70,7 @@ import nibib.spim.PlugInAlgorithmGenerateFusion.SampleMode;
  * @author Justin Senseney (SenseneyJ@mail.nih.gov)
  * @see http://mipav.cit.nih.gov
  */
-public class PlugInDialogGenerateFusion extends JDialogScriptableBase implements AlgorithmInterface {
+public class PlugInDialogGenerateFusion extends JDialogStandaloneScriptablePlugin implements AlgorithmInterface {
     
     //~ Static fields/initializers -------------------------------------------------------------------------------------
 
@@ -259,11 +261,12 @@ public class PlugInDialogGenerateFusion extends JDialogScriptableBase implements
         } else if (command.equals("Script")) {
             callAlgorithm();
         } else if (command.equals("Cancel")) {
-            dispose();
+            //dispose();
+        	this.windowClosing(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
         } else {
             super.actionPerformed(event);
         }
-        System.out.print(this.getSize());
+        //System.out.print(this.getSize());
     } // end actionPerformed()
 
     /**
