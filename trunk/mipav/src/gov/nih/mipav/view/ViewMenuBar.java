1b4a44df2b54eb970b7417d902fa87c2cbd7b571
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-05-10 17:34:02 +0000

    Convex hull 2D added to VOI menu.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@11721 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewMenuBar.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewMenuBar.java
@@ -871,6 +871,8 @@ public class ViewMenuBar {
                                 ViewMenuBar.separator,
                                 menuBuilder.buildMenuItem("Bounding VOIs", "BoundingVOIs", 0, null, true),
                                 ViewMenuBar.separator,
+                                menuBuilder.buildMenuItem("Convex hull 2D", "ConvexHull2D", 0, null, true),
+                                ViewMenuBar.separator,
                                 menuBuilder.makeMenu("Prostate VOI", true,
                                         new JMenuItem[] {
                                                 menuBuilder.buildMenuItem("Merge VOIs", "ProstateMergedVOIs", 0, null,
