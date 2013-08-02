c44113774eabc882b6e5e6ce8179382231926a7b
ruida@NIH.GOV <ruida@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-08-02 15:19:33 +0000

    During fly-thru, if the camera orientation get from m_spkCamera, rendering screwed near the target point.  So, change back to get the camera orientation from navigationBehavior.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12033 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/VolumeTriPlanarRender.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/VolumeTriPlanarRender.java
@@ -569,9 +569,9 @@ implements GLEventListener, KeyListener, MouseMotionListener,  MouseListener, Na
 		if (isNavigationEnabled) {
 
 			Vector3f cameraLocation = navigationBehavior.getViewPoint();
-			Vector3f cameraDir = m_spkCamera.GetDVector(); 
-			Vector3f cameraUp = m_spkCamera.GetUVector();
-			Vector3f cameraRight = m_spkCamera.GetRVector();
+			Vector3f cameraDir = navigationBehavior.getViewDirection();
+			Vector3f cameraUp = navigationBehavior.getViewUp();
+			Vector3f cameraRight = Vector3f.unitCross(cameraDir, cameraUp);
 			m_spkCamera.SetFrame(cameraLocation, cameraDir, cameraUp, cameraRight);
 		
 			float fRMin = m_spkCamera.GetRMin(); 
