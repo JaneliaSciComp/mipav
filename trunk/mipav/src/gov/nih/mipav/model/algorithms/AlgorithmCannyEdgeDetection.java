059a194f466e1c4954f91db68149f174752a3629
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-08-09 18:21:21 +0000

    Okay but could have one more edge-thinning step at the end.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12060 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmCannyEdgeDetection.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmCannyEdgeDetection.java
@@ -10,6 +10,10 @@ import java.io.*;
 
 public class AlgorithmCannyEdgeDetection extends AlgorithmBase {
     
+    // Reference: Digital Image Processing Third Edition by Rafael C. Gonzalez and Richard E. Woods,
+    // pp. 719-725.
+    
+    // Canny said that the ratio of high to low threshold should be two or three to one.
     private double highThreshold = 0.12;
     
     private double lowThreshold = 0.4 * highThreshold;
@@ -59,7 +63,6 @@ public class AlgorithmCannyEdgeDetection extends AlgorithmBase {
         int xDim;
         int yDim;
         int sliceSize;
-        double srcBuffer[];
         AlgorithmGradientMagnitudeSep gradMagAlgo = null;
         float mag[];
         float xMag[];
@@ -71,6 +74,14 @@ public class AlgorithmCannyEdgeDetection extends AlgorithmBase {
         int i;
         double scale;
         double maxMag = 0.0;
+        int x;
+        int y;
+        int yoff;
+        float gN[];
+        byte gNH[];
+        byte gNL[];
+        byte valid[];
+        boolean change;
         
         if (srcImage == null) {
             displayError("Source Image is null");
@@ -121,19 +132,151 @@ public class AlgorithmCannyEdgeDetection extends AlgorithmBase {
         }
         xMag = null;
         yMag = null;
+        gN = new float[sliceSize];
+        for (y = 0; y < yDim; y++)  {
+            yoff = y * xDim;
+            for (x = 0; x < xDim; x++) {
+                i = yoff + x;
+                if (((angle[i] >= -22.5) && (angle[i] < 22.5)) || (angle[i] < -157.5) || (angle[i] >= 157.5)) {
+                    // Horizontal edge
+                    if ((y > 0) && (mag[i - xDim] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else if ((y < yDim-1) && (mag[i + xDim] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else {
+                        gN[i] = mag[i];
+                    }
+                }
+                else if (((angle[i] >= 67.5) && (angle[i] < 112.5)) || ((angle[i] >= -112.5) && (angle[i] < -67.5))) {
+                    // Vertical edge
+                    if ((x > 0) && (mag[i - 1] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else if ((x < xDim-1) && (mag[i+1] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else {
+                        gN[i] = mag[i];
+                    }
+                }
+                else if (((angle[i] >= 22.5) && (angle[i] < 67.5)) || ((angle[i] >= -157.5) && (angle[i] < -112.5))) {
+                    // -45 degree edge
+                    if ((y > 0) && (x > 0) && (mag[i - xDim - 1] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else if ((y < yDim-1) && (x < xDim-1) && (mag[i + xDim + 1] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else {
+                        gN[i] = mag[i];
+                    }
+                }
+                else {
+                    // 45 degree edge
+                    if ((y > 0) && (x < xDim - 1) && (mag[i - xDim + 1] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else if ((x > 0) && (y < yDim - 1) && (mag[i + xDim - 1] > mag[i])) {
+                        gN[i] = 0.0f;
+                    }
+                    else {
+                        gN[i] = mag[i];
+                    }
+                }
+            } // for (x = 0; x < xDim; x++)
+        } // for (y = 0; y < yDim; y++)
+        mag = null;
+        gNH = new byte[sliceSize];
+        gNL = new byte[sliceSize];
+        lowThreshold = lowThreshold * maxMag;
+        highThreshold = highThreshold * maxMag;
         for (i = 0; i < sliceSize; i++) {
-            if (((angle[i] >= -22.5) && (angle[i] < 22.5)) || (angle[i] < -157.5) || (angle[i] >= 157.5)) {
-                // Horizontal edge
+            if (gN[i] >= lowThreshold) {
+                if (gN[i] >= highThreshold) {
+                    gNH[i] = 1;
+                }
+                else {
+                    gNL[i] = 1;
+                }
             }
-            else if (((angle[i] >= 67.5) && (angle[i] < 112.5)) || ((angle[i] >= -112.5) && (angle[i] < -67.5))) {
-                // Vertical edge
-            }
-            else if (((angle[i] >= 22.5) && (angle[i] < 67.5)) || ((angle[i] >= -157.5) && (angle[i] < -112.5))) {
-                // -45 degree edge
-            }
-            else {
-                // 45 degree edge
+        } // for (i = 0; i < sliceSize; i++)
+        valid = new byte[sliceSize];
+        for (y = 0; y < yDim; y++) {
+            yoff = y * xDim;
+            for (x = 0; x < xDim; x++) {
+                i = yoff + x;
+                if (gNH[i] == 1) {
+                    if ((x > 0) && (gNL[i-1] == 1)) {
+                        valid[i-1] = 1;
+                    }
+                    else if ((x < xDim-1) && (gNL[i+1] == 1)) {
+                        valid[i+1] = 1;
+                    }
+                    else if ((y > 0) && (gNL[i-xDim] == 1)) {
+                        valid[i-xDim] = 1;
+                    }
+                    else if ((y < yDim-1) && (gNL[i+xDim] == 1)) {
+                        valid[i+xDim] = 1;
+                    }
+                    else if ((x > 0) && (y > 0) && (gNL[i - xDim - 1] == 1)) {
+                        valid[i-xDim-1] = 1;
+                    }
+                    else if ((x > 0) && (y < yDim - 1) && (gNL[i + xDim - 1] == 1)) {
+                        valid[i + xDim - 1] = 1;
+                    }
+                    else if ((x < xDim-1) && (y > 0) && (gNL[i - xDim + 1] == 1)) {
+                        valid[i - xDim + 1] = 1;
+                    }
+                    else if ((x < xDim -1) && (y < yDim - 1) && (gNL[i + xDim + 1] == 1)) {
+                        valid[i + xDim + 1] = 1;
+                    }
+                } // if (gNH[i] == 1)
+            } // for (x = 0; x < xDim; x++)
+        } // for (y = 0; y < yDim; y++)
+        change = true;
+        while (change) {
+            change = false;
+            for (y = 0; y < yDim; y++) {
+                yoff = y * xDim;
+                for (x = 0; x < xDim; x++) {
+                    i = yoff + x;
+                    if ((valid[i] == 0) && (gNL[i] == 1)) {
+                        if (((x > 0) && (valid[i-1] == 1)) ||
+                            ((x < xDim - 1) && (valid[i+1] == 1)) ||
+                            ((y > 0) && (valid[i-xDim] == 1)) ||
+                            ((y < yDim-1) && (valid[i+xDim] == 1)) ||
+                            ((x > 0) && (y > 0) && (valid[i - xDim - 1] == 1)) ||
+                            ((x > 0) && (y < yDim -1) && (valid[i + xDim - 1] == 1)) ||
+                            ((x < xDim - 1) && (y > 0) && (valid[i - xDim + 1] == 1)) ||
+                            ((x < xDim -1) && (y < yDim - 1) && (valid[i + xDim + 1] == 1))) {
+                            valid[i] = 1;
+                            change = true;
+                        }
+                    }
+                } // for (x = 0; x < xDim; x++)
+            } // for (y = 0; y < yDim; y++)
+        } // while (change)
+        gNL = null;
+        
+        for (i = 0; i < sliceSize; i++) {
+            if (valid[i] == 1) {
+                gNH[i] = 1;
             }
         }
+        valid = null;
+        
+        try {
+            destImage.importData(0, gNH, true);
+        }
+        catch (IOException e) {
+            MipavUtil.displayError("IOException " + e + " on destImage.importData(0, gNH, true)");
+            setCompleted(false);
+            return;
+        }
+        
+        setCompleted(true);
+        return;
     }
 }
