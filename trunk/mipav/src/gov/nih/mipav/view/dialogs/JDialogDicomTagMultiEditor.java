d75e8fb5476a63de2998b121822f90ecc19fb4cd
jsenseney <jsenseney@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-08-15 16:16:48 +0000

    implemented scripting functionality for tag editor
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12106 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogDicomTagMultiEditor.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogDicomTagMultiEditor.java
@@ -4,6 +4,8 @@ import gov.nih.mipav.model.file.FileDicom;
 import gov.nih.mipav.model.file.FileDicomKey;
 import gov.nih.mipav.model.file.FileDicomTag;
 import gov.nih.mipav.model.file.FileInfoDicom;
+import gov.nih.mipav.model.scripting.ParserException;
+import gov.nih.mipav.model.scripting.parameters.ParameterFactory;
 import gov.nih.mipav.view.MipavUtil;
 
 import java.awt.event.ActionEvent;
@@ -15,6 +17,8 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import javax.swing.table.DefaultTableModel;
+
 public class JDialogDicomTagMultiEditor extends JDialogDicomTagSelector {
 
 	protected File file;
@@ -24,6 +28,10 @@ public class JDialogDicomTagMultiEditor extends JDialogDicomTagSelector {
 	protected static final String SAVE = "Process";
 	
 	private boolean processed = false;
+
+	private FileDicomKey[] keyArray;
+
+	private FileDicomTag[] tagArray;
 	
 	public JDialogDicomTagMultiEditor(Hashtable<FileDicomKey, FileDicomTag> tagList, JDialogBase parent, 
 			boolean isStandalone, File file, FileInfoDicom fileInfo) {
@@ -39,41 +47,85 @@ public class JDialogDicomTagMultiEditor extends JDialogDicomTagSelector {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource().equals(closeButton) && !processed) {
-			try {
-				FileDicom writeDicom = new FileDicom(file.getAbsolutePath());
-				int length = tagsTable.getRowCount();
-				FileDicomKey[] keyArray = new FileDicomKey[length];
-				FileDicomTag[] tagArray = new FileDicomTag[length];
-				Set<Entry<FileDicomKey, FileDicomTag>> entrySet = tagList.entrySet();
-				for(int i=0; i<length; i++) {
-					FileDicomKey key = new FileDicomKey(tagsTable.getValueAt(i, 0).toString());
-					Iterator<Entry<FileDicomKey, FileDicomTag>> keyItr = entrySet.iterator();
-					Entry<FileDicomKey, FileDicomTag> entry = null;
-		keySearch:	while(keyItr.hasNext()) {
-						entry = keyItr.next();
-						if(entry.getKey().equals(key)) {
-							keyArray[i] = key;
-							tagArray[i] = entry.getValue();
-							tagArray[i].setValue(tagsTable.getValueAt(i, 2));
-							break keySearch;
-						}
-					}
-				}
-				
-				RandomAccessFile raFile = new RandomAccessFile(file, "rw");
-				
-				System.out.println("Here again");
-				writeDicom.writeTags(raFile, fileInfo, keyArray, tagArray);
-				
-				this.dispose();
-				
-				processed = true;
-				MipavUtil.displayInfo("Tags processed successfully for image "+file.getAbsolutePath());
-			} catch(IOException ex) {
-				System.err.println("Unable to write to file: "+file.getAbsolutePath());
-			}
+			callAlgorithm();
 		} else {
 			super.actionPerformed(e);
+		}
+	}
+
+	@Override
+	protected void callAlgorithm() {
+		try {
+			FileDicom writeDicom = new FileDicom(file.getAbsolutePath());
+			int length = tagsTable.getRowCount();
+			keyArray = new FileDicomKey[length];
+			tagArray = new FileDicomTag[length];
+			Set<Entry<FileDicomKey, FileDicomTag>> entrySet = tagList.entrySet();
+			for(int i=0; i<length; i++) {
+				FileDicomKey key = new FileDicomKey(tagsTable.getValueAt(i, 0).toString());
+				Iterator<Entry<FileDicomKey, FileDicomTag>> keyItr = entrySet.iterator();
+				Entry<FileDicomKey, FileDicomTag> entry = null;
+	keySearch:	while(keyItr.hasNext()) {
+					entry = keyItr.next();
+					if(entry.getKey().equals(key)) {
+						keyArray[i] = key;
+						tagArray[i] = entry.getValue();
+						tagArray[i].setValue(tagsTable.getValueAt(i, 2));
+						break keySearch;
+					}
+				}
+			}
+			
+			RandomAccessFile raFile = new RandomAccessFile(file, "rw");
+			
+			System.out.println("Here again");
+			writeDicom.writeTags(raFile, fileInfo, keyArray, tagArray);
+			
+			this.dispose();
+			
+			processed = true;
+			if(!isScriptRunning()) {
+				MipavUtil.displayInfo("Tags processed successfully for image "+file.getAbsolutePath());
+				insertScriptLine();
+			}
+		} catch(IOException ex) {
+			System.err.println("Unable to write to file: "+file.getAbsolutePath());
+		}
+	}
+
+	@Override
+	protected void setGUIFromParams() {
+		int numKey = 0;
+		Iterator<String> itr = scriptParameters.getParams().keySet().iterator();
+		String name = null;
+		String keyName = "TagKey";
+		while(itr.hasNext()) {
+			name = itr.next();
+			if(name.contains(keyName)) {
+				numKey++;
+			}
+		}
+		
+		file = new File(scriptParameters.getParams().getString("DicomFile"));
+		DefaultTableModel model = ((DefaultTableModel)tagsTable.getModel());
+		for(int i=0; i<numKey; i++) {
+			model.addRow(new String[]{
+				scriptParameters.getParams().getString("TagKey"+i),
+				" ",
+				scriptParameters.getParams().getString("TagValue"+i)
+			});
+		}
+	}
+
+	@Override
+	protected void storeParamsFromGUI() throws ParserException {
+		super.setGUIFromParams();
+		scriptParameters.getParams().put(ParameterFactory.newParameter("DicomFile", file.getAbsolutePath()));
+		for(int i=0; i<keyArray.length; i++) {
+			scriptParameters.getParams().put(ParameterFactory.newParameter("TagKey"+i, keyArray[i].toString()));
+		}
+		for(int i=0; i<tagArray.length; i++) {
+			scriptParameters.getParams().put(ParameterFactory.newParameter("TagValue"+i, tagArray[i].getValue(true)));
 		}
 	}
 
