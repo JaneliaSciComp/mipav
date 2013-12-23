b06fc936d7a0c0c1d6175254a5d63dcea0767a79
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2013-12-23 16:33:56 +0000

    Changed string-based switch block to if/else.  String-based switching is a 1.7+ feature.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12416 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmCompleteCatalog.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmCompleteCatalog.java
@@ -1,3 +1,5 @@
+import gov.nih.mipav.model.algorithms.AlgorithmBase;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -5,145 +7,155 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Vector;
 
-import gov.nih.mipav.model.algorithms.AlgorithmBase;
 
+public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
 
-public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase{
-	
-	private File catalogFile;
-	private FileWriter csv;
-	private Vector<String> catalogID;
-	private Vector<String> visit;
-	private Vector<Integer> type;
-	private int[] complete;
-	private int remove;
-	
-	public PlugInAlgorithmCompleteCatalog(File file, FileWriter out, int lines){
-		super();
-		catalogFile = file;
-		csv = out;
-		remove = lines;
-	}
+    private final File catalogFile;
 
-	@Override
-	public void runAlgorithm() {
-		// TODO Auto-generated method stub
-		complete = new int[5]; //RNA, Plasma, Serum, CSF, Blood
-		String[] types = {"RNA", "Plasma", "Serum", "CSF", "Blood"};
-		String GUID;
-		Integer typeNum;
-		
-		readCSV();
-		while(catalogID.size()>0){
-			GUID = catalogID.get(0);
-			typeNum = type.get(0);
-			if (compare(GUID, typeNum))
-				complete[typeNum.intValue()]++;
-		}
-		
-		try {
-			csv.append("Sample Type,Number of Complete Sets\n");
-			for(int i=0;i<5;i++){
-				csv.append(types[i]+","+String.valueOf(complete[i])+"\n");
-			}
-			csv.flush();
-			csv.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private boolean compare(String GUID, Integer which){
-		
-		//First element is not removed already from each vector
-		
-		int index;
-		int max;
-		int month;
-		int num;
-		String[] visitType;
-		Vector<String> IDvisit = new Vector<String>();
-		IDvisit.add(visit.get(0));
-		while(catalogID.contains(GUID)){
-			index = catalogID.indexOf(GUID);
-			while(!type.get(index).equals(which)){
-				index = catalogID.indexOf(GUID,index+1);
-				if(index==-1) break;
-			}
-			if(index==-1) break;
-			catalogID.remove(index);
-			if(!IDvisit.contains(visit.get(index))){
-				IDvisit.add(visit.get(index));
-			}
-			visit.remove(index);
-			type.remove(index);
-			
-		}
-		
-		max = 0;
-		for(int i=0;i<IDvisit.size();i++){
-			visitType = IDvisit.get(i).split(" ");
-			if(visitType.length == 2){
-				month = Integer.parseInt(visitType[0]);
-				if(month > max) max = month;
-			}
-		}
-		num = max/6 + 1;
-		return (num == IDvisit.size());
-	}
-	
-	private void readCSV(){
+    private final FileWriter csv;
 
-		String[] lineArray;
-		String typeStr;
-		int typeNum;
-		try {
-			BufferedReader input =  new BufferedReader(new FileReader(catalogFile));
-			try {
-				String line = null; 
-				catalogID = new Vector<String>();
-				visit = new Vector<String>();
-				type = new Vector<Integer>();
-				
-				for(int i=0; i<remove; i++){
-					input.readLine();
-				}
+    private Vector<String> catalogID;
 
-				while (( line = input.readLine()) != null){
-					lineArray = line.split(",");
-					if(lineArray.length != 0){
-						
-						typeStr = lineArray[6].trim().toLowerCase();
-						switch(typeStr){
-						case "rna": 	typeNum = 0;
-										break;
-						case "plasma":	typeNum = 1;
-										break;
-						case "serum": 	typeNum = 2;
-										break;
-						case "csf":		typeNum = 3;
-										break;
-						case "blood":	typeNum = 4;
-										break;
-						default: 		typeNum = -1;
-										break;
-						}
-						if(typeNum != -1){
-							type.add(new Integer(typeNum));
-							catalogID.add(lineArray[1].trim());
-							visit.add(lineArray[2].trim());
-						}
-					}
-				}
-			}
-			finally {
-				input.close();
-			}
-		}
-		catch (IOException ex){
-			ex.printStackTrace();
-		}
-	}
+    private Vector<String> visit;
+
+    private Vector<Integer> type;
+
+    private int[] complete;
+
+    private final int remove;
+
+    public PlugInAlgorithmCompleteCatalog(final File file, final FileWriter out, final int lines) {
+        super();
+        catalogFile = file;
+        csv = out;
+        remove = lines;
+    }
+
+    @Override
+    public void runAlgorithm() {
+        // TODO Auto-generated method stub
+        complete = new int[5]; // RNA, Plasma, Serum, CSF, Blood
+        final String[] types = {"RNA", "Plasma", "Serum", "CSF", "Blood"};
+        String GUID;
+        Integer typeNum;
+
+        readCSV();
+        while (catalogID.size() > 0) {
+            GUID = catalogID.get(0);
+            typeNum = type.get(0);
+            if (compare(GUID, typeNum)) {
+                complete[typeNum.intValue()]++;
+            }
+        }
+
+        try {
+            csv.append("Sample Type,Number of Complete Sets\n");
+            for (int i = 0; i < 5; i++) {
+                csv.append(types[i] + "," + String.valueOf(complete[i]) + "\n");
+            }
+            csv.flush();
+            csv.close();
+        } catch (final IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    private boolean compare(final String GUID, final Integer which) {
+
+        // First element is not removed already from each vector
+
+        int index;
+        int max;
+        int month;
+        int num;
+        String[] visitType;
+        final Vector<String> IDvisit = new Vector<String>();
+        IDvisit.add(visit.get(0));
+        while (catalogID.contains(GUID)) {
+            index = catalogID.indexOf(GUID);
+            while ( !type.get(index).equals(which)) {
+                index = catalogID.indexOf(GUID, index + 1);
+                if (index == -1) {
+                    break;
+                }
+            }
+            if (index == -1) {
+                break;
+            }
+            catalogID.remove(index);
+            if ( !IDvisit.contains(visit.get(index))) {
+                IDvisit.add(visit.get(index));
+            }
+            visit.remove(index);
+            type.remove(index);
+
+        }
+
+        max = 0;
+        for (int i = 0; i < IDvisit.size(); i++) {
+            visitType = IDvisit.get(i).split(" ");
+            if (visitType.length == 2) {
+                month = Integer.parseInt(visitType[0]);
+                if (month > max) {
+                    max = month;
+                }
+            }
+        }
+        num = max / 6 + 1;
+        return (num == IDvisit.size());
+    }
+
+    private void readCSV() {
+
+        String[] lineArray;
+        String typeStr;
+        int typeNum;
+        try {
+            final BufferedReader input = new BufferedReader(new FileReader(catalogFile));
+            try {
+                String line = null;
+                catalogID = new Vector<String>();
+                visit = new Vector<String>();
+                type = new Vector<Integer>();
+
+                for (int i = 0; i < remove; i++) {
+                    input.readLine();
+                }
+
+                while ( (line = input.readLine()) != null) {
+                    lineArray = line.split(",");
+                    if (lineArray.length != 0) {
+
+                        typeStr = lineArray[6].trim().toLowerCase();
+
+                        if (typeStr.equals("rna")) {
+                            typeNum = 0;
+                        } else if (typeStr.equals("plasma")) {
+                            typeNum = 1;
+                        } else if (typeStr.equals("serum")) {
+                            typeNum = 2;
+                        } else if (typeStr.equals("csf")) {
+                            typeNum = 3;
+                        } else if (typeStr.equals("blood")) {
+                            typeNum = 4;
+                        } else {
+                            typeNum = -1;
+                        }
+
+                        if (typeNum != -1) {
+                            type.add(new Integer(typeNum));
+                            catalogID.add(lineArray[1].trim());
+                            visit.add(lineArray[2].trim());
+                        }
+                    }
+                }
+            } finally {
+                input.close();
+            }
+        } catch (final IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
