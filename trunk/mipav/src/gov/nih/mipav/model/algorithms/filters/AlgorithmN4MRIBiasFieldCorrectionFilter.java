2d108f8334a086da91b92828c20324b89dd2f7dc
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-03-25 21:28:37 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12555 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmN4MRIBiasFieldCorrectionFilter.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmN4MRIBiasFieldCorrectionFilter.java
@@ -2,13 +2,15 @@
 	
 	
 	import gov.nih.mipav.model.algorithms.AlgorithmBase;
-	import gov.nih.mipav.model.algorithms.AlgorithmConvolver;
-	import gov.nih.mipav.model.algorithms.AlgorithmInterface;
-	import gov.nih.mipav.model.algorithms.GenerateGaussian;
-	import gov.nih.mipav.model.structures.ModelImage;
+import gov.nih.mipav.model.algorithms.AlgorithmConvolver;
+import gov.nih.mipav.model.algorithms.AlgorithmInterface;
+import gov.nih.mipav.model.algorithms.GenerateGaussian;
+import gov.nih.mipav.model.structures.ModelImage;
 	
+
 	import java.io.IOException;
-	import gov.nih.mipav.view.ViewJProgressBar;
+
+import gov.nih.mipav.view.ViewJProgressBar;
 	
 	
 	/**
@@ -31,6 +33,12 @@
 	    private ModelImage targetImage = null;
 	    
 	    private double imageMax;
+	    
+	    private int numberOfHistogramBins = 200;
+	    
+	    private double WeinerFilterNoise = 0.01;
+	    
+	    private double biasFieldFullWidthAtHalfMaximum = 0.15;
 	    
 	    private int maximumNumberOfLevels = 1;
 	    
@@ -188,6 +196,129 @@
     
     private double[]sharpen(double[] unsharpened) {
     	double sharpened[] = new double[unsharpened.length];
+    	// Build the histogram for the uncorrected image.  Store copy
+    	// in a vnl_vector to utilize utilize vnl FFT routines.  Note that variables
+    	// in real space are denoted by a single uppercase letter whereas their
+    	// frequency counterparts are indicated by a trailing lowercase 'f'.
+    	double binMaximum = -Double.MAX_VALUE;
+    	double binMinimum = Double.MAX_VALUE;
+    	double pixel;
+    	int i;
+    	double histogramSlope;
+    	double H[] = new double[numberOfHistogramBins];
+    	double cidx;
+    	int idx;
+    	double offset;
+    	double exponent;
+    	int paddedHistogramSize;
+    	int histogramOffset;
+    	double VR[];
+    	double VI[];
+    	int n;
+    	FFTUtility fft;
+    	double VfR[];
+    	double VfI[];
+    	double scaledFWHM;
+    	double expFactor;
+    	double scaleFactor;
+    	double FR[];
+    	double FI[];
+    	int halfSize;
+    	double FfR[];
+    	double FfI[];
+    	double GfR[];
+    	double GfI[];
+    	double cR;
+    	double cI;
+    	
+    	for (i = 0; i < length; i++) {
+    		if (entireImage || mask.get(i)) {
+    			pixel = unsharpened[i];
+    			if (pixel > binMaximum) {
+    				binMaximum = pixel;
+    			}
+    			if (pixel < binMinimum) {
+    				binMinimum = pixel;
+    			}
+    		} // if (entireImage || (mask.get(i))
+    	} // for (i = 0; i < length; i++)
+    	
+    	histogramSlope = (binMaximum - binMinimum)/(double)(numberOfHistogramBins - 1);
+    	// Create the intensity profile (within the masked region, if applicable)
+    	// using a triangular parzen windowing scheme
+    	for (i = 0; i < length; i++) {
+    		if (entireImage || mask.get(i)) {
+    			pixel = unsharpened[i];
+    			cidx = (pixel - binMinimum)/histogramSlope;
+    			idx = (int)Math.floor(cidx);
+    			offset = cidx - idx;
+    			
+    			if (offset == 0.0) {
+    				H[idx] += 1.0;
+    			}
+    			else if (idx < numberOfHistogramBins - 1) {
+    				H[idx] += 1.0 - offset;
+    				H[idx+1] += offset;
+    			}
+    		} // if (entireImage || (mask.get(i))
+    	} // for (i = 0; i < length; i++)	
+    	
+    	// Determine information about thne intensity histogram and zero-pad
+    	// histogram to a power of 2.
+    	
+    	exponent = Math.ceil(Math.log(numberOfHistogramBins)/Math.log(2.0)) + 1.0;
+    	paddedHistogramSize = (int)(Math.pow(2.0, exponent) + 0.5);
+    	histogramOffset = (int)(0.5 * (paddedHistogramSize - numberOfHistogramBins));
+    	VR = new double[paddedHistogramSize];
+    	VI = new double[paddedHistogramSize];
+    	VfR = new double[paddedHistogramSize];
+    	VfI = new double[paddedHistogramSize];
+    	for (n = 0; n < numberOfHistogramBins; n++) {
+    		VR[n + histogramOffset] = H[n];
+    		VfR[n + histogramOffset] = H[n];
+    	}
+    	
+    	// Instantiate the 1d FFT routine
+    	// -1 for forward transform
+    	fft = new FFTUtility(VfR, VfI, 1, paddedHistogramSize, 1, -1, FFTUtility.FFT);
+        fft.run();
+        fft.finalize();
+        fft = null;
+        
+        // Create the Gaussian filter
+        scaledFWHM = biasFieldFullWidthAtHalfMaximum/histogramSlope;
+        expFactor = 4.0 * Math.log(2.0)/(scaledFWHM * scaledFWHM);
+        scaleFactor = 2.0 * Math.sqrt(Math.log(2.0)/Math.PI)/scaledFWHM;
+        
+        FR = new double[paddedHistogramSize];
+        FI = new double[paddedHistogramSize];
+        FfR = new double[paddedHistogramSize];
+        FfI = new double[paddedHistogramSize];
+        FR[0] = scaleFactor;
+        halfSize = (int)(0.5 * paddedHistogramSize);
+        for (n = 1; n <= halfSize; n++) {
+        	FR[n] = FR[paddedHistogramSize - n] = scaleFactor * Math.exp(-n * n * expFactor);
+        }
+        if (paddedHistogramSize % 2 == 0) {
+            FR[halfSize] = scaleFactor * Math.exp(-0.25 * paddedHistogramSize * paddedHistogramSize * expFactor);	
+        }
+        for (n = 0; n < paddedHistogramSize; n++) {
+        	FfR[n] = FR[n];
+        }
+        
+    	// -1 for forward transform
+    	fft = new FFTUtility(FfR, FfI, 1, paddedHistogramSize, 1, -1, FFTUtility.FFT);
+        fft.run();
+        fft.finalize();
+        fft = null;
+        
+        // Create the Weiner deconvolution filter
+        GfR = new double[paddedHistogramSize];
+        GfI = new double[paddedHistogramSize];
+        for (n = 0; n < paddedHistogramSize; n++) {
+        	cR = FfR[n];
+        	cI = - FfI[n];
+        }
     	return sharpened;
     }
     
