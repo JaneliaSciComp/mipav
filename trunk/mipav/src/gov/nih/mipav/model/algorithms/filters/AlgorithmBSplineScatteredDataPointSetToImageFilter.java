4fdaed5f47976f20a638f0efdbfa35966f0012dd
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-04-08 20:30:01 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12587 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmBSplineScatteredDataPointSetToImageFilter.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmBSplineScatteredDataPointSetToImageFilter.java
@@ -15,10 +15,12 @@ import gov.nih.mipav.model.structures.ModelImage;
 
 
 
+
 	import java.io.IOException;
 import java.util.Vector;
 
 import WildMagic.LibFoundation.Mathematics.Vector3d;
+import gov.nih.mipav.view.MipavUtil;
 import gov.nih.mipav.view.ViewJProgressBar;
 	
 	
@@ -47,9 +49,8 @@ import gov.nih.mipav.view.ViewJProgressBar;
 
   
 	 */
-	public class AlgorithmBSplineScatteredDataPointSetToImageFilter extends AlgorithmBase {
+	public class AlgorithmBSplineScatteredDataPointSetToImageFilter extends AlgorithmPointSetToImageFilter {
 		
-		private int nDims;
 		private int splineOrder[];
 		private int numberOfControlPoints[];
 		private CoxDeBoorBSplineKernelFunction kernel[];
@@ -66,6 +67,30 @@ import gov.nih.mipav.view.ViewJProgressBar;
 		ModelImage phiLattice;
 		private boolean constructPsiLattice;
 		ModelImage psiLattice;
+		private double[] inputPointdData[];
+		private double[] outputPointData[];
+		private double[] pointWeights[];
+		private boolean usePointWeights;
+		// Machine epsilon is the smallest positive epsilon such that
+        // (1.0 + epsilon) != 1.0.
+        // epsilon = 2**(1 - doubleDigits) = 2**(1 - 53) = 2**(-52)
+        // epsilon = 2.2204460e-16
+        // epsilon is called the largest relative spacing
+        //epsilon = 1.0;
+        //neweps = 1.0;
+
+        //while (true) {
+
+        //     if (1.0 == (1.0 + neweps)) {
+        //         break;
+        //     } else {
+        //         epsilon = neweps;
+        //        neweps = neweps / 2.0;
+        //     }
+        // } // while(true)
+		private double epsilon = 2.2204460e-16;
+		private double splineEpsilon;
+		private boolean isFittingComplete;
 	
 		/**
 	     * Constructor which sets the source and destination images
@@ -80,7 +105,7 @@ import gov.nih.mipav.view.ViewJProgressBar;
 	    }
 	    
 	    public AlgorithmBSplineScatteredDataPointSetToImageFilter(int nDims) {
-	        this.nDims = nDims;
+	        super(nDims);
 	        splineOrder = new int[nDims];
 	        int i;
 	        for (i = 0; i < nDims; i++) {
@@ -107,10 +132,15 @@ import gov.nih.mipav.view.ViewJProgressBar;
 	        	numberOfLevels[i] = 1;
 	        }
 	        maximumNumberOfLevels = 1;
+	        // itk only needs data type and extents to construct an image, but MIPAV needs extents[] as well.
 	        constructPhiLattice = false;
 	        phiLattice = null;
 	        constructPsiLattice = true;
 	        psiLattice = null;
+	        // Create inputPointData[] and outputPointData[]
+	        usePointWeights = false;
+	        splineEpsilon = epsilon;
+	        isFittingComplete = false;
 	    }
 	    
 	  //~ Methods --------------------------------------------------------------------------------------------------------
@@ -122,6 +152,94 @@ import gov.nih.mipav.view.ViewJProgressBar;
 	        destImage = null;
 	        srcImage = null;
 	        super.finalize();
+	    }
+	    
+	    public void setGenerateOutputImage(boolean generateOutputImage) {
+	    	this.generateOutputImage = generateOutputImage;
+	    }
+	    
+	    public void setNumberOfLevels(int nLevels) {
+	    	for (int i = 0; i < nDims; i++) {
+	    		this.numberOfLevels[i] = nLevels;
+	    	}
+	    	setNumberOfLevels(numberOfLevels);
+	    }
+	    
+	    public void setNumberOfLevels(int[] numberOfLevels) {
+	    	for (int i = 0; i < nDims; i++) {
+	    		this.numberOfLevels[i] = numberOfLevels[i];
+	    		if (numberOfLevels[i] > maximumNumberOfLevels) {
+	    			maximumNumberOfLevels = numberOfLevels[i];
+	    		}
+	    	}
+	    	if (maximumNumberOfLevels > 1) {
+    			doMultiLevel = true;
+    		}
+    		else {
+    			doMultiLevel = false;
+    		}
+	    	setSplineOrder(splineOrder);
+	    }
+	    
+	    public void setSplineOrder(int sOrder) {
+	    	for (int i = 0; i < nDims; i++) {
+	    		this.splineOrder[i] = sOrder;
+	    	}
+	    	setSplineOrder(splineOrder);
+	    	for (int i = 0; i < nDims; i++ ) {
+	            if (splineOrder[i] == 0 ) {
+	                MipavUtil.displayError("The spline order in each dimension must be greater than 0");
+	                return;
+	            }
+	            kernel[i] = new CoxDeBoorBSplineKernelFunction(splineOrder[i]);
+	            if (doMultiLevel) {
+	                double C[][] = kernel[i].getShapeFunctionsInZeroToOneInterval();	
+	            } // if (doMultiLevel)
+	    	} // for (int i = 0; i < nDims; i++)
+
+	       
+	        /*if( this->m_DoMultilevel )
+	          {
+	          typename KernelType::MatrixType C;
+	          C = this->m_Kernel[i]->GetShapeFunctionsInZeroToOneInterval();
+
+	          vnl_matrix<RealType> R;
+	          vnl_matrix<RealType> S;
+	          R.set_size( C.rows(), C.cols() );
+	          S.set_size( C.rows(), C.cols() );
+	          for( unsigned int j = 0; j < C.rows(); j++ )
+	            {
+	            for( unsigned int k = 0; k < C.cols(); k++ )
+	              {
+	              R(j, k) = S(j, k) = static_cast<RealType>( C(j, k) );
+	              }
+	            }
+	          for( unsigned int j = 0; j < C.cols(); j++ )
+	            {
+	            RealType c = vcl_pow( static_cast<RealType>( 2.0 ),
+	              static_cast<RealType>( C.cols() ) - j - 1 );
+
+	            for( unsigned int k = 0; k < C.rows(); k++ )
+	              {
+	              R(k, j) *= c;
+	              }
+	            }
+	          R = R.transpose();
+	          R.flipud();
+	          S = S.transpose();
+	          S.flipud();
+
+	          this->m_RefinedLatticeCoefficients[i] =
+	            ( vnl_svd<RealType>( R ).solve( S ) ).extract( 2, S.cols() );
+	          }
+	        }*/
+
+	    }
+	    
+	    public void setSplineOrder(int[] splineOrder) {
+	    	for (int i = 0; i < nDims; i++) {
+	    		this.splineOrder[i] = splineOrder[i];
+	    	}
 	    }
 
 	    /**
