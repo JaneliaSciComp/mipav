21e8d533e74aebef35476aa9cd15456f46cb34b4
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-04-09 20:36:37 +0000

    public Matrix extract(int r, int c) extracts a matrix of size r x c starting at the top left.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12591 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/Jama/Matrix.java
+++ trunk/mipav/src/Jama/Matrix.java
@@ -476,11 +476,23 @@ public class Matrix implements Cloneable, java.io.Serializable {
    /** Reverse order of rows */
    public Matrix flipud() {
 	   Matrix X = new Matrix(m, n);
-	   double C[][] = X.getArray();
+	   double[][] C = X.getArray();
 	   for (int i = 0; i < m; i++) {
 	       for (int j = 0; j < n; j++) {
 	    	   C[m-1-i][j] = A[i][j];
 	       }
+	   }
+	   return X;
+   }
+   
+   /** Extract a matrix of size r,c starting at the top left */
+   public Matrix extract(int r, int c) {
+	   Matrix X = new Matrix(r, c);
+	   double[][] C = X.getArray();
+	   for (int i = 0; i < r; i++) {
+		   for (int j = 0; j < c; j++) {
+			   C[i][j] = A[i][j];
+		   }
 	   }
 	   return X;
    }
