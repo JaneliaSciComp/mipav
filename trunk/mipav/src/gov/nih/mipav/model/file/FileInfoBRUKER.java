9809535ed7343d2bb33836b2fb0338a7b522acae
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-04-14 22:08:42 +0000

    Obtain more variables from FileBRUKER.readMethod.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12611 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileInfoBRUKER.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileInfoBRUKER.java
@@ -30,6 +30,34 @@ public class FileInfoBRUKER extends FileInfoBase {
 
     /** The inversion time of an MR scan. */
     private double inversionTime;
+    
+    private String method = null;
+    
+    private double effectiveSpectralBandwidth = Double.NaN;
+    
+    private double echoTime = Double.NaN;
+    
+    private int numberOfSegments = -1;
+    
+    private double repetitionTime = Double.NaN;
+    
+    private double delayBetweenVolumes = Double.NaN;
+    
+    private int numberOfAverages = -1;
+    
+    private int numberOfRepetitions = -1;
+    
+    private String scanTime = null;
+    
+    private String deriveGains = null;
+    
+    private String diffusionPreparation = null;
+    
+    private double usedSliceThickness = Double.NaN;
+    
+    private String showAllParameters = null;
+    
+    private String refocusingPulseType = null;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
@@ -68,6 +96,71 @@ public class FileInfoBRUKER extends FileInfoBase {
         
         if(inversionTime != 0) {
             dialog.append("Inversion time:\t\t" + inversionTime + " ms\n");
+        }
+        
+        if (method != null) {
+        	dialog.append("Method = " + method + "\n");
+        }
+        
+        if (!Double.isNaN(effectiveSpectralBandwidth)) {
+        	dialog.append("Effective bandwidth of data sampling \n\tduring the frequency encoding period:\t" +
+               effectiveSpectralBandwidth + "\n");
+        }
+        
+        if (!Double.isNaN(echoTime)) {
+            dialog.append("Delay between the effective centre of the excitation pulse"
+                                         + "\n\t(depends on pulse rephasing properties) and "
+                                         + "\n\tthe acquisition of the k-space centre:\t "
+                        		         + echoTime + "\n");	
+        }
+        
+        if (numberOfSegments != -1) {
+        	dialog.append("Number of segments in k-space:\t " + numberOfSegments + "\n");
+        }
+        
+        if (!Double.isNaN(repetitionTime)) {
+        	dialog.append("Delay between corresponding slices in consecutive volumes:\t " + repetitionTime + "\n");
+        }
+        
+        if (!Double.isNaN(delayBetweenVolumes)) {
+        	dialog.append("Delay between consecutive groups of slices (volume) " +
+                          "\n\twhen repetitions > 1:\t " + delayBetweenVolumes + "\n");
+        }
+        
+        if (numberOfAverages != -1) {
+        	dialog.append("Number of accumulations which are averaged to increase\n\tthe signal-to-noise ratio of the spectra:\t " +
+                numberOfAverages + "\n");
+        }
+        
+        if (numberOfRepetitions != -1) {
+        	dialog.append("Number of repetitions of experiments:\t " + numberOfRepetitions + "\n");
+        }
+        
+        if (scanTime != null) {
+        	dialog.append("Total duration of the experiment:\t " + scanTime + "\n");
+        }
+        
+        if (deriveGains != null) {
+        	dialog.append(deriveGains + "\n");
+        }
+        
+        if (diffusionPreparation != null) {
+        	dialog.append(diffusionPreparation + "\n");
+        }
+        
+        if (!Double.isNaN(usedSliceThickness)) {
+        	dialog.append("The slice thickness used to calculate the slice grFdients = " + usedSliceThickness + 
+                                          "\n\tFor methods providing a Bandwidth Scaling factor" + 
+                        		          "\n\tdifferent from 100% this value differs from PVM_SliceThick." +
+                        		          "\n");
+        }
+        
+        if (showAllParameters != null) {
+        	dialog.append(showAllParameters + "\n");
+        }
+        
+        if (refocusingPulseType != null) {
+        	dialog.append("Refocusing pulse type:\t " + refocusingPulseType + "\n");
         }
     }
 
@@ -132,5 +225,105 @@ public class FileInfoBRUKER extends FileInfoBase {
      */
     public void setInversionTime(double inversionTime) {
         this.inversionTime = inversionTime;        
+    }
+    
+    public void setMethod(String method) {
+    	this.method = method;
+    }
+    
+    /**
+     * 
+     * @param effectiveSpectralBandwidth
+     */
+    public void setEffectiveSpectralBandwidth(double effectiveSpectralBandwidth) {
+    	this.effectiveSpectralBandwidth = effectiveSpectralBandwidth;
+    }
+    
+    /**
+     * 
+     * @param echoTime
+     */
+    public void setEchoTime(double echoTime) {
+    	this.echoTime = echoTime;
+    }
+    
+    public void setNumberOfSegments(int numberOfSegments) {
+    	this.numberOfSegments = numberOfSegments;
+    }
+    
+    /**
+     * 
+     * @param repetitionTime
+     */
+    public void setRepetitionTime(double repetitionTime) {
+    	this.repetitionTime = repetitionTime;
+    }
+    
+    /**
+     * 
+     * @param delayBetweenVolumes
+     */
+    public void setDelayBetweenVolumes(double delayBetweenVolumes) {
+    	this.delayBetweenVolumes = delayBetweenVolumes;
+    }
+    
+    /**
+     * 
+     * @param numberOfAverages
+     */
+    public void setNumberOfAverages(int numberOfAverages) {
+    	this.numberOfAverages = numberOfAverages;
+    }
+    
+    /**
+     * 
+     * @param numberOfRepetitions
+     */
+    public void setNumberOfRepetitions(int numberOfRepetitions) {
+    	this.numberOfRepetitions = numberOfRepetitions;
+    }
+    
+    /**
+     * 
+     * @param scanTime
+     */
+    public void setScanTime(String scanTime) {
+    	this.scanTime = scanTime;
+    }
+    
+    /**
+     * 
+     * @param deriveGains
+     */
+    public void setDeriveGains(String deriveGains) {
+    	this.deriveGains = deriveGains;
+    }
+    
+    /**
+     * 
+     * @param diffusionPreparation
+     */
+    public void setDiffusionPreparation(String diffusionPreparation) {
+    	this.diffusionPreparation = diffusionPreparation;
+    }
+    
+    /**
+     * 
+     * @param usedSliceThickness
+     */
+    public void setUsedSliceThickness(double usedSliceThickness) {
+    	this.usedSliceThickness = usedSliceThickness;
+    }
+    
+    /**
+     * 
+     * @param showAllParameters
+     */
+    public void setShowAllParameters(String showAllParameters) {
+    	this.showAllParameters = showAllParameters;
+    }
+    
+    public void setRefocusingPulseType(String refocusingPulseType) {
+    	this.refocusingPulseType = refocusingPulseType;
     }
 }
