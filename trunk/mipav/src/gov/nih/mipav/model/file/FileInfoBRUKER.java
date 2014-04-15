e9328ab002eba28721188fd688380c1ce0e34267
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-04-15 22:22:05 +0000

    Display information on more PVM parameters from FileBRUKER.readMethod.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12618 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileInfoBRUKER.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileInfoBRUKER.java
@@ -58,6 +58,32 @@ public class FileInfoBRUKER extends FileInfoBase {
     private String showAllParameters = null;
     
     private String refocusingPulseType = null;
+    
+    private double sliceGradientDuration = Double.NaN;
+    
+    private double sliceGradient = Double.NaN;
+    
+    private double sliceGradientLimit = Double.NaN;
+    
+    private double TEsliceSpoilerGradientsDuration = Double.NaN;
+    
+    private double TESliceSpoilerGradientsAmplitude = Double.NaN;
+    
+    private double TESliceSpoilerGradientsLimit = Double.NaN;
+    
+    private double diffusionGradientDuration[] = null;
+    
+    private double diffusionGradientSeparation[] = null;
+    
+    private String directScaledSwitching = null;
+    
+    private String diffusionMeasurementMode = null;
+    
+    private int numberOfDiffusionDirections = -1;
+    
+    private int diffusionExperimentsPerDirection = -1;
+    
+    private int numberOfA0Images = -1;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
@@ -104,14 +130,22 @@ public class FileInfoBRUKER extends FileInfoBase {
         
         if (!Double.isNaN(effectiveSpectralBandwidth)) {
         	dialog.append("Effective bandwidth of data sampling \n\tduring the frequency encoding period:\t" +
-               effectiveSpectralBandwidth + "\n");
+               effectiveSpectralBandwidth +
+               "\n\tThe optimal bandwidth choice is a" + 
+               "\n\ttrade-off between the field strength (to" + 
+		       "\n\tdecrease chemical shift artifacts, increase" +
+               "\n\tbandwidth), the S/N ratio (decrease bandwidth)" +
+		       "\n\tand the minimum possible TE(increase bandwidth).\n");
         }
         
         if (!Double.isNaN(echoTime)) {
-            dialog.append("Delay between the effective centre of the excitation pulse"
+            dialog.append("Delay (abbreviation TE) between the effective centre of the excitation pulse"
                                          + "\n\t(depends on pulse rephasing properties) and "
                                          + "\n\tthe acquisition of the k-space centre:\t "
-                        		         + echoTime + "\n");	
+                        		         + echoTime +
+                        		         "\n\tTE determines the T2 weighting of the spectra. The minimum" +
+                                         "\n\tof TE depends on excitation pulse lengths and the" + 
+                        		         "\n\tduration of spoiler gradients\n");	
         }
         
         if (numberOfSegments != -1) {
@@ -161,6 +195,92 @@ public class FileInfoBRUKER extends FileInfoBase {
         
         if (refocusingPulseType != null) {
         	dialog.append("Refocusing pulse type:\t " + refocusingPulseType + "\n");
+        }
+        
+        if (!Double.isNaN(sliceGradientDuration)) {
+        	dialog.append("Slice gradient duration:\t " + sliceGradientDuration + 
+                          "\n\tIt is possible to modify this duration within the limits of" +
+                          "\n\t[min,min+4*risetime]. The minimum is defined by the actual" + 
+                          "\n\tduration of the RF pulse. With the help of this parameter a" +
+                          "\n\tcertain duration of the slice gradient plateau between the" +
+                          "\n\tgradient switching event of the slice gradient and the start" +
+                          "\n\tof the RF pulse can be specified. By default, the minimum" +
+                          "\n\tduration is set.\n");
+        }
+        
+        if (!Double.isNaN(sliceGradient)) {
+        	dialog.append("Slice gradient:\t " + sliceGradient + "\n");
+        }
+        
+        if (!Double.isNaN(sliceGradientLimit)) {
+        	dialog.append("Slice gradient limit:\t " + sliceGradientLimit + "\n");
+        }
+        
+        if (!Double.isNaN(TEsliceSpoilerGradientsDuration)) {
+        	dialog.append("Duration of the TE slice spoilers gradient:\t " + TEsliceSpoilerGradientsDuration + "\n" +
+        			      "\tAround the refocusing pulse in spin echo or in the echo periods" +
+        			      "\n\tin stimulated echo preparation.  In stimulated echo the" +
+        			      "\n\tduration of the TM spoiler gradient is fixed to be 3 times" +
+        			      "\n\tthe duration of this TE spoiler duration and the gradient" +
+        			      "\n\tamplitude of the TM spoiler is fixed to the value of the TE" +
+        			      "\n\tspoilers. This should prevent the excitation of unwanted" +
+        			      "\n\tcoherences of the other 3-Pulse signals in the stimulated" +
+        			      "\n\techo preparation mode.\n");
+        }
+        
+        if (!Double.isNaN(TESliceSpoilerGradientsAmplitude)) {
+        	dialog.append("Amplitude of the TE slice spoiler gradients:\t " + TESliceSpoilerGradientsAmplitude +
+                          "\n\t(expressed in% of max. gradient power)" +
+        			      "\n\tIn stimulated echo the TM slice spolier gradient is fixed" +
+                          "\n\tto the same value\n");
+        }
+        
+        if (!Double.isNaN(TESliceSpoilerGradientsLimit)) {
+        	dialog.append("TE slice spoiler gradients limit:\t " + TESliceSpoilerGradientsLimit + "\n");
+        }
+        
+        if (diffusionGradientDuration != null) {
+        	dialog.append("Array of duration of gradient pulses of the diffusion experiment:\n");
+        	for (int i = 0; i < diffusionGradientDuration.length; i++) {
+        	    dialog.append("Duration["+i+"]:\t " + diffusionGradientDuration[i] + "\n");	
+        	}
+        }
+        
+        if (diffusionGradientSeparation != null) {
+        	dialog.append("Array of separation of gradient pulses of the diffusion experiment:\n");
+        	for (int i = 0; i < diffusionGradientSeparation.length; i++) {
+        	    dialog.append("Separation["+i+"]:\t " + diffusionGradientSeparation[i] + "\n");	
+        	}
+        }
+        
+        if (directScaledSwitching != null) {
+        	dialog.append(directScaledSwitching + "\n");
+        }
+        
+        if (diffusionMeasurementMode != null) {
+        	dialog.append(diffusionMeasurementMode + "\n");
+        }
+        
+        if (numberOfDiffusionDirections != -1) {
+        	dialog.append("Number of different directions of the diffusion gradients:\t " + 
+                                           numberOfDiffusionDirections + "\n");
+        }
+        
+        if (diffusionExperimentsPerDirection != -1) {
+        	dialog.append("Number of diffusion experiments sequentially performed in each" + 
+                          "\n\tspecified direction:\t " + diffusionExperimentsPerDirection +
+                          "\n\tEach diffusion experiment is characterized by a certain" +
+                          "\n\tdiffusion gradient strength. The diffusion gradient strength" +
+                          "\n\tis varied during the execution of the diffusion loop.\n");
+        }
+        
+        if (numberOfA0Images != -1) {
+        	dialog.append("Number of A0 images = " + numberOfA0Images +
+        			      "\n\tNumber of Images performed without diffusion gradients." +
+        			      "\n\tIn experiments with a large number of high b-values (e.g." +
+                          "\n\thigh number of diffusion directions and 1 b-value per" +
+        			      "\n\tdirection) the A0 reference point might be underestimated" +
+                          "\n\tif set to 1.\n");
         }
     }
 
@@ -323,7 +443,115 @@ public class FileInfoBRUKER extends FileInfoBase {
     	this.showAllParameters = showAllParameters;
     }
     
+    /**
+     * 
+     * @param refocusingPulseType
+     */
     public void setRefocusingPulseType(String refocusingPulseType) {
     	this.refocusingPulseType = refocusingPulseType;
+    }
+    
+    /**
+     * 
+     * @param sliceGradientDuration
+     */
+    public void setSliceGradientDuration(double sliceGradientDuration) {
+    	this.sliceGradientDuration = sliceGradientDuration;
+    }
+    
+    /**
+     * 
+     * @param sliceGradient
+     */
+    public void setSliceGradient(double sliceGradient) {
+    	this.sliceGradient = sliceGradient;
+    }
+    
+    /**
+     * 
+     * @param sliceGradientLimit
+     */
+    public void setSliceGradientLimit(double sliceGradientLimit) {
+    	this.sliceGradientLimit = sliceGradientLimit;
+    }
+    
+    /**
+     * 
+     * @param TEsliceSpoilerGradientsDuration
+     */
+    public void setTESliceSpoilerGradientsDuration(double TEsliceSpoilerGradientsDuration) {
+    	this.TEsliceSpoilerGradientsDuration = TEsliceSpoilerGradientsDuration;
+    }
+    
+    /**
+     * 
+     * @param TEsliceSpoilerGradientsAmplitude
+     */
+    public void setTESliceSpoilerGradientsAmplitude(double TEsliceSpoilerGradientsAmplitude) {
+    	this.TESliceSpoilerGradientsAmplitude = TEsliceSpoilerGradientsAmplitude;
+    }
+    
+    /**
+     * 
+     * @param TEsliceSpoilerGradientsLimit
+     */
+    public void setTESliceSpoilerGradientsLimit(double TEsliceSpoilerGradientsLimit) {
+    	this.TESliceSpoilerGradientsLimit = TEsliceSpoilerGradientsLimit;
+    }
+    
+    /**
+     * 
+     * @param diffusionGradientDuration
+     */
+    public void setDiffusionGradientDuration(double[] diffusionGradientDuration) {
+    	this.diffusionGradientDuration = diffusionGradientDuration;
+    }
+    
+    /**
+     * 
+     * @param diffusionGradientSeparation
+     */
+    public void setDiffusionGradientSeparation(double[] diffusionGradientSeparation) {
+    	this.diffusionGradientSeparation = diffusionGradientSeparation;
+    }
+    
+    /**
+     * 
+     * @param directScaledSwitching
+     */
+    public void setDirectScaledSwitching(String directScaledSwitching) {
+    	this.directScaledSwitching = directScaledSwitching;
+    }
+    
+    /**
+     * 
+     * @param diffusionMeasurementMode
+     */
+    public void setDiffusionMeasurementMode(String diffusionMeasurementMode) {
+    	this.diffusionMeasurementMode = diffusionMeasurementMode;
+    }
+    
+    /**
+     * 
+     * @param numberOfDiffusionDirections
+     */
+    public void setNumberOfDiffusionDirections(int numberOfDiffusionDirections) {
+    	this.numberOfDiffusionDirections = numberOfDiffusionDirections;
+    }
+    
+    /**
+     * 
+     * @param diffusionExperimentsPerDirection
+     */
+    public void setDiffusionExperimentsPerDirection(int diffusionExperimentsPerDirection) {
+    	this.diffusionExperimentsPerDirection = diffusionExperimentsPerDirection;
+    }
+    
+    /**
+     * 
+     * @param numberOfA0Images
+     */
+    public void setNumberOfA0Images(int numberOfA0Images) {
+        this.numberOfA0Images = numberOfA0Images;	
     }
 }
