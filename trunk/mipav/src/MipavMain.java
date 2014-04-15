399fda315443d14718d8fc4fcdb06d27a960703e
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-04-15 16:04:22 +0000

    Added new default uncaught exception/error handler that sends an email to bug@mipav.cit.nih.gov with the stack trace and basic info.  We'll have to see if the number of exceptions gets overwhelming, but it could help track down unreported bugs.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12612 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/MipavMain.java
+++ trunk/mipav/src/MipavMain.java
@@ -1,16 +1,18 @@
 /**
  * Start of the application - Medical Image Processing, Analysis & Visualization(MIPAV).
- *
- * This application allows users to perform quantitative analysis of
- * multi-modality 1D/2D/3D/4D/5D image datasets to assist medical research.
- *
+ * 
+ * This application allows users to perform quantitative analysis of multi-modality 1D/2D/3D/4D/5D image datasets to
+ * assist medical research.
+ * 
  * @author Matthew J. McAuliffe
  */
 
+import gov.nih.mipav.util.MipavExceptionHandler;
+
+import gov.nih.mipav.view.Preferences;
+import gov.nih.mipav.view.ViewUserInterface;
 
 import javax.swing.UIManager;
-
-import gov.nih.mipav.view.*;
 
 
 /**
@@ -18,38 +20,45 @@ import gov.nih.mipav.view.*;
  */
 public class MipavMain {
 
-    //~ Methods --------------------------------------------------------------------------------------------------------
+    // ~ Methods
+    // --------------------------------------------------------------------------------------------------------
 
     /**
      * Passes control to the main GUI in the Model, View and Controller arch.
-     *
-     * @param  args  array of command line arguments input as strings
+     * 
+     * @param args array of command line arguments input as strings
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.setProperty("sun.awt.noerasebackground", "true");
 
-        int initArg = ViewUserInterface.parseStaticArguments(args, 0); //process static command line arguments
-        
-        ViewUserInterface ui = ViewUserInterface.create();
-        ui.parseArguments(args, initArg); //process command line arguments that require mipav objects
+        Thread.setDefaultUncaughtExceptionHandler(new MipavExceptionHandler());
 
-
-        if (ui.isAppFrameVisible() && gov.nih.mipav.view.Preferences.is(gov.nih.mipav.view.Preferences.PREF_SHOW_SPLASH)) {
-            ui.showSplashGraphics();
-        }
-
-        String lf = null;
         try {
-            
-			lf = Preferences.getProperty(Preferences.PREF_SHOW_UI_LF);
-			String os = System.getProperty("os.name");
-			if(lf != null && !lf.equals("None") && !os.toLowerCase().contains("mac")) {
-				UIManager.setLookAndFeel(lf);
-			}
-		} catch (Exception e) {
-			System.err.println("Unable to set requested look and feel: "+lf);
-		}
-        
-        ui.setVisible(ui.isAppFrameVisible());
+            final int initArg = ViewUserInterface.parseStaticArguments(args, 0); // process static command line
+                                                                                 // arguments
+
+            final ViewUserInterface ui = ViewUserInterface.create();
+            ui.parseArguments(args, initArg); // process command line arguments that require mipav objects
+
+            if (ui.isAppFrameVisible() && gov.nih.mipav.view.Preferences.is(gov.nih.mipav.view.Preferences.PREF_SHOW_SPLASH)) {
+                ui.showSplashGraphics();
+            }
+
+            String lf = null;
+            try {
+
+                lf = Preferences.getProperty(Preferences.PREF_SHOW_UI_LF);
+                final String os = System.getProperty("os.name");
+                if (lf != null && !lf.equals("None") && !os.toLowerCase().contains("mac")) {
+                    UIManager.setLookAndFeel(lf);
+                }
+            } catch (final Exception e) {
+                System.err.println("Unable to set requested look and feel: " + lf);
+            }
+
+            ui.setVisible(ui.isAppFrameVisible());
+        } catch (final Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ trunk/mipav/src/gov/nih/mipav/util/MipavExceptionHandler.java
@@ -0,0 +1,41 @@
+package gov.nih.mipav.util;
+
+
+import gov.nih.mipav.view.MipavUtil;
+import gov.nih.mipav.view.dialogs.reportbug.ReportBugBuilder;
+import gov.nih.mipav.view.dialogs.reportbug.ReportBugBuilder.BugType;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+
+
+public class MipavExceptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(final Thread thread, final Throwable e) {
+        final String summary = "New automatic error/exception reporting";
+        String name = ReportBugBuilder.getNameDefault();
+        if (name == null) {
+            name = "Anonymous user";
+        }
+        String email = ReportBugBuilder.getEmailDefault();
+        if (email == null) {
+            email = "bug@mipav.cit.nih.gov";
+        }
+        final String version = MipavUtil.getVersion();
+        final String os = System.getProperties().getProperty("os.name") + " - " + System.getProperty("os.arch");
+        final String urgency = "Low";
+        final String description = getStackTrace(e);
+        final BugType bugType = BugType.AUTOMATIC_ERROR_REPORTING;
+
+        ReportBugBuilder.sendReportWeb(summary, name, email, version, os, urgency, description, bugType, new ArrayList<String>(), new ArrayList<String>());
+    }
+
+    public static String getStackTrace(final Throwable e) {
+        final Writer result = new StringWriter();
+        final PrintWriter printWriter = new PrintWriter(result);
+        e.printStackTrace(printWriter);
+        return result.toString();
+    }
+}
