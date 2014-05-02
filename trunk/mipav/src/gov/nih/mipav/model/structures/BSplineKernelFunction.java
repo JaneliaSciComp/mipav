e8d79fc974072c8a6853d0220ec2f1cdf147c142
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-05-02 20:19:44 +0000

    All self test passed for spline orders 0, 1, 2, and 3.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12708 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/structures/BSplineKernelFunction.java
+++ trunk/mipav/src/gov/nih/mipav/model/structures/BSplineKernelFunction.java
@@ -1,6 +1,7 @@
 	package gov.nih.mipav.model.structures;
 
 import gov.nih.mipav.view.MipavUtil;
+import gov.nih.mipav.view.Preferences;
 	
 	
 	
@@ -11,8 +12,8 @@ import gov.nih.mipav.view.MipavUtil;
 	 * @version  0.1 April 4, 2014
 	 * @author   William Gandler
 	 * 
-	  * This a a port of itkBSplineKernelFunction.h from the itk package.  Here is the original itk header
-	  * from the itkBSplineKernelFunction.h file:
+	  * This a a port of itkBSplineKernelFunction.h  and itkBSplineKernelFunctionTest.cxxfrom the itk package.  
+	  * Here is the original itk header from the itkBSplineKernelFunction.h file:
 	  *  Copyright Insight Software Consortium
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,7 +34,10 @@ import gov.nih.mipav.view.MipavUtil;
 	public class BSplineKernelFunction  {
 		
 		private int splineOrder;
-	
+		
+		public BSplineKernelFunction() {
+			
+		}
 		
 	    
 	    public BSplineKernelFunction(int splineOrder) {
@@ -93,6 +97,147 @@ import gov.nih.mipav.view.MipavUtil;
 		    		MipavUtil.displayError("Evaluate not implemented for splineOrder = " + splineOrder);
 		    		return 0.0;
 	    	}
+	    }
+	    
+	    public void selfTest() {
+	    	// All 49 splineOrder = 0 tests passed
+	        // All 49 splineOrder = 1 tests passed
+	        // All 49 splineOrder = 2 tests passed
+	    	// All 49 splineOrder = 3 tests passed
+	    	int npoints = 49;
+	    	double x[] = new double[] { -3, -2.875, -2.75, -2.625, -2.5, 
+	    	                            -2.375, -2.25, -2.125, -2, -1.875, 
+	    	                            -1.75, -1.625, -1.5, -1.375, -1.25, 
+	    	                            -1.125, -1, -0.875, -0.75, -0.625, 
+	    	                            -0.5, -0.375, -0.25, -0.125, 0, 
+	    	                             0.125, 0.25, 0.375, 0.5, 0.625, 
+	    	                             0.75, 0.875, 1, 1.125, 1.25, 
+	    	                             1.375, 1.5, 1.625, 1.75, 1.875, 
+	    	                             2, 2.125, 2.25, 2.375, 2.5, 
+	    	                             2.625, 2.75, 2.875, 3 }; 
+	    
+	    	double b0[] = new double[] { 0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0.5, 1, 1, 1, 1, 
+	    	                             1, 1, 1, 0.5, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0 }; 
+	    
+	    	double b1[] = new double[] { 0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0.125, 0.25, 0.375, 
+	    	                             0.5, 0.625, 0.75, 0.875, 1, 
+	    	                             0.875, 0.75, 0.625, 0.5, 0.375, 
+	    	                             0.25, 0.125, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0 }; 
+	 
+	    	double b2[] = new double[] { 0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0.0078125, 0.03125, 
+	    	                             0.0703125, 0.125, 0.195313, 0.28125, 0.382813, 
+	    	                             0.5, 0.609375, 0.6875, 0.734375, 0.75, 
+	    	                             0.734375, 0.6875, 0.609375, 0.5, 0.382813, 
+	    	                             0.28125, 0.195313, 0.125, 0.0703125, 0.03125, 
+	    	                             0.0078125, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0 }; 
+	   
+	    	double b3[] = new double[] { 0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0, 0.000325521, 
+	    	                             0.00260417, 0.00878906, 0.0208333, 0.0406901, 0.0703125, 
+	    	                             0.111654, 0.166667, 0.236003, 0.315104, 0.398112, 
+	    	                             0.479167, 0.552409, 0.611979, 0.652018, 0.666667, 
+	    	                             0.652018, 0.611979, 0.552409, 0.479167, 0.398112, 
+	    	                             0.315104, 0.236003, 0.166667, 0.111654, 0.0703125, 
+	    	                             0.0406901, 0.0208333, 0.00878906, 0.00260417, 0.000325521, 
+	    	                             0, 0, 0, 0, 0, 
+	    	                             0, 0, 0, 0 }; 
+	    	int i;
+	    	Preferences.debug("Testing BSplineKernelFunction\n", Preferences.DEBUG_ALGORITHM);
+	    	int tests0Passed = 0;
+	    	int tests1Passed = 0;
+	    	int tests2Passed = 0;
+	    	int tests3Passed = 0;
+	    	
+	        splineOrder = 0;
+	        for (i = 0; i < npoints; i++) {
+	        	double result = evaluate(x[i]);
+	            if (Math.abs(result - b0[i]) > 1.0E-6) {
+	            	Preferences.debug("For splineOrder = 0 failure for x = " + x[i] + "\n", Preferences.DEBUG_ALGORITHM);
+	            	Preferences.debug("True answer = " + b0[i] + "  Calculated answer = " + result + "\n", Preferences.DEBUG_ALGORITHM);
+	            }
+	            else {
+	            	tests0Passed++;
+	            }
+	        }
+	        if (tests0Passed == npoints) {
+	            Preferences.debug("All " + npoints + " splineOrder = 0 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        else {
+	            Preferences.debug(tests0Passed + " out of " + npoints + " splineOrder = 0 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        
+	        splineOrder = 1;
+	        for (i = 0; i < npoints; i++) {
+	        	double result = evaluate(x[i]);
+	            if (Math.abs(result - b1[i]) > 1.0E-6) {
+	            	Preferences.debug("For splineOrder = 1 failure for x = " + x[i] + "\n", Preferences.DEBUG_ALGORITHM);
+	            	Preferences.debug("True answer = " + b1[i] + "  Calculated answer = " + result + "\n", Preferences.DEBUG_ALGORITHM);
+	            }
+	            else {
+	            	tests1Passed++;
+	            }
+	        }
+	        if (tests0Passed == npoints) {
+	            Preferences.debug("All " + npoints + " splineOrder = 1 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        else {
+	            Preferences.debug(tests1Passed + " out of " + npoints + " splineOrder = 1 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        
+	        splineOrder = 2;
+	        for (i = 0; i < npoints; i++) {
+	        	double result = evaluate(x[i]);
+	            if (Math.abs(result - b2[i]) > 1.0E-6) {
+	            	Preferences.debug("For splineOrder = 2 failure for x = " + x[i] + "\n", Preferences.DEBUG_ALGORITHM);
+	            	Preferences.debug("True answer = " + b2[i] + "  Calculated answer = " + result + "\n", Preferences.DEBUG_ALGORITHM);
+	            }
+	            else {
+	            	tests2Passed++;
+	            }
+	        }
+	        if (tests2Passed == npoints) {
+	            Preferences.debug("All " + npoints + " splineOrder = 2 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        else {
+	            Preferences.debug(tests2Passed + " out of " + npoints + " splineOrder = 2 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        
+	        splineOrder = 3;
+	        for (i = 0; i < npoints; i++) {
+	        	double result = evaluate(x[i]);
+	            if (Math.abs(result - b3[i]) > 1.0E-6) {
+	            	Preferences.debug("For splineOrder = 3 failure for x = " + x[i] + "\n", Preferences.DEBUG_ALGORITHM);
+	            	Preferences.debug("True answer = " + b3[i] + "  Calculated answer = " + result + "\n", Preferences.DEBUG_ALGORITHM);
+	            }
+	            else {
+	            	tests3Passed++;
+	            }
+	        }
+	        if (tests3Passed == npoints) {
+	            Preferences.debug("All " + npoints + " splineOrder = 3 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        else {
+	            Preferences.debug(tests3Passed + " out of " + npoints + " splineOrder = 3 tests passed\n", Preferences.DEBUG_ALGORITHM);	
+	        }
+	        
 	    }
 	    
 }
