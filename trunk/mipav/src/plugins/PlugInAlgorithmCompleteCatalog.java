a825272221082ca0983c8e0909c9176d033486c1
wangvg@NIH.GOV <wangvg@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-05-08 20:33:44 +0000

    Split up the unique samples from the complete catalog searcher since they work on different files
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12730 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmCompleteCatalog.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmCompleteCatalog.java
@@ -6,11 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.Vector;
 
 
@@ -32,11 +27,9 @@ public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
     
     private int GUID = -1;
     
-    private int site = -1;
+    private int specimen = -1;
     
     private int visitID = -1;
-    
-    private int specimen = -1;
 
     public PlugInAlgorithmCompleteCatalog(final File file, final FileWriter out/*,final int lines*/) {
         super();
@@ -48,17 +41,12 @@ public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
     @Override
     public void runAlgorithm() {
         // TODO Auto-generated method stub
-        complete = new int[6]; // RNA, Plasma, Serum, CSF, Blood
+        complete = new int[6]; // RNA, Plasma, Serum, CSF, Blood, DNA
         final String[] types = {"RNA", "Plasma", "Serum", "CSF", "Blood", "DNA"};
         String GUIDStr;
         Integer typeNum;
 
         readCSV();
-        
-        if(site == -1 || GUID == -1 || visitID == -1 || specimen == -1){
-        	MipavUtil.displayError("A header is missing");
-        	return;
-        }
         
         while (catalogID.size() > 0) {
             GUIDStr = catalogID.get(0);
@@ -75,8 +63,7 @@ public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
             }
             csv.flush();
             csv.close();
-            
-            uniqueSample();
+
         } catch (final IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -156,14 +143,18 @@ public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
                 String[] hArray = parseLine2(header);
                 for(int i=0;i<hArray.length;i++){
                 	String hStr = hArray[i].trim();
-                	if(hStr.equals("Site"))
-                		site = i;
-                	else if(hStr.equals("GUID"))
+                	
+                	if(hStr.contains("Catalog ID"))
                 		GUID = i;
-                	else if(hStr.equals("Visit ID"))
+                	else if(hStr.contains("Clinical Event"))
                 		visitID = i;
-                	else if(hStr.equals("Site Specimen Type"))
+                	else if(hStr.contains("Type"))
                 		specimen = i;
+                }
+                
+                if(GUID == -1 || visitID == -1 || specimen == -1){
+                	MipavUtil.displayError("A header is missing");
+                	return;
                 }
 
                 while ( (line = input.readLine()) != null) {
@@ -178,7 +169,7 @@ public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
                             typeNum = 1;
                         } else if (typeStr.contains("serum")) {
                             typeNum = 2;
-                        } else if (typeStr.contains("cereb")) {
+                        } else if (typeStr.contains("csf")) {
                             typeNum = 3;
                         } else if (typeStr.contains("blood")) {
                             typeNum = 4;
@@ -201,149 +192,6 @@ public class PlugInAlgorithmCompleteCatalog extends AlgorithmBase {
         } catch (final IOException ex) {
             ex.printStackTrace();
         }
-    }
-    
-    private void uniqueSample() throws IOException{
-    	
-        LinkedHashSet<String> siteSet = new LinkedHashSet<String>();
-        LinkedHashSet<String> visitSet = new LinkedHashSet<String>();
-        //LinkedHashSet<String> specSet = new LinkedHashSet<String>();
-        LinkedHashSet<String> lineSet = new LinkedHashSet<String>();
-        ArrayList<String> specSet = new ArrayList<String>();
-        specSet.add("DNA");
-        specSet.add("RNA");
-        specSet.add("Plasma");
-        specSet.add("Serum");
-        specSet.add("CSF");
-        specSet.add("Blood");
-        
-        final String[] types = {"RNA", "Plasma", "Serum", "CSF", "Blood", "DNA", "Unknown"};
-    	
-    	final BufferedReader input = new BufferedReader(new FileReader(catalogFile));
-    	String outFilename = catalogFile.getName();
-    	outFilename = outFilename.substring(0, outFilename.lastIndexOf("."));
-    	outFilename += "_unique.csv";
-    	outFilename = catalogFile.getParent() + File.separator + outFilename;
-    	File outFile = new File(outFilename);
-    	FileWriter uniqueCSV = new FileWriter(outFile);
-    	
-    	String uniqueHeader = "Visit Type,Study,DNA,RNA,Plasma,Serum,CSF,WB\n";
-    	uniqueCSV.append(uniqueHeader);
-    	/*for(int i=0;i<remove;i++){
-    		input.readLine();
-    	}*/
-    	input.readLine();
-    	//System.out.printf("%d %d %d %d\n", GUID, site, visitID, specimen);
-    	
-    	String line = null;
-    	while ( (line = input.readLine()) != null) {
-    		String[] lineArray = parseLine2(line);
-    		if(lineArray.length == 0)
-    			continue;
-    		String siteStr = lineArray[site].trim();
-    		String GUIDStr = lineArray[GUID].trim();
-    		String visitStr = lineArray[visitID].trim();
-    		String typeStr = lineArray[specimen].trim().toLowerCase();
-    		int typeNum;
-    		//System.out.println(typeStr);
-    		if (typeStr.contains("rna")) {
-                typeNum = 0;
-            } else if (typeStr.contains("plasma")) {
-                typeNum = 1;
-            } else if (typeStr.contains("serum")) {
-                typeNum = 2;
-            } else if (typeStr.contains("cereb")) {
-                typeNum = 3;
-            } else if (typeStr.contains("blood")) {
-                typeNum = 4;
-            } else if (typeStr.contains("dna")){
-            	typeNum = 5;
-            } else {
-                typeNum = 6;
-            }
-    		
-    		String setStr = siteStr + ";" + GUIDStr + ";" + visitStr + ";" + types[typeNum];
-    		
-    		siteSet.add(siteStr);
-    		visitSet.add(visitStr);
-    		//specSet.add(types[typeNum]);
-    		lineSet.add(setStr);
-    	}
-    	int siteSize = siteSet.size();
-    	int visitSize = visitSet.size();
-    	int specSize = specSet.size();
-    	int combinations = siteSize*visitSize*specSize;
-    	int[] counter = new int[combinations];
-    	ArrayList<String> siteList = new ArrayList<String>(siteSet);
-    	ArrayList<String> visitList = new ArrayList<String>(visitSet);
-    	//ArrayList<String> specList = new ArrayList<String>(specSet);
-    	
-    	Comparator<String> comp = new Comparator<String>(){
-
-			@Override
-			public int compare(String o1, String o2) {
-				if(o1.equals("Baseline"))
-					return -1;
-				else if(o2.equals("Baseline"))
-					return 1;
-				else{
-					String[] s1 = o1.split(" ");
-					String[] s2 = o2.split(" ");
-					int i1 = Integer.valueOf(s1[0]);
-					int i2 = Integer.valueOf(s2[0]);
-					if(i1 < i2)
-						return -1;
-					else return 1;
-				}
-			}
-    	};
-    	
-    	Collections.sort(siteList);
-    	Collections.sort(visitList, comp);
-    	
-    	Iterator<String> iter = lineSet.iterator();
-    	while(iter.hasNext()){
-    		String lineStr = iter.next();
-    		//System.out.println(lineStr);
-    		String[] lineSplit = lineStr.split(";");
-    		int siteInd = siteList.indexOf(lineSplit[0]);
-    		int visitInd = visitList.indexOf(lineSplit[2]);
-    		int specInd = specSet.indexOf(lineSplit[3]);
-    		int comboInd = siteInd + visitInd*siteSize + specInd*siteSize*visitSize;
-    		counter[comboInd]++;
-    	}
-    	
-    	for(int i=0;i<siteSize;i++){
-    		for(int j=0;j<visitSize;j++){
-    			String output = visitList.get(j) + "," + siteList.get(i);
-    			for(int k=0;k<specSize;k++){
-    				int index = i + j*siteSize + k*siteSize*visitSize;
-    				output += "," + counter[index];
-    			}
-    			output += "\n";
-    			boolean zeros = output.endsWith("0,0,0,0,0,0\n");
-    			if(!zeros)
-    				uniqueCSV.write(output);
-    		}
-    	}
-    	
-    	/*for(int i=0;i<specSize;i++){
-    		String spec = specSet.get(i);
-    		for(int j=0;j<visitSize;j++){
-    			String visit = visitList.get(j);
-    			for(int k=0;k<siteSize;k++){
-    				String site = siteList.get(k);
-    				int index = k + j*siteSize + i*siteSize*visitSize;
-    				String output = String.format("%s,%s,%s,%d\n", site, visit, spec, counter[index]);
-    				uniqueCSV.write(output);
-    				//Write output
-    			}
-    		}
-    	}*/
-    	
-    	uniqueCSV.close();
-    	input.close();
-    	//ArrayList<String> lineList = new ArrayList<String>(lineSet);
     }
     
 	private String[] parseLine2(String line){
---
+++ trunk/mipav/src/plugins/PlugInAlgorithmUniqueSamplesCount.java
@@ -0,0 +1,225 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+import gov.nih.mipav.model.algorithms.AlgorithmBase;
+import gov.nih.mipav.view.MipavUtil;
+
+
+public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
+
+	private File catalogFile;
+	
+	private int site = -1;
+	
+	private int GUID = -1;
+	
+	private int visitID = -1;
+	
+	private int specimen = -1;
+	
+	public PlugInAlgorithmUniqueSamplesCount(File inputFile){
+		super();
+		catalogFile = inputFile;
+	}
+	
+	@Override
+	public void runAlgorithm() {
+		// TODO Auto-generated method stub
+		try{
+			uniqueSample();
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+	
+	private String[] parseLine2(String line){
+		String[] output = new String[20];
+		int cnt = 0;
+		int ind = 0;
+		while(cnt<20){
+			ind = line.indexOf(",");
+			if(ind > 0)
+				output[cnt] = line.substring(0, ind).trim();
+			else if (ind == -1) break;
+			else output[cnt] = "null";
+			cnt++;
+			line = line.substring(ind+1);
+		}
+		String[] realout = new String[cnt];
+		for(int i=0;i<cnt;i++){
+			realout[i] = output[i];
+		}
+		
+		return realout;
+	}
+	
+	private void uniqueSample() throws IOException{
+    	
+        LinkedHashSet<String> siteSet = new LinkedHashSet<String>();
+        LinkedHashSet<String> visitSet = new LinkedHashSet<String>();
+        //LinkedHashSet<String> specSet = new LinkedHashSet<String>();
+        LinkedHashSet<String> lineSet = new LinkedHashSet<String>();
+        ArrayList<String> specSet = new ArrayList<String>();
+        specSet.add("DNA");
+        specSet.add("RNA");
+        specSet.add("Plasma");
+        specSet.add("Serum");
+        specSet.add("CSF");
+        specSet.add("Blood");
+        
+        final String[] types = {"RNA", "Plasma", "Serum", "CSF", "Blood", "DNA", "Unknown"};
+    	
+    	final BufferedReader input = new BufferedReader(new FileReader(catalogFile));
+    	String outFilename = catalogFile.getName();
+    	outFilename = outFilename.substring(0, outFilename.lastIndexOf("."));
+    	outFilename += "_unique.csv";
+    	outFilename = catalogFile.getParent() + File.separator + outFilename;
+    	File outFile = new File(outFilename);
+    	FileWriter uniqueCSV = new FileWriter(outFile);
+    	
+    	String uniqueHeader = "Visit Type,Study,DNA,RNA,Plasma,Serum,CSF,WB\n";
+    	uniqueCSV.append(uniqueHeader);
+    	/*for(int i=0;i<remove;i++){
+    		input.readLine();
+    	}*/
+    	String header = input.readLine();
+        String[] hArray = parseLine2(header);
+        for(int i=0;i<hArray.length;i++){
+        	String hStr = hArray[i].trim();
+        	if(hStr.equals("Site"))
+        		site = i;
+        	else if(hStr.equals("GUID"))
+        		GUID = i;
+        	else if(hStr.equals("Visit ID"))
+        		visitID = i;
+        	else if(hStr.equals("Site Specimen Type"))
+        		specimen = i;
+        }
+        
+        if(site == -1 || GUID == -1 || visitID == -1 || specimen == -1){
+        	MipavUtil.displayError("A header is missing");
+        	return;
+        }
+    	//System.out.printf("%d %d %d %d\n", GUID, site, visitID, specimen);
+    	
+    	String line = null;
+    	while ( (line = input.readLine()) != null) {
+    		String[] lineArray = parseLine2(line);
+    		if(lineArray.length == 0)
+    			continue;
+    		String siteStr = lineArray[site].trim();
+    		String GUIDStr = lineArray[GUID].trim();
+    		String visitStr = lineArray[visitID].trim();
+    		String typeStr = lineArray[specimen].trim().toLowerCase();
+    		int typeNum;
+    		//System.out.println(typeStr);
+    		if (typeStr.contains("rna")) {
+                typeNum = 0;
+            } else if (typeStr.contains("plasma")) {
+                typeNum = 1;
+            } else if (typeStr.contains("serum")) {
+                typeNum = 2;
+            } else if (typeStr.contains("cereb")) {
+                typeNum = 3;
+            } else if (typeStr.contains("blood")) {
+                typeNum = 4;
+            } else if (typeStr.contains("dna")){
+            	typeNum = 5;
+            } else {
+                typeNum = 6;
+            }
+    		
+    		String setStr = siteStr + ";" + GUIDStr + ";" + visitStr + ";" + types[typeNum];
+    		
+    		siteSet.add(siteStr);
+    		visitSet.add(visitStr);
+    		//specSet.add(types[typeNum]);
+    		lineSet.add(setStr);
+    	}
+    	int siteSize = siteSet.size();
+    	int visitSize = visitSet.size();
+    	int specSize = specSet.size();
+    	int combinations = siteSize*visitSize*specSize;
+    	int[] counter = new int[combinations];
+    	ArrayList<String> siteList = new ArrayList<String>(siteSet);
+    	ArrayList<String> visitList = new ArrayList<String>(visitSet);
+    	//ArrayList<String> specList = new ArrayList<String>(specSet);
+    	
+    	Comparator<String> comp = new Comparator<String>(){
+
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.equals("Baseline"))
+					return -1;
+				else if(o2.equals("Baseline"))
+					return 1;
+				else{
+					String[] s1 = o1.split(" ");
+					String[] s2 = o2.split(" ");
+					int i1 = Integer.valueOf(s1[0]);
+					int i2 = Integer.valueOf(s2[0]);
+					if(i1 < i2)
+						return -1;
+					else return 1;
+				}
+			}
+    	};
+    	
+    	Collections.sort(siteList);
+    	Collections.sort(visitList, comp);
+    	
+    	Iterator<String> iter = lineSet.iterator();
+    	while(iter.hasNext()){
+    		String lineStr = iter.next();
+    		//System.out.println(lineStr);
+    		String[] lineSplit = lineStr.split(";");
+    		int siteInd = siteList.indexOf(lineSplit[0]);
+    		int visitInd = visitList.indexOf(lineSplit[2]);
+    		int specInd = specSet.indexOf(lineSplit[3]);
+    		int comboInd = siteInd + visitInd*siteSize + specInd*siteSize*visitSize;
+    		counter[comboInd]++;
+    	}
+    	
+    	for(int i=0;i<siteSize;i++){
+    		for(int j=0;j<visitSize;j++){
+    			String output = visitList.get(j) + "," + siteList.get(i);
+    			for(int k=0;k<specSize;k++){
+    				int index = i + j*siteSize + k*siteSize*visitSize;
+    				output += "," + counter[index];
+    			}
+    			output += "\n";
+    			boolean zeros = output.endsWith("0,0,0,0,0,0\n");
+    			if(!zeros)
+    				uniqueCSV.write(output);
+    		}
+    	}
+    	
+    	/*for(int i=0;i<specSize;i++){
+    		String spec = specSet.get(i);
+    		for(int j=0;j<visitSize;j++){
+    			String visit = visitList.get(j);
+    			for(int k=0;k<siteSize;k++){
+    				String site = siteList.get(k);
+    				int index = k + j*siteSize + i*siteSize*visitSize;
+    				String output = String.format("%s,%s,%s,%d\n", site, visit, spec, counter[index]);
+    				uniqueCSV.write(output);
+    				//Write output
+    			}
+    		}
+    	}*/
+    	
+    	uniqueCSV.close();
+    	input.close();
+    	//ArrayList<String> lineList = new ArrayList<String>(lineSet);
+    }
+
+}
