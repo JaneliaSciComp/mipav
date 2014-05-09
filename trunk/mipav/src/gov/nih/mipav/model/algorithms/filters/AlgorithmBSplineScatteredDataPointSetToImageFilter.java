f0fbf6849d08a0bd8c1979f943db973be19cf52d
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-05-09 20:16:31 +0000

    Allow errors larger than BSplineEpsilon = 2.22E-14.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12737 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmBSplineScatteredDataPointSetToImageFilter.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmBSplineScatteredDataPointSetToImageFilter.java
@@ -484,7 +484,8 @@ import gov.nih.mipav.view.Preferences;
 	        	    int totalNumberOfSpans = currentNumberOfControlPoints[i] - splineOrder[i];
 	        	    
 	        	    p[i] = (point[i] - origin[i]) * r[i];
-	        	    if (Math.abs(p[i] - totalNumberOfSpans) <= BSplineEpsilon) {
+	        	    //if (Math.abs(p[i] - totalNumberOfSpans) <= BSplineEpsilon) {
+	        	    if ((Math.abs(p[i] - totalNumberOfSpans))/totalNumberOfSpans < 1.0E-6) {
 	        	    	p[i] = (double)(totalNumberOfSpans) - BSplineEpsilon;
 	        	    }
 	        	    if (p[i] >= totalNumberOfSpans) {
@@ -616,7 +617,8 @@ import gov.nih.mipav.view.Preferences;
 	            }
 	            for (int i = 0; i < nDims; i++) {
 	                U[i] = ((double)(totalNumberOfSpans[i] * (idx[i] /* - startIndex[i] */)))/(double)(extents[i] - 1);
-	                if (Math.abs(U[i] - totalNumberOfSpans[i]) <= BSplineEpsilon) {
+	                //if (Math.abs(U[i] - totalNumberOfSpans[i]) <= BSplineEpsilon) {
+	                if ((Math.abs(U[i] - totalNumberOfSpans[i]))/totalNumberOfSpans[i] < 1.0E-6) {
 	                	U[i] = totalNumberOfSpans[i] - BSplineEpsilon;
 	                }
 	                if (U[i] >= totalNumberOfSpans[i]) {
