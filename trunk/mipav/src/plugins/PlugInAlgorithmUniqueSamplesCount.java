041ebf08ffb21b34882a43d7f82cf79d3df2f8a5
wangvg@NIH.GOV <wangvg@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-05-12 18:54:03 +0000

    Changed what column to look at. Was looking at wrong column (looks at Site Sample ID now instead of GUID)
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12744 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmUniqueSamplesCount.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmUniqueSamplesCount.java
@@ -84,10 +84,7 @@ public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
     	outFilename += "_unique.csv";
     	outFilename = catalogFile.getParent() + File.separator + outFilename;
     	File outFile = new File(outFilename);
-    	FileWriter uniqueCSV = new FileWriter(outFile);
     	
-    	String uniqueHeader = "Visit Type,Study,DNA,RNA,Plasma,Serum,CSF,WB\n";
-    	uniqueCSV.append(uniqueHeader);
     	/*for(int i=0;i<remove;i++){
     		input.readLine();
     	}*/
@@ -97,7 +94,7 @@ public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
         	String hStr = hArray[i].trim();
         	if(hStr.equals("Site"))
         		site = i;
-        	else if(hStr.equals("GUID"))
+        	else if(hStr.equals("Site Tube ID"))
         		GUID = i;
         	else if(hStr.equals("Visit ID"))
         		visitID = i;
@@ -107,15 +104,23 @@ public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
         
         if(site == -1 || GUID == -1 || visitID == -1 || specimen == -1){
         	MipavUtil.displayError("A header is missing");
+        	input.close();
         	return;
         }
+        
+        FileWriter uniqueCSV = new FileWriter(outFile);
+    	
+    	String uniqueHeader = "Visit Type,Study,DNA,RNA,Plasma,Serum,CSF,WB\n";
+    	uniqueCSV.append(uniqueHeader);
     	//System.out.printf("%d %d %d %d\n", GUID, site, visitID, specimen);
     	
     	String line = null;
+    	int num = 0;
     	while ( (line = input.readLine()) != null) {
     		String[] lineArray = parseLine2(line);
     		if(lineArray.length == 0)
     			continue;
+    		num++;
     		String siteStr = lineArray[site].trim();
     		String GUIDStr = lineArray[GUID].trim();
     		String visitStr = lineArray[visitID].trim();
@@ -136,6 +141,7 @@ public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
             	typeNum = 5;
             } else {
                 typeNum = 6;
+                System.err.println("Unknown");
             }
     		
     		String setStr = siteStr + ";" + GUIDStr + ";" + visitStr + ";" + types[typeNum];
@@ -145,6 +151,8 @@ public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
     		//specSet.add(types[typeNum]);
     		lineSet.add(setStr);
     	}
+    	System.out.println("Set size: " + lineSet.size());
+    	System.out.println("Lines read: " + num);
     	int siteSize = siteSet.size();
     	int visitSize = visitSet.size();
     	int specSize = specSet.size();
@@ -192,34 +200,21 @@ public class PlugInAlgorithmUniqueSamplesCount extends AlgorithmBase {
     	for(int i=0;i<siteSize;i++){
     		for(int j=0;j<visitSize;j++){
     			String output = visitList.get(j) + "," + siteList.get(i);
+    			int total = 0;
     			for(int k=0;k<specSize;k++){
     				int index = i + j*siteSize + k*siteSize*visitSize;
+    				total += counter[index];
     				output += "," + counter[index];
     			}
     			output += "\n";
-    			boolean zeros = output.endsWith("0,0,0,0,0,0\n");
-    			if(!zeros)
+    			if(total > 0)
     				uniqueCSV.write(output);
     		}
     	}
     	
-    	/*for(int i=0;i<specSize;i++){
-    		String spec = specSet.get(i);
-    		for(int j=0;j<visitSize;j++){
-    			String visit = visitList.get(j);
-    			for(int k=0;k<siteSize;k++){
-    				String site = siteList.get(k);
-    				int index = k + j*siteSize + i*siteSize*visitSize;
-    				String output = String.format("%s,%s,%s,%d\n", site, visit, spec, counter[index]);
-    				uniqueCSV.write(output);
-    				//Write output
-    			}
-    		}
-    	}*/
-    	
     	uniqueCSV.close();
     	input.close();
-    	//ArrayList<String> lineList = new ArrayList<String>(lineSet);
+
     }
 
 }
