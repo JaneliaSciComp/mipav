d0c215287d252f798442f89fee984b5d826a0366
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-05-21 21:51:20 +0000

    New release
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12793 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/release-current/mipav/src/plugins/PlugInImageSubmit.java
+++
@@ -1,8 +0,0 @@
-
-import gov.nih.mipav.plugins.*; // needed to load PlugInAlgorithm / PlugInView / PlugInFile interface
-
-public class PlugInImageSubmit implements PlugInGeneric {
-	public void run() {
-		new PlugInDialogImageSubmit(false);
-	}
-}
