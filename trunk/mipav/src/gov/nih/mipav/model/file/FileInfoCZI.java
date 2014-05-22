201b4c98da6243426db0c14d5d183aa7228b2c22
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-05-22 22:21:16 +0000

    Work continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12797 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileInfoCZI.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileInfoCZI.java
@@ -10,6 +10,14 @@ import gov.nih.mipav.view.dialogs.*;
  */
 public class FileInfoCZI extends FileInfoBase {
 	
+	//~ Static fields/initializers -------------------------------------------------------------------------------------
+	private String focusPosition = null;
+	private String acquisitionTime = null;
+	private String stageXPosition = null;
+	private String stageYPosition = null;
+	private String validBitsPerPixel = null;
+	private double timeStamps[] = null;
+	
 	//~ Constructors ---------------------------------------------------------------------------------------------------
 
     /**
@@ -32,8 +40,84 @@ public class FileInfoCZI extends FileInfoBase {
      * @param  matrix  transformation matrix
      */
     public void displayAboutInfo(JDialogBase dlog, TransMatrix matrix) {
+    	int i;
         JDialogText dialog = (JDialogText) dlog;
         displayPrimaryInfo(dialog, matrix);
         dialog.append("\n\n                Other information\n\n");
+        
+        if (focusPosition != null) {
+        	dialog.append("Focus position in micrometers = " + focusPosition + "\n");
+        }
+        
+        if (acquisitionTime != null) {
+        	dialog.append("Acquisition time = " + acquisitionTime + "\n");
+        }
+        
+        if (stageXPosition != null) {
+        	dialog.append("Stage axis X position in micrometers = " + stageXPosition + "\n");
+        }
+        
+        if (stageYPosition != null) {
+        	dialog.append("Stage axis Y position in micrometers = " + stageYPosition + "\n");
+        }
+        
+        if (validBitsPerPixel != null) {
+        	dialog.append("Valid bits per pixel = " + validBitsPerPixel + "\n");
+        }
+        
+        if (timeStamps != null) {
+        	dialog.append("Time stamps in seconds relative to the start time of acquisition:\n");
+        	for (i = 0; i < timeStamps.length; i++) {
+        	    dialog.append("Time stamp  " + i + ":     " + timeStamps[i] + "\n");	
+        	}
+        }
+    }
+    
+    /**
+     * 
+     * @param focusPosition
+     */
+    public void setFocusPosition(String focusPosition) {
+    	this.focusPosition = focusPosition;
+    }
+    
+    /**
+     * 
+     * @param acquisitionTime
+     */
+    public void setAcquisitionTime(String acquisitionTime) {
+    	this.acquisitionTime = acquisitionTime;
+    }
+    
+    /**
+     * 
+     * @param stageXPosition
+     */
+    public void setStageXPosition(String stageXPosition) {
+    	this.stageXPosition = stageXPosition;
+    }
+    
+    /**
+     * 
+     * @param stageYPosition
+     */
+    public void setStageYPosition(String stageYPosition) {
+    	this.stageYPosition = stageYPosition;
+    }
+    
+    /**
+     * 
+     * @param validBitsPerPixel
+     */
+    public void setValidBitsPerPixel(String validBitsPerPixel) {
+    	this.validBitsPerPixel = validBitsPerPixel;
+    }
+    
+    /**
+     * 
+     * @param timeStamps
+     */
+    public void setTimeStamps(double timeStamps[]) {
+    	this.timeStamps = timeStamps;
     }
 }
