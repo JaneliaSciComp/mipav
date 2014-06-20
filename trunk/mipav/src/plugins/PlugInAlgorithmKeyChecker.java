d87a30a61b27afa42f8ee1af60e2650dd099de90
wangvg@NIH.GOV <wangvg@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-06-20 19:24:02 +0000

    Now with comments
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12856 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmKeyChecker.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmKeyChecker.java
@@ -12,11 +12,22 @@ import gov.nih.mipav.view.MipavUtil;
 
 public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 	
+	/**
+	 * Master file to compare other files to
+	 */
 	private File keyFile;
 	
 	private FileWriter outputCSV;
 	
+	/**
+	 * List of files to compare to the master file
+	 */
 	private ArrayList<File> dataFiles;
+	
+	/**
+	 * Indecies of the columns you want, as they sometimes
+	 * change in between files. SID is Subject ID
+	 */
 	
 	private int keySIDind;
 	
@@ -28,7 +39,11 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 	
 	private boolean demoGUID;
 	
-	//NOTE: Depending on format, it may not work to use hashtable
+	/**
+	 * Hash of Subject ID as the key and the associated GUID
+	 * in the master file as the value. Used as the comparison
+	 * data structure.
+	 */
 	private Hashtable<String, String> keyTable;
 	
 	public PlugInAlgorithmKeyChecker(File key, ArrayList<File> data, boolean demo){
@@ -37,6 +52,8 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 		keyTable = new Hashtable<String, String>();
 		demoGUID = demo;
 		
+		//Initial guesses for where the columns are. For the
+		//Master file, it shouldn't change.
 		if(demoGUID)
 			keyGUIDind = 1;
 		else keyGUIDind = 2;
@@ -45,6 +62,7 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 		dataSIDind = 2;
 		dataGUIDind = 1;
 		
+		//Build the output CSV file
 		String keyDir = keyFile.getParent() + File.separator;
 		String keyName = keyFile.getName();
 		keyName = keyName.substring(0, keyName.lastIndexOf("."));
@@ -90,6 +108,12 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 		
 	}
 	
+	/**
+	 * Using the hash produced from the master file, go through each
+	 * provided file and compare each Subject ID with the one in the
+	 * hash table. Any differences should be noted in the output CSV.
+	 * @throws IOException
+	 */
 	private void compareDataFiles() throws IOException{
 		
 		for(File f : dataFiles){
@@ -102,6 +126,9 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 			//Remove header lines from file
 			dataReader.next();
 			
+			//The column each piece is in has changed, so need
+			//to actively look at the column headers to determine
+			//which columns we need to pull out
 			String headerStr = dataReader.next();
 			String[] header = headerStr.split(","); //Assumed CSV, change if not
 			for(int i=0;i<header.length;i++){
@@ -111,7 +138,7 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 				else if(cell.contains("GUID"))
 					dataGUIDind = i;
 			}
-			//System.out.println(f.getName());
+			
 			
 			while(dataReader.hasNext()){
 				line = dataReader.next();
@@ -123,12 +150,15 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 					continue;
 				}
 				
-				//System.out.println(line);
 				String SID = split[dataSIDind];
 				String GUID = split[dataGUIDind];
 				
 				String keyGUID = keyTable.get(SID);
 				
+				//Check to see if the GUID in the data file matches
+				//with the GUID from the master hash
+				//If the GUID is null (as is the case when weird 
+				//formatting occurs), just skip this
 				if(!GUID.equals(keyGUID) && keyGUID != null){
 					//write to output
 					outList.add(String.format(",%s,%s,%s%n", SID, keyGUID, GUID));
@@ -150,12 +180,18 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 		
 	}
 	
+	/**
+	 * Read through the master file to pull out the Subject ID
+	 * and GUID pairings. It will also check to see if any GUIDs
+	 * are duplicated for some reason. 
+	 * @throws IOException
+	 */
 	private void readKeyFile() throws IOException{
 		Scanner scan = new Scanner(keyFile);
 		Scanner keyReader = scan.useDelimiter("\\r\\n");
 		String line;
 		
-		/*String headerStr = keyReader.readLine();
+		/*String headerStr = keyReader.next();
 		String[] header = headerStr.split(","); //Assumed CSV, change if not
 		for(int i=0;i<header.length;i++){
 			String cell = header[i];
@@ -166,6 +202,9 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 		}*/
 		
 		//Remove header lines from file
+		//Assume position of Subject ID and GUID will not change
+		//between different master files. The code commented above
+		//can be used in case that is not the case.
 		keyReader.next();
 		keyReader.next();
 		
@@ -182,10 +221,13 @@ public class PlugInAlgorithmKeyChecker extends AlgorithmBase {
 			String SID = split[keySIDind];
 			String GUID = split[keyGUIDind];
 			
+			//Check GUID hash to determine if duplicate GUID occurs
 			if(!GUIDset.add(GUID)){
 				outputCSV.append(GUID + "is duplicated in key file \r\n");
 			}
 			
+			//Add SID/GUID combo to hash. Do nothing if SID key already
+			//exists, not sure if there needs to be anything done
 			keyTable.put(SID, GUID);
 		}
 		
