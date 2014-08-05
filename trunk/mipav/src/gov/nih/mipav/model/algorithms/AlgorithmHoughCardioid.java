ceaee1d76bbbd0d054a3e0830c2aef2987b0cc43
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-08-05 20:33:48 +0000

    Only works for cusp on left hand side.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@12977 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmHoughCardioid.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmHoughCardioid.java
@@ -9,17 +9,19 @@ import gov.nih.mipav.view.dialogs.*;
 import java.io.*;
 
 /**
- *  This Hough transform uses (xi, yi) points in the original image space to generate x0, y0, rad, theta0 points in the Hough
+ *  This Hough transform uses (xi, yi) points in the original image space to generate x0, y0, rad points in the Hough
  *  transform.  This Hough transform module only works with binary images.   Before it is used the user must 
  *  compute the gradient of an image and threshold it to obtain a binary image.  Noise removal and thinning should also
  *  be performed, if necessary, before this program is run. 
+ *  Only works for cusp on left hand side.
  *  
  *  The user is asked for the number of x0 bins, y0 bins, theta0 bins, rad bins, and number of cardioids.  The default size for x0 is 
  *  min(512, image.getExtents()[0]).  The default size for y0 is min(512, image.getExtents()[1]).
  *  The default size for rad is min(512, max(image.getExtents()[0], image.getExtents()[1]).
- *  The default size for theta0 is 360.
  *  The default number of cardioids is 1. The program generates a Hough transform of the source image using the basic
- *  equation sqrt((x - x0)**2 + (y - y0)**2) = rad*(1 - cos(theta - theta0)).  
+ *  equation sqrt((x - x0)**2 + (y - y0)**2) = rad*(1 + cos(theta)).
+ *  x = (rad/2)*(1 + 2*cos(theta) + cos(2*theta)))
+ *  y = (rad/2)*(2*sin(theta) + sin(2*theta))
  *  The program finds the cardioids containing the largest number of points.
  *  The program produces a dialog which allows the user to select which cardioids should be drawn.
  *  
@@ -27,18 +29,17 @@ import java.io.*;
  *  For each (xi, yi) point in the original image not having a value of zero, calculate the first dimension value d1 = 
  *  j * (xDim - 1)/(x0 - 1), with j = 0 to x0 - 1.  Calculate the second dimension value d2 = k * (yDim - 1)/(y0 - 1),
  *  with k = 0 to y0 - 1. 
- *  d3 = 2*PI*m/theta0, with m = 0 to theta0 - 1.
  *  Calculate theta = atan2(y - d2, x - d1).
- *  Calculate d4 = sqrt((x - d1)**2 + (y - d2)**2)/(1 - cos(theta - d3)) if theta != d3.
- *  Calculate d4 = 0 if theta = d3.
+ *  Calculate d4 = sqrt((x - d1)**2 + (y - d2)**2)/(1 + cos(theta)) if theta != PI and theta != -PI.
+ *  Don't calculate d4 if theta = PI or theta = -PI.
  *  d4 goes from 0 to maxRad = max(xDim-1, yDim-1)/2.0.  s4 is the dimension 4 scaling factor.
  *  s4 * (rad - 1) = maxRad.
  *  s4 = maxRad/(rad - 1)
  *  n = d4*(rad - 1)/maxRad.
  *  Only calculate the Hough transform for d4 <= maxRad.
  *  
- *  Find the peak point in the x0, y0, theta0, rad Hough transform space.
- *  Put the values for this peak point in x0Array[c], y0Array[c], theta0Array[c], radArray[c], and
+ *  Find the peak point in the x0, y0, rad Hough transform space.
+ *  Put the values for this peak point in x0Array[c], y0Array[c], radArray[c], and
  *  countArray[c].
  *  
  *  If more cardioids are to be found, then zero the houghBuffer and run through the
@@ -47,7 +48,7 @@ import java.io.*;
  *  point in the Hough buffer. So on the next run of the Hough transform the source points that
  *  contributed to the Hough peak value just detected will not be present.
  *  
- *  Create a dialog with numCardioidsFound x0Array[i], y0Array[i], theta0Array[i], radArray[i], and
+ *  Create a dialog with numCardioidsFound x0Array[i], y0Array[i], radArray[i], and
  *  countArray[i] values, where the user will select a check box to have that circle drawn.
  *  
  *  References: 1.) Digital Image Processing, Second Edition by Richard C. Gonzalez and Richard E. Woods, Section 10.2.2
@@ -65,8 +66,8 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
     // Number of dimension 2 bins in Hough transform space
     private int y0;
     
-    // number of dimension 3 bins in Hough transform space
-    private int theta0;
+    // Cusp angle
+    private double theta0;
     
     // number of dimension 4 bins in Hough transform space
     private int rad;
@@ -94,7 +95,7 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
      * @param  rad   number of dimension 4 bins in Hough transform space
      * @param  numCardioids number of cardioids to be found
      */
-    public AlgorithmHoughCardioid(ModelImage destImg, ModelImage srcImg, int x0, int y0, int theta0, 
+    public AlgorithmHoughCardioid(ModelImage destImg, ModelImage srcImg, int x0, int y0, double theta0, 
     		                      int rad, int numCardioids) {
         super(destImg, srcImg);
         this.x0 = x0;
@@ -131,13 +132,11 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
         
         int houghSlice;
         byte[] srcBuffer;
-        float theta0Buffer[];
         float radBuffer[];
         int[] houghBuffer;
         double theta;
         double d1Array[];
         double d2Array[];
-        double d3Array[];
         double d4;
         double d4Scale;
         boolean test = false;
@@ -151,7 +150,6 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
         int numCardioidsFound;
         double x0Array[];
         double y0Array[];
-        double theta0Array[];
         double radArray[];
         int countArray[];
         boolean selectedCardioid[];
@@ -160,7 +158,7 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
         int maxCardioidPoints;
         double maxRad;
         int x0y0;
-        int x0y0theta0;
+        int radCount;
         double xSum;
         double xSum2;
         double xSum3;
@@ -187,8 +185,7 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
         maxRad = Math.max(xDimSource - 1, yDimSource - 1)/2.0;
 
         x0y0 = x0 * y0;
-        x0y0theta0 = x0y0 * theta0;
-        houghSlice = x0y0theta0 * rad;
+        houghSlice = x0y0 * rad;
         srcBuffer = new byte[sourceSlice];
 
         try {
@@ -217,11 +214,11 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
                 } // for (x = 0; x < xDimSource; x++)
             } // for (y = 0; y < yDimSource; y++)
             
-            xCenter = (xDimSource-1)/2.0;
+            xCenter = (xDimSource-1)/4.0;
             yCenter = (yDimSource-1)/2.0;
-            radius = 50.0;
-            radius2 = 70.0;
-            radius3 = 90.0;
+            radius = 20.0;
+            radius2 = 30.0;
+            radius3 = 40.0;
             xSum = 0.0;
             ySum = 0.0;
             xSum2 = 0.0;
@@ -230,20 +227,20 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
             ySum3 = 0.0;
             for (i = 0; i < 20; i++) {
                 theta = i * Math.PI/10.0;
-                x = (int)Math.round(xCenter + radius * (2.0 * Math.cos(theta) - Math.cos(2.0*theta)));
-                y = (int)Math.round(yCenter + radius * (2.0 * Math.sin(theta) - Math.sin(2.0*theta)));
+                x = (int)Math.round(xCenter + radius * (1.0 + 2.0 * Math.cos(theta) + Math.cos(2.0*theta)));
+                y = (int)Math.round(yCenter + radius * (2.0 * Math.sin(theta) + Math.sin(2.0*theta)));
                 index = x + y * xDimSource;
                 srcBuffer[index] = 1;
                 xSum = xSum + x;
                 ySum = ySum + y;
-                x = (int)Math.round(xCenter + radius2 * (2.0 * Math.cos(theta) - Math.cos(2.0*theta)));
-                y = (int)Math.round(yCenter + radius2 * (2.0 * Math.sin(theta) - Math.sin(2.0*theta)));
+                x = (int)Math.round(xCenter + radius2 * (1.0 + 2.0 * Math.cos(theta) + Math.cos(2.0*theta)));
+                y = (int)Math.round(yCenter + radius2 * (2.0 * Math.sin(theta) + Math.sin(2.0*theta)));
                 index = x + y * xDimSource;
                 srcBuffer[index] = 1;
                 xSum2 = xSum2 + x;
                 ySum2 = ySum2 + y;
-                x = (int)Math.round(xCenter + radius3 * (2.0 * Math.cos(theta) - Math.cos(2.0*theta)));
-                y = (int)Math.round(yCenter + radius3 * (2.0 * Math.sin(theta) - Math.sin(2.0*theta)));
+                x = (int)Math.round(xCenter + radius3 * (1.0 + 2.0 * Math.cos(theta) + Math.cos(2.0*theta)));
+                y = (int)Math.round(yCenter + radius3 * (2.0 * Math.sin(theta) + Math.sin(2.0*theta)));
                 index = x + y * xDimSource;
                 srcBuffer[index] = 1;
                 xSum3 = xSum3 + x;
@@ -264,63 +261,69 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
             xSum = xSum/20.0;
             ySum = ySum/20.0;
             radSum = 0.0;
+            radCount = 0;
             for (i = 0; i < 20; i++) {
                 theta = i * Math.PI/10.0;
-                x = (int)Math.round(xCenter + radius *Math.cos(theta));
-                y = (int)Math.round(yCenter + radius * Math.sin(theta));
-                radSum = radSum + Math.sqrt((x - xSum)*(x - xSum) + (y - ySum)*(y - ySum));
+                x = (int)Math.round(xCenter + radius * (1.0 + 2.0 * Math.cos(theta) + Math.cos(2.0*theta)));
+                y = (int)Math.round(yCenter + radius * (2.0 * Math.sin(theta) + Math.sin(2.0*(theta))));
+                if (Math.abs(theta) != Math.PI) {
+                    radSum = radSum + Math.sqrt((x - xSum)*(x - xSum) + (y - ySum)*(y - ySum))/(1.0 + Math.cos(theta));
+                    radCount++;
+                }
             }
-            radSum = radSum/20.0;
-            System.out.println(" x = " + xSum + " y = " + ySum + " radius = " + radSum);
+            radSum = radSum/radCount;
+            System.out.println(" x = " + xSum + " y = " + ySum + " theta0 = " + theta0 + " radius = " + radSum);
             
             xSum2 = xSum2/20.0;
             ySum2 = ySum2/20.0;
             radSum = 0.0;
+            radCount = 0;
             for (i = 0; i < 20; i++) {
                 theta = i * Math.PI/10.0;
-                x = (int)Math.round(xCenter + radius2 *Math.cos(theta));
-                y = (int)Math.round(yCenter + radius2 * Math.sin(theta));
-                radSum = radSum + Math.sqrt((x - xSum2)*(x - xSum2) + (y - ySum2)*(y - ySum2));
+                x = (int)Math.round(xCenter + radius2 * (1.0 + 2.0 * Math.cos(theta) + Math.cos(2.0*theta)));
+                y = (int)Math.round(yCenter + radius2 * (2.0 * Math.sin(theta) + Math.sin(2.0*theta)));
+                if (Math.abs(theta) != Math.PI) {
+                    radSum = radSum + Math.sqrt((x - xSum2)*(x - xSum2) + (y - ySum2)*(y - ySum2))/(1.0 + Math.cos(theta));
+                    radCount++;
+                }
             }
-            radSum = radSum/20.0;
-            System.out.println(" x2 = " + xSum2 + " y2 = " + ySum2 + " radius2 = " + radSum);
+            radSum = radSum/radCount;
+            System.out.println(" x2 = " + xSum2 + " y2 = " + ySum2 + "theta0 = " + theta0 + " radius2 = " + radSum);
             
             xSum3 = xSum3/20.0;
             ySum3 = ySum3/20.0;
             radSum = 0.0;
+            radCount = 0;
             for (i = 0; i < 20; i++) {
                 theta = i * Math.PI/10.0;
-                x = (int)Math.round(xCenter + radius3 *Math.cos(theta));
-                y = (int)Math.round(yCenter + radius3 * Math.sin(theta));
-                radSum = radSum + Math.sqrt((x - xSum3)*(x - xSum3) + (y - ySum3)*(y - ySum3));
+                x = (int)Math.round(xCenter + radius3 * (1.0 + 2.0 * Math.cos(theta) + Math.cos(2.0*theta)));
+                y = (int)Math.round(yCenter + radius3 * (2.0 * Math.sin(theta) + Math.sin(2.0*theta)));
+                if (Math.abs(theta) != Math.PI) {
+                    radSum = radSum + Math.sqrt((x - xSum3)*(x - xSum3) + (y - ySum3)*(y - ySum3))/(1.0 + Math.cos(theta));
+                    radCount++;
+                }
             }
-            radSum = radSum/20.0;
-            System.out.println(" x3 = " + xSum3 + " y3 = " + ySum3 + " radius3 = " + radSum);
+            radSum = radSum/radCount;
+            System.out.println(" x3 = " + xSum3 + " y3 = " + ySum3 + " theta0 = " + theta0 + " radius3 = " + radSum);
         }
 
-        theta0Buffer = new float[houghSlice];
         radBuffer = new float[houghSlice];
         houghBuffer = new int[houghSlice];
         
-        // Calculate d1Array, d2Array, and d3Array values
+        // Calculate d1Array and d2Array
         d1Array = new double[x0];
         d2Array = new double[y0];
-        d3Array = new double[theta0];
         for (i = 0; i < x0; i++) {
             d1Array[i] = ((double)(i * (xDimSource - 1)))/((double)(x0 - 1));
         }
         for (i = 0; i < y0; i++) {
             d2Array[i] = ((double)(i * (yDimSource - 1)))/((double)(y0 - 1));
         }
-        for (i = 0; i < theta0; i++) {
-        	d3Array[i] = ((double)i * 2.0 * Math.PI/theta0);
-        }
         d4Scale = ((double)(rad - 1))/maxRad;
         maxCardioidPoints = (int)Math.ceil(2.0 * Math.PI * maxRad);
         
         x0Array = new double[numCardioids];
         y0Array = new double[numCardioids];
-        theta0Array = new double[numCardioids];
         radArray = new double[numCardioids];
         countArray = new int[numCardioids];
         numCardioidsFound = 0;
@@ -335,23 +338,20 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
                     if (srcBuffer[index] != 0) {
                         for (j = 0; j < x0; j++) {
                             for (k = 0; k < y0; k++) {
+                            	theta = Math.atan2(y - d2Array[k], x - d1Array[j]);
                                 num = Math.sqrt((x - d1Array[j])*(x - d1Array[j]) + (y - d2Array[k])*(y - d2Array[k]));
-                                theta = Math.atan2(y - d2Array[k], x - d1Array[j]);
-                                for (m = 0; m < theta0; m++) {
-                                	if (theta != theta0Array[m]) {
-                                	    d4 = num/(1.0 - Math.cos(theta - theta0Array[m]));	
-                                	}
-                                	else {
-                                		d4 = 0.0;
-                                	}
-	                                if (d4 <= maxRad) {
-	                                    n = (int)Math.round(d4*d4Scale);
-	                                    indexDest = j + k * x0 + m * x0y0 + n * x0y0theta0;
-	                                    theta0Buffer[indexDest] += theta0Array[m];
-	                                    houghBuffer[indexDest]++;
-	                                    radBuffer[indexDest] += d4;
-	                                }
-                                } // for (m = 0; m < theta0; m++)
+                            	if ((theta != Math.PI) && (theta != -Math.PI)) {
+                            	    d4 = num/(1.0 + Math.cos(theta));	
+                            	}
+                            	else {
+                            		d4 = Double.MAX_VALUE;
+                            	}
+                                if (d4 <= maxRad) {
+                                    m = (int)Math.round(d4*d4Scale);
+                                    indexDest = j + k * x0 + m * x0y0;
+                                    houghBuffer[indexDest]++;
+                                    radBuffer[indexDest] += d4;
+                                }
                             } // for (k = 0; k < y0; k++)
                         } // for (j = 0; j < x0; j++)
                     } // if (srcBuffer[index] != 0)
@@ -381,7 +381,6 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
             x0Array[c] = x0Array[c] * ((double)(xDimSource - 1))/((double)(x0 - 1));
             y0Array[c] = (largestIndex % x0y0)/x0;
             y0Array[c] = y0Array[c] * ((double)(yDimSource - 1))/((double)(y0 - 1));
-            theta0Array[c] = theta0Buffer[largestIndex]/largestValue;
             radArray[c] = radBuffer[largestIndex]/largestValue;
             countArray[c] = largestValue;
             
@@ -402,21 +401,19 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
                                 for (k = 0; k < y0; k++) {
                                 	theta = Math.atan2(y - d2Array[k], x - d1Array[j]);
                                     num = Math.sqrt((x - d1Array[j])*(x - d1Array[j]) + (y - d2Array[k])*(y - d2Array[k]));
-                                    for (m = 0; m < theta0; m++) {
-                                    	if (theta != theta0Array[m]) {
-                                    	    d4 = num/(1.0 - Math.cos(theta - theta0Array[m]));	
-                                    	}
-                                    	else {
-                                    		d4 = 0.0;
-                                    	}
-	                                    if (d4 <= maxRad) {
-	                                        n = (int)Math.round(d4*d4Scale);
-	                                        indexDest = j + k * x0 + m * x0y0 + n * x0y0theta0;
-	                                        if (indexDest == largestIndex) {
-	                                            srcBuffer[index] = 0;
-	                                        }
-	                                    }
-                                    } // for (m = 0; m < theta0; m++)
+                                    if ((theta != Math.PI) && (theta != -Math.PI)) {
+                                	    d4 = num/(1.0 + Math.cos(theta));	
+                                	}
+                                	else {
+                                		d4 = Double.MAX_VALUE;
+                                	}
+                                    if (d4 <= maxRad) {
+                                        m = (int)Math.round(d4*d4Scale);
+                                        indexDest = j + k * x0 + m * x0y0;
+                                        if (indexDest == largestIndex) {
+                                            srcBuffer[index] = 0;
+                                        }
+                                    }
                                 } // for (k = 0; k < y0; k++)
                             } // for (j = 0; j < x0; j++)
                         } // if (srcBuffer[index] != 0)
@@ -455,13 +452,15 @@ public class AlgorithmHoughCardioid extends AlgorithmBase {
             if (selectedCardioid[i]) {
                 for (j = 0; j < maxCardioidPoints; j++) {
                     theta = j * 2.0 * Math.PI/maxCardioidPoints;
-                    x = (int)Math.round(x0Array[i] + radArray[i]*Math.cos(theta));
-                    y = (int)Math.round(y0Array[i] + radArray[i]*Math.sin(theta));
+                    x = (int)Math.round(x0Array[i] + (radArray[i]/2.0)*( 1.0 + 2.0*Math.cos(theta) + 
+                    		Math.cos(2.0*theta)));
+                    y = (int)Math.round(y0Array[i] + (radArray[i]/2.0)*(2.0*Math.sin(theta) +
+                    		   Math.sin(2.0*theta)));
                     indexDest = x + y * xDimSource;
                     srcBuffer[indexDest] = value;
                 }
-            } // if (selectedCircle[i])
-        } // for (i = 0; i < numCirclesFound; i++)
+            } // if (selectedCardioid[i])
+        } // for (i = 0; i < numCardioidsFound; i++)
         
         try {
             destImage.importData(0, srcBuffer, true);
