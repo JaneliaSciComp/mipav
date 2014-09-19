b49665dcbb25c35790ae1d59ae9c3921decbe064
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-09-19 19:19:55 +0000

    Removed match type and change from multiple destination images to 1 destination image.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13088 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogBRISK.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogBRISK.java
@@ -2,34 +2,23 @@ package gov.nih.mipav.view.dialogs;
 
 
 import gov.nih.mipav.model.algorithms.*;
-import gov.nih.mipav.model.file.FileIO;
-import gov.nih.mipav.model.file.FileInfoDicom;
-import gov.nih.mipav.model.file.FileUtility;
 import gov.nih.mipav.model.scripting.*;
 import gov.nih.mipav.model.scripting.parameters.*;
 import gov.nih.mipav.model.structures.*;
 import gov.nih.mipav.view.*;
-import gov.nih.mipav.view.components.JPanelColorChannels;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.io.File;
+import java.util.Enumeration;
 import java.util.Vector;
 
 import javax.swing.ButtonGroup;
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JFileChooser;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.filechooser.FileFilter;
 
 //import javax.swing.*;
 
@@ -37,8 +26,8 @@ import javax.swing.filechooser.FileFilter;
 /**
  * DOCUMENT ME!
  */
-public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInterface, ActionDiscovery, ScriptableActionInterface,
-                                                                   ListSelectionListener{
+@SuppressWarnings("serial")
+public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInterface, ActionDiscovery, ScriptableActionInterface {
 
     //~ Static fields/initializers -------------------------------------------------------------------------------------
 
@@ -52,7 +41,7 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
     private ModelImage image; // source image
 
     /** DOCUMENT ME! */
-    private ModelImage destImage[] = null; // match image array
+    private ModelImage destImage = null; // match image array
 
     /** DOCUMENT ME! */
     private AlgorithmBRISK bAlgo;
@@ -68,12 +57,6 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
 	private boolean scaleInvariant = true;
 	
 	private double patternScale = 1.0;
-   	
-    private static final int radiusMatch = 1;
-	
-	private static final int knnMatch = 2;
-	
-	private int match = radiusMatch;
 	
 	private Vector<Double>radiusList = null;
 	
@@ -86,24 +69,6 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
 	private double dMin = 8.2;
 	
 	private Vector<Integer>indexChange = new Vector<Integer>();
-	
-	private JPanel imagePanel;
-	
-	private DefaultListModel model;
-	
-	 private JList imageList;
-	 
-	 private JButton chooserButton;
-	 
-	 private JButton removeButton;
-	 
-	 private ButtonGroup matchGroup;
-	 
-	 private JRadioButton  radiusButton;
-	 
-	 private JRadioButton  knnButton;
-	 
-	 private JPanel matchPanel;
 	 
 	 private JPanel paramPanel;
 	 
@@ -115,15 +80,7 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
 	 
 	 private JRadioButton VOIRegions;
 	 
-	 private boolean changeRemoveIndex = true;
-	 
-	 private int removeIndex;
-	 
 	 private ViewUserInterface userInterface;
-	 
-	 private int destNumber = 0;
-	 
-	 private ModelImage[] tempImage = null;
 	 
 	 private JLabel labelThreshold;
 	 
@@ -142,6 +99,8 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
 	 private JCheckBox rotationCheckBox;
 	 
 	 private JCheckBox scaleCheckBox;
+	 
+	 private JComboBox comboBoxImage;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
@@ -172,8 +131,6 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
      */
     public void actionPerformed(ActionEvent event) {
         String command = event.getActionCommand();
-        int i;
-        int j;
 
         if (command.equals("OK")) {
 
@@ -181,77 +138,6 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
                 callAlgorithm();
             }
         }
-            
-        else if (command.equals("Choose")) {
-            ModelImage newImage = open();
-
-            if (!checkImage(newImage)) {
-                return;
-            }
-
-            destNumber++;
-            if (destNumber > 1) {
-	            tempImage = new ModelImage[destNumber-1];
-	
-	            for (i = 0; i < (destNumber-1); i++) {
-	                tempImage[i] = destImage[i];
-	            }
-	
-	            destImage = null;
-	            destImage = new ModelImage[destNumber];
-	
-	            for (i = 0; i < (destNumber - 1); i++) {
-	                destImage[i] = tempImage[i];
-	            }
-	
-	            tempImage = null;
-            } // if (destNumber > 1)
-            else {
-            	destImage = new ModelImage[destNumber];
-            }
-            destImage[destNumber - 1] = newImage;
-            model.addElement(destImage[destNumber - 1].getImageName());
-
-            newImage = null;
-            removeButton.setEnabled(true);
-        } // if (command.equals("Choose"))
-        else if ((command.equals("Remove")) && (removeIndex == 0)) {
-
-            // Cannot remove original image
-            MipavUtil.displayError("Cannot remove original loaded image");
-        } else if ((command.equals("Remove")) && (removeIndex <= (destNumber - 1))) {
-
-            // changeRemoveIndex = false is needed because the model.removeElement
-            // line causes valueChanged to execute.  Without changeRemoveIndex an
-            // unselected element causes removeIndex to be changed to -1.
-            changeRemoveIndex = false;
-            model.removeElement(destImage[removeIndex].getImageName());
-            tempImage = new ModelImage[destNumber - 1];
-
-            for (i = 0, j = 0; i < destNumber; i++) {
-
-                if (i != removeIndex) {
-                    tempImage[j++] = destImage[i];
-                }
-            } // for ( i = 0, j=0; i < destNumber; i++)
-
-            destImage[removeIndex].disposeLocal();
-            destImage[removeIndex] = null;
-            changeRemoveIndex = true;
-            destImage = null;
-            destImage = new ModelImage[destNumber - 1];
-
-            for (i = 0; i < (destNumber - 1); i++) {
-                destImage[i] = tempImage[i];
-            }
-
-            tempImage = null;
-            destNumber--;
-
-            if (destNumber == 0) {
-                removeButton.setEnabled(false);
-            }
-        } // else if ((command.equals("Remove"))  && (removeIndex <= (destNumber - 1)))
         else if (command.equals("Help")) {
             //MipavUtil.showHelp("");
         } else if (command.equals("Cancel")) {
@@ -317,7 +203,7 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
 
             // Make algorithm
             bAlgo = new AlgorithmBRISK(destImage, image, wholeImageFlag, threshold, HammingDistanceThreshold, octaves, 
-            		rotationInvariant, scaleInvariant, patternScale, match, radiusList, numberList,
+            		rotationInvariant, scaleInvariant, patternScale, radiusList, numberList,
                     dMax, dMin, indexChange);
 
             // This is very important. Adding this object as a listener allows the algorithm to
@@ -380,6 +266,11 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
     	setForeground(Color.black);
 
         setTitle("BRISK");
+        
+        JLabel labelImage = new JLabel("Base image [" + image.getImageName() + "] for:");
+        labelImage.setForeground(Color.black);
+        labelImage.setFont(serif12);
+        comboBoxImage = buildImgComboBox(image);
 
         labelThreshold = new JLabel("Detection threshold");
         labelThreshold.setForeground(Color.black);
@@ -425,38 +316,45 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
         gbc.gridy = 0;
         gbc.weightx = 0;
         gbc.fill = GridBagConstraints.NONE;
+        upperPanel.add(labelImage, gbc);
+        gbc.gridy = 1;
+        upperPanel.add(comboBoxImage, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 0;
+        gbc.fill = GridBagConstraints.NONE;
         upperPanel.add(labelThreshold, gbc);
         gbc.gridx = 1;
-        gbc.gridy = 0;
+        gbc.gridy = 2;
         gbc.weightx = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         upperPanel.add(textThreshold, gbc);
         gbc.gridx = 0;
-        gbc.gridy = 1;
+        gbc.gridy = 3;
         gbc.weightx = 0;
         gbc.fill = GridBagConstraints.NONE;
         upperPanel.add(labelHammingDistanceThreshold, gbc);
         gbc.gridx = 1;
-        gbc.gridy = 1;
+        gbc.gridy = 3;
         gbc.weightx = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         upperPanel.add(textHammingDistanceThreshold, gbc);
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 4;
         gbc.weightx = 0;
         gbc.fill = GridBagConstraints.NONE;
         upperPanel.add(labelOctaves, gbc);
         gbc.gridx = 1;
-        gbc.gridy = 2;
+        gbc.gridy = 4;
         gbc.weightx = 1;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         upperPanel.add(textOctaves, gbc);
         gbc.gridx = 0;
-        gbc.gridy = 3;
+        gbc.gridy = 5;
         gbc.gridwidth = 2;
         upperPanel.add(rotationCheckBox, gbc);
         gbc.gridx = 0;
-        gbc.gridy = 4;
+        gbc.gridy = 6;
         gbc.gridwidth = 2;
         upperPanel.add(scaleCheckBox, gbc);
 
@@ -483,27 +381,6 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
         gbc.gridy = 1;
         imageVOIPanel.add(VOIRegions, gbc);
 
-        matchGroup = new ButtonGroup();
-        radiusButton = new JRadioButton("Radius match", true);
-        radiusButton.setFont(serif12);
-        matchGroup.add(radiusButton);
-
-        knnButton = new JRadioButton("knn match", false);
-        knnButton.setFont(serif12);
-        matchGroup.add(knnButton);
-        
-        matchPanel = new JPanel(new GridBagLayout());
-        matchPanel.setBorder(buildTitledBorder("Matching method"));
-
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 0;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.gridwidth = 1;
-        matchPanel.add(radiusButton, gbc);
-        gbc.gridy = 1;
-        matchPanel.add(knnButton, gbc);
-
         paramPanel = new JPanel(new GridBagLayout());
         paramPanel.setForeground(Color.black);
         paramPanel.setBorder(buildTitledBorder("Parameters"));
@@ -518,46 +395,6 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
         gbc.gridy = ypos++;
         gbc.gridwidth = 1;
         paramPanel.add(imageVOIPanel, gbc);
-        gbc.gridx = 1;
-        paramPanel.add(matchPanel, gbc);
-
-        
-
-        imagePanel = new JPanel(new BorderLayout());
-        imagePanel.setBorder(buildTitledBorder("Load Image(s)"));
-
-        model = new DefaultListModel();
-        imageList = new JList(model);
-        imageList.setVisibleRowCount(6);
-        imageList.setPreferredSize(new Dimension(300, 120));
-        imageList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        imageList.addListSelectionListener(this);
-        imagePanel.add(imageList);
-
-        JPanel chooserPanel = new JPanel();
-        chooserButton = new JButton("Load");
-        chooserButton.setPreferredSize(MipavUtil.defaultButtonSize);
-        chooserButton.setFont(serif12B);
-        chooserPanel.add(chooserButton);
-        chooserButton.addActionListener(this);
-        chooserButton.setActionCommand("Choose");
-
-        removeButton = new JButton("Remove");
-        removeButton.setPreferredSize(MipavUtil.defaultButtonSize);
-        removeButton.setFont(serif12B);
-        removeButton.setEnabled(false);
-        chooserPanel.add(removeButton);
-        removeButton.addActionListener(this);
-        removeButton.setActionCommand("Remove");
-
-        imagePanel.add(chooserPanel, BorderLayout.SOUTH);
-
-        gbc.gridx = 0;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.gridy = ypos++;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 1;
-        paramPanel.add(imagePanel, gbc);
 
         getContentPane().add(paramPanel);
         getContentPane().add(buildButtons(), BorderLayout.SOUTH);
@@ -567,141 +404,37 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
     }
     
     /**
-     * Open an image based on the suffix of the file.
+     * Builds a list of images. Returns combobox.
      *
-     * @return  The image.
+     * @param   image  DOCUMENT ME!
+     *
+     * @return  Newly created combo box.
      */
-    private ModelImage open() {
-        JFileChooser chooser = null;
-        FileIO fileIO = null;
-        boolean multiFile = false;
-        String fileName;
-        String directory;
+    private JComboBox buildImgComboBox(ModelImage image) {
+        JComboBox comboBox = new JComboBox();
+        comboBox.setFont(serif12);
+        comboBox.setBackground(Color.white);
 
-        try {
+        Enumeration<String> names = userInterface.getRegisteredImageNames();
 
-            chooser = new JFileChooser();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
 
-            if (userInterface.getDefaultDirectory() != null) {
-                File file = new File(userInterface.getDefaultDirectory());
+            if (!name.equals(image.getImageName())) {
+                ModelImage img = userInterface.getRegisteredImageByName(name);
 
-                if (file != null) {
-                    chooser.setCurrentDirectory(file);
-                } else {
-                    chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
+                if ((img.getNDims() == 2) && (!img.isColorImage()) &&
+                        (userInterface.getFrameContainingImage(img) != null)) {
+                    comboBox.addItem(name);
                 }
-            } else {
-                chooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-            }
-
-            chooser.addChoosableFileFilter(new ViewImageFileFilter(ViewImageFileFilter.GEN));
-            chooser.addChoosableFileFilter(new ViewImageFileFilter(ViewImageFileFilter.TECH));
-            chooser.addChoosableFileFilter(new ViewImageFileFilter(ViewImageFileFilter.MICROSCOPY));
-            chooser.addChoosableFileFilter(new ViewImageFileFilter(ViewImageFileFilter.MISC));
-
-            FileFilter currentFileFilter = getFileFilter(chooser, Preferences.getFileFilter());
-            chooser.setFileFilter(currentFileFilter);
-
-            chooser.setDialogTitle("Open Image");
-
-            int returnValue = chooser.showOpenDialog(userInterface.getMainFrame());
-
-            if (returnValue == JFileChooser.APPROVE_OPTION) {
-                fileName = chooser.getSelectedFile().getName();
-                directory = String.valueOf(chooser.getCurrentDirectory()) + File.separatorChar;
-                userInterface.setDefaultDirectory(directory);
-            } else {
-                return null;
-            }
-        } catch (OutOfMemoryError e) {
-            MipavUtil.displayError("Out of memory!");
-
-            return null;
-        }
-
-        try {
-            fileIO = new FileIO();
-
-            return fileIO.readImage(fileName, directory, multiFile, null);
-        } catch (OutOfMemoryError e) {
-            MipavUtil.displayError("Out of memory!");
-
-            return null;
-        }
-    }
-    
-    /**
-     * A private helper function to get the current used FileFilter from JFileChooser.
-     *
-     * @param   chooser  DOCUMENT ME!
-     * @param   index    the index of the choosable file filters.
-     *
-     * @return  the current used file filter.
-     */
-    private FileFilter getFileFilter(JFileChooser chooser, int index) {
-        FileFilter[] filters = chooser.getChoosableFileFilters();
-        String[] descriptions = ViewImageFileFilter.getDescriptions();
-
-        for (int i = 0; i < filters.length; i++) {
-
-            if (filters[i].getDescription().equals(descriptions[index])) {
-                return filters[i];
             }
         }
+        comboBox.addItemListener(this);
 
-        return null;
+        return comboBox;
     }
     
-    /**
-     * Checks the color and dimensionality of the new image vs. the original source image. All new images should have
-     * the same color modality as the source and be of the same dimensions.
-     *
-     * @param   testImage  DOCUMENT ME!
-     *
-     * @return  Flag indicating if the image checks out.
-     */
-    private boolean checkImage(ModelImage testImage) {
-
-        if (testImage == null) {
-            return false;
-        }
-
-        if (testImage.isColorImage()) {
-            MipavUtil.displayError("Cannot load a color (" + testImage.getImageName() +
-                                   ")");
-
-            return false;
-        }
-        
-        if (testImage.isComplexImage()) {
-        	MipavUtil.displayError("Cannot load a complex (" + testImage.getImageName() +
-                    ")");
-
-            return false;	
-        }
-
-        if (testImage.getNDims() != 2) {
-            MipavUtil.displayError("Error! " + testImage.getImageName() + " is " + testImage.getNDims() + "D" + "instead of 2D");
-
-            return false;
-        }
-
-        return true;
-
-    }
-    
-    /**
-     * Sets the remove index based on the selected index in the list.
-     *
-     * @param  evt  Event that caused this method to fire.
-     */
-    public void valueChanged(ListSelectionEvent evt) {
-
-        if (changeRemoveIndex) {
-            JList source = (JList) evt.getSource();
-            removeIndex = source.getSelectedIndex();
-        }
-    }
+   
 
     /**
      * Use the GUI results to set up the variables needed to run the algorithm.
@@ -710,6 +443,11 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
      */
     private boolean setVariables() {
     	String tmpStr;
+    	
+    	// assign destImage to image selected in comboBox
+        String selectedName = (String) comboBoxImage.getSelectedItem();
+
+        destImage = ViewUserInterface.getReference().getRegisteredImageByName(selectedName);
 
         if (wholeImage.isSelected()) {
             wholeImageFlag = true;
@@ -854,7 +592,7 @@ public class JDialogBRISK extends JDialogScriptableBase implements AlgorithmInte
      * @return  the result image
      */
     public ModelImage getResultImage() {
-        return destImage[0];
+        return destImage;
     }
 
     
