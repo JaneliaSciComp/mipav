1c6f3749bc0b2ed5f81bebc530e569434f772e36
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2014-12-16 20:38:56 +0000

    Added optional parameter to SaveTab to override default directory ("output_directory")
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13279 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/scripting/actions/ActionSaveTab.java
+++ trunk/mipav/src/gov/nih/mipav/model/scripting/actions/ActionSaveTab.java
@@ -1,8 +1,12 @@
 package gov.nih.mipav.model.scripting.actions;
 
 
-import gov.nih.mipav.model.scripting.*;
-import gov.nih.mipav.model.scripting.parameters.*;
+import gov.nih.mipav.model.scripting.ParserException;
+import gov.nih.mipav.model.scripting.ScriptRecorder;
+import gov.nih.mipav.model.scripting.ScriptableActionInterface;
+import gov.nih.mipav.model.scripting.parameters.ParameterFactory;
+import gov.nih.mipav.model.scripting.parameters.ParameterTable;
+
 import gov.nih.mipav.view.MipavUtil;
 import gov.nih.mipav.view.ViewUserInterface;
 
@@ -11,59 +15,73 @@ import gov.nih.mipav.view.ViewUserInterface;
  * A script action which records saves the text contained in one of the output frame's tabs.
  */
 public class ActionSaveTab implements ScriptableActionInterface {
+    /**
+     * The (optional) label to use for the directory in which to save the tab contents.
+     */
+    private static final String DIR_LABEL = "output_directory";
 
     /**
      * The label to use for the tab name parameter.
      */
     private static final String TAB_NAME_LABEL = "output_tab_name";
-    
+
     /**
      * The name of the tab to record the saving of.
      */
     private String recordingTabName;
-    
+
     /**
      * Constructor for the dynamic instantiation and execution of the SaveTab script action.
      */
     public ActionSaveTab() {}
-    
+
     /**
      * Constructor used to record the SaveTab script action line.
-     * @param tabName  The name of the tab being saved.
+     * 
+     * @param tabName The name of the tab being saved.
      */
-    public ActionSaveTab(String tabName) {
+    public ActionSaveTab(final String tabName) {
         recordingTabName = tabName;
     }
-    
-    //~ Methods --------------------------------------------------------------------------------------------------------
+
+    // ~ Methods
+    // --------------------------------------------------------------------------------------------------------
 
     /**
      * {@inheritDoc}
      */
+    @Override
     public void insertScriptLine() {
-        ParameterTable parameters = new ParameterTable();
+        final ParameterTable parameters = new ParameterTable();
         try {
             parameters.put(ParameterFactory.newString(TAB_NAME_LABEL, recordingTabName));
-        } catch (ParserException pe) {
+        } catch (final ParserException pe) {
             MipavUtil.displayError("Error encountered creating tab name parameter while recording SaveTab script action:\n" + pe);
             return;
         }
-        
+
         ScriptRecorder.getReference().addLine("SaveTab", parameters);
     }
 
     /**
      * {@inheritDoc}
      */
-    public void scriptRun(ParameterTable parameters) {
-        ViewUserInterface.getReference().getMessageFrame().save(parameters.getString(TAB_NAME_LABEL));
+    @Override
+    public void scriptRun(final ParameterTable parameters) {
+        if (parameters.containsParameter(DIR_LABEL)) {
+            ViewUserInterface.getReference().getMessageFrame().save(parameters.getString(TAB_NAME_LABEL), parameters.getString(TAB_NAME_LABEL));
+        } else {
+            ViewUserInterface.getReference().getMessageFrame().save(parameters.getString(dir, TAB_NAME_LABEL));
+        }
+
     }
-    
+
     /**
      * Changes the name of the tab being saved.
-     * @param tabName  The name of the tab being saved.
+     * 
+     * @param tabName The name of the tab being saved.
      */
-    public void setTabName(String tabName) {
+    public void setTabName(final String tabName) {
         recordingTabName = tabName;
     }
 }
