c1e8e67b42562ef3bb4065d9e3fb8c5aab9a39af
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-02-04 23:11:09 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13382 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmTextureSegmentation.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmTextureSegmentation.java
@@ -2,6 +2,8 @@ package gov.nih.mipav.model.algorithms;
 
 import java.io.IOException;
 
+import de.jtem.numericalMethods.algebra.linear.decompose.Eigenvalue;
+import Jama.Matrix;
 import gov.nih.mipav.model.structures.ModelImage;
 import gov.nih.mipav.model.structures.ModelStorageBase;
 import gov.nih.mipav.view.MipavUtil;
@@ -30,26 +32,44 @@ public class AlgorithmTextureSegmentation extends AlgorithmBase implements Algor
 	
 	private double filter[];
 	
+	// An integration scale.  A windowSize by windowSize square window
 	private int windowSize = 25;
 	
 	// Number of segments.  Determined automatically if set to 0.
 	private int segmentNumber = 0;
 	
+	// True when nonnegativity constraint imposed
+	private boolean nonNegativity = true;
+	
+	// For segment number estimation based on singular values.  May need to be
+	// tuned if the choice of filters are changed.
+	private double omega = 0.05;
+	
+	private int filterNumber = 11;
+	
+	private int binNumber = 11;
+	
+	private int xDim;
+	
+	private int yDim;
+	
+	
 	// ~ Constructors
 	// ---------------------------------------------------------------------------------------------------
 	public AlgorithmTextureSegmentation(ModelImage destImage, ModelImage srcImage,
-			                            int windowSize, int segmentNumber) {
+			                            int windowSize, int segmentNumber, boolean nonNegativity) {
 		super(destImage, srcImage);
 		this.windowSize = windowSize;
 		this.segmentNumber = segmentNumber;
+		this.nonNegativity = nonNegativity;
 	}
 	
 	public void runAlgorithm() {
-		int xDim = srcImage.getExtents()[0];
-		int yDim = srcImage.getExtents()[0];
+		xDim = srcImage.getExtents()[0];
+		yDim = srcImage.getExtents()[0];
 		int length = xDim * yDim;
 		double srcBuffer[];
-		double Ig[][];
+		double Ig[][][];
 		int i;
 		int kExtents[] = new int[2];
 		int halfMask;
@@ -72,6 +92,30 @@ public class AlgorithmTextureSegmentation extends AlgorithmBase implements Algor
 		double xPrime;
 		double yPrime;
 		double theta = 0.0;
+		int ws; 
+		double sh_mx[][][];
+		int bb;
+		double Y[][];
+		Matrix matY;
+		double SE[][];
+		double eigenvalue[];
+	    double eigenvector[][];
+	    int m;
+	    int n;
+	    int index;
+	    double temp;
+	    double tempCol[];
+	    double sqk[];
+	    double lse[];
+	    int dimn;
+	    double U1[][];
+	    Matrix matU1;
+	    double Y1[][];
+	    double Y2[][][];
+	    double tmp[][];
+	    byte intreg[][];
+	    byte intreg1[][];
+	    double maxtmp;
 		if (srcImage.isColorImage()) {
 			
 		}
@@ -82,15 +126,13 @@ public class AlgorithmTextureSegmentation extends AlgorithmBase implements Algor
 			try {
 				srcImage.exportData(0, length, srcBuffer);
 			}
-			catch(IOException e) {
-				MipavUtil.displayError("IOException " + e + " on srcImage.exportData(0, length, srcBuffer");
-				setCompleted(false);
-				return;
-			}
+			catch(IOException e) {}
 			
-			Ig = new double[length][11];
-			for (i = 0; i < length; i++) {
-			    Ig[i][0] = srcBuffer[i];	
+			Ig = new double[yDim][xDim][filterNumber];
+			for (y = 0; y < yDim; y++)  {
+				for (x = 0; x < xDim; x++) {
+			        Ig[y][x][0] = srcBuffer[x + y * xDim];
+				}
 			}
 			
 			for (i = 1; i <= 10; i++) {
@@ -218,12 +260,341 @@ public class AlgorithmTextureSegmentation extends AlgorithmBase implements Algor
 		        convolver.run();
 		        for (y = halfMask; y <= yDim + halfMask -1; y++) {
 		        	for (x = halfMask; x <= xDim + halfMask - 1; x++) {
-		        	    Ig[(x - halfMask) + (y - halfMask)*xDim][i]= filter[x + y * (xDim + 2 * halfMask)];	
+		        	    Ig[y - halfMask][x - halfMask][i]= filter[x + y * (xDim + 2 * halfMask)];	
 		        	}
 		        }
 			} // for (i = 1; i <= 10; i++)
+			
+			ws = windowSize/2;
+			sh_mx = new double[filterNumber*binNumber][yDim][xDim];
+			SHcomp(sh_mx,ws,Ig);
+			for (y = 0; y < yDim; y++) {
+				for (x = 0;x < xDim; x++) {
+					Ig[y][x] = null;
+				}
+				Ig[y] = null;
+			}
+			Ig = null;
+			bb = filterNumber * binNumber;
+			Y = new double[bb][yDim * xDim];
+			for (i = 0; i < bb; i++) {
+				for (y = 0; y < yDim; y++) {
+					for (x = 0; x < xDim; x++) {
+						Y[i][x + y * xDim] = sh_mx[i][y][x];
+					}
+				}
+			}
+			for (i = 0; i < bb; i++) {
+				for (y = 0; y < yDim; y++) {
+					sh_mx[i][y] = null;
+				}
+				sh_mx[i] = null;
+			}
+			sh_mx = null;
+			matY = new Matrix(Y);
+			SE = (matY.times(matY.transpose())).getArray();
+			eigenvalue = new double[SE[0].length];
+			eigenvector = new double[SE.length][SE[0].length];
+			Eigenvalue.decompose(SE, eigenvector, eigenvalue);
+			
+			// Arrange the eigenvalues and corresponding eigenvectors
+	        // in descending order so that e0 >= e1 >= e2
+	        // Only the largest eigenvalue should be positive
+			tempCol = new double[SE.length];
+	        for (m = 0; m < SE[0].length; m++) {
+	            index = m;
+
+	            for (n = m + 1; n < SE[0].length; n++) {
+
+	                if (eigenvalue[n] > eigenvalue[index]) {
+	                    index = n;
+	                }
+	            } // for (m = m+1; n < SE[0].length; n++)
+
+	            if (index != m) {
+	                temp = eigenvalue[m];
+	                eigenvalue[m] = eigenvalue[index];
+	                eigenvalue[index] = temp;
+
+	                for (n = 0; n < SE.length; n++) {
+	                    tempCol[n] = eigenvector[n][m];
+	                    eigenvector[n][m] = eigenvector[n][index];
+	                    eigenvector[n][index] = tempCol[n];
+	                }
+	            } // if (index != m)
+	        } // for (m = 0; m < Se[0].length; m++)
+	        sqk = new double[SE[0].length];
+	        for (i = 0; i < SE[0].length; i++) {
+	        	sqk[i] = Math.sqrt(Math.abs(eigenvalue[i]));
+	        }
+	        
+	        if (segmentNumber == 0) {
+	        	// Estimate the segment number
+	        	temp = 0.0;
+	        	lse = new double[sqk.length];
+	        	for (i = sqk.length-1; i >= 0; i--) {
+	        	    temp = temp + sqk[i]*sqk[i];
+	        	    lse[i] = temp/yDim/xDim;
+	        	    if (lse[i] > omega) {
+	        	    	segmentNumber++;
+	        	    }
+	        	} // for (i = sqk.length-1; i >= 0; i--)
+	        	if (segmentNumber <= 1) {
+	        		segmentNumber = 2;
+	        		MipavUtil.displayWarning("Segment numnber is set to 2.  Adjust omega for better results.");
+	        	}
+	        } // if (segmentNumber == 0)
+	        
+	        dimn = segmentNumber;
+	        U1 = new double[eigenvector.length][dimn];
+	        for (y = 0; y < eigenvector.length; y++) {
+	        	for (x = 0; x < dimn; x++) {
+	        		U1[y][x] = eigenvector[y][x];
+	        	}
+	        }
+	        matU1 = new Matrix(U1);
+	        // Project features onto the subspace
+	        // Y1 is dimn by yDim*xDim
+	        Y1 = (((matY.transpose()).times(matU1)).transpose()).getArray();
+	        Y2 = new double[dimn][yDim][xDim];
+	        for (i = 0; i < dimn; i++) {
+	        	for (y = 0; y < yDim; y++) {
+	        		for (x = 0; x < xDim; x++) {
+	        			Y2[i][y][x] = Y1[i][x + y * xDim];
+	        		}
+	        	}
+	        }
+	        tmp = new double[yDim][xDim];
+	        SHedge_ls(tmp, ws, 2, Y2);
+	        intreg = new byte[yDim][xDim];
+	        intreg1 = new byte[yDim][xDim];
+	        maxtmp = -Double.MAX_VALUE;
+	        for (y = 0; y < yDim; y++) {
+	        	for (x = 0; x < xDim; x++) {
+	        		if (tmp[y][x] > maxtmp) {
+	        			maxtmp = tmp[y][x];
+	        		}
+	        	}
+	        }
 		} // else not color
 		
+	}
+	
+	private void SHedge_ls(double EdgeMap[][], int ws, int dism, double sh_mx[][][]) {
+	    int y;
+	    int x;
+	    int k;
+	    int dn = sh_mx.length;
+	    double up[] = new double[dn];
+	    double bt[] = new double[dn];
+	    double lf[] = new double[dn];
+	    double rt[] = new double[dn];
+	    
+	    for (y = 0; y < yDim; y++) {
+	    	for (x = 0; x < xDim; x++) {
+	    	    for (k = 0; k < dn; k++) {
+	    	        up[k] = sh_mx[k][Math.max(0, y-ws)][x];	
+	    	    }
+	    	    for (k = 0; k < dn; k++) {
+	    	        bt[k] = sh_mx[k][Math.min(yDim-1, y + ws)][x];	
+	    	    }
+	    	    for (k = 0; k < dn; k++) {
+	    	    	lf[k] = sh_mx[k][y][Math.max(0,  x - ws)];
+	    	    }
+	    	    for (k = 0; k < dn; k++) {
+	    	    	rt[k] = sh_mx[k][y][Math.min(xDim-1,  x + ws)];
+	    	    }
+	    	    if (dism == 1) {
+	    	        EdgeMap[y][x] = x2dist(up,bt,dn) + x2dist(lf,rt,dn);	
+	    	    }
+	    	    else {
+	    	        EdgeMap[y][x] = Math.sqrt(l2dist(up,bt,dn) + l2dist(lf,rt,dn));	
+	    	    }
+	    	}
+	    }
+	}
+	
+	private double x2dist(double a[], double b[], int k) {
+		int i;
+		double T;
+		double sum;
+		double diff;
+		T = 0.0;
+		for (i = 0; i <k; i++) {
+			sum = a[i] + b[i];
+			if (sum != 0.0) {
+				diff = a[i] - b[i];
+				T = T + diff*diff/sum;
+			}
+		}
+		return T;
+	}
+	
+	private double l2dist(double a[], double b[], int k) {
+	    int i;
+	    double T;
+	    double diff;
+	    T = 0.0;
+	    for (i = 0; i < k; i++) {
+	    	diff = a[i] - b[i];
+	    	T = T + diff*diff;
+	    }
+	    return T;
+	}
+	
+	private void SHcomp(double sh_mx[][][], int ws, double Ig[][][]) {
+	    // Compute integral histograms	
+		double HImap[] = new double[binNumber*filterNumber*xDim*yDim];
+		int b;
+		double Imax;
+		double Imin;
+		int y;
+		int x;
+		double U;
+		double binc[] = new double[binNumber];
+		int k;
+		double md;
+		double tmp;
+		int mdid = 0;
+		double tmpm[] = new double[binNumber];
+		int dims[] = new int[3];
+		int wtl[] = new int[2];
+		int wbr[] = new int[2];
+		int sz;
+		
+		dims[0] = filterNumber*binNumber;
+		dims[1] = yDim;
+		dims[2] = xDim;
+		
+		for (b = 0; b < filterNumber; b++) {
+			Imax = -Double.MAX_VALUE;
+			Imin = Double.MAX_VALUE;
+			for (y = 0; y < yDim; y++) {
+				for (x = 0; x < xDim; x++) {
+					if (Ig[y][x][b] > Imax) {
+						Imax = Ig[y][x][b];
+					}
+					if (Ig[y][x][b] < Imin) {
+						Imin = Ig[y][x][b];
+					}
+				}
+			} // for (y = 0; y < yDim; y++)
+			U = (Imax - Imin)/binNumber;
+			
+			for (k = 0; k < binNumber; k++) {
+				binc[k] = Imin + k*U + U/2.0;
+			}
+			
+			md = Imax;
+			for (k = 0; k < binNumber; k++) {
+			    tmp = Math.abs(Ig[0][0][b] - binc[k]);
+			    if (tmp < md) {
+			    	md = tmp;
+			    	mdid = k;
+			    }
+			} // for (k = 0; k < binNumber; k++)
+			HImap[b*binNumber + mdid] = 1.0;
+			
+			for (x = 1; x < xDim; x++) {
+			    md = Imax;
+			    for (k = 0; k < binNumber; k++) {
+			        tmp = Math.abs(Ig[0][x][b] - binc[k]);
+			        if (tmp < md) {
+			        	md = tmp;
+			        	mdid = k;
+			        }
+			    } // for (k = 0; k < binNumber; k++)
+			    HImap[binNumber*b+mdid+x*binNumber*filterNumber*yDim] = 1.0;
+			    for (k = 0; k < binNumber; k++) {
+			    	HImap[binNumber*b+k+x*dims[0]*dims[1]] = HImap[binNumber*b+k+x*dims[0]*dims[1]] +
+                                                             HImap[binNumber*b+k+(x-1)*dims[0]*dims[1]];
+			    }
+			} // for (x = 1; x < xDim; x++)
+			
+			for (y = 1; y < yDim; y++) {
+			    md = Imax;
+			    for (k = 0; k < binNumber; k++) {
+			        tmp = Math.abs(Ig[y][0][b] - binc[k]);
+			        tmpm[k] = 0.0;
+			        if (tmp < md) {
+			        	md = tmp;
+			        	mdid = k;
+			        }
+			    } // for (k = 0; k < binNumber; k++)
+			    HImap[binNumber*b+mdid+y*dims[0]] = 1.0;
+			    tmpm[mdid] = 1.0;
+			    for (k = 0; k < binNumber; k++) {
+			    	HImap[binNumber*b+k+y*dims[0]] = HImap[binNumber*b+k+y*dims[0]] + HImap[binNumber*b+k+(y-1)*dims[0]];
+			    }
+			    for (x = 1; x < xDim; x++) {
+			        md = Imax;
+			        for (k = 0; k < binNumber; k++) {
+			            tmp = Math.abs(Ig[y][x][b] - binc[k]);
+			            if (tmp < md) {
+			            	md = tmp;
+			            	mdid = k;
+			            }
+			        } // for (k = 0; k < binNumber; k++)
+			        tmpm[mdid] = tmpm[mdid] + 1.0;
+			        for (k = 0; k < binNumber; k++) {
+			            HImap[binNumber*b+k+y*dims[0]+x*dims[0]*dims[1]] = HImap[binNumber*b+k+(y-1)*dims[0]+x*dims[0]*dims[1]] +
+			            		                                           tmpm[k];
+			        } // for (k = 0; k < binNumber; k++)
+			    } // for (x = 1; x < xDim; x++)
+			} // for (y = 1; y < yDim; y++)
+		} // for (b = 0; b < filterNumber; b++)
+		
+		// Compute local spectral histograms
+		for (y = 0; y < yDim; y++) {
+		    for (x = 0; x < xDim; x++) {
+		        if (y - ws > 0)	{
+		        	wtl[0] = y - ws;
+		        }
+		        else {
+		        	wtl[0] = 0;
+		        }
+		        if (x - ws > 0) {
+		        	wtl[1] = x - ws;
+		        }
+		        else {
+		        	wtl[1] = 0;
+		        }
+		        if (y + ws < yDim - 1) {
+		        	wbr[0] = y + ws;
+		        }
+		        else {
+		        	wbr[0] = yDim - 1;
+		        }
+		        if (x + ws < xDim - 1) {
+		        	wbr[1] = x + ws;
+		        }
+		        else {
+		        	wbr[1] = xDim - 1;
+		        }
+		        
+		        sz = (wbr[1]-wtl[1]+1)*(wbr[0]-wtl[0]+1);
+
+	            if (wtl[0]==0 && wtl[1]==0) {
+	                for (k=0; k<filterNumber*binNumber; k++) {
+	                    sh_mx[k][y][x] = HImap[k+wbr[0]*dims[0]+wbr[1]*dims[0]*dims[1]]; }}             
+	            else if (wtl[0]==0 && wtl[1]!=0) {
+	                for (k=0; k<filterNumber*binNumber; k++) {
+	                   sh_mx[k][y][x] = HImap[k+wbr[0]*dims[0]+wbr[1]*dims[0]*dims[1]] 
+	                                 - HImap[k+wbr[0]*dims[0]+(wtl[1]-1)*dims[0]*dims[1]];} }                        
+	            else if (wtl[0]!=0 && wtl[1]==0) {
+	                for (k=0; k<filterNumber*binNumber; k++) {
+	                    sh_mx[k][y][x] = HImap[k+wbr[0]*dims[0]+wbr[1]*dims[0]*dims[1]] 
+	                                 - HImap[k+(wtl[0]-1)*dims[0]+wbr[1]*dims[0]*dims[1]];} }
+	            else   
+	                for (k=0; k<filterNumber*binNumber; k++) {
+	                    sh_mx[k][y][x] = HImap[k+wbr[0]*dims[0]+wbr[1]*dims[0]*dims[1]] 
+	                                   + HImap[k+(wtl[0]-1)*dims[0]+(wtl[1]-1)*dims[0]*dims[1]] 
+	                                   - HImap[k+wbr[0]*dims[0]+(wtl[1]-1)*dims[0]*dims[1]] 
+									   - HImap[k+(wtl[0]-1)*dims[0]+wbr[1]*dims[0]*dims[1]];}
+	            for (k=0; k<filterNumber*binNumber; k++) {
+	                sh_mx[k][y][x] = sh_mx[k][y][x]/sz;} 
+		    } // for (x = 0; x < xDim; x++)
+		} // for (y = 0; y < yDim; y++)
 	}
 	
 	public void algorithmPerformed(AlgorithmBase algorithm){
