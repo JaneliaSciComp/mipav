1305bec1660a7fa11ec4c35e4c45b55749f1dc38
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-04-23 21:02:49 +0000

    Dialog completed.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13557 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/nibib/spim/PlugInAlgorithmStageScan.java
+++ trunk/mipav/src/plugins/nibib/spim/PlugInAlgorithmStageScan.java
@@ -1,12 +1,24 @@
 package nibib.spim;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
+import nibib.spim.PlugInAlgorithmGenerateFusion.FusionAlg;
 import gov.nih.mipav.model.algorithms.AlgorithmBase;
+import gov.nih.mipav.model.file.FileIO;
+import gov.nih.mipav.model.file.FileUtility;
+import gov.nih.mipav.model.file.FileWriteOptions;
 import gov.nih.mipav.model.structures.ModelImage;
+import gov.nih.mipav.model.structures.ModelStorageBase;
+import gov.nih.mipav.view.MipavUtil;
+import gov.nih.mipav.view.ViewJFrameImage;
 import gov.nih.mipav.view.ViewUserInterface;
 
 
@@ -15,15 +27,11 @@ public class PlugInAlgorithmStageScan extends AlgorithmBase {
 	
 	private final Collection<ModelImage> resultImageList;
 	
-	private final String AFileDirectory;
-	
 	private final String AFileDark2D;
 	
 	private final double ALeftShift;
 	
 	private File[] AImageAr;
-	
-    private final String BFileDirectory;
 	
 	private final String BFileDark2D;
 	
@@ -33,18 +41,26 @@ public class PlugInAlgorithmStageScan extends AlgorithmBase {
 	
 	private File resultDirectory;
 	
-	public PlugInAlgorithmStageScan(final String AFileDirectory, final String AFileDark2D, final double ALeftShift,
-			File[] AImageAr, final String BFileDirectory, final String BFileDark2D, final double BLeftShift,
-			File[] BImageAr, File resultDirectory) {
-		this.AFileDirectory = AFileDirectory;
+	private final int concurrentNum;
+	
+	private Semaphore semaphore;
+	
+	private boolean bufferAvailable[];
+	
+	private boolean allowScriptRecording = false;
+
+	
+	public PlugInAlgorithmStageScan(final String AFileDark2D, final double ALeftShift,
+			File[] AImageAr, final String BFileDark2D, final double BLeftShift,
+			File[] BImageAr, File resultDirectory, final int concurrentNum) {
 		this.AFileDark2D = AFileDark2D;
 		this.ALeftShift = ALeftShift;
 		this.AImageAr = AImageAr;
-		this.BFileDirectory = BFileDirectory;
 		this.BFileDark2D = BFileDark2D;
 		this.BLeftShift = BLeftShift;
 		this.BImageAr = BImageAr;
 		this.resultDirectory = resultDirectory;
+		this.concurrentNum = concurrentNum;
 		this.resultImageList = Collections.synchronizedCollection(new ArrayList<ModelImage>());
 		
 	}
@@ -69,9 +85,318 @@ public class PlugInAlgorithmStageScan extends AlgorithmBase {
      */
     @Override
     public void runAlgorithm() {
-
+        int i;
+        int j;
+        int z;
+        boolean found;
+        int cIndex = 0;
+        double leftShift;
+        int zDim = AImageAr.length;
         final boolean appFrameFlag = ViewUserInterface.getReference().isAppFrameVisible();
         ViewUserInterface.getReference().setAppFrameVisible(false);
+        final FileIO io = new FileIO();
+        io.setQuiet(true);
+        io.setSuppressProgressBar(true);
+        io.setTIFFOrientation(false);
+        ModelImage ADarkImage = io.readImage(AFileDark2D);
+        int extents[] = ADarkImage.getExtents();
+        int xDim = extents[0];
+        int yDim = extents[1];
+        int sliceSize = xDim * yDim;
+        float darkBuffer[][] = new float[concurrentNum][sliceSize];
+        try {
+        	ADarkImage.exportData(0, sliceSize,darkBuffer[0]);
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        	setCompleted(false);
+        	return;
+        }
+        ADarkImage.disposeLocal();
+        ADarkImage = null;
+        for (i = 1; i < concurrentNum; i++) {
+            for (j = 0; j < sliceSize; j++) {
+                darkBuffer[i][j] = darkBuffer[0][j];
+            }
+        }
+        
+        bufferAvailable = new boolean[concurrentNum];
+        for (i = 0; i < concurrentNum; i++) {
+        	bufferAvailable[i] = true;
+        }
+        float resultBuffer[][] = new float[zDim][sliceSize];
+        ThreadPoolExecutor exec = new ThreadPoolExecutor(concurrentNum, concurrentNum, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        semaphore = new Semaphore(concurrentNum);
+        
+        for (i = 0; i < zDim; i++) {
+        	try {
+                semaphore.acquire();
+            } catch (final InterruptedException e) {
+                System.out.println("Interrupted exception on semaphore.acquire() " + e);
+            }
+        	found = false;
+        	for (j = 0; j < concurrentNum && !found; j++) {
+        		if (bufferAvailable[j]) {
+        			bufferAvailable[j] = false;
+        			found = true;
+        			cIndex = j;
+        		}
+        	}
+        	leftShift = i * ALeftShift;
+        	final StageScanAlg algInstance = new StageScanAlg(cIndex, xDim, yDim, darkBuffer[cIndex], AImageAr[i], 
+        			resultBuffer[zDim - 1 - i], leftShift);
+            exec.execute(algInstance);
+        } // for (i = 0; i < zDim; i++)
+        
+        exec.shutdown();
+
+        try {
+            exec.awaitTermination(1, TimeUnit.DAYS);
+        } catch (final InterruptedException e) {
+            MipavUtil.displayError("Program did not execute correctly");
+            e.printStackTrace();
+        }
+        
+        int extents3D[] = new int[]{xDim, yDim, zDim};
+        ModelImage AImage = new ModelImage(ModelStorageBase.FLOAT, extents3D, "AResultImage");
+        int length = sliceSize * zDim;
+        float rBuffer[] = new float[length];
+        for (z = 0; z < zDim; z++) {
+            for (i = 0; i < sliceSize; i++) {
+            	rBuffer[z*sliceSize + i] = resultBuffer[z][i];
+            }
+        }
+        try {
+        	AImage.importData(0, rBuffer, true);
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        	setCompleted(false);
+        	return;
+        }
+        
+        final FileWriteOptions options = new FileWriteOptions(null, null, true);
+        options.setFileType(FileUtility.TIFF);
+        options.setIsScript(true);
+        String directory = resultDirectory.getAbsolutePath() + File.separatorChar;
+
+        File file = new File(directory);
+
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        
+        String Adirectory = directory + "SPIMA" + File.separator;
+        file = new File(Adirectory);
+
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        options.setFileDirectory(Adirectory);
+        String name = AImageAr[0].getName();
+        int upper = -1;
+        found = false;
+        for(i=name.length()-1; i >= 0 && upper == -1; i--) {
+            if( (Character.isDigit(name.charAt(i))) || (name.substring(i,i+1).equals("_"))) {
+            	found = true;
+            }
+            else if (found){
+            	upper = i+1;
+            }
+        }
+        name = name.substring(0,upper);
+        options.setFileName(name);
+        options.setBeginSlice(0);
+        options.setEndSlice(zDim - 1);
+        options.setOptionsSet(true);
+        io.writeImage(AImage, options, false, allowScriptRecording);
+        AImage.disposeLocal();
+        AImage = null;
+        
+        ModelImage BDarkImage = io.readImage(BFileDark2D);
+        try {
+        	BDarkImage.exportData(0, sliceSize,darkBuffer[0]);
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        	setCompleted(false);
+        	return;
+        }
+        BDarkImage.disposeLocal();
+        BDarkImage = null;
+        for (i = 1; i < concurrentNum; i++) {
+            for (j = 0; j < sliceSize; j++) {
+                darkBuffer[i][j] = darkBuffer[0][j];
+            }
+        }
+        
+        for (i = 0; i < concurrentNum; i++) {
+        	bufferAvailable[i] = true;
+        }
+        exec = new ThreadPoolExecutor(concurrentNum, concurrentNum, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        
+        for (i = 0; i < zDim; i++) {
+        	try {
+                semaphore.acquire();
+            } catch (final InterruptedException e) {
+                System.out.println("Interrupted exception on semaphore.acquire() " + e);
+            }
+        	found = false;
+        	for (j = 0; j < concurrentNum && !found; j++) {
+        		if (bufferAvailable[j]) {
+        			bufferAvailable[j] = false;
+        			found = true;
+        			cIndex = j;
+        		}
+        	}
+        	leftShift = i * BLeftShift;
+        	final StageScanAlg algInstance = new StageScanAlg(cIndex, xDim, yDim, darkBuffer[cIndex], BImageAr[i], 
+        			resultBuffer[i], leftShift);
+            exec.execute(algInstance);
+        } // for (i = 0; i < zDim; i++)
+        
+        exec.shutdown();
+
+        try {
+            exec.awaitTermination(1, TimeUnit.DAYS);
+        } catch (final InterruptedException e) {
+            MipavUtil.displayError("Program did not execute correctly");
+            e.printStackTrace();
+        }
+        
+        ModelImage BImage = new ModelImage(ModelStorageBase.FLOAT, extents3D, "BResultImage");
+        for (z = 0; z < zDim; z++) {
+            for (i = 0; i < sliceSize; i++) {
+            	rBuffer[z*sliceSize + i] = resultBuffer[z][i];
+            }
+        }
+        try {
+        	BImage.importData(0, rBuffer, true);
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
+        	setCompleted(false);
+        	return;
+        }
+        
+        String Bdirectory = directory + "SPIMB" + File.separator;
+        file = new File(Bdirectory);
+
+        if (!file.exists()) {
+            file.mkdir();
+        }
+        options.setFileDirectory(Bdirectory);
+        name = BImageAr[0].getName();
+        upper = -1;
+        found = false;
+        for(i=name.length()-1; i >= 0 && upper == -1; i--) {
+            if( (Character.isDigit(name.charAt(i))) || (name.substring(i,i+1).equals("_"))) {
+            	found = true;
+            }
+            else if (found) {
+            	upper = i+1;
+            }
+        }
+        name = name.substring(0,upper);
+        options.setFileName(name);
+        io.writeImage(BImage, options, false, allowScriptRecording);
+        BImage.disposeLocal();
+        BImage = null;
+        ViewUserInterface.getReference().setAppFrameVisible(appFrameFlag);
+        setCompleted(true);
+        return;
+    }
+    
+    public class StageScanAlg implements Runnable {
+    	private int cIndex;
+    	private int xDim;
+    	private int yDim;
+    	private float darkBuffer[];
+    	private File imageFile;
+    	private float resultBuffer[];
+    	private double leftShift;
+    	
+    	public StageScanAlg(int cIndex, int xDim, int yDim,
+    			float darkBuffer[], File imageFile, float resultBuffer[], double leftShift) {
+    	    this.cIndex = cIndex;
+    	    this.xDim = xDim;
+    	    this.yDim = yDim;
+    	    this.darkBuffer = darkBuffer;
+    	    this.imageFile = imageFile;
+    	    this.resultBuffer = resultBuffer;
+    	    this.leftShift = leftShift;
+    	}
+    	
+    	@Override
+        public void run() {
+            call();
+            bufferAvailable[cIndex] = true;
+            semaphore.release();
+        }
+
+        public Boolean call() {
+        	int i;
+        	int x;
+        	int y;
+        	int shift;
+        	int upperXShift;
+        	int lowerShift;
+            int upperShift;
+            float lowerFraction;
+            float upperFraction;
+        	int ypos;
+        	final FileIO io = new FileIO();
+            io.setQuiet(true);
+            io.setSuppressProgressBar(true);
+            io.setTIFFOrientation(false);
+            ModelImage image = io.readImage(imageFile.getAbsolutePath());
+            int sliceSize = darkBuffer.length;
+            try {
+            	image.exportData(0, sliceSize, resultBuffer);
+            }
+            catch(IOException e) {
+            	e.printStackTrace();
+            	return false;
+            }
+            image.disposeLocal();
+            image = null;
+            for (i = 0; i < sliceSize; i++) {
+            	resultBuffer[i] -= darkBuffer[i];
+            }
+            if (Math.round(leftShift) == leftShift) {
+            	shift = (int)leftShift;
+            	upperXShift = xDim - 1 - shift;
+                for (y = 0; y < yDim; y++) {
+                	ypos = y * xDim;
+            	    for (x = 0; x <= upperXShift; x++) {
+            	    	resultBuffer[x + ypos] = resultBuffer[x + shift + ypos];
+            	    }
+            	    for (x = upperXShift + 1; x <= xDim-1; x++) {
+            	    	resultBuffer[x + ypos] = 0.0f;
+            	    }
+                } // for (y = 0; y < yDim; y++)
+            } // if (Math.round(leftShift) == leftShift)
+            else {
+            	lowerShift = (int)leftShift;
+                upperShift = lowerShift + 1;
+                upperXShift = xDim - 1 - upperShift;
+                lowerFraction = (float)(Math.ceil(leftShift) - leftShift);
+                upperFraction = 1.0f - lowerFraction;
+                for (y = 0; y < yDim; y++) {
+                    ypos = y * xDim;
+                    for (x = 0; x <= upperXShift; x++) {
+                    	resultBuffer[x + ypos] = lowerFraction * resultBuffer[x + lowerShift + ypos] +
+                    			                 upperFraction * resultBuffer[x + upperShift + ypos];
+                    }
+                    for (x = upperXShift + 1; x <= xDim-1; x++) {
+            	    	resultBuffer[x + ypos] = 0.0f;
+            	    }
+                } // for (y = 0; y < yDim; y++)
+            }
+            return true;
+        }
     }
     
     public Collection<ModelImage> getResultImageList() {
