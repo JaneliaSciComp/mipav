38ccc31d237a615e7c2e4a7f2b14b93984c3ab11
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-08-11 15:38:25 +0000

    Added eye tracker image to icons directory.  Changed the icon loading to use MipavUtil.getIconImage() instead of a custom buffered image.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13738 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmFullScreenDisplay.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmFullScreenDisplay.java
@@ -1,94 +1,96 @@
+import gov.nih.mipav.model.algorithms.AlgorithmBase;
 
-import java.awt.Component;
-import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JTextArea;
 
-import gov.nih.mipav.model.algorithms.AlgorithmBase;
-
-
 
 public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
-	private BufferedImage inputImage;
-	private BufferedImage cornerImage;
-	
-	private JTextArea outputTextArea;
+    private final BufferedImage inputImage;
 
+    private final Image cornerImage;
 
-	public PlugInAlgorithmFullScreenDisplay(BufferedImage inputImage, BufferedImage cornerImage, JTextArea outputTextArea) {
-		this.inputImage = inputImage;
-		this.cornerImage = cornerImage;
-		this.outputTextArea = outputTextArea;
-		
-	}
-	
-	
-	public void runAlgorithm() {
-		outputTextArea.append("Running Algorithm v1.0" + "\n");
-        
+    private final JTextArea outputTextArea;
+
+    public PlugInAlgorithmFullScreenDisplay(final BufferedImage inputImage, final Image cornerImage, final JTextArea outputTextArea) {
+        this.inputImage = inputImage;
+        this.cornerImage = cornerImage;
+        this.outputTextArea = outputTextArea;
+
+    }
+
+    @Override
+    public void runAlgorithm() {
+        outputTextArea.append("Running Algorithm v1.0" + "\n");
+
         final long begTime = System.currentTimeMillis();
         final int inputWidth = inputImage.getWidth();
         final int inputHeight = inputImage.getHeight();
-        Frame frame = new Frame("Test");
+        final Frame frame = new Frame("Test");
         frame.setUndecorated(true);
         frame.add(new Component() {
-        	public void paint(Graphics g) {
-        		super.paint(g);
-        		double widthRatio = (double)(getWidth()-160)/(double)inputWidth;
-        		double heightRatio = (double)(getHeight()-158)/(double)inputHeight;
-        		BufferedImage backgroundImage = new BufferedImage(getWidth(), getHeight(),BufferedImage.TYPE_INT_ARGB);
-        		int length = getWidth() * getHeight();
-        		int[] bufferData = new int[length*4];
-    			for (int i = 0; i < length; i++)
-    			{
-    				bufferData[i*4 + 0] = 0;
-    				bufferData[i*4 + 1] = 0;
-    				bufferData[i*4 + 2] = 0;
-    				bufferData[i*4 + 3] = 255;
-    			}
-    			backgroundImage.getRaster().setPixels(0,0, getWidth(), getHeight(), bufferData );
-    			g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
-        		if (widthRatio > heightRatio) {
-        			// Can only expand by the heightRatio
-        			int expWidth = (int)Math.floor(inputWidth*heightRatio);
-        			int leftPadding = (getWidth() - 160 - expWidth)/2;
-        		    g.drawImage(inputImage, 80 + leftPadding, 79, expWidth, getHeight()-158, this);
-        		}
-        		else {
-        			// Can only expand by the widthRatio
-        			int expHeight = (int)Math.floor(inputHeight*widthRatio);
-        			int topPadding = (getHeight() - 158 - expHeight)/2;
-        			g.drawImage(inputImage, 80, 79 + topPadding, getWidth() - 160, expHeight, this);
-        		}
-        		if (cornerImage != null) {
-        		    g.drawImage(cornerImage, 0, 0, 80, 79, this);
-        		    g.drawImage(cornerImage, getWidth() - 80, 0, 80, 79, this);
-        		    g.drawImage(cornerImage, 0, getHeight() - 79, 80, 79, this);
-        		    g.drawImage(cornerImage, getWidth() - 80, getHeight() - 70, 80, 79, this);
-        		} // if (cornerImage != null)
-        	}
+            @Override
+            public void paint(final Graphics g) {
+                super.paint(g);
+                final double widthRatio = (double) (getWidth() - 160) / (double) inputWidth;
+                final double heightRatio = (double) (getHeight() - 158) / (double) inputHeight;
+                final BufferedImage backgroundImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                final int length = getWidth() * getHeight();
+                final int[] bufferData = new int[length * 4];
+                for (int i = 0; i < length; i++) {
+                    bufferData[i * 4 + 0] = 0;
+                    bufferData[i * 4 + 1] = 0;
+                    bufferData[i * 4 + 2] = 0;
+                    bufferData[i * 4 + 3] = 255;
+                }
+                backgroundImage.getRaster().setPixels(0, 0, getWidth(), getHeight(), bufferData);
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+                if (widthRatio > heightRatio) {
+                    // Can only expand by the heightRatio
+                    final int expWidth = (int) Math.floor(inputWidth * heightRatio);
+                    final int leftPadding = (getWidth() - 160 - expWidth) / 2;
+                    g.drawImage(inputImage, 80 + leftPadding, 79, expWidth, getHeight() - 158, this);
+                } else {
+                    // Can only expand by the widthRatio
+                    final int expHeight = (int) Math.floor(inputHeight * widthRatio);
+                    final int topPadding = (getHeight() - 158 - expHeight) / 2;
+                    g.drawImage(inputImage, 80, 79 + topPadding, getWidth() - 160, expHeight, this);
+                }
+                if (cornerImage != null) {
+                    g.drawImage(cornerImage, 0, 0, 80, 79, this);
+                    g.drawImage(cornerImage, getWidth() - 80, 0, 80, 79, this);
+                    g.drawImage(cornerImage, 0, getHeight() - 79, 80, 79, this);
+                    g.drawImage(cornerImage, getWidth() - 80, getHeight() - 70, 80, 79, this);
+                } // if (cornerImage != null)
+            }
         });
         frame.addMouseListener(new MouseListener() {
-            public void mouseClicked (MouseEvent e) {
-            	System.exit(0);
+            @Override
+            public void mouseClicked(final MouseEvent e) {
+                System.exit(0);
             }
-            public void mousePressed(MouseEvent e){}
-            public void mouseReleased(MouseEvent e){}
-            public void mouseEntered(MouseEvent e){}
-            public void mouseExited(MouseEvent e){}
+
+            @Override
+            public void mousePressed(final MouseEvent e) {}
+
+            @Override
+            public void mouseReleased(final MouseEvent e) {}
+
+            @Override
+            public void mouseEntered(final MouseEvent e) {}
+
+            @Override
+            public void mouseExited(final MouseEvent e) {}
         });
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice gs = ge.getDefaultScreenDevice();
+        final GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        final GraphicsDevice gs = ge.getDefaultScreenDevice();
         gs.setFullScreenWindow(frame);
         frame.validate();
-		
-		final long endTime = System.currentTimeMillis();
+
+        final long endTime = System.currentTimeMillis();
         final long diffTime = endTime - begTime;
         final float seconds = ((float) diffTime) / 1000;
 
@@ -96,9 +98,6 @@ public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
 
         setCompleted(true);
 
-	}
-	
-	
-	
+    }
 
 }
--- trunk/mipav/src/plugins/PlugInDialogFullScreenDisplay.java
+++ trunk/mipav/src/plugins/PlugInDialogFullScreenDisplay.java
@@ -1,125 +1,102 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Cursor;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-
-import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.border.LineBorder;
-
 import gov.nih.mipav.model.algorithms.AlgorithmBase;
 import gov.nih.mipav.model.algorithms.AlgorithmInterface;
 import gov.nih.mipav.model.file.FileIO;
 import gov.nih.mipav.model.structures.ModelImage;
+
 import gov.nih.mipav.view.MipavUtil;
 import gov.nih.mipav.view.Preferences;
 import gov.nih.mipav.view.ScrollCorrector;
 import gov.nih.mipav.view.dialogs.JDialogBase;
-import gov.nih.mipav.view.icons.PlaceHolder;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 
 public class PlugInDialogFullScreenDisplay extends JDialogBase implements AlgorithmInterface, ActionListener {
 
-	private JPanel mainPanel;
-	
-	private GridBagConstraints gbc;
-	
-	private JLabel fileLabel;
-	
-	private JTextField filePathTextField;
-	
-	private JButton fileBrowseButton;
-	
-	private JTextArea outputTextArea;
-	
-	private JScrollPane scrollPane;
-	
-	private String fileName;
-	
-	String directory;
-	
-	private String currDir;
-	
-	
-	private PlugInAlgorithmFullScreenDisplay alg;
-	
-	
-	
-	
-	public PlugInDialogFullScreenDisplay() {
-		
-	}
-	
-	
-	public PlugInDialogFullScreenDisplay(boolean modal) {
-		super(modal);
-		init();
-	}
-	
-	
-	
-	
-	private void init() {
-		setForeground(Color.black);
+    private JPanel mainPanel;
+
+    private GridBagConstraints gbc;
+
+    private JLabel fileLabel;
+
+    private JTextField filePathTextField;
+
+    private JButton fileBrowseButton;
+
+    private JTextArea outputTextArea;
+
+    private JScrollPane scrollPane;
+
+    private String fileName;
+
+    String directory;
+
+    private String currDir;
+
+    private PlugInAlgorithmFullScreenDisplay alg;
+
+    public PlugInDialogFullScreenDisplay() {
+
+    }
+
+    public PlugInDialogFullScreenDisplay(final boolean modal) {
+        super(modal);
+        init();
+    }
+
+    private void init() {
+        setForeground(Color.black);
         setTitle("Full Screen Display v1.0");
         mainPanel = new JPanel(new GridBagLayout());
         gbc = new GridBagConstraints();
-        
+
         fileLabel = new JLabel("File ");
         filePathTextField = new JTextField(35);
         filePathTextField.setEditable(false);
         filePathTextField.setBackground(Color.white);
         fileBrowseButton = new JButton("Browse");
         fileBrowseButton.addActionListener(this);
-        fileBrowseButton.setActionCommand("FileBrowse");        
-        
+        fileBrowseButton.setActionCommand("FileBrowse");
+
         outputTextArea = new JTextArea();
         outputTextArea.setRows(15);
-		outputTextArea.setEditable(false);
-		outputTextArea.setBackground(Color.lightGray);
-		outputTextArea.setBorder(new LineBorder(Color.black));
-		outputTextArea.setForeground(Color.black);
-		scrollPane = new JScrollPane(outputTextArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        
-		scrollPane.getVerticalScrollBar().addAdjustmentListener(new ScrollCorrector());
-		
-		
-		
-		gbc.gridx = 0;
+        outputTextArea.setEditable(false);
+        outputTextArea.setBackground(Color.lightGray);
+        outputTextArea.setBorder(new LineBorder(Color.black));
+        outputTextArea.setForeground(Color.black);
+        scrollPane = new JScrollPane(outputTextArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        scrollPane.getVerticalScrollBar().addAdjustmentListener(new ScrollCorrector());
+
+        gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(15,5,5,15);
+        gbc.insets = new Insets(15, 5, 5, 15);
         gbc.gridwidth = 1;
         gbc.anchor = GridBagConstraints.EAST;
-        mainPanel.add(fileLabel,gbc);
+        mainPanel.add(fileLabel, gbc);
         gbc.gridx = 1;
-        mainPanel.add(filePathTextField,gbc);
+        mainPanel.add(filePathTextField, gbc);
         gbc.gridx = 2;
-        mainPanel.add(fileBrowseButton,gbc);
-		
+        mainPanel.add(fileBrowseButton, gbc);
+
         gbc.anchor = GridBagConstraints.CENTER;
         gbc.fill = GridBagConstraints.BOTH;
-        
+
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.gridwidth = 3;
-        mainPanel.add(scrollPane,gbc);
-        
-        JPanel OKCancelPanel = new JPanel();
+        mainPanel.add(scrollPane, gbc);
+
+        final JPanel OKCancelPanel = new JPanel();
         buildOKButton();
         OKButton.setActionCommand("ok");
         OKCancelPanel.add(OKButton, BorderLayout.WEST);
@@ -129,170 +106,140 @@ public class PlugInDialogFullScreenDisplay extends JDialogBase implements Algori
 
         getContentPane().add(mainPanel, BorderLayout.CENTER);
         getContentPane().add(OKCancelPanel, BorderLayout.SOUTH);
-        
+
         pack();
         setMinimumSize(getSize());
-        
+
         setVisible(true);
         setResizable(false);
-	        
-	        
-		
-		
-	}
-	
-	
-	
-	private void callAlgorithm() {
-		URL imageURL = null;
-		BufferedImage cornerImage = null;
-		//imageURL = PlaceHolder.class.getResource("WhiteCircle.png");
-		File cornerFile = new File("C:" + File.separator + "images" + File.separator + "WhiteCircle.png");
-		if (cornerFile.exists() && cornerFile.canRead()) {
-			try {
-                imageURL = cornerFile.toURI().toURL();
-			}
-			catch (IOException e) {
-				MipavUtil.displayError("IOException " + e + "cornerFIle.toURI().toURL()");
-				return;
-			}
+
+    }
+
+    private void callAlgorithm() {
+        Image cornerImage;
+        try {
+            cornerImage = MipavUtil.getIconImage("WhiteCircle.png");
+        } catch (final FileNotFoundException e) {
+            MipavUtil.displayError("Eye tracker landmark image not found.");
+            e.printStackTrace();
+            return;
         }
-		if (imageURL != null) {
-			try {
-	            cornerImage = ImageIO.read(imageURL);
-			}
-			catch (IOException e) {
-				MipavUtil.displayError("IOException " + e + " on ImageIO.read(imageURL)");
-				return;
-			}
-		}
-		FileIO fileIO = new FileIO();
-        boolean multiFile = false;
-        ModelImage image = fileIO.readImage(fileName, directory, multiFile, null);
-        int xDim = image.getExtents()[0];
-        int yDim = image.getExtents()[1];
-        int length = xDim * yDim;
+
+        final FileIO fileIO = new FileIO();
+        final boolean multiFile = false;
+        final ModelImage image = fileIO.readImage(fileName, directory, multiFile, null);
+        final int xDim = image.getExtents()[0];
+        final int yDim = image.getExtents()[1];
+        final int length = xDim * yDim;
         BufferedImage inputImage = null;
-        inputImage = new BufferedImage( xDim, yDim, BufferedImage.TYPE_INT_ARGB );
+        inputImage = new BufferedImage(xDim, yDim, BufferedImage.TYPE_INT_ARGB);
         if (image.isColorImage()) {
-			int[] imageData = new int[length*4];
-			try {
-				image.exportData(0, length*4, imageData);
-			}
-			catch (IOException e) {
-				MipavUtil.displayError("IOException " + e + " on image.exportData(0, length*4, imageData)");
-				return;
-			}
-			int[] bufferData = new int[length*4];
-			for (int i = 0; i < length; i++)
-			{
-				bufferData[i*4 + 0] = imageData[i * 4 + 1];
-				bufferData[i*4 + 1] = imageData[i * 4 + 2];
-				bufferData[i*4 + 2] = imageData[i * 4 + 3];
-				bufferData[i*4 + 3] = 255;
-			}
-			inputImage.getRaster().setPixels(0,0, xDim, yDim, bufferData );
+            final int[] imageData = new int[length * 4];
+            try {
+                image.exportData(0, length * 4, imageData);
+            } catch (final IOException e) {
+                MipavUtil.displayError("IOException " + e + " on image.exportData(0, length*4, imageData)");
+                return;
+            }
+            final int[] bufferData = new int[length * 4];
+            for (int i = 0; i < length; i++) {
+                bufferData[i * 4 + 0] = imageData[i * 4 + 1];
+                bufferData[i * 4 + 1] = imageData[i * 4 + 2];
+                bufferData[i * 4 + 2] = imageData[i * 4 + 3];
+                bufferData[i * 4 + 3] = 255;
+            }
+            inputImage.getRaster().setPixels(0, 0, xDim, yDim, bufferData);
+        } else {
+            final int[] imageData = new int[length];
+            try {
+                image.exportData(0, length, imageData);
+            } catch (final IOException e) {
+                MipavUtil.displayError("IOException " + e + " on image.exportData(0, length, imageData)");
+                return;
+            }
+            final int[] bufferData = new int[length * 4];
+            for (int i = 0; i < length; i++) {
+                bufferData[i * 4 + 0] = imageData[i];
+                bufferData[i * 4 + 1] = imageData[i];
+                bufferData[i * 4 + 2] = imageData[i];
+                bufferData[i * 4 + 3] = 255;
+            }
+            inputImage.getRaster().setPixels(0, 0, xDim, yDim, bufferData);
+
         }
-        else {
-        	int[] imageData = new int[length];
-			try {
-				image.exportData(0, length, imageData);
-			}
-			catch (IOException e) {
-				MipavUtil.displayError("IOException " + e + " on image.exportData(0, length, imageData)");
-				return;
-			}
-			int[] bufferData = new int[length*4];
-			for (int i = 0; i < length; i++)
-			{
-				bufferData[i*4 + 0] = imageData[i];
-				bufferData[i*4 + 1] = imageData[i];
-				bufferData[i*4 + 2] = imageData[i];
-				bufferData[i*4 + 3] = 255;
-			}
-			inputImage.getRaster().setPixels(0,0, xDim, yDim, bufferData );
-        	
+        /*
+         * try { inputImage = ImageIO.read( new File(directory + fileName) ); } catch (IOException e) {
+         * MipavUtil.displayError("IOException " + e + " on ImageIO.read( new File(directory + fileName)"); return; }
+         */
+        alg = new PlugInAlgorithmFullScreenDisplay(inputImage, cornerImage, outputTextArea);
+
+        alg.addListener(this);
+
+        if (isRunInSeparateThread()) {
+
+            // Start the thread as a low priority because we wish to still
+            // have user interface work fast.
+            if (alg.startMethod(Thread.MIN_PRIORITY) == false) {
+                MipavUtil.displayError("A thread is already running on this object");
+            }
+        } else {
+            alg.run();
         }
-		/*try {
-	        inputImage = ImageIO.read( new File(directory + fileName) );
-		}
-		catch (IOException e) {
-			MipavUtil.displayError("IOException " + e + " on ImageIO.read( new File(directory + fileName)");
-			return;
-		}*/
-		alg = new PlugInAlgorithmFullScreenDisplay(inputImage, cornerImage, outputTextArea);
-	
-		alg.addListener(this);
-		
-		if (isRunInSeparateThread()) {
+    }
 
-			// Start the thread as a low priority because we wish to still
-			// have user interface work fast.
-			if (alg.startMethod(Thread.MIN_PRIORITY) == false) {
-				MipavUtil.displayError("A thread is already running on this object");
-			}
-		} else {
-			alg.run();
-		}
-	}
-	
-	
-	
-	
-	public void algorithmPerformed(AlgorithmBase algorithm) {
-		if(alg.isCompleted()) {
-			 setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-			 OKButton.setEnabled(false);
-			 cancelButton.setText("Close");
-			 
-			 outputTextArea.append("Finished" + "\n");
+    @Override
+    public void algorithmPerformed(final AlgorithmBase algorithm) {
+        if (alg.isCompleted()) {
+            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            OKButton.setEnabled(false);
+            cancelButton.setText("Close");
 
-		}
+            outputTextArea.append("Finished" + "\n");
 
-	}
+        }
 
-	
-	public void actionPerformed(ActionEvent e) {
-		String command = e.getActionCommand();
-		if(command.equalsIgnoreCase("FileBrowse")) {
-			JFileChooser chooser = new JFileChooser(Preferences.getImageDirectory());
-	        /*if (currDir != null) {
-				chooser.setCurrentDirectory(new File(currDir));
-	        }*/
-	        chooser.setDialogTitle("Choose File");
-	        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-	        int returnValue = chooser.showOpenDialog(this);
-	        if (returnValue == JFileChooser.APPROVE_OPTION) {
-	        	currDir = chooser.getSelectedFile().getAbsolutePath();
-	        	Preferences.setImageDirectory(new File(currDir));
-	        	fileName = chooser.getSelectedFile().getName();
-	        	directory = chooser.getCurrentDirectory() + File.separator;
-	        	filePathTextField.setText(currDir);
-	        }
-		}else if(command.equalsIgnoreCase("ok")) {
-			 if(setVariables()) {
-				 callAlgorithm();
-			 }
-		 }else if(command.equalsIgnoreCase("cancel")){
-			 
-			 dispose();
-		 }else {
-		     super.actionPerformed(e);
-		 }
+    }
 
-	}
-	
-	
-	private boolean setVariables() {
-	
-		if(filePathTextField.getText().trim().equals("")) {
-			MipavUtil.displayError("File is required");
-			return false;
-			
-		}
-		
-		
-		return true;
-	}
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        final String command = e.getActionCommand();
+        if (command.equalsIgnoreCase("FileBrowse")) {
+            final JFileChooser chooser = new JFileChooser(Preferences.getImageDirectory());
+            /*
+             * if (currDir != null) { chooser.setCurrentDirectory(new File(currDir)); }
+             */
+            chooser.setDialogTitle("Choose File");
+            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            final int returnValue = chooser.showOpenDialog(this);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                currDir = chooser.getSelectedFile().getAbsolutePath();
+                Preferences.setImageDirectory(new File(currDir));
+                fileName = chooser.getSelectedFile().getName();
+                directory = chooser.getCurrentDirectory() + File.separator;
+                filePathTextField.setText(currDir);
+            }
+        } else if (command.equalsIgnoreCase("ok")) {
+            if (setVariables()) {
+                callAlgorithm();
+            }
+        } else if (command.equalsIgnoreCase("cancel")) {
+
+            dispose();
+        } else {
+            super.actionPerformed(e);
+        }
+
+    }
+
+    private boolean setVariables() {
+
+        if (filePathTextField.getText().trim().equals("")) {
+            MipavUtil.displayError("File is required");
+            return false;
+
+        }
+
+        return true;
+    }
 
 }
