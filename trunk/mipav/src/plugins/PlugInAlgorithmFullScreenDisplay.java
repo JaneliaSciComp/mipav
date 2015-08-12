069c15eee2eb1f09125be46f96008ba6cc4dfd7a
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-08-12 19:53:45 +0000

    Handles 3D images and movement through slices with mouse wheel.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13741 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmFullScreenDisplay.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmFullScreenDisplay.java
@@ -1,23 +1,48 @@
 import gov.nih.mipav.model.algorithms.AlgorithmBase;
+import gov.nih.mipav.view.MipavUtil;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JTextArea;
 
 
-public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
+public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase implements MouseWheelListener{
     private final BufferedImage inputImage;
 
     private final Image cornerImage;
+    
+    private int[][] imageData;
+    
+    private int zOffset;
+    
+    private boolean isColorImage;
+    
+    private int inputWidth;
+    
+    private int inputHeight;
+    
+    private int zDim;
+    
+    private int bufferData[] = null;
+    
+    private int length;
+    
+    private Frame frame;
 
     private final JTextArea outputTextArea;
 
-    public PlugInAlgorithmFullScreenDisplay(final BufferedImage inputImage, final Image cornerImage, final JTextArea outputTextArea) {
+    public PlugInAlgorithmFullScreenDisplay(final BufferedImage inputImage, final Image cornerImage, 
+    		int imageData[][], int zOffset, boolean isColorImage, final JTextArea outputTextArea) {
         this.inputImage = inputImage;
         this.cornerImage = cornerImage;
+        this.imageData = imageData;
+        this.zOffset = zOffset;
+        this.isColorImage = isColorImage;
         this.outputTextArea = outputTextArea;
 
     }
@@ -27,9 +52,11 @@ public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
         outputTextArea.append("Running Algorithm v1.0" + "\n");
 
         final long begTime = System.currentTimeMillis();
-        final int inputWidth = inputImage.getWidth();
-        final int inputHeight = inputImage.getHeight();
-        final Frame frame = new Frame("Test");
+        inputWidth = inputImage.getWidth();
+        inputHeight = inputImage.getHeight();
+        length = inputWidth * inputHeight;
+        zDim = imageData.length;
+        frame = new Frame("Test");
         frame.setUndecorated(true);
         frame.add(new Component() {
             @Override
@@ -38,15 +65,15 @@ public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
                 final double widthRatio = (double) (getWidth() - 160) / (double) inputWidth;
                 final double heightRatio = (double) (getHeight() - 158) / (double) inputHeight;
                 final BufferedImage backgroundImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-                final int length = getWidth() * getHeight();
-                final int[] bufferData = new int[length * 4];
-                for (int i = 0; i < length; i++) {
-                    bufferData[i * 4 + 0] = 0;
-                    bufferData[i * 4 + 1] = 0;
-                    bufferData[i * 4 + 2] = 0;
-                    bufferData[i * 4 + 3] = 255;
+                final int screenLength = getWidth() * getHeight();
+                final int[] screenData = new int[screenLength * 4];
+                for (int i = 0; i < screenLength; i++) {
+                    screenData[i * 4 + 0] = 0;
+                    screenData[i * 4 + 1] = 0;
+                    screenData[i * 4 + 2] = 0;
+                    screenData[i * 4 + 3] = 255;
                 }
-                backgroundImage.getRaster().setPixels(0, 0, getWidth(), getHeight(), bufferData);
+                backgroundImage.getRaster().setPixels(0, 0, getWidth(), getHeight(), screenData);
                 g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
                 if (widthRatio > heightRatio) {
                     // Can only expand by the heightRatio
@@ -70,7 +97,9 @@ public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
         frame.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(final MouseEvent e) {
-                System.exit(0);
+            	if (e.getButton() == MouseEvent.BUTTON1) {
+                    System.exit(0);
+            	}
             }
 
             @Override
@@ -85,6 +114,10 @@ public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
             @Override
             public void mouseExited(final MouseEvent e) {}
         });
+        if (zDim > 1) {
+        	frame.addMouseWheelListener(this);
+            bufferData = new int[4 * length];
+        } // if (zDim > 1) 
         final GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         final GraphicsDevice gs = ge.getDefaultScreenDevice();
         gs.setFullScreenWindow(frame);
@@ -99,5 +132,54 @@ public class PlugInAlgorithmFullScreenDisplay extends AlgorithmBase {
         setCompleted(true);
 
     }
+    
+public void mouseWheelMoved(final MouseWheelEvent mouseWheelEvent) {
+        final int wheelRotation = mouseWheelEvent.getWheelRotation();
+        if ((wheelRotation < 0) && (zOffset < zDim - 1)) {
+        	// Increment slice
+        	zOffset++;
+        	if (isColorImage) {
+        		for (int i = 0; i < length; i++) {
+                    bufferData[i * 4 + 0] = imageData[zOffset][i * 4 + 1];
+                    bufferData[i * 4 + 1] = imageData[zOffset][i * 4 + 2];
+                    bufferData[i * 4 + 2] = imageData[zOffset][i * 4 + 3];
+                    bufferData[i * 4 + 3] = 255;
+                }
+        	} // if (isColorImage)
+        	else {
+        		for (int i = 0; i < length; i++) {
+                    bufferData[i * 4 + 0] = imageData[zOffset][i];
+                    bufferData[i * 4 + 1] = imageData[zOffset][i];
+                    bufferData[i * 4 + 2] = imageData[zOffset][i];
+                    bufferData[i * 4 + 3] = 255;
+                }
+        	}
+        	inputImage.getRaster().setPixels(0, 0, inputWidth, inputHeight, bufferData);
+        	frame.repaint();
+        } // if ((wheelRotation < 0) && (zOffset < zDim - 1))
+        else if ((wheelRotation > 0) && (zOffset > 0)) {
+        	// Decrement slice
+        	zOffset--;
+        	if (isColorImage) {
+        		for (int i = 0; i < length; i++) {
+                    bufferData[i * 4 + 0] = imageData[zOffset][i * 4 + 1];
+                    bufferData[i * 4 + 1] = imageData[zOffset][i * 4 + 2];
+                    bufferData[i * 4 + 2] = imageData[zOffset][i * 4 + 3];
+                    bufferData[i * 4 + 3] = 255;
+                }
+        	} // if (isColorImage)
+        	else {
+        		for (int i = 0; i < length; i++) {
+                    bufferData[i * 4 + 0] = imageData[zOffset][i];
+                    bufferData[i * 4 + 1] = imageData[zOffset][i];
+                    bufferData[i * 4 + 2] = imageData[zOffset][i];
+                    bufferData[i * 4 + 3] = 255;
+                }
+        	}
+        	inputImage.getRaster().setPixels(0, 0, inputWidth, inputHeight, bufferData);
+        	frame.repaint();
+        } // else if ((wheelRotation > 0) && (zOffset > 0))
+}
+
 
 }
