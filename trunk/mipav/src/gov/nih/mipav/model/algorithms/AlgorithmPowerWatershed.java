e9b5f3cf042feffd04f8acd30fb54ed94bcc2ac2
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-10-08 21:56:39 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13861 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmPowerWatershed.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmPowerWatershed.java
@@ -3,14 +3,13 @@ import WildMagic.LibFoundation.Mathematics.Vector3f;
 
 
 import gov.nih.mipav.model.algorithms.filters.*;
+import gov.nih.mipav.model.algorithms.utilities.AlgorithmChangeType;
 import gov.nih.mipav.model.file.*;
 import gov.nih.mipav.model.scripting.*;
 import gov.nih.mipav.model.structures.*;
-
 import gov.nih.mipav.view.*;
 
 import java.io.*;
-
 import java.util.*;
 
 
@@ -57,6 +56,8 @@ public class AlgorithmPowerWatershed extends AlgorithmBase {
     
     // Geodesic reconstruction
     private boolean geod;
+    
+    private boolean error = false;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
@@ -102,7 +103,23 @@ public class AlgorithmPowerWatershed extends AlgorithmBase {
      * Starts the program.
      */
     public void runAlgorithm() {
-
+        int M; // Number of edges
+        int xDim = srcImage.getExtents()[0];
+        int yDim = srcImage.getExtents()[1];
+        String imageName = srcImage.getImageName();
+        int zDim;
+        int edges[][];
+        int weights[];
+        int max_weight;
+        boolean quicksort = false;
+        int size_seeds = index_seeds.size();
+        ModelImage colorImage = null;
+        if (srcImage.getNDims() == 2) {
+        	zDim = 1;
+        }
+        else {
+        	zDim = srcImage.getExtents()[2];
+        }
         if (srcImage == null) {
             displayError("Source Image is null");
 
@@ -118,9 +135,197 @@ public class AlgorithmPowerWatershed extends AlgorithmBase {
 
 
         fireProgressStateChanged("Power Watershed ...");
+        
+        M = zDim * xDim * (yDim - 1) + zDim * (xDim - 1) * yDim + (zDim - 1) * xDim * yDim;
+        edges = new int[2][M];
+        
+        compute_edges(edges, xDim, yDim, zDim);
+        
+        if (srcImage.isColorImage()) {
+        	if ((srcImage.getFileInfo()[0].getDataType() == ModelStorageBase.ARGB_USHORT) ||
+        		(srcImage.getFileInfo()[0].getDataType() == ModelStorageBase.ARGB_FLOAT)) {
+        	    colorImage = new ModelImage(ModelStorageBase.ARGB, srcImage.getExtents(), "ColorImage");
+        	    boolean image25D = true;
+        	    AlgorithmChangeType changeTypeAlgo = new AlgorithmChangeType(colorImage, srcImage, srcImage.getMin(),
+        	    		srcImage.getMax(), 0, 255, image25D);
+        	    changeTypeAlgo.run();
+          		changeTypeAlgo.finalize();
+          		changeTypeAlgo = null;
+          		FileInfoBase[] fileInfo = colorImage.getFileInfo();
+                fileInfo[0].setModality(srcImage.getFileInfo()[0].getModality());
+                fileInfo[0].setFileDirectory(srcImage.getFileInfo()[0].getFileDirectory());
+                fileInfo[0].setUnitsOfMeasure(srcImage.getFileInfo()[0].getUnitsOfMeasure());
+                fileInfo[0].setResolutions(srcImage.getFileInfo()[0].getResolutions());
+                fileInfo[0].setExtents(colorImage.getExtents());
+                fileInfo[0].setMax(colorImage.getMax());
+                fileInfo[0].setMin(colorImage.getMin());
+                fileInfo[0].setImageOrientation(srcImage.getImageOrientation());
+                fileInfo[0].setAxisOrientation(srcImage.getFileInfo()[0].getAxisOrientation());
+                fileInfo[0].setOrigin(srcImage.getFileInfo()[0].getOrigin());
+        	} // if ((srcImage.getFileInfo()[0].getDataType() == ModelStorageBase.ARGB_USHORT) ||
+        	else {
+        		colorImage = srcImage;
+        	}
+        } // if (srcImage.isColorImage())
+        if (algo == Kruskal) {
+            weights = new int[M];
+            max_weight = 255;
+            if (srcImage.isColorImage()) {
+            	max_weight = color_standard_weights(colorImage, weights, edges, index_seeds, size_seeds, geod, quicksort);
+            	if (error) {
+            		setCompleted(false);
+            		return;
+            	}
+            }
+        } // if (algo == Kruskal)
+        setCompleted(true);
+        return;
 
         
-    }
+    } // runAlgorithm
+    
+     private void compute_edges(  int edges[][], /*array of node indexes composing edges */
+    				     int rs, /* rowsize of the image */
+    				     int cs, /* colsize of the image */
+    				     int ds) /* depth of the image */
+    		{
+    		  int i,j,k,l,M;
+    		  M=0; 
+    		  int rs_cs = rs*cs;
+    		  for(k=0;k<ds;k++) 
+    		    {
+    		      for(i=0;i<cs;i++) 
+    			{
+    			  for(j=0;j<rs;j++)
+    			    {
+    			      if(i<(cs-1))
+    				{
+    				  edges[0][M]=j+i*rs+k*rs_cs;
+    				  edges[1][M]=j+(i+1)*rs+k*rs_cs;
+    				  M++;
+    				}
+    			    }
+    			}
+    		      for(i=0;i<cs;i++) 
+    			{
+    			  for(j=0;j<rs;j++)
+    			    {
+    			      if(j<(rs-1))
+    				{
+    				  edges[0][M]=j+i*rs+k*rs_cs;
+    				  edges[1][M]=j+1+i*rs+k*rs_cs;
+    				  M++;
+    				}
+    			    }
+    			}
+    		      if (k != ds-1)
+    			for(l=k*rs*cs;l<(k+1)*rs_cs;l++) 
+    			  {
+    			    edges[0][M]=l;
+    			    edges[1][M]=l+rs_cs;
+    			    M++;
+    			  }
+    		    }
+    		} // compute_edges;
+
+    private int color_standard_weights(ModelImage image , /* image */
+			    int weights[], /* array to store the values of weights on the edges */
+			    int edges[][],       /* array of node indexes composing edges */ 
+			    Vector<Integer> seeds,        /* vector of seeded nodes indexes */
+			    int size_seeds,     /* nb of seeded nodes */
+			    boolean geod,          /* if true, geodesic reconstruction is performed */
+			    boolean quicksort)     /* true : bucket sort used; false : stochastic sort o(n log n) */
+/* ================================================================================================== */
+/* Computes weights inversely proportionnal to the image gradient for 2D color (ppm) images */
+{
+int maxi = 0;
+int i,M, xDim, yDim, zDim;
+xDim = image.getExtents()[0];
+yDim = image.getExtents()[1];
+if (image.getNDims() == 2) {
+	zDim = 1;
+}
+else {
+	zDim = image.getExtents()[2];
+}
+
+int wr, wg, wb;
+int imgLength = xDim * yDim;
+if (image.getNDims() > 2) {
+	imgLength *= zDim;
+}
+
+short img_r[] = new short[imgLength];
+short img_g[] = new short[imgLength];
+short img_b[] = new short[imgLength];
+
+try {
+	image.exportRGBData(1, 0, imgLength, img_r);
+}
+catch (IOException e) {
+    MipavUtil.displayError("IOException " + e + " on image.exportRGBData(1, 0, imgLength, img_r)");
+    error = true;
+    return -1;
+}
+try {
+	image.exportRGBData(2, 0, imgLength, img_g);
+}
+catch (IOException e) {
+    MipavUtil.displayError("IOException " + e + " on image.exportRGBData(2, 0, imgLength, img_g)");
+    error = true;
+    return -1;
+}
+try {
+	image.exportRGBData(3, 0, imgLength, img_b);
+}
+catch (IOException e) {
+    MipavUtil.displayError("IOException " + e + " on image.exportRGBData(3, 0, imgLength, img_b)");
+    error = true;
+    return -1;
+}
+
+
+M = zDim*xDim*(yDim-1)+zDim*(xDim-1)*yDim+(zDim-1)*yDim*xDim; 
+	 
+for (i=0;i<M;i++)
+ {
+   wr = Math.abs(img_r[edges[0][i]]-img_r[edges[1][i]]) ;
+   wg = Math.abs(img_g[edges[0][i]]-img_g[edges[1][i]]) ;
+   wb = Math.abs(img_b[edges[0][i]]-img_b[edges[1][i]]) ;
+   weights[i] = wr*wr+wg*wg+wb*wb;
+   if (weights[i]> maxi) (maxi) = weights[i];
+ }
+if (geod==false)
+ { 
+   for (i=0;i<M;i++)
+	weights[i]= maxi-weights[i];
+ }
+else
+ { 
+   int j,k,n;
+   int weights_tmp[] = new int[M];
+   int seeds_function[] = new int[M];
+   int numvoisins = 4;
+   if (zDim>1) numvoisins = 6;
+   for (i=0;i<M;i++)
+	weights_tmp[i]=maxi-weights[i];
+	
+   for (j=0;j<size_seeds;j++)
+	for (k=1;k<=numvoisins; k++)
+	  {
+	    //n = neighbor_node_edge(seeds.get(j), k, xDim, yDim, zDim);
+	    //if (n != -1)
+	      //seeds_function[n]=weights_tmp[n];
+	  } 
+   //gageodilate_union_find(seeds_function, weights_tmp, weights, edges, xDim, yDim, zDim, maxi, quicksort);
+   weights_tmp = null;
+   seeds_function = null;
+ }
+img_r = null;
+img_g = null;
+img_b = null;
+return maxi;
+}
 
     
 }
