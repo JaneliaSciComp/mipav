17453769d8261ea72855786478346f7f4f76495e
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-10-23 19:54:37 +0000

    Call to JDialogGraphBasedSegmentation added.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13904 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/ViewJFrameImage.java
+++ trunk/mipav/src/gov/nih/mipav/view/ViewJFrameImage.java
@@ -1885,6 +1885,8 @@ public class ViewJFrameImage extends ViewJFrameBase implements KeyListener, Mous
             }
         } else if (command.equals("PowerWatershed")) {
         	new JDialogPowerWatershed(this, getActiveImage());
+        } else if (command.equals("GraphBasedSeg")) {
+        	new JDialogGraphBasedSegmentation(this, getActiveImage());
         } else if (command.equals("DiffusionLevelsetITK")) {
             new JDialogFastMarching(this, getActiveImage(), 0);
             /*
