5fc8bd9d93ffc05af733b75638b67e183c6a0b93
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-11-09 20:27:05 +0000

    Removed for updated version.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13946 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/worm-untwisting-plugin-elife/src/plugins/PlugInEyeTracking.java
+++
@@ -1,8 +0,0 @@
-import gov.nih.mipav.plugins.*; // needed to load PlugInAlgorithm / PlugInView / PlugInFile interface
-
-
-public class PlugInEyeTracking implements PlugInGeneric {
-    public void run() {
-        new PlugInDialogEyeTracking();
-    }
-}
--- tags/worm-untwisting-plugin-elife/src/plugins/PlugInFITBIRImage.java
+++
@@ -1,8 +0,0 @@
-import gov.nih.mipav.plugins.PlugInGeneric;
-
-
-public class PlugInFITBIRImage implements PlugInGeneric {
-    public void run() {
-        new PlugInDialogFITBIR();
-    }
-}
