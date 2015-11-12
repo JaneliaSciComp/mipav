47e0e55fa6a96e2a9b769d25d34d4cbe8a409edf
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-11-12 15:25:25 +0000

    Removed for updated version.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13958 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/worm-untwisting-plugin-elife/src/WildMagic/Shaders/GLSL/DefaultV.glsl
+++
@@ -1,6 +0,0 @@
-uniform mat4 WVPMatrix;
-in vec3 inPosition;
-void  v_DefaultV()
-{
-    gl_Position = WVPMatrix * vec4(inPosition, 1.0);
-} 
--- tags/worm-untwisting-plugin-elife/src/WildMagic/Shaders/GLSL/PassThrough3.glsl
+++
@@ -1,7 +0,0 @@
-in vec4 varColor;
-out vec4     fragColor;
-void p_PassThrough3 ()
-{
-    fragColor.xyz = varColor.xyz;
-    fragColor.w = 1.0;
-}
