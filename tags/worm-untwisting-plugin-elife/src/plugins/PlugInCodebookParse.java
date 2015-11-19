7c3e325be26391dc2cfc79b08a4505fa6b375c9a
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-11-19 20:12:00 +0000

    Removed for updated version.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13973 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/worm-untwisting-plugin-elife/src/plugins/PlugInCodebookParse.java
+++
@@ -1,8 +0,0 @@
-
-import gov.nih.mipav.plugins.*; // needed to load PlugInAlgorithm / PlugInView / PlugInFile interface
-
-public class PlugInCodebookParse implements PlugInGeneric {
-	public void run() {
-		new PlugInDialogCodebookParse(false);
-	}
-}
