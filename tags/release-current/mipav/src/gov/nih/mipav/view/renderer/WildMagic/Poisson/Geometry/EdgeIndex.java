838cccc7a2684d66e3a77d9cefd83eabc1206028
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2015-12-23 21:14:20 +0000

    Removed old branch.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@13998 ba61647d-9d00-f842-95cd-605cb4296b96

--- tags/release-current/mipav/src/gov/nih/mipav/view/renderer/WildMagic/Poisson/Geometry/EdgeIndex.java
+++
@@ -1,6 +0,0 @@
-package gov.nih.mipav.view.renderer.WildMagic.Poisson.Geometry;
-
-
-public class EdgeIndex {
-	public int[] idx = new int[2];
-}
--- tags/release-current/mipav/src/gov/nih/mipav/view/renderer/WildMagic/ProstateFramework/libsvm/svm_print_interface.java
+++
@@ -1,5 +0,0 @@
-package gov.nih.mipav.view.renderer.WildMagic.ProstateFramework.libsvm;
-public interface svm_print_interface
-{
-	public void print(String s);
-}
