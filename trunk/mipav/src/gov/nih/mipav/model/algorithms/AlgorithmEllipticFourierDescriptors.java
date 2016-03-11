0279575407cc5da99f6a3217d6e11ad7cec039ca
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2016-03-11 23:21:36 +0000

    New file created.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14074 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmEllipticFourierDescriptors.java
@@ -0,0 +1,59 @@
+package gov.nih.mipav.model.algorithms;
+
+import WildMagic.LibFoundation.Mathematics.Vector3f;
+import gov.nih.mipav.model.structures.*;
+import gov.nih.mipav.view.*;
+
+import java.util.*;
+
+public class AlgorithmEllipticFourierDescriptors extends AlgorithmBase {
+	
+	//~ Instance fields ------------------------------------------------------------------------------------------------
+
+    /** The voi selected by the user. */
+    private VOI activeVOI;
+
+    /** Number of coefficients. */
+    private int coefficients;
+
+    /** The resultant polygon and the evolution has completed. */
+    private VOI resultVOI;
+
+    /** Source image. */
+    private ModelImage srcImage;
+    
+//~ Constructors ---------------------------------------------------------------------------------------------------
+    
+    public AlgorithmEllipticFourierDescriptors() {
+       
+    }
+
+    /**
+     * Creates a new AlgorithmEllipticFourierDescriptors object.
+     *
+     * @param  srcImg     2D or 3D source image
+     * @param  activeVOI  the selected voi
+     * @param  coefficients       number of coefficients
+     */
+    public AlgorithmEllipticFourierDescriptors(ModelImage srcImg, VOI activeVOI, int coefficients) {
+
+        srcImage = srcImg;
+        this.activeVOI = activeVOI;
+        this.coefficients = coefficients;
+    }
+    
+    /**
+     * Starts the smooth algorithm.
+     */
+    public void runAlgorithm() {
+
+        if (srcImage == null) {
+            displayError("Source Image is null");
+
+            return;
+        } else {
+        	
+        }
+    }
+
+}
