b6ac74f363292cf06a7d7cbb4ba8145e87ae7429
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2016-05-23 21:37:19 +0000

    Updated setSecondaryCaptureTags.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14219 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogMode.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogMode.java
@@ -283,7 +283,8 @@ public class JDialogMode extends JDialogScriptableBase implements AlgorithmInter
                     resultImage.setImageName(name);
 
                     if ((resultImage.getFileInfo()[0]).getFileFormat() == FileUtility.DICOM) {
-                        ((FileInfoDicom) (resultImage.getFileInfo(0))).setSecondaryCaptureTags();
+                        ((FileInfoDicom) (resultImage.getFileInfo(0))).setSecondaryCaptureTags(false,
+                        		resultImage.getFileInfo()[0].getDataType());
                     }
 
                     // Make algorithm
@@ -379,7 +380,8 @@ public class JDialogMode extends JDialogScriptableBase implements AlgorithmInter
                     if ((resultImage.getFileInfo()[0]).getFileFormat() == FileUtility.DICOM) {
 
                         for (int i = 0; i < resultImage.getExtents()[2]; i++) {
-                            ((FileInfoDicom) (resultImage.getFileInfo(i))).setSecondaryCaptureTags();
+                            ((FileInfoDicom) (resultImage.getFileInfo(i))).setSecondaryCaptureTags(true,
+                            		resultImage.getFileInfo()[0].getDataType());
                         }
                     }
 
