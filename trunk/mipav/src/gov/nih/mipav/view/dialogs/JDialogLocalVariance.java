cc36e395185a01e98ba3253f275e531f272e6826
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2016-05-23 21:33:54 +0000

    Updated setSecondaryCaptureTags.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14216 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogLocalVariance.java
+++ trunk/mipav/src/gov/nih/mipav/view/dialogs/JDialogLocalVariance.java
@@ -258,7 +258,8 @@ public class JDialogLocalVariance extends JDialogScriptableBase implements Algor
                     resultImage     = new ModelImage(ModelStorageBase.DOUBLE, destExtents, name);
 
                     if ((resultImage.getFileInfo()[0]).getFileFormat() == FileUtility.DICOM) {
-                        ((FileInfoDicom) (resultImage.getFileInfo(0))).setSecondaryCaptureTags();
+                        ((FileInfoDicom) (resultImage.getFileInfo(0))).setSecondaryCaptureTags(false,
+                		resultImage.getFileInfo()[0].getDataType());
                     }
 
                     // Make algorithm
@@ -356,7 +357,8 @@ public class JDialogLocalVariance extends JDialogScriptableBase implements Algor
                     if ((resultImage.getFileInfo()[0]).getFileFormat() == FileUtility.DICOM) {
 
                         for (int i = 0; i < resultImage.getExtents()[2]; i++) {
-                            ((FileInfoDicom) (resultImage.getFileInfo(i))).setSecondaryCaptureTags();
+                            ((FileInfoDicom) (resultImage.getFileInfo(i))).setSecondaryCaptureTags(true,
+                            		resultImage.getFileInfo()[0].getDataType());
                         }
                     }
 
