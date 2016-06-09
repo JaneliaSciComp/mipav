a93712ee33c2948731649f3631c88720b0cabe71
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2016-06-09 21:20:28 +0000

    In writeHeader pad odd length strings to even length.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14326 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileDicom.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileDicom.java
@@ -4533,6 +4533,9 @@ public class FileDicom extends FileDicomBase {
             if (pixelSpacingIndex >= 0) {
             	String xyPixelSpacingString = (String) table.getValue("0028,0030");
             	// VM = 2 so don't reduce length to 16
+            	if ((xyPixelSpacingString.length() %2) == 1) {
+            		xyPixelSpacingString = xyPixelSpacingString + " ";
+            	}
             	item.setValue("0028,0030", xyPixelSpacingString, xyPixelSpacingString.length());
             	excludeVector.add(pixelSpacingIndex);
             }
@@ -4541,6 +4544,9 @@ public class FileDicom extends FileDicomBase {
             	if (sliceThicknessString.length() > 16) {
             		sliceThicknessString = reduceStringLengthTo16(sliceThicknessString);
             	}
+            	if ((sliceThicknessString.length() % 2) == 1) {
+            	    sliceThicknessString = sliceThicknessString + " ";	
+            	}
             	item.setValue("0018,0050", sliceThicknessString, sliceThicknessString.length());
             	excludeVector.add(sliceThicknessIndex);
             }
@@ -4548,6 +4554,9 @@ public class FileDicom extends FileDicomBase {
             	String spacingBetweenSlicesString = (String)table.getValue("0018,0088");
             	if (spacingBetweenSlicesString.length() > 16) {
             		spacingBetweenSlicesString = reduceStringLengthTo16(spacingBetweenSlicesString);
+            	}
+            	if ((spacingBetweenSlicesString.length() % 2) == 1) {
+            		spacingBetweenSlicesString = spacingBetweenSlicesString + " ";
             	}
             	item.setValue("0018,0088", spacingBetweenSlicesString, spacingBetweenSlicesString.length());
             	excludeVector.add(spacingBetweenSlicesIndex);
@@ -4575,6 +4584,9 @@ public class FileDicom extends FileDicomBase {
             table.setValue("0020,9113", seqPos, -1);
             String imagePositionString = (String)table.getValue("0020,0032");
             // VM = 3 so don't reduce length to 16
+            if ((imagePositionString.length() % 2) == 1) {
+            	imagePositionString = imagePositionString + " ";
+            }
             itemPos.setValue("0020,0032", imagePositionString, imagePositionString.length());
             excludeVector.add(imagePositionIndex);
             itemPos.setWriteAsUnknownLength(false); // items are always written using known length
@@ -4600,6 +4612,9 @@ public class FileDicom extends FileDicomBase {
             table.setValue("0020,9116", seqOrient, -1);
             String imageOrientationString = (String)table.getValue("0020,0037");
             // VM = 6 so don't reduce length to 16
+            if ((imageOrientationString.length() % 2) == 1) {
+            	imageOrientationString = imageOrientationString + " ";
+            }
             itemOrient.setValue("0020,0037", imageOrientationString, imageOrientationString.length());
             excludeVector.add(imageOrientationIndex);
             itemOrient.setWriteAsUnknownLength(false); // items are always written using known length
