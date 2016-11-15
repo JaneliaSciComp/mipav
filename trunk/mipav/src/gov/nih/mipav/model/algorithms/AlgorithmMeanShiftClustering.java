7fe8abd68c42ce795d79200d150365be162b9a02
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2016-11-15 22:55:19 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14710 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmMeanShiftClustering.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/AlgorithmMeanShiftClustering.java
@@ -49,6 +49,11 @@ public class AlgorithmMeanShiftClustering extends AlgorithmBase {
 	private static final int FAMS_MAX_K = 70;
 	// Maximum valid L
 	private static final int FAMS_MAX_L = 500;
+	// first hash table block size
+	private static final int FAMS_BLOCKSIZE = 4096;
+	
+	//private static final int Bs = FAMS_BLOCKSIZE/sizeof(fams_hash_entry);
+	private static final int Bs = FAMS_BLOCKSIZE/14;
 	// K, L are LSH parameters.  If either K or L is not greater than zero, the LSH data structure is not built
 	// and the linear algorithm is run.
 	private int K;
@@ -107,9 +112,17 @@ public class AlgorithmMeanShiftClustering extends AlgorithmBase {
 	private int npm;
 	
 	// hash table data
+	private int M;
+	private int M2;
 	private int hashCoeffs[];
 	
+	// alg_params
+	private int K_;
+	private int L_;
+	
 	// temporary
+	private boolean t_cut_res[][] = new boolean[FAMS_MAX_L][FAMS_MAX_K];
+	private boolean t_old_cut_res[][] = new boolean[FAMS_MAX_L][FAMS_MAX_K];
 	private int nnres1;
 	private int nnres2;
 	
@@ -177,7 +190,7 @@ public class AlgorithmMeanShiftClustering extends AlgorithmBase {
 	public void runAlgorithm() {
 	    String fdata_file_name;
 	    int Lmax = 0;
-	    int Kmax;
+	    int Kmax = 0;
 	    RandomAccessFile raFile;
 	    String firstLine;
 	    String values[];
@@ -382,6 +395,13 @@ public class AlgorithmMeanShiftClustering extends AlgorithmBase {
 	        KBest = new int[FAMS_MAX_K];
 	        
 	        Lcrt = Lmax;
+	        Preferences.debug("About to find valid pairs\n", Preferences.DEBUG_ALGORITHM);
+	        for (Kcrt = Kmax, nBest = 0; Kcrt >= Kmin; Kcrt -= Kjump, nBest++) {
+	            // Do iterations for crt K and L = 1...Lcrt
+	        	for (ntimes = 0; ntimes < FAMS_FKL_TIMES; ntimes++) {
+	        		
+	        	}
+	        } // for (Kcrt = Kmax, nBest = 0; Kcrt >= Kmin; Kcrt -= Kjump, nBest++)
 	    } // if (findOptimalKL)
 	}
 	
@@ -461,6 +481,353 @@ public class AlgorithmMeanShiftClustering extends AlgorithmBase {
 	    	}
 	    } // else
 	}
+	
+	private long doFindKLIteration(int K, int L, float scores[]) {
+		int i;
+		int j;
+		fams_hash_entry HT[][];
+	    int hs[];
+	    fams_cut cuts[][];
+	    boolean cut_res[];
+	    long t1;
+	    long t2;
+	    long timeElapsed;
+	    int hjump[] = new int[1];
+	    int m;
+	    int m2;
+	    
+	    K_ = K;
+	    L_ = L;
+		
+		// Allocate memory for the hash table
+		M = getPrime(3*nPoints*L_/Bs);
+		HT = new fams_hash_entry[M][Bs];
+		hs = new int[M];
+		initHash(K_+L_);
+		
+		// Build partitions
+		cuts = new fams_cut[L_][FAMS_MAX_K];
+		cut_res = new boolean[FAMS_MAX_K];
+		makeCuts(cuts);
+		
+		// Insert data into partitions
+		for (j = 0; j < nPoints; j++) {
+		    for (i = 0; i < L_; i++) {
+		        evalCutRes(points[j], cuts[i],cut_res);
+		        m = hashFunction(cut_res, 0, i, K_, M, hjump);
+		        m2 = hashFunction(cut_res, 1, i, K_ - 1, 0, null);
+		        addDataToHash(HT, hs, points[j], m, Bs, M, i, m2, hjump[0]);
+		    } // for (j = 0; j < nPoints; j++)
+		} // for (j = 0; j < nPoints; j++)
+		
+		// Compute scores
+		t1 = System.currentTimeMillis();
+		computeScores(HT, hs, cuts, scores);
+		t2 = System.currentTimeMillis();
+		timeElapsed = t2 - t1;
+		return timeElapsed;
+	}
+	
+	// compute the pilot h_i's for the data points
+	private void computeScores(fams_hash_entry HT[][], int hs[], fams_cut cuts[][], float scores[])
+	{
+	   final int win_j = 10,max_win=7000;
+	   int i,j,who;
+	   int nn;
+	   int wjd = (int)(win_j*nDims);
+	   int num_l[] = new int[1000];
+	   for (i = 0; i < L_; i++) {
+		   scores[i] = 0.0f;
+	   }
+	
+	   fams_res_cont res = new fams_res_cont(nPoints);
+	   for(j=0; j<nsel; j++)
+	   { 
+	      who = psel[j];
+	      int numn;
+	      int nl=0;
+	      int numns[] = new int[max_win/win_j];
+	      int nel;
+	      if(noLSH)
+	      {
+	         nel = nPoints;
+	         num_l[L_]=nPoints+1;
+	         for(i=0; i<nel; i++)
+	         {
+	            famsPoint pt = points[i];
+	            nn = distL1(points[who],pt) / wjd;
+	            if(nn <max_win/win_j)
+	               numns[nn]++;	
+	            if(i == (num_l[nl]-1))
+	            {
+	               numn=0;
+	               for(nn=0; nn<max_win/win_j; nn++)
+	               {
+	                  if(numn>k_neigh)
+	                     break;
+	               }
+	               for(; (num_l[nl]-1) == i; nl++)
+	                  scores[nl] += (float) (((nn+1.0)*win_j)/points[who].window);
+	            }
+	         }
+	      }
+	      else
+	      {
+	         getNearestNeighbours(points[who],HT,hs,cuts,res,0,num_l);
+	         nel = res.getNel();
+	         num_l[L_] = nPoints+1;
+	         for(i=0; i<nel; i++)
+	         {
+	            famsPoint pt = res.getVec()[i];
+	            nn = distL1(points[who],pt) / wjd;
+	            if(nn <max_win/win_j)
+	               numns[nn]++;
+	            if(i == (num_l[nl]-1))
+	            {
+	               numn=0;
+	               for(nn=0; nn<max_win/win_j; nn++)
+	               {
+	                  numn+=numns[nn];
+	                  if(numn>k_neigh)
+	                     break;
+	               }
+	               for(; (num_l[nl]-1) == i; nl++)
+	                  scores[nl] += (float) (((nn+1.0)*win_j)/points[who].window);
+	            }
+	         }
+	      }
+	      numn=0;
+	      for(nn=0; nn<max_win/win_j; nn++)
+	      {
+	         numn+=numns[nn];
+	         if(numn>k_neigh)
+	            break;
+	      }	
+	   }
+	   for(j=0; j<L_; j++)
+	      scores[j]/=nsel;
+
+	}
+	
+	//perform an LSH query
+
+	private void getNearestNeighbours(famsPoint who, fams_hash_entry HT[][], int hs[],
+	     fams_cut cuts[][], fams_res_cont res, int print,int num_l[])
+	{
+	   int i;
+	   for(i=0; i<L_; i++)
+	      evalCutRes(who,cuts[i],t_cut_res[i]);
+	   if(!compareCutRes(t_cut_res,t_old_cut_res))
+	   {
+	      return;
+	   }
+	   for (i = 0; i < t_old_cut_res.length; i++) {
+		   t_old_cut_res[i] = t_cut_res[i];
+	   }
+	   res.clear();
+	   nnres1++;
+	   for(i=0; i<L_; i++)
+	   {
+	      int hjump[] = new int[1];
+	      int m = hashFunction(t_cut_res[i],0,i,K_,M,hjump);
+	      int m2 = hashFunction(t_cut_res[i],1,i,K_-1, 0, null);
+	      addDataToRes(HT,hs,res,m,Bs,M,i,nnres1,m2,hjump[0]);
+	      num_l[i] = res.getNel();
+	   }
+	}
+	
+	// Perform a query to one partition and retreive all the points in the cell 
+
+	private void addDataToRes(fams_hash_entry HT[][],int hs[],fams_res_cont res,int where,
+	        int Bs,int M,int which, int nnres, int which2, int hjump)
+	{
+	   int uu;
+	   for(;;where = (where+hjump)%M)
+	   {
+	      for(uu=0; uu<hs[where]; uu++)
+	      {
+	         if(HT[where][uu].getWhichCut() == which && HT[where][uu].getWhich2() == which2)
+	         {
+	            if((HT[where][uu].getPt()).getUsedFlag()!=nnres)
+	            {
+	               res.push_back(HT[where][uu].getPt());
+	               (HT[where][uu].getPt()).setUsedFlag(nnres);
+	            }
+	         }
+	      }
+	      if(hs[where] < Bs)
+	         break;
+	   }
+	}
+
+
+	//Compare a pair of L binary vectors
+	private boolean compareCutRes(boolean in_cr1[][], boolean in_cr2[][])
+	{
+	   for (int in_i=0; in_i<L_; in_i++)
+	      for (int in_j=0; in_j<K_; in_j++)
+	         if (in_cr1[in_i][in_j] != in_cr2[in_i][in_j])
+	            return true;
+//	      if (memcmp(in_cr1[in_i], in_cr2[in_i], (K_*sizeof(int))) != 0)
+//	         return 1;
+	   return false;	
+	}
+	
+	// Add a point to the LSH hash table using double hashing 
+	private void addDataToHash(fams_hash_entry HT[][],int hs[],famsPoint pt,int where,int Bs,int M,int which,int which2,
+	                      int hjump)
+	{
+	   int nw=0;
+	   for(;;where = (where+hjump)%M)
+	   {
+	      nw++;
+	      if(nw > M)
+	      {
+	         Preferences.debug("LSH hash table overflow exiting\n", Preferences.DEBUG_ALGORITHM);
+	         System.exit(-1);
+	      }
+	      if(hs[where] == Bs)
+	         continue;
+	      HT[where][hs[where]].setPt(pt);
+	      HT[where][hs[where]].setWhichCut((short)which);
+	      HT[where][hs[where]].setWhich2(which2);
+	      hs[where]++;
+	      break;
+	   }
+	}
+
+	
+	/* compute the hash key and and the double hash key if needed 
+	It is possible to give M the the size of the hash table and 
+	hjump for the double hash key 
+	*/
+
+	private int hashFunction(boolean cutVals[], int offset, int whichPartition, int kk,int M,int hjump[])
+	{
+	   int i;
+	   int res = whichPartition;
+	   for(i=0; i<kk; i++)
+	   {
+		  if (cutVals[i+offset]) {
+	          res += hashCoeffs[i];
+		  }
+	   }
+	   if(M > 0)
+	   {
+	      res = Math.abs(res);
+	      if(hjump != null)
+	         hjump[0] = (res%(M-1))+1;
+	      res = res%M;
+	   }
+	   return res;
+	}
+
+	
+	//Produce the boolean vector of a data point with a partition 
+	private void evalCutRes(famsPoint in_pt, fams_cut in_part[],boolean in_cut_res[])
+	{
+	   for(int in_i=0; in_i<K_; in_i++)
+	      in_cut_res[in_i] =  in_pt.getData()[in_part[in_i].getWhich()] >= in_part[in_i].getWhere();
+	}
+	
+	private void makeCuts(fams_cut cuts[][])
+	{
+	   int i;
+	   for(i=0; i<L_; i++)
+	      makeCutL(cuts[i]);
+	}
+	
+	// data-driven uniform partition
+
+	private void makeCutL(fams_cut cut[])
+	{
+	   int n1 = (int)Math.floor(K_/(1.0*nDims));
+	   int i,j;
+	   int ncu=0;
+	   int w;
+	   int which[];
+	   int wh;
+	   for(i=0; i<nDims; i++)
+	   {
+	      for(j=0; j<n1; j++)
+	      {
+	         cut[ncu].setWhich(i);
+	         w = Math.min((int)(srand.nextDouble()*nPoints),nPoints-1);
+	         cut[ncu].setWhere(points[w].getData()[i]);
+	         ncu++;
+	      }
+	   }
+	   /*
+	   int *which = new int[d_];
+	   for (i=0; i<d_; i++)
+	      which[i]=i;
+	   int wh, ndleft, itmp;
+	   ndleft = d_;
+	   for (i=0; i<(K_-ncu); i++)
+	   {
+	      wh = MyMin((int)(drand48()*ndleft),ndleft-1);
+	      itmp = which[wh+i];
+	      which[wh+i] = which[i];
+	      which[i] = itmp;
+	      ndleft--;
+	   }
+
+	   for(i=0 ; ncu < K_; ncu++, i++)
+	   {
+	      w = MyMin((int)(drand48()*n_),n_-1);
+	      cut[ncu].which_ = which[i];
+	      cut[ncu].where_ = points_[w].data_[which[i]];
+	   }
+	   delete [] which;
+	   */
+	   which = new int[nDims];
+	   for (; ncu<K_; )
+	   {
+	      wh = Math.min((int)(srand.nextDouble()*nDims),nDims-1);
+	      if(which[wh]!=0)
+	         continue;
+	      which[wh]=1;
+	      w = Math.min((int)(srand.nextDouble()*nPoints), nPoints-1);
+	      cut[ncu].setWhich(wh);
+	      cut[ncu].setWhere(points[w].getData()[wh]);
+	      ncu++;
+	   }
+	   which = null;
+	}
+
+
+	
+	private void initHash(int nk)
+	{  
+	   long t1;
+	   Random rand;
+	   hashCoeffs = null;
+	   hashCoeffs = new int[nk];
+	   t1 = System.currentTimeMillis();
+	   rand = new Random(t1);
+	   for(int i=0; i<nk; i++)
+	      hashCoeffs[i] = rand.nextInt(Integer.MAX_VALUE);
+	}
+
+	
+	// return a prime number greater than minp
+	private int getPrime(int minp)
+	{	
+	   int i,j;
+	   for(i=minp%2==0? minp+1 :minp; ; i+=2)
+	   {
+	      int sqt = (int)Math.sqrt(i);
+	      if(i % 2==0)
+	         continue;
+	      for(j=3; j<sqt; j+=2)
+	      {
+	         if(i % j == 0)
+	            break;
+	      }
+	      if(j >= sqt)
+	         return i;
+	   }
+	};
 	
 	private int distL1(famsPoint inPt1, famsPoint inPt2) {
 		int ini;
@@ -570,5 +937,92 @@ public class AlgorithmMeanShiftClustering extends AlgorithmBase {
 			return weightdp2;
 		}
 	}
+	
+    private class fams_hash_entry {
+		  private short whichCut;
+		  private int which2;
+		  private famsPoint pt;
+		  
+		  public fams_hash_entry() {
+		      
+		  }
+		  
+		  public void setWhichCut(short whichCut) {
+			  this.whichCut = whichCut;
+		  }
+		  
+		  public void setWhich2(int which2) {
+			  this.which2 = which2;
+		  }
+		  
+		  public void setPt(famsPoint pt) {
+			  this.pt = pt;
+		  }
+		  
+		  public short getWhichCut() {
+			  return whichCut;
+		  }
+		  
+		  public int getWhich2() {
+			  return which2;
+		  }
+		  
+		  public famsPoint getPt() {
+			  return pt;
+		  }
+	}
+    
+    private class fams_cut {
+    	private int which;
+	    private int where;
+	    
+	    public fams_cut() {
+	    	
+	    }
+	    
+	    public void setWhich(int which) {
+	    	this.which = which;
+	    }
+	    
+	    public void setWhere(int where) {
+	    	this.where = where;
+	    }
+	    
+	    public int getWhich() {
+	    	return which;
+	    }
+	    
+	    public int getWhere() {
+	    	return where;
+	    }
+    }
+    
+    private class fams_res_cont{
+    	    private int nel;
+    		private famsPoint vec[];
+    		public fams_res_cont() {
+    			nel = 0;
+    			vec = new famsPoint[1];
+    		}
+    		public fams_res_cont(int n){
+    			nel=0;
+    			vec = new famsPoint[n];
+    		};
+    		
+    		public int getNel() {
+    			return nel;
+    		}
+    		
+    		public famsPoint[] getVec() {
+    			return vec;
+    		}
+    		
+    		public void clear() {
+    		    nel = 0;	
+    		}
+    		
+    		public void push_back(famsPoint in_el)
+    		   {vec[nel++] = in_el;};
+    }
 	
 }
