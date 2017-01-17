b7504e6aa771577b51f5a1fd3533674e8df52a81
mccreedy@NIH.GOV <mccreedy@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2017-01-17 20:57:31 +0000

    Removed unused import that was causing build problems on older Java versions.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14796 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInDialogUntwistedToTwisted.java
+++ trunk/mipav/src/plugins/PlugInDialogUntwistedToTwisted.java
@@ -53,7 +53,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
