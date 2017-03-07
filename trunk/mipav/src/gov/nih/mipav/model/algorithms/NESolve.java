797e419eaa4a2fe1cf363542b8092880e36d68a7
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2017-03-07 23:14:57 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14826 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/NESolve.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/NESolve.java
@@ -76,6 +76,9 @@ MathWorks Technical Support Department
     	// Number of function evaluations
     	protected int nofun[] = new int[1];
     	
+    	// eps returns the distance from 1.0 to the next larger double-precision number, that is, eps = 2^-52.
+    	private double eps;
+    	
 	    public NESolve(double x0[], double fparam[], boolean analyticJacobian, double scale[][], Vector<Double> path,
 	    		double btrack[], int scaling) {
 	    	this.x0 = x0;
@@ -123,6 +126,26 @@ MathWorks Technical Support Department
 	    	double hc[][];
 	    	double sn[];
 	    	// Initialization
+	    	// eps returns the distance from 1.0 to the next larger double-precision number, that is, eps = 2^-52.
+	    	// epsilon = D1MACH(4)
+	        // Machine epsilon is the smallest positive epsilon such that
+	        // (1.0 + epsilon) != 1.0.
+	        // epsilon = 2**(1 - doubleDigits) = 2**(1 - 53) = 2**(-52)
+	        // epsilon = 2.2204460e-16
+	        // epsilon is called the largest relative spacing
+	        eps = 1.0;
+	        double neweps = 1.0;
+
+	        while (true) {
+
+	            if (1.0 == (1.0 + neweps)) {
+	                break;
+	            } else {
+	                eps = neweps;
+	                neweps = neweps / 2.0;
+	            }
+	        } // while(true)
+	    	
 	        if (fparam != null) {
 	        	details[15] = 1;
 	        }
@@ -346,25 +369,6 @@ MathWorks Technical Support Department
 	    	} // if (dout[16] == 1)
 	    	
 	    	// Step 4
-	    	// eps returns the distance from 1.0 to the next larger double-precision number, that is, eps = 2^-52.
-	    	// epsilon = D1MACH(4)
-	        // Machine epsilon is the smallest positive epsilon such that
-	        // (1.0 + epsilon) != 1.0.
-	        // epsilon = 2**(1 - doubleDigits) = 2**(1 - 53) = 2**(-52)
-	        // epsilon = 2.2204460e-16
-	        // epsilon is called the largest relative spacing
-	        double eps = 1.0;
-	        double neweps = 1.0;
-
-	        while (true) {
-
-	            if (1.0 == (1.0 + neweps)) {
-	                break;
-	            } else {
-	                eps = neweps;
-	                neweps = neweps / 2.0;
-	            }
-	        } // while(true)
 	    	if (dout[12] <= 0) {
 	    		dout[13] = eps;
 	    	}
@@ -480,6 +484,7 @@ MathWorks Technical Support Department
 	    	// Algorithm step 1.
 	    	int i;
 	    	int j;
+	    	double est;
 	    	int n = Math.max(J.length, J[0].length);
 	    	for (i = 0; i < sf.length; i++) {
 	    		for (j = 0; j < sf.length; j++) {
@@ -496,5 +501,435 @@ MathWorks Technical Support Department
 	    	m = (matM.times(matJ)).getArray();
 	    	
 	    	// Algorithm step 2.
+	    	int n2 = Math.max(m.length, m[0].length);
+	    	double m1[] = new double[n2];
+	    	double m2[] = new double[n2];
+	    	int sing[] = new int[1];
+	    	neqrdcmp(m, m1, m2, sing);
+	    	
+	    	// Algorithm step 3.
+	    	if (sing[0] == 0) {
+	    	    for (j = 1; j < n; j++) {
+	    	    	for (i = 0; i <= j-1; i++) {
+	    	    		m[i][j] = m[i][j]/sx[j];
+	    	    	}
+	    	    } // for (j = 1; j < n; j++)
+	    	    for (i = 0; i < m2.length; i++) {
+	    	    	m2[i] = m2[i]/sx[i];
+	    	    }
+	    	    est = neconest(m,m2);
+	    	} // if (sing[0] == 0)
+	    	else {
+	    		est = 0.0;
+	    	}
+	    	
+	    	// Algorithm step 4.
+	    	if ((sing[0] == 1) || (est > 1.0/eps) || Double.isNaN(est)) {
+	    	    matJ = new Matrix(J);
+	    	    double diagsf[][] = new double[sf.length][sf.length];
+	    	    for (i = 0; i < sf.length; i++) {
+	    	    	diagsf[i][i] = sf[i];
+	    	    }
+	    	    Matrix matSF = new Matrix(diagsf);
+	    	    Matrix matH = (matJ.transpose()).times(matSF);
+	    	    h = (matH.times(matH.transpose())).getArray();
+	    	    // Calculate hnorm = norm(invDxHinvDx)
+	    	    double tem = 0.0;
+	    	    for (i = 0; i < n; i++) {
+	    	    	tem += (Math.abs(h[0][i]) * (1.0/sx[i]));
+	    	    }
+	    	    double hnorm = (1.0/sx[0]) * tem;
+	    	    for (i = 1; i < n; i++) {
+	    	    	double tem1 = 0.0;
+	    	    	for (j = 0; j < n; j++) {
+	    	    		tem1 += (Math.abs(h[j][i])/sx[j]);
+	    	    	}
+	    	    	double tem2 = 0.0;
+	    	    	for (j = 0; j < n; j++) {
+	    	    		tem2 += (Math.abs(h[i][j])/sx[j]);
+	    	    	}
+	    	    	double temp = (1.0/sx[i])/(tem1+tem2);
+	    	    	hnorm = Math.max(temp, hnorm);
+	    	    } // for (i = 1; i < n; i++)
+	    	    double diagsx2[][] = new double[n][n];
+	    	    for (i = 0; i < n; i++) {
+	    	    	diagsx2[i][i] = (sx[i]*sx[i]);
+	    	    }
+	    	    for (i = 0; i < n; i++) {
+	    	    	for (j = 0; j < n; j++) {
+	    	    		h[i][j] = h[i][j] + Math.sqrt(n*eps) * hnorm * diagsx2[i][j];
+	    	    	}
+	    	    } // for (i = 0; i < n; i++)
+	    	    // Calculate sn = inv(H) * g, and keep m (the cholesky factor) for later use.
+	    	    double maxadd[] = new double[1];
+	    	    nechdcmp(m, maxadd, h, 0);
+	    	    matM = new Matrix(m);
+	    	    double mInverse[][] = (matM.inverse()).getArray();
+	    	    double mg[] = new double[n];
+	    	    for (i = 0; i < n; i++) {
+	    	    	for (j = 0; j < n; j++) {
+	    	    		mg[i] += mInverse[i][j] * g[j];
+	    	    	}
+	    	    }
+	    	    double mTInverse[][] = ((matM.transpose()).inverse()).getArray();
+	    	    for (i = 0; i < n; i++) {
+	    	    	for (j = 0; j < n; j++) {
+	    	    		sn[i] = -mTInverse[i][j] * mg[j];
+	    	    	}
+	    	    }
+	    	} // if ((sing[0] == 1) || (est > 1.0/eps) || Double.isNaN(est))
+	    	else {
+	    		// Calculate normal Newton step
+	    		for (j = 1; j < n; j++) {
+	    			for (i = 0; i <= j-1; i++) {
+	    				m[i][j] = m[i][j] * sx[j];
+	    			}
+	    		}
+	    		for (i = 0; i < m2.length; i++) {
+	    			m2[i] = m2[i] * sx[i];
+	    		}
+	    		for (i = 0; i < sf.length; i++) {
+	    			sn[i] = -sf[i] * fc[i];
+	    		}
+	    		neqrsolv(sn,m,m1,m2);
+	    		if ((globmeth == 2) || (globmeth == 3)) {
+	    		    // The cholesky factor (for later use) is the same as R' from QR.
+	    			double trium[][] = new double[n][n];
+	    			double triumTranspose[][] = new double[n][n];
+	    	    	for (i = 0; i < n; i++) {
+	    	    		for (j = i; j < n; j++) {
+	    	    		    trium[i][j] = m[i][j];
+	    	    		    triumTranspose[j][i] = m[i][j];
+	    	    		}
+	    	    	}
+	    	    	for (i = 0; i < n; i++) {
+	    	    		for (j = 0; j < n; j++) {
+	    	    			m[i][j] = trium[i][j] + triumTranspose[i][j];
+	    	    		}
+	    	    	}
+	    		} // if ((globmeth == 2) || (globmeth == 3))
+	    	} // else
 	    } // nemodel
+	    
+	    private void neqrdcmp(double M[][], double M1[], double M2[], int sing[]) {
+	        // M is input/output
+	        // M1, M2, and sing are output
+	    	// This function is part of the Nonlinear Equations package.
+	    	// It is a QR decomposition function.  It differs from the one built into MATLAB
+	    	// in that the result is encoded as rotation angles.  Also, it is designed for
+	    	// square matrices only.
+	    	// Algorithm A3.2.1: Part of the modular software system from the appendix of the book
+	    	// "Numerical Methods for Unconstrained Optimization and Nonlinear Equations" by
+	    	// Dennis & Schnabel, 1983.
+	    	// Coded in MATLAB by Richard T. Behrens, March, 1988.
+	    	int i;
+	    	int j;
+	    	
+	    	int n = Math.max(M.length, M[0].length);
+	    	// Algorithm step 1.
+	    	sing[0] = 0;
+	    	
+	    	// Algorithm step 2.
+	    	for (int k = 0; k < n-1; k++) {
+	    	    double eta = -Double.MAX_VALUE;
+	    	    for (i = k; i < n; i++) {
+	    	    	if (M[i][k] > eta) {
+	    	    		eta = M[i][k];
+	    	        }
+	    	    } // for (i = k; i < n; i++)
+	    	    if (eta == 0.0) {
+	    	    	M1[k] = 0;
+	    	    	M2[k] = 0;
+	    	    	sing[0] = 1;
+	    	    }
+	    	    else {
+	    	    	for (i = k; i < n; i++) {
+	    	    		M[i][k] = M[i][k]/eta;
+	    	    	}
+	    	    	double normM = 0;
+	    	    	for (i = k; i < n; i++) {
+	    	    		normM += (M[i][k]*M[i][k]);
+	    	    	}
+	    	    	normM = Math.sqrt(normM);
+	    	    	double addOne = 0.0;
+	    	    	if (M[k][k] == 0.0) {
+	    	    		addOne = 1.0;
+	    	    	}
+	    	    	double sigma = (sign(M[k][k]) + addOne) * normM;
+	    	    	M[k][k] = M[k][k] + sigma;
+	    	    	M1[k] = sigma * M[k][k];
+	    	    	M2[k] = -eta * sigma;
+	    	    	double tau[] = new double[n-k-1];
+	    	    	for (i = 0; i < n-k-1; i++) {
+	    	    		for (j = k; j < n; j++) {
+	    	    			tau[i] += (M[j][k] * M[j][i+k+1])/M1[k];
+	    	    		}
+	    	    	}
+	    	    	double prod[][] = new double[n-k][n-k-1];
+	    	    	for (i = 0; i < n-k; i++) {
+	    	    		for (j = 0; j < n-k-1; j++) {
+	    	    			prod[i][j] = M[i+k][k] * tau[j];
+	    	    		}
+	    	    	}
+	    	    	for (i = 0; i < n-k; i++) {
+	    	    		for (j = 0; j < n-k-1; j++) {
+	    	    			M[i+k][j+k+1] = M[i+k][j+k+1] - prod[i][j];
+	    	    		}
+	    	    	}
+	    	    } // else
+	    	} // for (int k = 0; k < n-1; k++)
+	    	
+	    	// Algorithm step 3
+	    	M2[n-1] = M[n-1][n-1];
+	    	return;
+	    } // neqrdcmp
+	    
+	    private double neconest(double M[][], double M2[]) {
+	    	// This function is part of the Nonlinear Equations package.
+	    	// This is an estimate of the 1-1 condition number of an upper triangular matrix.
+	    	// Algorithm A3.3.1: Part of the modular software system from the appendix of the book
+	    	// "Numerical Methods for Unconstrained Optimization Methods and Nonlinear Equations"
+	    	// by Dennis & Schnabel, 1983.
+	    	// Coded in MATLAB by Richard T. Behrens, March, 1988.
+	    	int i;
+	    	int j;
+	    	double est;
+	    	
+	    	// Allocate variables
+	    	int n = Math.max(M.length, M[0].length);
+	    	double p[] = new double[n];
+	    	double pm[] = new double[n];
+	    	double x[] = new double[n];
+	    	
+	    	// Algorithm steps 1 & 2
+	    	double triuM[][] = new double[M.length][M.length];
+	    	for (i = 0; i < M.length; i++) {
+	    		for (j = i; j < M.length; j++) {
+	    		    triuM[i][j] = M[i][j];	
+	    		}
+	    	}
+	    	double diagdiagM[][] = new double[M.length][M.length];
+	    	for (i = 0; i < M.length; i++) {
+	    		diagdiagM[i][i] = M[i][i];
+	    	}
+	    	double diagM2[][] = new double[M2.length][M2.length];
+	    	for (i = 0; i < M2.length; i++) {
+	    		diagM2[i][i] = M2[i];
+	    	}
+	    	double sumM[][] = new double[M.length][M.length];
+	    	for (i = 0; i < M.length; i++) {
+	    		for (j = 0; j < M.length; j++) {
+	    			sumM[i][j] = triuM[i][j] - diagdiagM[i][j] + diagM2[i][j];
+	    		}
+	    	}
+	    	double colsum[] = new double[M.length];
+	    	for (j = 0; j < M.length; j++) {
+	    		for (i = 0; i < M.length; i++) {
+	    			colsum[j] += Math.abs(sumM[i][j]);
+	    		}
+	    	}
+	    	est = 0.0;
+	    	for (i = 0; i < M.length; i++) {
+	    		if (colsum[i] > est) {
+	    			est = colsum[i];
+	    		}
+	    	}
+	    	
+	    	// Algorithm step 3.
+	    	x[0] = 1.0/M2[0];
+	    	
+	    	// Algorithm step 4.
+	    	for (i = 1; i < n; i++) {
+	    		p[i] = M[0][i] * x[0];
+	    	}
+	    	
+	    	// Algorithm step 5.
+	    	for (j = 1; j < n; j++) {
+	    		double xp = (+1 - p[j])/M2[j];
+	    		double xm = (-1 - p[j])/M2[j];
+	    		double temp = Math.abs(xp);
+	    		double tempm = Math.abs(xm);
+	    		for (i = j+1; i < n; i++) {
+	    			pm[i] = p[i] + M[j][i] * xm;
+	    			tempm = tempm + Math.abs(p[i])/Math.abs(M2[i]);
+	    			p[i] = p[i] + M[j][i] * xp;
+	    			temp = temp + Math.abs(p[i])/Math.abs(M2[i]);
+	    		} // for (i = j+1; i < n; i++)
+	    		if (temp > tempm) {
+	    			x[j] = xp;
+	    		}
+	    		else {
+	    			x[j] = xm;
+	    			for (i = j+1; i < n; i++) {
+	    				p[i] = pm[i];
+	    			}
+	    		}
+	    	} // for (j = 1; j < n; j++)
+	    	
+	    	// Algorithm steps 6 & 7.
+	    	double sum = 0.0;
+	    	for (i = 0; i < n; i++) {
+	    		sum += Math.abs(x[i]);
+	    	}
+	    	est = est / sum;
+	    	
+	    	// Algorithm step 8.
+	    	nersolv(x, M, M2);
+	    	
+	    	// Algorithm steps 9 & 10.
+	    	sum = 0.0;
+	    	for (i = 0; i < n; i++) {
+	    		sum += Math.abs(x[i]);
+	    	}
+	    	est = est * sum;
+	    	return est;
+	    } // neconest
+	    
+	    private void nersolv(double b[], double M[][], double M2[]) {
+	        // b is input/outpt
+	    	// input M and M2
+	    	// This routine is part of the Nonlinear Equations package.
+	    	// It a a linear equation solve function for upper triangular systems.
+	    	// Algorithm A3.2.2a: Part of the modular software system from the appendix of the book
+	    	// "Numerical Methods for Unconstrained Optimization and Nonlinear Equations" by
+	    	// Dennis & Schnabel, 1983.
+	    	// Coded in MATLAB by Richard T. Behrens, March, 1988.
+	    	int i;
+	    	int j;
+	    	// Algorithm step 1.
+	    	int n = Math.max(M.length,M[0].length);
+	    	b[n-1] = b[n-1]/M2[n-1];
+	    	
+	    	// Algorithm step 2.
+	    	for (i = (n-2); i >= 0; i--) {
+	    		double sum = 0;
+	    	    for (j = i+1; j < n; j++) {
+	    	    	sum += (M[i][j] * b[j]);
+	    	    }
+	    	    b[i] = (b[i] - sum)/M2[i];
+	    	} // for (i = (n-2); i >= 0; i--) 
+	    } // nersolv
+	    
+	    private void nechdcmp(double L[][], double maxadd[], double H[][], double maxoffl) {
+	    	// outputs L and maxadd
+	    	// input H and maxoff1
+	    	// This routine is part of the Nonlinear Equations package.
+	    	// This is a "Perturbed Cholesky Decomposition".  It finds a lower triangular
+	    	// matrix L such that LL' is a factorization of H+D, where D is a diagonal (non-negative)
+	    	// matrix that is added to H if necessary to make it positive definite (so that the
+	    	// factorization is possible).  If H is already positive definite, the ordinary Cholesky
+	    	// decomposition (D=0) is carried out.
+	    	// Algorithm A5.5.2: Part of the modular software system from the appendix of the book
+	    	// "Numerical Methods for Unconstrained Optimization and Nonlinear Equations" by
+	    	// Dennis & Schnabel, 1983.
+	    	// Coded in MATLAB by Sherkat Masoum M., April, 1988.
+	    	// Edited by Richard T. Behrens, June, 1988.
+	    	int i;
+	    	int j;
+	    	int k;
+	    	double sum;
+	    	double minl2 = 0.0;
+	    	
+	    	// Check input arguments
+	    	int m = H.length;
+	    	int n = H[0].length;
+	    	if (m != n) {
+	    		MipavUtil.displayError("Matrix H Must be square");
+	    	}
+	    	
+	    	// Algorithm step 1.
+	    	double minl = Math.pow(eps, 0.25) * maxoffl;
+	    	
+	    	// Algorithm step 2.
+	    	if (maxoffl == 0) {
+	    	    // This is the case when H is known to be positive def.
+	    	    double maxdiagH = -Double.MAX_VALUE;
+	    	    for (i = 0; i < n; i++) {
+	    	    	if (H[i][i] > maxdiagH) {
+	    	    		maxdiagH = H[i][i];
+	    	    	}
+	    	    }
+	    	    maxoffl = Math.sqrt(maxdiagH);
+	    	    minl2 = Math.sqrt(eps) * maxoffl;
+	    	} // if (maxoffl == 0)
+	    	
+	    	// Algorithm step 3.
+	    	maxadd[0] = 0; // The maximum diagonal element (so far) in D.
+	    	
+	    	// Algorithm step 4.
+	    	for (j = 0; j < n; j++) {
+	    		if (j == 0) {
+	    			L[j][j] = H[j][j];
+	    		}
+	    		else {
+	    		    sum = 0.0;
+	    		    for (i = 0; i <= j-1; i++) {
+	    		    	sum += (L[j][i] *L[j][i]);
+	    		    }
+	    		    L[j][j] = H[j][j] - sum;
+	    		} // else
+	    		double minljj = 0.0;
+	    		for (i = j+1; i < n; i++) {
+	    		    if (j == 0) {
+	    		    	L[i][j] = H[j][i];
+	    		    }
+	    		    else {
+	    		    	sum = 0.0;
+	    		    	for (k = 0; k <= j-1; k++) {
+	    		    		sum += (L[i][k]*L[j][k]);
+	    		    	}
+	    		    	L[i][j] = H[j][i] - sum;
+	    		    } // else
+	    		    minljj = Math.max(Math.abs(L[i][j]), minljj);
+	    		} // for (i = j+1; i < n; i++) 
+	    		minljj = Math.max(minljj/maxoffl, minl);
+	    		if (L[j][j] > minljj*minljj) {
+	    		    // Normal Cholesky iteration
+	    			L[j][j] = Math.sqrt(L[j][j]);
+	    		}
+	    		else {
+	    			// Augment H[j][j]
+	    			if (minljj < minl2) {
+	    				minljj = minl2;
+	    				// Only possible when input maxoffl == 0
+	    			}
+	    			maxadd[0] = Math.max(maxadd[0],  (minljj*minljj - L[j][j]));
+	    			L[j][j] = minljj;
+	    		} // else
+	    		for (i = j+1; i < n; i++) {
+	    			L[i][j] = L[i][j]/L[j][j];
+	    		}
+	    	} // for (j = 0; j < n; j++)
+	    	return;
+	    } // nechdcmp
+	    
+	    private void neqrsolv(double b[], double M[][], double M1[], double M2[]) {
+	    	// b is input/output
+	    	// input M, M1, and M2
+	    	// This routine is part of the Nonlinear Equations package.
+	    	// It is a linear equation solve function using the QR decomposition.
+	    	// Algorithm A3.2.2: Part of the modular software system from the appendix of the book
+	    	// "Numerical Methods for Unconstrained Optimization and Nonlinear Equations" by
+	    	// Dennis & Schnabel, 1983.
+	    	// Coded in MATLAB by Richard T. Behrens, March, 1988.
+	    	int i;
+	    	int j;
+	    	double sum;
+	    	// Algorithm step 1.
+	    	int n = Math.max(M.length, M[0].length);
+	    	for (j = 0; j < n-1; j++) {
+	    	    sum = 0.0;
+	    	    for (i = j; i < n; i++) {
+	    	    	sum += (M[j][i] * b[i]);
+	    	    }
+	    	    double tau = sum/M1[j];
+	    	    for (i = j; i < n; i++) {
+	    	    	b[i] = b[i] - tau * M[i][j];
+	    	    }
+	    	} // for (j = 0; j < n-1; j++)
+	    	
+	    	// Algorithm step 2.
+	    	nersolv(b, M, M2);
+	    	return;
+	    } // neqrsolv
     }
