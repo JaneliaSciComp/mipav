fc8ccd5f523616e8f76f5b40d3f341f5583d435d
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2017-03-24 22:14:50 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@14861 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/SchwarzChristoffelMapping.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/SchwarzChristoffelMapping.java
@@ -24,7 +24,30 @@ public class SchwarzChristoffelMapping extends AlgorithmBase {
 	// Desired accuracy in the map.  This may not be met exactly.
 	private double tolerance = 1.0E-8;
 	
+	// eps returns the distance from 1.0 to the next larger double-precision number, that is, eps = 2^-52.
+	private double eps;
+	
 	public void runAlgorithm() {
+		// eps returns the distance from 1.0 to the next larger double-precision number, that is, eps = 2^-52.
+    	// epsilon = D1MACH(4)
+        // Machine epsilon is the smallest positive epsilon such that
+        // (1.0 + epsilon) != 1.0.
+        // epsilon = 2**(1 - doubleDigits) = 2**(1 - 53) = 2**(-52)
+        // epsilon = 2.2204460e-16
+        // epsilon is called the largest relative spacing
+        eps = 1.0;
+        double neweps = 1.0;
+
+
+        while (true) {
+
+            if (1.0 == (1.0 + neweps)) {
+                break;
+            } else {
+                eps = neweps;
+                neweps = neweps / 2.0;
+            }
+        } // while(true)
 		
 	}
 	
@@ -129,7 +152,7 @@ public class SchwarzChristoffelMapping extends AlgorithmBase {
 	// rparam uses z0 as an initial guess for z.  In this case, z0 represents the image of 
 	// prevertices on the strip 0 <= Im z <= 1.  You can use r2strip to transform from the rectangle to 
 	// the strip.
-	// rparam attempts to find an aswer within the tolerance tol.  (Also see scparopt.)
+	// rparam attempts to find an answer within the tolerance tol.  (Also see scparopt.)
 	// rparam uses a vector of control parameters.  See scparopt.
 	// Original MATLAB code copyright 1998 by Toby Driscoll.
 	// If NESolve, method will be be NESolve.TRUST_REGION (default) or NESolve.LINE_SEARCH.
@@ -137,6 +160,20 @@ public class SchwarzChristoffelMapping extends AlgorithmBase {
 			double w[][], double beta[], int cnr[], double z0[][], boolean trace, double tol,
 			int method) {
 		int i;
+		double diffR;
+		double diffI;
+		double sum;
+		int num;
+		double mean;
+		double sum1;
+		double sum2;
+		double len;
+		double wid;
+		double modest;
+		int npoints;
+		double spacing;
+		double dx;
+		int upper;
 	    int n = w.length;  // number of vertices
 	    
 	    // cnr should be set by initial MIPAV dialog, so this should never happen.
@@ -172,9 +209,9 @@ public class SchwarzChristoffelMapping extends AlgorithmBase {
 	    }
 	    wtemp = null;
 	    betatemp = null;
-	    int offset = n + 1 - cnr[0]; // add 1 to go to 1 based beofre remainder operation
+	    int offset = n - cnr[0]; // cnr - cnr[0] so no need to add one
 	    for (i = 0; i < n; i++) {
-	    	cnr[i] = ((cnr[i] + offset) % offset);
+	    	cnr[i] = ((cnr[i] + offset) % n);
 	    }
 	    
 	    if (z0.length == 1) {
@@ -186,6 +223,98 @@ public class SchwarzChristoffelMapping extends AlgorithmBase {
 	    
 	    // Check input data
 	    int err = sccheck("r", w, beta, cnr);
+	    if (err == -1) {
+	    	return;
+	    }
+	    if (err == 1) {
+	    	MipavUtil.displayError("Use scfix to make polygon obey requirements");
+	    	return;
+	    }
+	    
+	    boolean atinf[] = new boolean[n];
+	    for (i = 0; i < n; i++) {
+	    	atinf[i] = (beta[i] <= -1);
+	    }
+	    
+	    if (z0 == null) {
+	        // Try to find a reasonable initial guess
+	    	// side lengths
+	    	double dw[]= new double[n];
+	    	for (i = 0; i < n-1; i++) {
+	    		diffR = w[i+1][0] - w[i][0];
+	    		diffI = w[i+1][1] - w[i][1];
+	    		dw[i] = Math.sqrt(diffR*diffR + diffI*diffI);
+	    	} // for (i = 0; i < n-1; i++)
+	    	diffR = w[0][0] - w[n-1][0];
+	    	diffI = w[0][1] - w[n-1][1];
+	    	dw[n-1] = Math.sqrt(diffR*diffR + diffI*diffI);
+	        sum = 0.0;
+	        num = 0;
+	        for (i = 0; i < n; i++) {
+	        	if ((!Double.isInfinite(dw[i])) && (!Double.isNaN(dw[i]))) {
+	        		sum += dw[i];
+	        		num++;
+	        	}
+	        } // for (i = 0; i < n; i++)
+	        mean = sum/num;
+	        for (i = 0; i < n; i++) {
+	        	if (Double.isInfinite(dw[i]) || Double.isNaN(dw[i])) {
+	        		dw[i] = mean;
+	        	}
+	        } // for (i = 0; i < n; i++)
+	        // Estimate length and width (conformal modulus)
+	        sum1 = 0.0;
+	        sum2 = 0.0;
+	        for (i = cnr[0]; i <= cnr[1]-1; i++) {
+	        	sum1 += dw[i];
+	        }
+	        for (i = cnr[2]; i <= cnr[3]-1; i++) {
+	        	sum2 += dw[i];
+	        }
+	        len = (sum1 + sum2)/2.0;
+	        sum1 = 0.0;
+	        sum2 = 0.0;
+	        for (i = cnr[1]; i <= cnr[2]-1; i++) {
+	        	sum1 += dw[i];
+	        }
+	        for (i = cnr[3]; i <= n-1; i++) {
+	        	sum2 += dw[i];
+	        }
+	        for (i = 0; i <= cnr[0]-1; i++) {
+	        	sum2 += dw[i];
+	        }
+	        wid = (sum1 + sum2)/2.0;
+	        modest = Math.min(len/wid, 100);
+	        // Evenly space prevertices to match this conformal modulus.
+	        npoints = cnr[1] - cnr[0] +1;
+	        spacing = modest/(npoints - 1.0);
+	        z0 = new double[n][2];
+	        for (i = 0; i < npoints-1; i++) {
+	            z0[cnr[0]+i][0] = i*spacing;	
+	        }
+	        z0[cnr[1]][0] = modest;
+	        dx = z0[cnr[0]+1][0] - z0[cnr[0]][0];
+	        for (i = 1; i <= cnr[0]-1; i++) {
+	        	z0[cnr[0]-i][0] = z0[cnr[0]][0] - dx * i;
+	        }
+	        upper = cnr[2] - cnr[1] - 1;
+	        for (i = 1; i <= upper; i++) {
+	        	z0[cnr[1]+i][0] = z0[cnr[1]][0] + dx * i;
+	        }
+	        npoints = cnr[3] - cnr[2] + 1;
+	        spacing = modest/(npoints - 1.0);
+	        for (i = 0; i < npoints-1; i++) {
+	        	z0[cnr[3]-i][0] = i*spacing;
+	        	z0[cnr[3]-i][1] = 1;
+	        }
+	        z0[cnr[2]][0] = modest; 
+	        z0[cnr[2]][1] = 1;
+	        dx = z0[cnr[3]-1][0] - z0[cnr[3]][0];
+	        for (i = 1; i <= n-1-cnr[3]; i++) {
+	        	z0[cnr[3]+i][0] = z0[cnr[3]][0] - dx*i;
+	        	z0[cnr[3]+i][1] = 1;
+	        }
+	    } // if (z0 == null)
 	}
 	
 	// Gauss-Jacobi quadrature data for SC Toolbox.
@@ -302,9 +431,264 @@ public class SchwarzChristoffelMapping extends AlgorithmBase {
 	    }
 	}
 	
+	// sccheck checks polygon inputs to the Schwarz-Christoffel functions.
+	// sccheck is used by the xxparam functions to check the validity of 
+	// inputs describing the polygon to be mapped.  type is a string consisting
+	// of the prefix to param ("d", "dp", etc.).  If errors are found, execution 
+	// will terminate.  Sometimes the trouble has to do with how the parameter
+	// problem is posed, which imposes a few nonobvious constraints.  The function
+	// scfix is provided to automatically fix such difficulties, by renumbering or
+	// perhaps adding vertices.  sccheck output is 1 if the problem is rectifiable
+	// by scfix, 2 if warning status only.
+	// Original MATLAB code copyright 1998 by Toby Driscoll.
 	private int sccheck(String type, double w[][], double beta[], int aux[]) {
+		int i;
 	    int err;
+	    int sumb;
+	    double sumbeta;
+	    int n = w.length;
+	    boolean atinf[] = new boolean[beta.length];
+	    for (i = 0; i < beta.length; i++) {
+	    	if (beta[i] <= -1.0) {
+	    		atinf[i] = true;
+	    	}
+	    }
 	    err = 0;
+	    
+	    // Universal truths
+	    if (beta.length != n) {
+	    	MipavUtil.displayError("Mismatched angles and vertices");
+	    	err = -1;
+	    	return err;
+	    }
+	    for (i = 0; i < n; i++) {
+	    	if ((beta[i] > 1) || (beta[i] <-3)) {
+	    		MipavUtil.displayError("beta["+i+"] = " + beta[i] + " not in the required [-3,1] range");
+	    		err = -1;
+	    		return err;
+	    	}
+	    } // for (i = 0; i < n; i++)
+	    
+	    // Infinite vertices
+	    if ((!type.equalsIgnoreCase("de")) && (!type.equalsIgnoreCase("cr"))) {
+	        for (i = 0; i < n; i++) {
+	        	if ((!atinf[i]) && ((Double.isInfinite(w[i][0])) || (Double.isInfinite(w[i][1])))) {
+	        	    MipavUtil.displayError("Infinite vertex " + i + " illegally has an angle > -1");
+	        	    err = -1;
+	        	    return err;
+	        	}
+	            if (atinf[i] && (!Double.isInfinite(w[i][0])) && (!Double.isInfinite(w[i][1]))) {
+	        	    MipavUtil.displayError("Finite vertex " + i + " illegally has an angle <= -1");	
+	        	    err = -1;
+	        	    return err;
+	        	}
+	        } // for (i = 0; i < n; i++)
+	        for (i = 0; i < n-1; i++) {
+	        	if (atinf[i] && atinf[i+1]) {
+	        		MipavUtil.displayError(i + " and " + (i+1) + " are unallowed adjacent infinite vertices");
+	        		err = -1;
+	        		return err;
+	        	}
+	        } // for (i = 0; i < n-1; i++)
+	    } // if ((!type.equalsIgnoreCase("de")) && (!type.equalsIgnoreCase("cr")))
+	    else {
+	    	for (i = 0; i < n; i++) {
+	    		if (atinf[i]) {
+	    			MipavUtil.displayError("vertex " + i + " illegally has a beta <= -1");
+	    			err = -1;
+	    			return err;
+	    		}
+	    		if (Double.isInfinite(w[i][0]) || Double.isInfinite(w[i][1])) {
+	    			MipavUtil.displayError(i + " is an illegal infinite vertex");
+	    			err = -1;
+	    			return err;
+	    		}
+	    	} // for (i = 0; i < n; i++)
+	    } // else
+	    
+	    if (type.equalsIgnoreCase("de")) {
+	    	sumb = 2;
+	    }
+	    else {
+	    	sumb = -2;
+	    }
+	    
+	    // Orientation conventions
+	    sumbeta = 0.0;
+	    for (i = 0; i < n; i++) {
+	    	sumbeta += beta[i];
+	    }
+	    if (Math.abs(sumbeta + sumb) < 1.0E-9) {
+	    	MipavUtil.displayError("Vertices were probably given in the wrong order");
+	    	err = 1;
+	    	return err;
+	    }
+	    else if (Math.abs(sumbeta - sumb) > 1.0E-9) {
+	    	MipavUtil.displayWarning("Angles do not sum to " + sumb);
+	    	err = 2;
+	    	return err;
+	    }
+	    
+	    // Some finer points
+	    if (type.equalsIgnoreCase("hp") || type.equalsIgnoreCase("d")) {
+	        if (n < 3) {
+	            MipavUtil.displayError("Polygon must have at least 3 vertices");
+	            err = -1;
+	            return err;
+	        }
+	        else if (Double.isInfinite(w[0][0]) || Double.isInfinite(w[0][1]) || Double.isInfinite(w[1][0]) ||
+	        		Double.isInfinite(w[1][1]) || Double.isInfinite(w[n-2][0]) || Double.isInfinite(w[n-2][1])) {
+	        	MipavUtil.displayError("Infinite vertices must not be at positions 0, 1, or n-2");
+	        	err = 1;
+	        	return err;
+	        }
+	        else if ((Math.abs(beta[n-1]) < eps) || (Math.abs(beta[n-1] - 1) < eps)) {
+	        	MipavUtil.displayError("Sides adjacent to w[n-1] must not be collinear");
+	        	err = 1;
+	        	return err;
+	        }
+	    } // if (type.equalsIgnoreCase("hp") || type.equalsIgnoreCase("d"))
+	    else if (type.equalsIgnoreCase("cr")) {
+	        if (n < 4) {
+	        	MipavUtil.displayError("Polygon must have at least 4 vertices");
+	        	err = -1;
+	        	return err;
+	        }
+	    } // else if (type.equalsIgnoreCase("cr"))
+	    else if (type.equalsIgnoreCase("de")) {
+	        if (n < 2) {
+	        	MipavUtil.displayError("polygon must have at least 2 vertices");
+	        	err = -1;
+	        	return err;
+	        }
+	        else if (((beta[n-1] == 0) || (beta[n-1] == 1)) && (n > 2)) {
+	        	MipavUtil.displayError("Sides adjacent to w[n-1] must not be collinear");
+	        	err = 1;
+	        	return err;
+	        }
+	    } // else if (type.equalsIgnoreCase("de"))
+	    else if (type.equalsIgnoreCase("st")) {
+	        if (n < 5) {
+	        	MipavUtil.displayError("Polygon must have at least 5 vertices");
+	        	err = -1;
+	        	return err;
+	        }
+	        double wrenum[][] = new double[n][2];
+	        double betarenum[] = new double[n];
+	        for (i = aux[0]; i < n; i++) {
+	        	wrenum[i - aux[0]][0] = w[i][0];
+	        	wrenum[i - aux[0]][1] = w[i][1];
+	        	betarenum[i - aux[0]] = beta[i];
+	        }
+	        for (i = 0; i <= aux[0]-1; i++) {
+	        	wrenum[n - aux[0] + i][0] = w[i][0];
+	        	wrenum[n - aux[0] + i][1] = w[i][1];
+	        	betarenum[n- aux[0] + i] = beta[i];
+	        }
+	        for (i = 0; i < n; i++) {
+	        	w[i][0] = wrenum[i][0];
+	        	w[i][1] = wrenum[i][1];
+	        	beta[i] = betarenum[i];
+	        }
+	        for (i = 0; i < n; i++) {
+	        	wrenum[i] = null;
+	        }
+	        wrenum = null;
+	        betarenum = null;
+	        int renum[] = new int[n];
+	        for (i = aux[0]; i < n; i++) {
+	        	renum[i-aux[0]] = i;
+	        }
+	        for (i = 0; i <= aux[0] - 1; i++) {
+	        	renum[n - aux[0] + i] = i;
+	        }
+	        int k = -1;
+	        for (i = 0; i < n; i++) {
+	        	if (renum[i] == aux[1]) {
+	        		k = i + 1;
+	        	}
+	        } // for (i = 0; i < n; i++)
+	        renum = null;
+	        if (atinf[1] || atinf[2] || atinf[n-1]) {
+	        	MipavUtil.displayError("Vertices at (w[aux[0] + [1, 2, -1]) must be finite");
+	        	err = 1;
+	        	return err;
+	        }
+	        else if (k - 2 < 2) {
+	        	MipavUtil.displayError("There must be at least 2 vertices between ends 0 and 1");
+	        	err = 1;
+	        	return err;
+	        }
+	        else if (k == n) {
+	        	MipavUtil.displayError("There must be at least one vertex between ends 1 and 0");
+	        	err = 1;
+	        	return err;
+	        }
+	    } // else if (type.equalsIgnoreCase("st"))
+	    else if (type.equalsIgnoreCase("r")) {
+	        int corner[] = new int[aux.length];	
+	        for (i = 0; i < aux.length; i++) {
+	        	corner[i] = aux[i];
+	        }
+	        double wrenum[][] = new double[n][2];
+	        double betarenum[] = new double[n];
+	        for (i = aux[0]; i < n; i++) {
+	        	wrenum[i - aux[0]][0] = w[i][0];
+	        	wrenum[i - aux[0]][1] = w[i][1];
+	        	betarenum[i - aux[0]] = beta[i];
+	        }
+	        for (i = 0; i <= aux[0]-1; i++) {
+	        	wrenum[n - aux[0] + i][0] = w[i][0];
+	        	wrenum[n - aux[0] + i][1] = w[i][1];
+	        	betarenum[n- aux[0] + i] = beta[i];
+	        }
+	        for (i = 0; i < n; i++) {
+	        	w[i][0] = wrenum[i][0];
+	        	w[i][1] = wrenum[i][1];
+	        	beta[i] = betarenum[i];
+	        }
+	        for (i = 0; i < n; i++) {
+	        	wrenum[i] = null;
+	        }
+	        wrenum = null;
+	        betarenum = null;
+	        int offset = n - corner[0]; // corner - corner[0] so no need to add one
+		    for (i = 0; i < n; i++) {
+		    	corner[i] = ((corner[i] + offset) % n);
+		    }
+		    if (n < 4) {
+		    	MipavUtil.displayError("Polygon must have at least 4 vertices");
+		    	err = -1;
+		    	return err;
+		    }
+		    int sortcorner[] = new int[corner.length];
+		    for (i = 0; i < corner.length; i++) {
+		    	sortcorner[i] = corner[i];
+		    }
+		    Arrays.sort(sortcorner);
+		    for (i = 0; i < corner.length; i++) {
+		    	if (corner[i] != sortcorner[i]) {
+		    		MipavUtil.displayError("Corners must be specified in ccw order");
+		    		err = -1;
+		    		return err;
+		    	}
+		    } // for (i = 0; i < corner.length; i++)
+		    if (Double.isInfinite(w[0][0]) || Double.isInfinite(w[0][1])) {
+		    	MipavUtil.displayError("Corner[0] must be finite");
+		    	err = -1;
+		    	return err;
+		    }
+		    if (Double.isInfinite(w[1][0]) || Double.isInfinite(w[1][1])) {
+		        MipavUtil.displayError("Corner[0] + 1 must be finite");
+		        err = 1;
+		        return err;
+		    }
+		    if ((Math.abs(beta[n-1]) < eps) || (Math.abs(beta[n-1] - 1) < eps)) {
+	        	MipavUtil.displayError("Sides adjacent to w[corner[0]-1] must not be collinear");
+	        	err = 1;
+	        	return err;
+	        }
+	    } // else if (type.equalsIgnoreCase("r"))
 	    return err;
 	}
 	
