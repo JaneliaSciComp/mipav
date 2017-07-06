8e7c16f1bcf1c5b4a37ea5359155815f207e350d
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2017-07-06 20:36:13 +0000

    Corrected Preferences.debug for ENRIGHT_AND_PRYCE_B5.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@15015 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/ODE.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/ODE.java
@@ -482,7 +482,7 @@ public abstract class ODE {
     	Preferences.debug("Enright and Pryce #B5 neqn = 3 y1' = y2*y3\n");
     	Preferences.debug("y2' = -y1*y3\n");
     	Preferences.debug("y3' = -0.51*y1*y2\n");
-    	Preferences.debug("y[0] = 2.0  y[1] = 1.0 y[2] = 1.0\n");
+    	Preferences.debug("y[0] = 0.0  y[1] = 1.0 y[2] = 1.0\n");
     	neqn = 3;
     	y = new double[3];
     	t = new double[1];
