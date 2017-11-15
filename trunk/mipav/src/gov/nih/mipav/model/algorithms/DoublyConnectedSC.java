8f8f0f9634e2bdc35e382e49e5fb268808f0e75b
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2017-11-15 23:03:56 +0000

    Starting work on.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@15259 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/DoublyConnectedSC.java
@@ -0,0 +1,14 @@
+package gov.nih.mipav.model.algorithms;
+
+public class DoublyConnectedSC extends AlgorithmBase {
+	// This is a port of the FORTRAN ACM TOMS Algorithm 785, collected
+	// algorithms form ACM.  The original ACM work was published in Transactions
+	// on Mathematical Software, Vol. 24, No. 3, September, 1998, pp. 317-333.
+	// The original FORTRAN code was written by Dr. Chenglie Hu as described in
+	// his article "A Software Package for Computing Schwarz-Christoffel Conformal
+	// Transformation for Doubly Connected Polyhgonal Regions."
+	public void runAlgorithm() {
+		
+	}
+	
+}
