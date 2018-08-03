cd38e421c60c96563c7a473e4dd40e5243f5c5e8
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2018-08-03 19:37:53 +0000

    Added boolean useCIELab to AlgorithmAHE call in histoEqual.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@15565 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/ProstateFramework/JDialogProstateSPIEcancerChallengeNIH_train_ced_ext_wp.java
+++ trunk/mipav/src/gov/nih/mipav/view/renderer/WildMagic/ProstateFramework/JDialogProstateSPIEcancerChallengeNIH_train_ced_ext_wp.java
@@ -438,8 +438,9 @@ public class JDialogProstateSPIEcancerChallengeNIH_train_ced_ext_wp extends JDia
 		   // Make algorithm
 		int heightDivisions = 1;
 		int widthDivisions = 1;
+		boolean useCIELab = false;
 		
-		AlgorithmAHE aheAlgo = new AlgorithmAHE(resultImage, image, heightDivisions, widthDivisions);
+		AlgorithmAHE aheAlgo = new AlgorithmAHE(resultImage, image, heightDivisions, widthDivisions, useCIELab);
         aheAlgo.setRGBChannelFilter(false, false, false);                        
         aheAlgo.setContrastLimited(true);
         aheAlgo.setClipLevel(75);
