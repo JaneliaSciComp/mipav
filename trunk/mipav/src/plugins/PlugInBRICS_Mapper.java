0dc94399d2fb930ffc2c4fd8e67ae4a3e0708471
mcmatt@NIH.GOV <mcmatt@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2018-10-29 18:53:42 +0000

    Version 0.7 - fixed pairing (mapped column) to adapt to searching.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@15701 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInBRICS_Mapper.java
+++ trunk/mipav/src/plugins/PlugInBRICS_Mapper.java
@@ -4,5 +4,5 @@ import gov.nih.mipav.plugins.PlugInGeneric;
 public class PlugInBRICS_Mapper implements PlugInGeneric {
     public void run() {
         new PlugInDialogBRICS_Mapper();
-    }
+    } 
 }
