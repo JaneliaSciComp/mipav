a055953fe8b7a83bebfd1ad2cd4fd8a84a9e7494
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2019-03-19 21:50:32 +0000

    Work continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@15896 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInDialogTSPAnalysis.java
+++ trunk/mipav/src/plugins/PlugInDialogTSPAnalysis.java
@@ -54,6 +54,10 @@ public class PlugInDialogTSPAnalysis extends JDialogStandaloneScriptablePlugin i
 	
 	private int TSP_iter = 4;
 	
+	private JTextField PsvdText;
+	
+	private double Psvd = 0.1;
+	
 	/**
      * Constructor used for instantiation during script execution (required for dynamic loading).
      */
@@ -134,7 +138,8 @@ public class PlugInDialogTSPAnalysis extends JDialogStandaloneScriptablePlugin i
         inputPanel.add(masking_thresholdLabel, gbc);
         
         gbc.gridx = 1;
-        masking_thresholdText = new JTextField("800");
+        masking_thresholdText = new JTextField(10);
+        masking_thresholdText.setText("800");
         masking_thresholdText.setFont(serif12);
         masking_thresholdText.setForeground(Color.black);
         inputPanel.add(masking_thresholdText, gbc);
@@ -147,7 +152,8 @@ public class PlugInDialogTSPAnalysis extends JDialogStandaloneScriptablePlugin i
         inputPanel.add(TSP_thresholdLabel, gbc);
         
         gbc.gridx = 1;
-        TSP_thresholdText = new JTextField("0.8");
+        TSP_thresholdText = new JTextField(10);
+        TSP_thresholdText.setText("0.8");
         TSP_thresholdText.setFont(serif12);
         TSP_thresholdText.setForeground(Color.black);
         inputPanel.add(TSP_thresholdText, gbc);
@@ -160,10 +166,25 @@ public class PlugInDialogTSPAnalysis extends JDialogStandaloneScriptablePlugin i
         inputPanel.add(TSP_iterLabel, gbc);
         
         gbc.gridx = 1;
-        TSP_iterText = new JTextField("4");
+        TSP_iterText = new JTextField(10);
+        TSP_iterText.setText("4");
         TSP_iterText.setFont(serif12);
         TSP_iterText.setForeground(Color.black);
         inputPanel.add(TSP_iterText, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        JLabel PsvdLabel = new JLabel("Psvd");
+        PsvdLabel.setFont(serif12);
+        PsvdLabel.setForeground(Color.black);
+        inputPanel.add(PsvdLabel, gbc);
+        
+        gbc.gridx = 1;
+        PsvdText = new JTextField(10);
+        PsvdText.setText("0.1");
+        PsvdText.setFont(serif12);
+        PsvdText.setForeground(Color.black);
+        inputPanel.add(PsvdText, gbc);
         
         getContentPane().add(inputPanel, BorderLayout.NORTH);
 
@@ -228,7 +249,7 @@ public class PlugInDialogTSPAnalysis extends JDialogStandaloneScriptablePlugin i
         try {
 
             TSPAnalysisAlgo = new PlugInAlgorithmTSPAnalysis(pwiImageFileDirectory, masking_threshold,
-            		TSP_threshold, TSP_iter);
+            		TSP_threshold, TSP_iter, Psvd);
 
             // This is very important. Adding this object as a listener allows the algorithm to
             // notify this object when it has completed or failed. See algorithm performed event.
@@ -341,6 +362,23 @@ public class PlugInDialogTSPAnalysis extends JDialogStandaloneScriptablePlugin i
     		MipavUtil.displayError("TSP_iter cannot exceed 100");
     		TSP_iterText.requestFocus();
     	    TSP_iterText.selectAll();
+    	    return false;
+    	}
+    	
+    	tmpStr = PsvdText.getText();
+    	try {
+    		Psvd = Double.valueOf(tmpStr).doubleValue();
+    	}
+    	catch (NumberFormatException e) {
+    	    MipavUtil.displayError("Psvd text does not have a proper double number");
+    	    PsvdText.requestFocus();
+    	    PsvdText.selectAll();
+    	    return false;
+    	}
+    	if (Psvd < 0) {
+    		MipavUtil.displayError("Psvd must be at least 0");
+    		PsvdText.requestFocus();
+    	    PsvdText.selectAll();
     	    return false;
     	}
     	return true;
