6d87a744cbfaa5e5bdab13a5fe217c6888894676
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2019-05-14 21:07:36 +0000

    getConstrainedValues now constrains x[1] to be between 1.0E-10 and 74.999999.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16013 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/plugins/PlugInAlgorithmTSPAnalysis.java
+++ trunk/mipav/src/plugins/PlugInAlgorithmTSPAnalysis.java
@@ -1745,24 +1745,14 @@ public class PlugInAlgorithmTSPAnalysis extends AlgorithmBase implements MouseLi
 			return sum;
         }
         
-        double round2(double num, int precision)
-   	 {
-   	 	double rnum;
-   	 	int tnum;
-   	
-   	 	rnum = num*Math.pow(10,precision);
-   	 	tnum = (int)(rnum < 0 ? rnum-0.5 : rnum + 0.5);
-   	 	rnum = tnum/Math.pow(10,precision);
-   	
-   	 	return rnum;
-   	 }
+        
    		public void getConstrainedValues(double x[], int n) {
-   			// round to 2 decimal places
-   	   int i;
-   	
-   	   for (i=0; i<n; i++) {
-   	     x[i] = round2(x[i],2);
-   	   }
+   	        if (x[1] < 1.0E-10) {
+   	        	x[1] = 1.0E-10;
+   	        }
+   	        else if (x[1] > 74.999999) {
+   	        	x[1] = 74.999999;
+   	        }
    		}
    		
    		/**
