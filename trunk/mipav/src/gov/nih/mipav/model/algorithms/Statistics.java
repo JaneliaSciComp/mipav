5ba0fc1516879fef6a366e1d0a2c041ffbf124af
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2019-07-02 15:38:01 +0000

    Added tInverseCumulativeDistributionFunction().
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16120 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/Statistics.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/Statistics.java
@@ -2,6 +2,8 @@ package gov.nih.mipav.model.algorithms;
 
 import de.jtem.mfc.field.Complex;
 import de.jtem.mfc.specialFunctions.HyperGeometric2F1;
+import gov.nih.mipav.view.MipavUtil;
+import gov.nih.mipav.view.Preferences;
 
 
 public class Statistics {
@@ -15,9 +17,11 @@ public class Statistics {
     
     public static final int STUDENTS_T_DISTRIBUTION_CUMULATIVE_DISTRIBUTION_FUNCTION = 4;
     
-    public static final int CHI_SQUARED_PROBABILITY_DENSITY_FUNCTION = 5;
+    public static final int STUDENTS_T_INVERSE_CUMULATIVE_DISTRIBUTION_FUNCTION = 5;
     
-    public static final int CHI_SQUARED_CUMULATIVE_DISTRIBUTION_FUNCTION = 6;
+    public static final int CHI_SQUARED_PROBABILITY_DENSITY_FUNCTION = 6;
+    
+    public static final int CHI_SQUARED_CUMULATIVE_DISTRIBUTION_FUNCTION = 7;
     
     private int statisticType;
     
@@ -48,6 +52,9 @@ public class Statistics {
             case STUDENTS_T_DISTRIBUTION_CUMULATIVE_DISTRIBUTION_FUNCTION:
                 tCumulativeDistributionFunction();
                 break;
+            case STUDENTS_T_INVERSE_CUMULATIVE_DISTRIBUTION_FUNCTION:
+            	tInverseCumulativeDistributionFunction();
+            	break;
             case CHI_SQUARED_PROBABILITY_DENSITY_FUNCTION:
                 chiSquaredProbabilityDensityFunction();
                 break;
@@ -115,7 +122,6 @@ public class Statistics {
         hyper = new Hypergeometric(0.5, (v + 1.0)/2.0, 1.5, -x*x/v, result);
         hyper.run();
         ansR[0] = 0.5 + x*(g1[0]/(Math.sqrt(v*Math.PI)*g2[0]))*result[0];
-        System.out.println("ansR[0] = " + ansR[0]);
         // In fact JTEM produces error message Z is not inner point of the unit disc when I run circle generation.
         // JTEM Solution (Produces same result testR.getRe() = result[0].
         //Complex testR = HyperGeometric2F1.evaluateSeries( new Complex(0.5), new Complex((v + 1.0)/2.0),
@@ -123,6 +129,54 @@ public class Statistics {
         //System.err.println( result[0] + "   " + testR.getRe() );
         return;
     }
+    
+    private void tInverseCumulativeDistributionFunction() {
+    	double desiredtCum = x;
+    	double epsilon = 1.0E-10;
+    	x = 1.0;
+    	double xlow = 1.0;
+    	double xhigh = 1.0;
+    	double tCum;
+    	tCumulativeDistributionFunction();
+    	tCum = ansR[0];
+    	if (Math.abs(tCum-desiredtCum) < epsilon) {
+    		ansR[0] = x;
+    		return;
+    	}
+    	if (tCum < desiredtCum) {
+    		while (tCum < desiredtCum) {
+    			xlow = x;
+    			x *= 2.0;
+    			tCumulativeDistributionFunction();
+    	    	tCum = ansR[0];
+    		}
+    		xhigh = x;
+    	}
+    	else {
+    		while (tCum > desiredtCum) {
+    			xhigh = x;
+    			x /= 2.0;
+    			tCumulativeDistributionFunction();
+    	    	tCum = ansR[0];
+    		}
+    		xlow = x;
+    	}
+    	while (Math.abs(tCum - desiredtCum) >= epsilon) {
+    		x = Math.sqrt(xlow * xhigh);
+    		tCumulativeDistributionFunction();
+	    	tCum = ansR[0];
+    		if (tCum < desiredtCum) {
+    			xlow = x;
+    		}
+    		else {
+    			xhigh = x;
+    		}
+    	}
+    	ansR[0] = x;
+    	return;
+    }
+    
+    
     
     private void chiSquaredProbabilityDensityFunction() {
         Gamma gamm;
