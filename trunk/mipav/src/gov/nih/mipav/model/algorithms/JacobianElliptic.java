85cf74cb1065261002c9858f0ed8577c397e8925
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2019-07-25 17:01:04 +0000

    testrealArgument() detects 0 errors in 82 tests
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16169 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/JacobianElliptic.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/JacobianElliptic.java
@@ -2,6 +2,7 @@ package gov.nih.mipav.model.algorithms;
 
 
 import gov.nih.mipav.view.MipavUtil;
+import gov.nih.mipav.view.Preferences;
 
 
 /**
@@ -72,7 +73,7 @@ public class JacobianElliptic {
     public static final int SD = 4;
 
     /** Tells whether real number or complex number version */
-    private final int version;
+    private int version;
 
     /** Outputted results */
     /** sn (u, hk) */
@@ -108,6 +109,10 @@ public class JacobianElliptic {
 
     // ~ Constructors
     // ---------------------------------------------------------------------------------------------------
+    
+    public JacobianElliptic() {
+    	
+    }
 
     /**
      * @param u input argument
@@ -175,7 +180,7 @@ public class JacobianElliptic {
         }
     } // run()
     
-    private void testrealArgument() {
+    public void testrealArgument() {
     	dph = new double[1];
     	esn = new double[1];
     	ecn = new double[1];
@@ -184,7 +189,9 @@ public class JacobianElliptic {
     	int errorsDetected = 0;
     	boolean errorFound;
     	int i;
-    	double testu[] = new double[]{0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
+    	double u0;
+    	double K = 1.596242222;
+    	double testu0[] = new double[]{0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
     			2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0,
     			0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,
         			2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0};
@@ -192,6 +199,8 @@ public class JacobianElliptic {
     			0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,
     			0.25,0.25,0.25,0.25,0.25,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,
     			0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5};
+    	// For k = 0.25, K = K(k) = 1.596242222
+    	// For k - 0.50, K = K(k) = 1.685750355
     	double anssn[] = new double[]{0.0000000,.1589056,.3135378,.4598096,.5939825,.7127879,.8134972,.8939435,
     			.9525064,.9880738,1.0000000,.9880738,.9525064,.8939435,.8134972,.7127879,.5939825,.4598096,.3135378,
     			.1589056,.0000000,-.1589056,-.3135378,-.4598096,-.5939825,-.7127879,-.8134972,-.8939435,-.9525064,
@@ -208,21 +217,79 @@ public class JacobianElliptic {
     			.9495757,.9872938,1.0000000,1.0000000,.9858581,.9442158,.8772862,.7883459,.6812500,.5599589,
     			.4281768,.2891529,.1456426,.0000000,-.1456426,-.2891529,-.4281768,-.5599589,-.6812500,-.7883459,
     			-.8772862,-.9442158,-.9858581,-1.0000000,-.9858581,-.9442158,-.8772862,-.7883459,-.6812500,-.5599589,
-    			-.4281768,-.2891529,-.1456426,.0000000,.1456426,.2891529,.4281768,.559589,.6812500,.7883459,
+    			-.4281768,-.2891529,-.1456426,.0000000,.1456426,.2891529,.4281768,.5599589,.6812500,.7883459,
     			.8772862,.9442158,.9858581,1.0000000};
-    	double ansdn[] = new double[]{1.0000000,.9992106,.9969232,.9933710,.9889131,.9797011,.9747072,.9712341,
+    	double ansdn[] = new double[]{1.0000000,.9992106,.9969232,.9933710,.9889131,.9839948,.9791011,.9747072,.9712341,
     			.9690108,.9682458,.9690108,.9712341,.9747072,.9791011,.9839948,.9889131,.9933710,.9969232,.9992106,
-    			1.0000000,.9992196,.9969232,.9933710,.9889131,.9839948,.9791011,.9747072,.9712341,.9690108,
+    			1.0000000,.9992106,.9969232,.9933710,.9889131,.9839948,.9791011,.9747072,.9712341,.9690108,
     			.9682458,.9690108,.9712341,.9747072,.9791011,.9839948,.9889131,.9933710,.9969232,.9992106,1.0000000,
     			1.0000000,.9964833,.9863498,.9707769,.9515105,.9306049,.9101585,.8920952,.8780105,.8690817,
-    			.8660254,.8690817,.8780105,.8920952,.9101585,.9306049,.9515105,.97077569,.9863498,.9964833,
+    			.8660254,.8690817,.8780105,.8920952,.9101585,.9306049,.9515105,.9707769,.9863498,.9964833,
     			1.0000000,.9964833,.9863498,.9707769,.9515105,.9306049,.9101585,.8920952,.8780105,.8690817,
     			.8660254,.8690817,.8780105,.8920952,.9101585,.9306049,.9515105,.9707769,.9863498,.9964833,1.0000000};
-    	for (i = 0; i < testu.length; i++) {
-    		u = testu[i];
+    	for (i = 0; i < testu0.length; i++) {
+    		errorFound = false;
+    		u0 = testu0[i];
     		hk = testhk[i];
+    		if (hk == 0.25) {
+    			K = 1.596242222;
+    		}
+    		else if (hk == 0.5) {
+    			K = 1.685750355;
+    		}
+    		u = u0 * K;
     		realArgument();
+    		Preferences.debug("u0 = " + u0 + " hk = " + hk + " sn u result = " + esn[0] + " sn u answer = " + anssn[i] +
+    				" cn u result = " + ecn[0] + " cn u answer = " + anscn[i] + " dn u result = " + edn[0] +
+    				" dn u answer = " + ansdn[i] + "\n", Preferences.DEBUG_ALGORITHM);
+    		if (Double.isNaN(esn[0])) {
+    			errorFound = true;
+    			Preferences.debug("Error found sn u is NaN\n", Preferences.DEBUG_ALGORITHM);
+    		}
+    		else if (anssn[i] != 0.0) {
+		        if ((esn[0]/anssn[i] < 1-1.0E-6) || (esn[0]/anssn[i] > 1+1.0E-6)) {
+		     		   Preferences.debug("Error detected in sn u\n",Preferences.DEBUG_ALGORITHM);
+		     		   errorFound = true;
+		        }
+	        }
+    		 else if (Math.abs(esn[0]) > 1.0E-6) {
+         		Preferences.debug("Error detected in sn u\n",Preferences.DEBUG_ALGORITHM);
+ 	     		errorFound = true;
+ 	        }
+    		if (Double.isNaN(ecn[0])) {
+    			errorFound = true;
+    			Preferences.debug("Error found cn u is NaN\n", Preferences.DEBUG_ALGORITHM);
+    		}
+    		else if (anscn[i] != 0.0) {
+		        if ((ecn[0]/anscn[i] < 1-1.0E-6) || (ecn[0]/anscn[i] > 1+1.0E-6)) {
+		     		   Preferences.debug("Error detected in cn u\n",Preferences.DEBUG_ALGORITHM);
+		     		   errorFound = true;
+		        }
+	        }
+    		 else if (Math.abs(ecn[0]) > 1.0E-6) {
+         		Preferences.debug("Error detected in cn u\n",Preferences.DEBUG_ALGORITHM);
+ 	     		errorFound = true;
+ 	        }
+    		if (Double.isNaN(edn[0])) {
+    			errorFound = true;
+    			Preferences.debug("Error found dn u is NaN \n", Preferences.DEBUG_ALGORITHM);
+    		}
+    		else if (ansdn[i] != 0.0) {
+		        if ((edn[0]/ansdn[i] < 1-1.0E-6) || (edn[0]/ansdn[i] > 1+1.0E-6)) {
+		     		   Preferences.debug("Error detected in dn u\n",Preferences.DEBUG_ALGORITHM);
+		     		   errorFound = true;
+		        }
+	        }
+    		 else if (Math.abs(edn[0]) > 1.0E-6) {
+         		Preferences.debug("Error detected in dn u\n",Preferences.DEBUG_ALGORITHM);
+ 	     		errorFound = true;
+ 	        }
+    		if (errorFound) {
+    			errorsDetected++;
+    		}
     	}
+    	System.out.println(errorsDetected + " errors detected in " + testu0.length + " tests of realArgument");
+    	Preferences.debug(errorsDetected + " erors detected in " + testu0.length + " tests of realArgument\n",Preferences.DEBUG_ALGORITHM);
     }
 
     /**
@@ -264,7 +331,7 @@ public class JacobianElliptic {
             d = 0.5 * (dn + sa);
             dn = d;
         } // for (k = n-1; k >= 0; k--)
-        dph[0] = d * 180.0 / Math.PI;
+        dph[0] = d * 180/Math.PI;
         esn[0] = Math.sin(d);
         ecn[0] = Math.cos(d);
         edn[0] = Math.sqrt(1.0 - hk * hk * esn[0] * esn[0]);
