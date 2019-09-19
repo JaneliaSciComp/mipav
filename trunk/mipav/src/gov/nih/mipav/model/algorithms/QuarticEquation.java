fc15b03bb3ea95c2ae478b943f7adedd7e45ee90
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2019-09-19 17:41:24 +0000

    Completed typing in port.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16234 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/QuarticEquation.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/QuarticEquation.java
@@ -57,7 +57,6 @@ public class QuarticEquation {
 	private int num_sols[];
 	private double solsreal[]; // up to 4 elements
 	private double solsimag[]; // up to 4 elements
-	private double sols[]; // up to 4 elements
 	private final int MAX_DEGREE = 4;
 	final int ONLY_REAL_ROOTS = 1;
 	final int COMPLEX_ROOTS = 2;
@@ -92,7 +91,7 @@ public class QuarticEquation {
 		this.dreal = dreal;
 		this.ereal = ereal;
 		this.num_sols = num_sols;
-		this.sols = sols;
+		this.solsreal = sols;
 		inputType = ONLY_REAL_ROOTS;
 	}
 	
@@ -108,7 +107,7 @@ public class QuarticEquation {
 	public void runReal() {
 		int i;
 	    double poly[] = new double[degree+1];
-	    double sols[] = new double[4];
+	    double solsreal[] = new double[4];
 	    poly[0] = ereal;
 	    if (degree >= 1) {
 	        poly[1] = dreal;
@@ -122,11 +121,223 @@ public class QuarticEquation {
 	        	} // if (degree >= 3)
 	        } // if (degree >= 2)
 	    } // if (degree >= 1)
-		//num_sols[0] = solve_real_poly(degree, poly, sols);
+		num_sols[0] = solve_real_poly(degree, poly, solsreal);
 		for (i = 0; i < num_sols[0]; i++) {
-		    System.out.println(sols[i]);	
+		    System.out.println(solsreal[i]);	
 		}
 	}
+	
+	/* poly: pointer to coefficients array of size degree + 1.
+	 * results: pointer to results output array of size degree.
+	 */
+	int solve_real_poly(int degree, double[] poly, double[] results) {
+	    double normalized_poly[] = new double[MAX_DEGREE + 1];
+	    int i;
+	    double a = poly[degree];
+	    if (degree == 0)
+	        return 0;
+	    if (a == 0)
+	        return solve_real_poly(degree - 1, poly, results);
+	    if (degree > MAX_DEGREE)
+	        return -1;
+	    if (degree > 2 && stableness_score(poly[degree], poly[degree - 1]) > stableness_score(poly[0], poly[1])) {
+	        double rev_poly[] = new double[MAX_DEGREE + 1];
+	        int num_results;
+	        for (i = 0; i <= degree; ++i)
+	            rev_poly[i] = poly[degree - i];
+	        num_results = solve_real_poly(degree, rev_poly, results);
+	        for (i = 0; i < num_results; ++i)
+	            results[i] = 1.0 / results[i];
+	        return num_results;
+	    }
+	    for (i = 0; i < degree; ++i)
+	        normalized_poly[i] = poly[i] / a;
+	    normalized_poly[degree] = 1.0;
+	    return solve_normalized_poly(degree, normalized_poly, results);
+	}
+	
+	private double stableness_score(double a, double b) {
+	    double t = Math.abs(a / b);
+	    return t + 1.0 / t;
+	}
+	
+	/* Normalized polynomials have the form of
+	 *   x^n + a*x^(n-1) + ..
+	 * The coefficient for x^n is one.
+	 * solve_normalized_poly does expect to get this coefficient despite it being known.
+	 */
+	private int solve_normalized_poly(int degree, double[] poly, double[] results) {
+	    double shift = -poly[degree - 1] / (double) degree;
+	    double shifted_coefs[] = new double[MAX_DEGREE + 1];
+	    int i, num_results;
+	    calc_shifted_coefs(shift, degree, poly, shifted_coefs);
+	    num_results = solve_depressed_poly(degree, shifted_coefs, results);
+	    for (i = 0; i < num_results; ++i)
+	        results[i] += shift;
+	    return num_results;
+	}
+	
+	private void calc_shifted_coefs(double shift, int degree, double[] src, double[] dst) {
+	    double binomials[][] = new double[MAX_DEGREE + 1][MAX_DEGREE + 1];
+	    double shift_powers[] = new double[MAX_DEGREE + 1];
+	    int dst_i, src_i;
+	    for (dst_i = 0; dst_i <= degree; ++dst_i)
+	        dst[dst_i] = 0;
+	    calc_binomials(degree+1, binomials[0].length, binomials[0]);
+	    calc_powers(shift, degree, shift_powers);
+	    for (src_i = 0; src_i <= degree; ++src_i)
+	        for (dst_i = 0; dst_i <= src_i; ++dst_i)
+	            dst[dst_i] += src[src_i] * shift_powers[src_i - dst_i] * binomials[src_i][dst_i];
+	}
+
+
+	private void calc_powers(double x, int max_power, double[] dst) {
+	    int i;
+	    dst[0] = 1.0;
+	    if (max_power >= 1)
+	        dst[1] = x;
+	    for (i = 2; i <= max_power; ++i)
+	        dst[i] = x * dst[i - 1];
+	}
+	
+	/* Depressed polynomials have the form of:
+	 *   x^n + a*x^(n-2) + ..
+	 * The coefficient for x^n is 1 and for x^(n-1) is zero.
+	 * So it gets 3 coefficients for a depressed quartic polynom.
+	 */
+	private int solve_depressed_poly(int degree, double[] poly, double[] results) {
+		int i;
+		int num;
+	    if (degree > 0 && poly[0] == 0.0) {
+	        results[0] = 0;
+	        double poly1[] = new double[poly.length-1];
+	        for (i = 0; i < poly1.length; i++) {
+	        	poly1[i] = poly[i+1];
+	        }
+	        double results1[] = new double[results.length-1];
+	        for (i = 0; i < results1.length; i++) {
+	        	results1[i] = results[i+1];
+	        }
+	        num = solve_depressed_poly(degree-1, poly1, results1);
+	        for (i = 0; i < poly1.length; i++) {
+	        	poly[i+1] = poly1[i];
+	        }
+	        for (i = 0; i < results1.length; i++) {
+	            results[i+1] = results1[i];;
+	        }
+	        return 1 + num;
+	    }
+	    switch (degree) {
+	    case 4:
+	        return solve_depressed_quartic(poly, results);
+	    case 3:
+	        return solve_depressed_cubic(poly, results);
+	    case 2:
+	        return solve_depressed_quadratic(poly, results);
+	    case 1:
+	        results[0] = 0.0;
+	        return 1;
+	    case 0:
+	        return 0;
+	    default:
+	        return -1;
+	    }
+	}
+	
+	/* Based on http://en.wikipedia.org/wiki/Quartic_function#Quick_and_memorable_solution_from_first_principles */
+	private int solve_depressed_quartic(double[] poly, double[] results) {
+		int i;
+		int num;
+	    double helper_cubic[] = new double[4];
+	    double helper_results[] = new double[3];
+	    double quadratic_factor[] = new double[3];
+	    double p;
+	    double e = poly[0];
+	    double d = poly[1];
+	    double c = poly[2];
+	    int num_results;
+	    if (d == 0) {
+	        int num_quad_results;
+	        double quadratic[] = new double[3];
+	        double quadratic_results[] = new double[2];
+	        quadratic[0] = e;
+	        quadratic[1] = c;
+	        quadratic[2] = 1;
+	        num_quad_results = solve_real_poly(2, quadratic, quadratic_results);
+	        for (i = 0; i < num_quad_results; ++i) {
+	            double s = Math.sqrt(quadratic_results[i]);
+	            results[2*i] = -s;
+	            results[2*i + 1] = s;
+	        }
+	        return 2 * num_quad_results;
+	    }
+	    helper_cubic[0] = -d*d;
+	    helper_cubic[1] = c*c - 4*e;
+	    helper_cubic[2] = 2*c;
+	    helper_cubic[3] = 1;
+	    if (solve_real_poly(3, helper_cubic, helper_results) < 1)
+	        return 0;
+	    p = Math.sqrt(helper_results[0]);
+	    quadratic_factor[0] = c + p*p - d/p;
+	    quadratic_factor[1] = 2*p;
+	    quadratic_factor[2] = 2;
+	    num_results = solve_real_poly(2, quadratic_factor, results);
+	    quadratic_factor[0] = c + p*p + d/p;
+	    quadratic_factor[1] = -2*p;
+	    double resultsn[] = new double[2];
+	    for (i = 0; i < 2; i++) {
+	    	resultsn[i] = results[i + num_results];
+	    }
+	    num = solve_real_poly(2, quadratic_factor, resultsn);
+	    for (i = 0; i < 2; i++) {
+	    	results[i + num_results] = resultsn[i];
+	    }
+	    return num_results + num;
+	}
+
+	
+	/* Based on http://en.wikipedia.org/wiki/Cubic_equation#Cardano.27s_method
+	 *
+	 * Only provides one real result out of 3 possibly complex results.
+	 */
+	private int solve_depressed_cubic(double[] poly, double[] results) {
+	    double q = poly[0];
+	    double p = poly[1];
+	    double t, s_real, s_abs, s_phase, u_abs, u_phase, u_real;
+	    if (p == 0.0) {
+	        results[0] = cubic_root(-q);
+	        return 1;
+	    }
+	    t = q*q/4 + p*p*p/27;
+	    if (t >= 0.0) {
+	        double u = cubic_root(-q/2 + Math.sqrt(t));
+	        results[0] = u - p/3.0/u;
+	        return 1;
+	    }
+	    s_real = -q/2;
+	    s_abs = Math.sqrt(s_real*s_real - t);
+	    s_phase = Math.atan(Math.sqrt(-t) / s_real) + (s_real >= 0 ? 0 : Math.PI);
+	    u_abs = cubic_root(s_abs);
+	    u_phase = s_phase / 3.0;
+	    u_real = u_abs * Math.cos(u_phase);
+	    results[0] = 2 * u_real;
+	    return 1;
+	}
+
+	private double cubic_root(double x) {
+	    double t = Math.pow(Math.abs(x), 1.0 / 3.0);
+	    return x >= 0.0 ? t : -t;
+	}
+
+
+
+	private int solve_depressed_quadratic(double[] poly, double[] results) {
+	    double t = Math.sqrt(-poly[0]);
+	    results[0] = -t;
+	    results[1] = t;
+	    return 2;
+	}
+
 	
 	public void runComplex() {
 		int i;
@@ -257,6 +468,7 @@ public class QuarticEquation {
 	 */
 	private int solve_depressed_poly(int degree, complex_t[] poly, complex_t[] results) {
 		int i;
+		int num;
 	    if (degree > 0 && complex_eq(poly[0], complex_from_real(0.0))) {
 	        results[0] = complex_from_real(0.0);
 	        complex_t poly1[] = new complex_t[poly.length-1];
@@ -271,7 +483,16 @@ public class QuarticEquation {
 	        	results1[i].real = results[i+1].real;
 	        	results1[i].imag = results[i+1].imag;
 	        }
-	        return 1 + solve_depressed_poly(degree - 1, poly1, results1);
+	        num = solve_depressed_poly(degree-1, poly1, results1);
+	        for (i = 0; i < poly1.length; i++) {
+	        	poly[i+1].real = poly1[i].real;
+	        	poly[i+1].imag = poly1[i].imag;
+	        }
+	        for (i = 0; i < results1.length; i++) {
+	        	results[i+1].real = results1[i].real;
+	        	results[i+1].imag = results1[i].imag;
+	        }
+	        return 1 + num;
 	    }
 	    switch (degree) {
 	    case 4:
@@ -294,6 +515,7 @@ public class QuarticEquation {
 	private int solve_depressed_quartic(complex_t[] poly, complex_t[] results)
 	{
 		int i;
+		int num;
 	    complex_t helper_cubic[] = new complex_t[4];
 	    for (i = 0; i < 4; i++) {
 	    	helper_cubic[i] = new complex_t();
@@ -378,7 +600,12 @@ public class QuarticEquation {
 	    	resultsn[i].real = results[i + num_results].real;
 	    	resultsn[i].imag = results[i + num_results].imag;
 	    }
-	    return num_results + solve_poly(2, quadratic_factor, resultsn);
+	    num = solve_poly(2, quadratic_factor, resultsn);
+	    for (i = 0; i < 2; i++) {
+	    	results[i + num_results].real = resultsn[i].real;
+	    	results[i + num_results].imag = resultsn[i].imag;
+	    }
+	    return num_results + num;
 	}
 
 	
