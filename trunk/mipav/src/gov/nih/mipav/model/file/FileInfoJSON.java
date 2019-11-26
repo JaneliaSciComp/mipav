3d1400c73ced0324650fb263432a15837ce658cc
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2019-11-26 23:12:06 +0000

    6 info variables are added.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16333 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/file/FileInfoJSON.java
+++ trunk/mipav/src/gov/nih/mipav/model/file/FileInfoJSON.java
@@ -16,31 +16,13 @@ public class FileInfoJSON extends FileInfoBase {
     /** Use serialVersionUID for interoperability. */
     //private static final long serialVersionUID;
     
-    private String origin = null;
-    private String dateAcquired = null;
-    private String timeAcquired = null;
-    private String dateProcessed = null;
-    private String timeProcessed = null;
-    private String dateWritten = null;
-    private String timeWritten = null;
-    private String jobName = null;
-    private String object = null;
-    private String observer = null;
-    private String instrument = null;
-    private float focalRatio = -1.0f;
-    private String CTYPE1 = null;
-    private String CTYPE2 = null;
-    private String CTYPE3 = null;
-    private String CTYPE4 = null;
-    private String CTYPE5 = null;
-    private String CUNIT1 = null;
-    private String CUNIT2 = null;
-    private String CUNIT3 = null;
-    private String CUNIT4 = null;
-    private String CUNIT5 = null;
-    private String BUNIT = null;
-    private String author = null;
-
+    private String description = null;
+    private String url = null;
+    private String version = null;
+    private String year = null;
+    private String contributor = null;
+    private String date_created = null;
+	
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
     /**
@@ -68,198 +50,54 @@ public class FileInfoJSON extends FileInfoBase {
         
         dialog.append("\n\n                Other information\n\n");
         
-        if (BUNIT != null) {
-            dialog.append("Pixel value units = " + BUNIT + "\n");
+        if (description != null) {
+        	dialog.append("description = " + description + "\n");
         }
         
-        if (CTYPE1 != null) {
-            dialog.append("CTYPE1, name along x-axis = " + CTYPE1 + "\n");
+        if (url != null) {
+        	dialog.append("url = " + url + "\n");
         }
         
-        if (CUNIT1 != null) {
-            dialog.append("CUNIT1, units along x-axis = " + CUNIT1 + "\n");
+        if (version != null) {
+        	dialog.append("version = " + version + "\n");
         }
         
-        if (CTYPE2 != null) {
-            dialog.append("CTYPE2, name along y-axis = " + CTYPE2 + "\n");
+        if (year != null) {
+        	dialog.append("year = " + year + "\n");
         }
         
-        if (CUNIT2 != null) {
-            dialog.append("CUNIT2, units along y-axis = " + CUNIT2 + "\n");
+        if (contributor != null) {
+        	dialog.append("contributor = " + contributor + "\n");
         }
         
-        if (CTYPE3 != null) {
-            dialog.append("CTYPE3, name along z-axis = " + CTYPE3 + "\n");
-        }
-        
-        if (CUNIT3 != null) {
-            dialog.append("CUNIT3, units along z-axis = " + CUNIT3 + "\n");
-        }
-        
-        if (CTYPE4 != null) {
-            dialog.append("CTYPE4, name along t-axis = " + CTYPE4 + "\n");
-        }
-        
-        if (CUNIT4 != null) {
-            dialog.append("CUNIT4, units along t-axis = " + CUNIT4 + "\n");
-        }
-        
-        if (CTYPE5 != null) {
-            dialog.append("CTYPE5, name along fifth-axis = " + CTYPE5 + "\n");
-        }
-        
-        if (CUNIT5 != null) {
-            dialog.append("CUNIT5, units along fifth-axis = " + CUNIT5 + "\n");
-        }
-        
-        if (origin != null) {
-            dialog.append("ORIGIN, installation where file is written = " + origin + "\n");
-        }
-        
-        if (dateAcquired != null) {
-            dialog.append("DATE data acquired = " + dateAcquired + "\n");
-        }
-        
-        if (timeAcquired != null) {
-            dialog.append("TIME data acquired = " + timeAcquired + "\n");
-        }
-        
-        if (dateProcessed != null) {
-            dialog.append("DATE data last processed = " + dateProcessed + "\n");
-        }
-        
-        if (timeProcessed != null) {
-            dialog.append("TIME data last processed = " + timeProcessed + "\n");
-        }
-        
-        if (dateWritten != null) {
-            dialog.append("DATE file written = " + dateWritten + "\n");
-        }
-        
-        if (timeWritten != null) {
-            dialog.append("TIME file written = " + timeWritten + "\n");
-        }
-        
-        if (jobName != null) {
-            dialog.append("JOB NAME = " + jobName + "\n");
-        }
-        
-        if (author != null) {
-            dialog.append("AUTHOR = " + author + "\n");
-        }
-        
-        if (object != null) {
-            dialog.append("OBJECT observed = " + object + "\n");
-        }
-        
-        if (observer != null) {
-            dialog.append("OBSERVER = " + observer + "\n");
-        }
-        
-        if (instrument != null) {
-            dialog.append("INSTRUMENT in use = " + instrument + "\n");
-        }
-        
-        if (focalRatio > 0.0f) {
-            dialog.append("FOCAL RATIO = " + focalRatio + "\n");
+        if (date_created != null) {
+        	dialog.append("date_created = " + date_created + "\n");
         }
         
     }
     
-    public void setBUNIT(String BUNIT) {
-        this.BUNIT = BUNIT;
+    public void setDescription(String description) {
+    	this.description = description;
     }
     
-    public void setCTYPE1(String CTYPE1) {
-        this.CTYPE1 = CTYPE1;
+    public void setURL(String url) {
+    	this.url = url;
     }
     
-    public void setCTYPE2(String CTYPE2) {
-        this.CTYPE2 = CTYPE2;
+    public void setVersion(String version) {
+    	this.version = version;
     }
     
-    public void setCTYPE3(String CTYPE3) {
-        this.CTYPE3 = CTYPE3;
+    public void setYear(String year) {
+    	this.year = year;
     }
     
-    public void setCTYPE4(String CTYPE4) {
-        this.CTYPE4 = CTYPE4;
+    public void setContributor(String contributor) {
+    	this.contributor = contributor;
     }
     
-    public void setCTYPE5(String CTYPE5) {
-        this.CTYPE5 = CTYPE5;
-    }
-    
-    public void setCUNIT1(String CUNIT1) {
-        this.CUNIT1 = CUNIT1;
-    }
-    
-    public void setCUNIT2(String CUNIT2) {
-        this.CUNIT2 = CUNIT2;
-    }
-    
-    public void setCUNIT3(String CUNIT3) {
-        this.CUNIT3 = CUNIT3;
-    }
-    
-    public void setCUNIT4(String CUNIT4) {
-        this.CUNIT4 = CUNIT4;
-    }
-    
-    public void setCUNIT5(String CUNIT5) {
-        this.CUNIT5 = CUNIT5;
-    }
-    
-    public void setDateAcquired(String dateAcquired) {
-        this.dateAcquired = dateAcquired;
-    }
-    
-    public void setTimeAcquired(String timeAcquired) {
-        this.timeAcquired = timeAcquired;
-    }
-    
-    public void setDateProcessed(String dateProcessed) {
-        this.dateProcessed = dateProcessed;
-    }
-    
-    public void setTimeProcessed(String timeProcessed) {
-        this.timeProcessed = timeProcessed;
-    }
-    
-    public void setDateWritten(String dateWritten) {
-        this.dateWritten = dateWritten;
-    }
-    
-    public void setTimeWritten(String timeWritten) {
-        this.timeWritten = timeWritten;
-    }
-    
-    public void setJobName(String jobName) {
-        this.jobName = jobName;
-    }
-    
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-    
-    public void setObject(String object) {
-        this.object = object;
-    }
-    
-    public void setObserver(String observer) {
-        this.observer = observer;
-    }
-    
-    public void setInstrument(String instrument) {
-        this.instrument = instrument;
-    }
-    
-    public void setFocalRatio(float focalRatio) {
-        this.focalRatio = focalRatio;
-    }
-    
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setDate_created(String date_created) {
+    	this.date_created = date_created;
     }
     
 }
