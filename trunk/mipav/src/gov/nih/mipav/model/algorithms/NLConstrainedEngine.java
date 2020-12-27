253af7190f9058fd456b490888cd02145c4a1d79
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2020-12-27 17:23:06 +0000

    Added CERES_CURVE_FITTING testCase.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16830 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/NLConstrainedEngine.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/NLConstrainedEngine.java
@@ -899,12 +899,173 @@ public abstract class NLConstrainedEngine {
     private final int HOCK3 = 1003;
     
     private final int HOCK4 = 1004;
+    
+    private final int CERES_CURVE_FITTING = 1005;
 
     //~ Constructors ---------------------------------------------------------------------------------------------------
 
     public NLConstrainedEngine() {
     	int i;
     	double h;
+    	// Ceres Solver from curve_fitting.cc
+    	// y = exp(a1*x + a0)
+    	// Number of iterations: 4
+    	// Chi-squared: 2.113502580804931
+    	// a0 = 0.1314013888081673
+    	// a1 = 0.29187119399433387
+    	Preferences.debug("Ceres curve fitting problem\n", Preferences.DEBUG_ALGORITHM);
+    	testMode = true;
+    	testCase = CERES_CURVE_FITTING;
+    	nPts = 67;
+    	param = 2;
+    	xSeries = new double[] {0.000000e+00,
+    			  7.500000e-02,
+    			  1.500000e-01, 
+    			  2.250000e-01,
+    			  3.000000e-01,
+    			  3.750000e-01,
+    			  4.500000e-01,
+    			  5.250000e-01,
+    			  6.000000e-01,
+    			  6.750000e-01,
+    			  7.500000e-01,
+    			  8.250000e-01,
+    			  9.000000e-01,
+    			  9.750000e-01,
+    			  1.050000e+00,
+    			  1.125000e+00, 
+    			  1.200000e+00,
+    			  1.275000e+00,
+    			  1.350000e+00,
+    			  1.425000e+00,
+    			  1.500000e+00, 
+    			  1.575000e+00, 
+    			  1.650000e+00, 
+    			  1.725000e+00, 
+    			  1.800000e+00, 
+    			  1.875000e+00, 
+    			  1.950000e+00, 
+    			  2.025000e+00,
+    			  2.100000e+00, 
+    			  2.175000e+00,
+    			  2.250000e+00, 
+    			  2.325000e+00, 
+    			  2.400000e+00, 
+    			  2.475000e+00, 
+    			  2.550000e+00, 
+    			  2.625000e+00,
+    			  2.700000e+00,
+    			  2.775000e+00, 
+    			  2.850000e+00,
+    			  2.925000e+00, 
+    			  3.000000e+00,
+    			  3.075000e+00,
+    			  3.150000e+00,
+    			  3.225000e+00, 
+    			  3.300000e+00,
+    			  3.375000e+00,
+    			  3.450000e+00,
+    			  3.525000e+00,
+    			  3.600000e+00,
+    			  3.675000e+00,
+    			  3.750000e+00, 
+    			  3.825000e+00, 
+    			  3.900000e+00, 
+    			  3.975000e+00, 
+    			  4.050000e+00, 
+    			  4.125000e+00, 
+    			  4.200000e+00, 
+    			  4.275000e+00, 
+    			  4.350000e+00,
+    			  4.425000e+00, 
+    			  4.500000e+00,
+    			  4.575000e+00,
+    			  4.650000e+00,
+    			  4.725000e+00, 
+    			  4.800000e+00,
+    			  4.875000e+00,
+    			  4.950000e+00};
+        ySeries = new double[] {1.133898e+00,
+        		  1.334902e+00,
+        		  1.213546e+00,
+        		  1.252016e+00,
+        		  1.392265e+00,
+        		  1.314458e+00,
+        		  1.472541e+00,
+        		  1.536218e+00,
+        		  1.355679e+00,
+        		  1.463566e+00,
+        		  1.490201e+00,
+        		  1.658699e+00,
+        		  1.067574e+00,
+        		  1.464629e+00,
+        		  1.402653e+00,
+        		  1.713141e+00,
+        		  1.527021e+00,
+        		  1.702632e+00,
+        		  1.423899e+00,
+        		  1.543078e+00,
+        		  1.664015e+00,
+        		  1.732484e+00,
+        		  1.543296e+00,
+        		  1.959523e+00,
+        		  1.685132e+00,
+        		  1.951791e+00,
+        		  2.095346e+00,
+        		  2.361460e+00,
+        		  2.169119e+00,
+        		  2.061745e+00,
+        		  2.178641e+00,
+        		  2.104346e+00,
+        		  2.584470e+00,
+        		  1.914158e+00,
+        		  2.368375e+00,
+        		  2.686125e+00,
+        		  2.712395e+00,
+        		  2.499511e+00,
+        		  2.558897e+00,
+        		  2.309154e+00,
+        		  2.869503e+00,
+        		  3.116645e+00,
+        		  3.094907e+00,
+        		  2.471759e+00,
+        		  3.017131e+00,
+        		  3.232381e+00,
+        		  2.944596e+00,
+        		  3.385343e+00,
+        		  3.199826e+00,
+        		  3.423039e+00,
+        		  3.621552e+00,
+        		  3.559255e+00,
+        		  3.530713e+00,
+        		  3.561766e+00,
+        		  3.544574e+00,
+        		  3.867945e+00,
+        		  4.049776e+00,
+        		  3.885601e+00,
+        		  4.110505e+00,
+        		  4.345320e+00,
+        		  4.161241e+00,
+        		  4.363407e+00,
+        		  4.161576e+00,
+        		  4.619728e+00,
+        		  4.737410e+00,
+        		  4.727863e+00,
+        		  4.669206e+00};
+        gues = new double[param];
+        fitTestModel();
+        gues[0] = 0.0;
+        gues[1] = 0.0;
+        
+        bounds = 0;  // bounds = 0 means unconstrained
+
+        // bounds = 1 means same lower and upper bounds for
+        // all parameters
+        // bounds = 2 means different lower and upper bounds
+        // for all parameters
+        bl = new double[param];
+        bu = new double[param];
+        driverCalls();
     	// Below is an example used to fit y = a0 - a1*(a2**x)
     	// This example implements the solution of problem D of chapter 24 of Applied Regression Analysis, Third Edition by
     	// Norman R. Draper and Harry Smith 
@@ -3141,6 +3302,29 @@ public abstract class NLConstrainedEngine {
             try {
                 ctrl = ctrlMat[0];
                 switch (testCase) {
+                case CERES_CURVE_FITTING:
+                	if ((ctrl == -1) || (ctrl == 1)) {
+                		
+	                    // evaluate the residuals[i] = ymodel[i] - ySeries[i]
+	                    for (i = 0; i < nPts; i++) {
+	                        ymodel = Math.exp(a[1] * xSeries[i] + a[0]);
+	                        residuals[i] = ymodel - ySeries[i];
+	                    }
+	                } // if ((ctrl == -1) || (ctrl == 1))
+	                else if (ctrl == 2) {
+	                    if (analyticalJacobian) {
+		                    // Calculate the Jacobian analytically
+		                    for (i = 0; i < nPts; i++) {
+		                        jacobian[i][0] = Math.exp(a[1] * xSeries[i] + a[0]);
+		                        jacobian[i][1] = xSeries[i] * jacobian[i][0];
+		                    }
+	                    }
+	                    else {
+	                    	// If the user wishes to calculate the Jacobian numerically
+	                         ctrlMat[0] = 0; 	
+	                    }
+	                } // else if (ctrl == 2)
+	                break;
                 case DRAPER24D:
 	                if ((ctrl == -1) || (ctrl == 1)) {
 	
