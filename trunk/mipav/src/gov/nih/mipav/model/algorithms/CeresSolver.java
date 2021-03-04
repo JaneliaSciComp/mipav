1c93b71d2e0b93dc471bc381815f457ebafc2e23
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2021-03-04 08:21:22 +0000

    Added class CRSMatrix.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@16999 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/CeresSolver.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/CeresSolver.java
@@ -24026,6 +24026,54 @@ public abstract class CeresSolver {
 	  public abstract boolean Evaluate(double[] parameters, double[] cost, double[] gradient);
 	  public abstract int NumParameters();
 	};
+	
+	class CRSMatrix {
+		  Vector<Integer> cols;
+		  Vector<Integer> rows;
+		  Vector<Double> values;
+		  int num_rows;
+		  int num_cols;
+		  public CRSMatrix() {
+			  num_rows = 0;
+			  num_cols = 0;
+			  cols = new Vector<Integer>();
+			  rows = new Vector<Integer>();
+			  values = new Vector<Double>();
+		  }
+
+		  
+
+		  // A compressed row matrix stores its contents in three arrays,
+		  // rows, cols and values.
+		  //
+		  // rows is a num_rows + 1 sized array that points into the cols and
+		  // values array. For each row i:
+		  //
+		  // cols[rows[i]] ... cols[rows[i + 1] - 1] are the indices of the
+		  // non-zero columns of row i.
+		  //
+		  // values[rows[i]] .. values[rows[i + 1] - 1] are the values of the
+		  // corresponding entries.
+		  //
+		  // cols and values contain as many entries as there are non-zeros in
+		  // the matrix.
+		  //
+		  // e.g, consider the 3x4 sparse matrix
+		  //
+		  //  [ 0 10  0  4 ]
+		  //  [ 0  2 -3  2 ]
+		  //  [ 1  2  0  0 ]
+		  //
+		  // The three arrays will be:
+		  //
+		  //
+		  //            -row0-  ---row1---  -row2-
+		  //  rows   = [ 0,      2,          5,     7]
+		  //  cols   = [ 1,  3,  1,  2,  3,  0,  1]
+		  //  values = [10,  4,  2, -3,  2,  1,  2]
+
+		  
+		};
 
 } // public abstract class CeresSolver
 
