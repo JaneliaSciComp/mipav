d2e62402a412714d467164d5f7b7d8e173fb40ab
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2021-11-29 09:18:08 +0000

    Porting continues.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@17616 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/PseudoPolarFourierTransform.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/PseudoPolarFourierTransform.java
@@ -2,6 +2,7 @@ package gov.nih.mipav.model.algorithms.filters;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -3447,6 +3448,386 @@ public class PseudoPolarFourierTransform extends AlgorithmBase {
     		D4=topprep(m4c,m4r);
     }
 
+    private double[][] invDecimatedFreqs(double[][][] fim) {
+    		
+    		// Recover the image whose decimated Fourier transform is fim.
+    		// The input is of size (n+1)x(n+1). 
+    		// The function computed the toeplitz matrix that corresponds to the
+    		// transform that map rim to fim, computes its inverse (O(n^2) operations
+    		// for rim of size nxn), and applies this matrix to all rows and columns of
+    		// fim. Each application requires O(nlogn) operations and hence the total
+    		// complexity for inverting fim is O(n^2logn).
+    		
+    		// Input parameters
+    		//   fim    Frequnecy image to recover.
+    		
+    		// Output parameters
+    		//   rim    The recovered image.
+    		
+    		// Yoel Shkolnisky 04/10/04
+            
+            int i,l,k,n;
+            double c[][];
+            double r[][];
+            double m1[][];
+            double m2[][];
+            double m3[][];
+            double m4[][];
+            double D1[][];
+            double D2[][];
+            double D3[][];
+            double D4[][];
+            double tmpres[][][];
+            double rim[][];
+            double v[][] = new double[2][];
+            double u[][];
+    		int n1 = fim[0].length;
+    		int n2 = fim[0][0].length;
+
+    		if (n1 != n2) {
+    		    System.err.println("In invDecimatedFreqs input matrix must be square");
+    		    System.exit(0);
+    		}
+
+    	    n=n1-1;
+    		c= new double[2][n];
+    		r= new double[2][n];
+    		for (k=-n/2; k <= n/2-1; k++) {
+    		    for (l=-n/2; l <= n/2; l++) {
+    		        c[0][k+n/2]=c[0][k+n/2]+Math.cos(4.0*Math.PI*l*(-n/2.0-k)/(2.0*n+1.0));
+    		        c[1][k+n/2]=c[1][k+n/2]+Math.sin(4.0*Math.PI*l*(-n/2.0-k)/(2.0*n+1.0));
+    		        r[0][k+n/2]=r[0][k+n/2]+Math.cos(4.0*Math.PI*l*(k+n/2.0)/(2.0*n+1.0));
+    		        r[1][k+n/2]=r[1][k+n/2]+Math.sin(4.0*Math.PI*l*(k+n/2.0)/(2.0*n+1.0));
+    		    } // for (l=-n/2; l <= n/2; l++)
+    		} // for (k=-n/2; k <= n/2-1; k++)
+    		
+    		m1 = new double[2][2*n];
+    		m2 = new double[2][2*n];
+    		m3 = new double[2][2*n];
+    		m4 = new double[2][2*n];
+    		topinv(m1,m2,m3,m4,c,r);
+    		D1 = new double[2][2*n];
+    		D2 = new double[2][2*n];
+    		D3 = new double[2][2*n];
+    		D4 = new double[2][2*n];
+		    topprepinv(D1,D2,D3,D4,m1,m2,m3,m4);
+
+		    tmpres= new double[2][n+1][n];
+		    rim=new double[n][n];
+
+		    // apply inverse to rows
+		    for (k=0; k < n+1; k++) {
+		        v[0]=fim[0][k];
+		        v[1]=fim[1][k];
+		        v=adjF(v);
+		        u=topinvmul(D1,D2,D3,D4,v);
+		        tmpres[0][k] = u[0];
+		        tmpres[1][k] = u[1];
+		    } // for (k=0; k < n+1; k++)
+
+		    // apply inverse to columns
+		    for (k=0; k < n; k++) {
+		    	for (i = 0; i < n+1; i++) {
+		           v[0][i]=tmpres[0][i][k];
+		           v[1][i]=tmpres[1][i][k];
+		    	}
+		        v=adjF(v);
+		        u=topinvmul(D1,D2,D3,D4,v);
+		        for (i = 0; i < n; i++) {
+		            rim[i][k]=u[0][i];
+		        }
+		    } // for (k=0; k < n; k++)
+		    return rim;
+    }
+    
+    private void ippftconsts(double c[][][], double d[][][], int n) {
+    		// Prepare the tables with the constants used by the interpolation.
+    		// If the tables exists as file then the functions simply reads them.
+    		// Otherwise, it generates the using a slow algorithm. In such a case a
+    		// warning is issued.
+    		
+    		// Input parameters
+    		//   n    The size of the the pseudo-polar grid is 2x(2n+1)x(n+1)
+    		
+    		// Output parameters
+    		//   c    Constants cl. [4][n/2][n] 
+    		//   d    Constants dj. [4][n/2][n]
+    		// Both arrays c and d are 3D arrays of size 4xn2xn . The vectors d(type,:,:) and
+    		// c(type,:,:), type=1,2,3,4, contain constants which correspond to a
+    		// row/column of the given type, where Type 1 refers to rows from -n/2 to -1; type
+    		// 2 refers to columns from -n/2 to -1; type 3 refers to rows from 1 to n/2;
+    		// and type 4 refers to columns from 1 to n/2. 
+    		
+    		// Yoel Shkolnisky 11/10/04
+
+    		prepConstD(d, n);
+    		prepConstC(c, n);
+    }
+
+    		
+    private void prepConstD(double d[][][], int n) {
+		//d=zeros(4,n/2,n);
+        int j;
+		for (j=n/2; j >= 1; j--) {
+		    d[0][n/2-j] = compD(n,-2*j,1);
+		    d[2][n/2-j] = compD(n,2*j,3);
+		    d[1][n/2-j] = compD(n,-2*j,2);
+		    d[3][n/2-j] = compD(n,2*j,4);       
+		} // for (j=n/2; j >= 1; j--)
+    }
+    
+    private int N(int k, int s) {
+    		// s is the length of the vector x. This means that s=n+1 where n is even
+    		// and the vector x is indexed by
+    		//   [x(-n/2),...,x(0),...,x(n/2)].
+    	    // The parameter k should in the range -n/2<=k<=n/2, or when using the
+    		// argument s: -(s-1)/2 <= k <= (s-1)/2.
+    		return(int)Math.round(2.0*((s-1.0)/2.0-Math.abs(k)));
+    }
+    
+    private int[] createArray(int lower, int upper) {
+    	int i, len;
+    	int result[];
+    	if (upper >= lower) {
+    	    len = upper - lower + 1;
+    	    result = new int[len];
+    	    for (i = lower; i <= upper; i++) {
+    	    	result[i-lower] = i;
+    	    }
+    	}
+    	else {
+    		len = lower - upper + 1;
+    		result = new int[len];
+    		for (i = lower; i >= upper; i--) {
+    			result[lower-i] = i;
+    		}
+    	}
+    	return result;
+    }
+
+    		
+    private double[] compD(int n, int j, int type) {
+    		
+    		// Compute the constants required for the interpolation of row/column j in
+    		// the the pseudo-polar grid.
+    		int i, k, m;
+    		double alpha;
+    		int idx1[];
+    		int idx2[];
+    		int idx3[];
+    		double y[];
+    		double LARGE;
+    		double EPS;
+    		double dj[];
+    		
+    		if ((n % 2)==1) {
+    		    System.err.println("In compD the length n must be even");
+    		    System.exit(0);
+    		}
+    		m=2*n+1;
+    		alpha=-2.0*j/n;
+
+    		// Compute the frequencies in the vector of length N(k,n+1)+n+1. These
+    		// frequnecies are denoted as the vector y
+    		if ((type==1) || (type==4)) {
+    		    idx1= createArray(-n/2,-n/2+N(j/2,n+1)/2-1);
+    		    idx3=createArray(n/2-N(j/2,n+1)/2+1,n/2-1);
+    		}
+    		else {
+    		    idx1=createArray(-n/2+1,-n/2+N(j/2,n+1)/2-1);
+    		    idx3=createArray(n/2-N(j/2,n+1)/2+1,n/2);
+    		}
+
+    		if (alpha==-2) {
+    			idx2 = createArray(-n/2+N(j/2,n+1)/2+1,n/2-N(j/2,n+1)/2);
+    			for (i = 0; i < idx2.length; i++) {
+    				idx2[i] = (int)Math.round(idx2[i]/(alpha/2.0));
+    			}
+    		}
+    		else if (alpha==2) {
+    			    idx2 = createArray(-n/2+N(j/2,n+1)/2,n/2-N(j/2,n+1)/2-1);
+    			    for (i = 0; i < idx2.length; i++) {
+        				idx2[i] = (int)Math.round(idx2[i]/(alpha/2.0));
+        			}
+    		}
+    		else if (alpha!=0) {
+    			idx2 = createArray(-n/2+N(j/2,n+1)/2,n/2-N(j/2,n+1)/2);
+    			for (i = 0; i < idx2.length; i++) {
+    				idx2[i] = (int)Math.round(idx2[i]/(alpha/2.0));
+    			}
+    		}
+    		else {
+    		    idx2=new int[] {0};
+    		}
+    		
+    		y = new double[idx1.length + idx2.length + idx3.length];
+    		for (i = 0; i < idx1.length; i++) {
+    			y[i] = -4.0*Math.PI*idx1[i]/m;
+    		}
+    		for (i = 0; i < idx2.length; i++) {
+    			y[i + idx1.length] = -2.0*Math.PI*alpha*idx2[i]/m;
+    		}
+    		for (i = 0; i < idx3.length; i++) {
+    			y[i + idx1.length + idx2.length] = -4.0*Math.PI*idx3[i]/m;
+    		}
+
+    		Arrays.sort(y);
+
+    		
+
+    		LARGE=1.0E15;
+    		EPS=1.0E-15;
+    		dj= new double[n];
+
+    		// Compute the factors dj
+    		for (j=0; j < n; j++) {
+    		    dj[j]=0.0;
+    		    for (k=0; k < n; k++) {
+    		        if (j != k) {
+    		            if (Math.abs(y[j]-y[k]) > EPS) {
+    		                dj[j]=dj[j]-Math.log(Math.sin((y[j]-y[k])/2.0));
+    		            }
+    		            else {
+    		                System.err.println("In compD two values of y are too close to each other");
+    		                System.exit(0);
+    		            }
+    		        } // if (j != k)
+    		    } // for (k=0; k < n; k++)
+    		} // for (j=0; j < n; j++) 
+
+    		for (i = 0; i < n; i++) {
+    		    dj[i]=Math.exp(dj[i]);
+    		}
+    		return dj;
+    }
+
+    		
+    private void prepConstC(double c[][][], int n) {
+    		//c=zeros(4,n/2,n);
+
+    		int j;
+    	    for (j=n/2; j >= 1; j--) {
+    		    c[0][n/2-j] = compC(n,-2*j,1);
+    		    c[2][n/2-j] = compC(n,2*j,3);
+    		    c[1][n/2-j] = compC(n,-2*j,2);
+    		    c[3][n/2-j] = compC(n,2*j,4);   
+    	    } // for (j=n/2; j >= 1; j--)
+    }
+
+    		
+    private double[] compC(int n, int j, int type) {
+    		
+    		// Compute the constant required for the interpolation of row/column j in
+    		// the the pseudo-polar grid.
+    		int i, m;
+    		double alpha;
+    		int idx1[];
+    		int idx2[];
+    		int idx3[];
+    		double y[];
+
+    		m=2*n+1;
+    		alpha=-2.0*j/n;
+
+    		// Compute the frequencies in the vector of length N(k,n+1)+n+1. These
+    		// frequencies are denoted as the vector y
+    		if ((type==1) || (type==4)) {
+    		    idx1=createArray(-n/2,-n/2+N(j/2,n+1)/2-1);
+    		    idx3=createArray(n/2-N(j/2,n+1)/2+1,n/2-1);
+    		}
+    		else {
+    		    idx1=createArray(-n/2+1,-n/2+N(j/2,n+1)/2-1);
+    		    idx3=createArray(n/2-N(j/2,n+1)/2+1,n/2);
+    		}
+
+    		/*if (alpha==-2)
+    		    idx2=round([-n/2+N(j/2,n+1)/2+1:n/2-N(j/2,n+1)/2]/(alpha/2));
+    		elseif (alpha==2)
+    		        idx2=round([-n/2+N(j/2,n+1)/2:n/2-N(j/2,n+1)/2-1]/(alpha/2));
+    		elseif (alpha~=0)
+    		    idx2=round([-n/2+N(j/2,n+1)/2:n/2-N(j/2,n+1)/2]/(alpha/2));
+    		else
+    		    idx2=0;
+    		end
+
+    		y=[-2*pi*(2)*idx1/m -2*pi*alpha*idx2/m -2*pi*(2)*idx3/m];
+
+    		%Compute the frequencies on the Cartesian grid.
+    		if (type==1) | (type==4)
+    		    x=-2*pi*(2)*[-n/2:n/2-1]/m;
+    		else
+    		    x=-2*pi*(2)*[-n/2+1:n/2]/m;
+    		end
+
+    		if (n~=length(x)) || (n~=length(y))
+    		    error('The arrays f,x,y should be of the same length');
+    		end
+
+    		if (mod(n,2)==1)
+    		    error('The length n must be even');
+    		end
+
+    		% sort x and then sort everything to the same order as x.
+    		[x,srtXidx]=sort(x);
+    		[y,srtYidx]=sort(y);
+
+    		% remove x points that are equal to some y point
+    		idxX=1;
+    		idxY=1;
+    		idxDroppedElems=zeros(1,n); % the indices of the x that appear in y
+
+    		j=1;
+    		while (idxX<=n) & (idxY<=n)
+    		    if (abs(x(idxX)-y(idxY))<1.0d-15)
+    		        idxDroppedElems(j)=idxX;
+    		        j=j+1;
+    		        idxX=idxX+1;
+    		    elseif x(idxX)<y(idxY)
+    		        idxX=idxX+1;
+    		    else
+    		        idxY=idxY+1;
+    		    end
+    		end
+
+    		% idxUsed is the set of indices from x which we need to
+    		% compute. In other words, these are the elements that were not dropped in
+    		% the previous iteration.
+    		idxUsed=setdiff(1:n,idxDroppedElems(:));
+    		x=x(idxUsed);
+    		m=length(x);
+    		lendropped=j-1; % number of dropped elements from x
+
+    		% Now we can resample the polynomial using the modified vector x
+
+    		LARGE=1.0E15;
+    		EPS=1.0E-15;
+    		clt=zeros(1,m);
+
+    		% Compute the factors cl
+    		for l=1:m
+    		    %compute cl
+    		    clt(l)=0;
+    		    for k=1:n
+    		        clt(l)=clt(l)+log(sin((x(l)-y(k))/2));
+    		    end
+    		%    
+    		    if (abs(clt(l))>LARGE)
+    		        warning('Value of cl too large...');
+    		    end
+    		end
+
+    		clt=exp(clt);
+
+    		% Put zeros in the elements that were not computed by this function.
+    		% These zeros will be dropped when the invserion function gets the table of
+    		% cl. We use zero padding so that all arrays will have the same size,
+    		% although this wastes storage space. 
+    		cl=zeros(1,n);
+    		cl(idxUsed)=clt;*/
+    		return null;
+    }
+
+    		
 
     
 }
