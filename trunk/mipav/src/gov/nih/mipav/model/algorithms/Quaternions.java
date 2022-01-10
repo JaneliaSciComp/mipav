39d285edda3d35f71f6225a282ef1da5294cee8f
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2022-01-10 18:49:44 +0000

    test_dcm2q passes.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@17699 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/Quaternions.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/Quaternions.java
@@ -1917,6 +1917,7 @@ public class Quaternions extends AlgorithmBase {
 
 
 	public void test_dcm2q() {
+		// Test passes.
 		// TEST_DCM2Q runs unit tests for the DCM2Q function.
 	
 		// Release: $Name: quaternions-1_3 $
@@ -1929,12 +1930,12 @@ public class Quaternions extends AlgorithmBase {
 		
 		int failures=0;
 	    int r,c;
-        int wrong_values;
 	
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		//disp_test_name('Algorithm');
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         double t;
+        int count;
         double a[] = new double[17];
         double b[] = new double[17];
         double z[] = new double[17];
@@ -1951,60 +1952,111 @@ public class Quaternions extends AlgorithmBase {
         	}
         }
         double Qin[][] = qnorm(preQin);
-		/*t=0:2*pi/16:2*pi;
-		a=sin(t)';
-		b=cos(t)';
-		z=0*a;
+        double Qin2[][] = new double[10 + 16*17][4];
+	    for (r = 0; r < 10; r++) {
+	    	for (c = 0; c < 4; c++) {
+	    	    Qin2[r][c] = Qin[r][c];
+	    	}
+	    }
+	    for (r = 0; r < 17; r++) {
+	    	for (c = 0; c < 4; c++) {
+	    		Qin2[r+10][0] = a[r];
+	    		Qin2[r+10][1] = b[r];
+	    		Qin2[r+10][2] = z[r];
+	    		Qin2[r+10][3] = z[r];
+	    		Qin2[r+27][0] = a[r];
+	    		Qin2[r+27][1] = z[r];
+	    		Qin2[r+27][2] = b[r];
+	    		Qin2[r+27][3] = z[r];
+	    		Qin2[r+44][0] = a[r];
+	    		Qin2[r+44][1] = z[r];
+	    		Qin2[r+44][2] = z[r];
+	    		Qin2[r+44][3] = b[r];
+	    		Qin2[r+61][0] = b[r];
+	    		Qin2[r+61][1] = a[r];
+	    		Qin2[r+61][2] = z[r];
+	    		Qin2[r+61][3] = z[r];
+	    		Qin2[r+78][0] = z[r];
+	    		Qin2[r+78][1] = a[r];
+	    		Qin2[r+78][2] = b[r];
+	    		Qin2[r+78][3] = z[r];
+	    		Qin2[r+95][0] = z[r];
+	    		Qin2[r+95][1] = a[r];
+	    		Qin2[r+95][2] = z[r];
+	    		Qin2[r+95][3] = b[r];
+	    		Qin2[r+112][0] = b[r];
+	    		Qin2[r+112][1] = z[r];
+	    		Qin2[r+112][2] = a[r];
+	    		Qin2[r+112][3] = z[r];
+	    		Qin2[r+129][0] = z[r];
+	    		Qin2[r+129][1] = b[r];
+	    		Qin2[r+129][2] = a[r];
+	    		Qin2[r+129][3] = z[r];
+	    		Qin2[r+146][0] = z[r];
+	    		Qin2[r+146][1] = z[r];
+	    		Qin2[r+146][2] = a[r];
+	    		Qin2[r+146][3] = b[r];
+	    		Qin2[r+163][0] = b[r];
+	    		Qin2[r+163][1] = z[r];
+	    		Qin2[r+163][2] = z[r];
+	    		Qin2[r+163][3] = a[r];
+	    		Qin2[r+180][0] = z[r];
+	    		Qin2[r+180][1] = b[r];
+	    		Qin2[r+180][2] = z[r];
+	    		Qin2[r+180][3] = a[r];
+	    		Qin2[r+197][0] = z[r];
+	    		Qin2[r+197][1] = z[r];
+	    		Qin2[r+197][2] = b[r];
+	    		Qin2[r+197][3] = a[r];
+	    	}
+	    }
 	
-		Qin = qnorm(2*rand(10,4)-1);
+		// make sure all quaternions have q4>=0
+		for (count = 0; count < Qin2.length; count++) {
+		  if (Qin2[count][3] <0) {
+			  for (c = 0; c < 4; c++) {
+		          Qin2[count][c]=-Qin2[count][c];
+			  }
+		  }
+		}
 	
-		Qin = [Qin; [a b z z]];
-		Qin = [Qin; [a z b z]];
-		Qin = [Qin; [a z z b]];
+		double A[][][] = q2dcm(Qin2);
+		
+		//double T[] = new double[Qin2.length];
+		//for (count = 0; count < Qin2.length; count++) {
+		//	T[count] = A[0][0][count] + A[1][1][count] + A[2][2][count];
+		//}
 	
-		Qin = [Qin; [b a z z]];
-		Qin = [Qin; [z a b z]];
-		Qin = [Qin; [z a z b]];
+		double Qout[][] = dcm2q(A);
 	
-		Qin = [Qin; [b z a z]];
-		Qin = [Qin; [z b a z]];
-		Qin = [Qin; [z z a b]];
+		double qdiff[][] = qmult(qconj(Qout,true),Qin2);
 	
-		Qin = [Qin; [b z z a]];
-		Qin = [Qin; [z b z a]];
-		Qin = [Qin; [z z b a]];
+		//double truth_value[] = new double[] {0.0,  0.0,  0.0};
+		double test_value[] = new double[3];
+		double maxAbsVal;
+		for (c = 0; c < 3; c++) {
+		    maxAbsVal = 0;	
+		    for (r = 0; r < Qin2.length; r++) {
+		    	if (Math.abs(qdiff[r][c]) > maxAbsVal) {
+		    		maxAbsVal = Math.abs(qdiff[r][c]);
+		    	}
+		    }
+		    test_value[c] = maxAbsVal;
+		}
+	    
+		for (r = 0; r < 3; r++) {
+			if (Math.abs(test_value[r]) > 2.0*epsilon) {
+				failures++;
+			}
+		}
 	
-		% make sure all quaternions have q4>=0
-		for count=1:length(Qin)
-		  if Qin(count,4)<0
-		    Qin(count,:)=-Qin(count,:);
-		  end
-		end
-		Qin, disp(' ') %#ok<NOPTS>
-	
-		A='q2dcm(Qin)', disp(' ') %#ok<NOPTS>
-		A=eval(A);
-	
-		T=(1:length(Qin))*0;
-		for count=1:length(Qin)
-		  T(count) = trace(A(:,:,count));
-		end
-	
-		Qout='dcm2q(A)', disp(' ') %#ok<NOPTS>
-		Qout=eval(Qout);
-	
-		qdiff='qmult(qconj(Qout),Qin)', disp(' ') %#ok<NOPTS>
-		qdiff=eval(qdiff);
-	
-		truth_value = '[0.0  0.0  0.0]';
-		test_value  = 'max(abs(qdiff(:,1:3)))';
-	
-		failures=failures+check_value(truth_value, test_value);
-	
-		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-		%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-	
-		disp_num_failures(test_title, failures)*/
+		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		UI.setDataText("In test_dcm2q " + failures + " failures\n");
+		if (failures > 0) {
+			UI.setDataText("test_dcm2q FAILED\n");
+		}
+		
 	}
 
 	
