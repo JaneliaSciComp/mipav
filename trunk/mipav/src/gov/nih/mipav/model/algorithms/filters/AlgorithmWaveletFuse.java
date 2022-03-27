3db60907ad5404e37d3ba51b8348595e9fa77831
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2022-03-27 10:20:17 +0000

    Started work on.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@17834 ba61647d-9d00-f842-95cd-605cb4296b96

---
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/filters/AlgorithmWaveletFuse.java
@@ -0,0 +1,78 @@
+package gov.nih.mipav.model.algorithms.filters;
+
+
+import gov.nih.mipav.model.algorithms.*;
+import gov.nih.mipav.model.algorithms.filters.PyWavelets.DiscreteWavelet;
+import gov.nih.mipav.model.structures.*;
+
+import gov.nih.mipav.view.*;
+
+import java.io.*;
+
+/**
+MIT License
+
+Copyright (c) 2021 Hans Brouwer, Riyo Wanagiri
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Reference:
+    Image Fusion Using Wavelet Transform by Aggariyo Wanagiri and 
+    Hans Brouwer, Delft University of Technology.
+
+This is a port of wavelet_fuse.py
+
+*/
+
+public class AlgorithmWaveletFuse extends AlgorithmBase {
+	
+	//~ Instance fields ------------------------------------------------------------------------------------------------
+	private ModelImage fuseImage;
+	private int kernel_size = 37;
+	private PyWavelets.WAVELET_NAME names[] = new PyWavelets.WAVELET_NAME[] {PyWavelets.WAVELET_NAME.DB};
+	private int orders[] = new int[] {13};
+	private int max_depth = 999;
+	boolean slow = false;
+	
+    public AlgorithmWaveletFuse() {
+		
+	}
+	
+	public AlgorithmWaveletFuse(ModelImage destImg, ModelImage srcImg, ModelImage fuseImage,
+			int kernel_size, PyWavelets.WAVELET_NAME names[], int orders[],
+			short max_depth, boolean slow) {
+		super(destImg, srcImg);
+		this.fuseImage = fuseImage;
+		this.kernel_size = kernel_size;
+		this.names = names;
+		this.orders = orders;
+		this.max_depth = max_depth;
+	}
+	
+	public void runAlgorithm() {
+	   int xDim = srcImage.getExtents()[0];
+	   int yDim = srcImage.getExtents()[1];
+	   PyWavelets py = new PyWavelets();
+	   DiscreteWavelet w = py.discrete_wavelet(names[0], orders[0]);
+	   short xDim_max_level = py.dwt_max_level(xDim, w);
+	   short yDim_max_level = py.dwt_max_level(yDim, w);
+	   max_depth = Math.min(max_depth, Math.min(xDim_max_level,yDim_max_level));
+    }
+
+}
