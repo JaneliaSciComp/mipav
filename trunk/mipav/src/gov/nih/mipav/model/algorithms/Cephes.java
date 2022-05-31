fb8e8548e548ede1c18510d32e0365c6e5f3f476
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2022-05-31 07:09:29 +0000

    Code needed to run chdtri is now present.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@17943 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/Cephes.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/Cephes.java
@@ -35,33 +35,47 @@ OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 public class Cephes {
 	public final static int CHDTRI = 1;
+	public final static int IGAMI = 2;
+	public final static int IGAMC = 3;
+	public final static int IGAM = 4;
 	// For IEEE arithmetic (IBMPC):
     private final static double MACHEP =  1.11022302462515654042E-16; // 2**-53
     private final static double MAXLOG =  7.09782712893383996843E2;   // log(2**1024)
     private final static double MINLOG = -7.08396418532264106224E2;   // log(2**-1022)
     private final static double MAXNUM =  Double.MAX_VALUE; // 1.7976931348623158E308 2**1024
+    private final static double big = 4.503599627370496e15;
+	private final static double biginv =  2.22044604925031308085e-16;
 	private double result[];
 	
 	private int version;
 	
-	private double df;
+	private double par1;
 	
-	private double y;
+	private double par2;
 	
 	public Cephes() {
 		
 	}
 	
-	public Cephes(double df, double y, int version, double result[]) {
-		this.df = df;
-		this.y = y;
+	public Cephes(double par1, double par2, int version, double result[]) {
+		this.par1 = par1;
+		this.par2 = par2;
 		this.version = version;
 		this.result = result;
 	}
 	
 	public void run() {
 		if (version == CHDTRI) {
-			chdtri();
+			chdtri(par1, par2);
+		}
+		else if (version == IGAMI) {
+			igami(par1, par2);
+		}
+		else if (version == IGAMC) {
+			igamc(par1, par2);
+		}
+		else if (version == IGAM) {
+			igam(par1,par2);
 		}
 	}
 	
@@ -111,14 +125,14 @@ public class Cephes {
 	Copyright 1984, 1987 by Stephen L. Moshier
 	Direct inquiries to 30 Frost Street, Cambridge, MA 02140
 	*/
-	private void chdtri() {
+	private void chdtri(double df, double y) {
 	    double x;
 	    if ((y < 0) || ( y > 1.0) || (df < 1.0)) {
 	    	MipavUtil.displayError("Domain error in chdtri()");
-	    	result[0] = Double.NaN;
+	    	result[0] = 0.0;
 	    }
 	    
-	    x = igami( 0.5 * df, y );
+	    x = igami( 0.5 * df, y )[0];
 	    result[0] = (2.0 * x );
 	    
 	}
@@ -172,7 +186,7 @@ public class Cephes {
 	Cephes Math Library Release 2.3:  March, 1995
 	Copyright 1984, 1987, 1995 by Stephen L. Moshier
 	*/
-	double igami(double a, double y0) {
+	private double[] igami(double a, double y0) {
 		double x0, x1, x, yl, yh, y, d, lgm, dithresh;
 		int i, dir;
 
@@ -201,7 +215,7 @@ public class Cephes {
 		{
 		if( x > x0 || x < x1 )
 			break;
-		y = igamc(a,x);
+		y = igamc(a,x)[0];
 		if( y < yl || y > yh )
 			break;
 		if( y < y0 )
@@ -221,8 +235,10 @@ public class Cephes {
 		d = -Math.exp(d);
 	/* compute the step to the next approximation of x */
 		d = (y - y0)/d;
-		if( Math.abs(d/x) < MACHEP )
-			return x;
+		if( Math.abs(d/x) < MACHEP ) {
+			result[0] = x;
+			return result;
+		}
 		x = x - d;
 		} // for( i=0; i<10; i++ )
 		
@@ -237,7 +253,7 @@ public class Cephes {
 			while( x0 == MAXNUM )
 				{
 				x = (1.0 + d) * x;
-				y = igamc( a, x );
+				y = igamc( a, x )[0];
 				if( y < y0 )
 					{
 					x0 = x;
@@ -253,7 +269,7 @@ public class Cephes {
 		for( i=0; i<400; i++ )
 			{
 			x = x1  +  d * (x0 - x1);
-			y = igamc( a, x );
+			y = igamc( a, x )[0];
 			lgm = (x0 - x1)/(x1 + x0);
 			if( Math.abs(lgm) < dithresh )
 				break;
@@ -295,14 +311,226 @@ public class Cephes {
 			}
 		if( x == 0.0 ) {
 			MipavUtil.displayError( "igami UNDERFLOW ERROR");
-			return Double.NaN;
+			result[0] = 0.0;
+			return result;
 		}
-
-		return( x );
+        result[0] = x;
+		return result;
 	}
 	
-	private double igamc(double a, double x) {
-		return 0.0;
+	/*							igamc()
+	 *
+	 *	Complemented incomplete gamma integral
+	 *
+	 *
+	 *
+	 * SYNOPSIS:
+	 *
+	 * double a, x, y, igamc();
+	 *
+	 * y = igamc( a, x );
+	 *
+	 * DESCRIPTION:
+	 *
+	 * The function is defined by
+	 *
+	 *
+	 *  igamc(a,x)   =   1 - igam(a,x)
+	 *
+	 *                            inf.
+	 *                              -
+	 *                     1       | |  -t  a-1
+	 *               =   -----     |   e   t   dt.
+	 *                    -      | |
+	 *                   | (a)    -
+	 *                             x
+	 *
+	 *
+	 * In this implementation both arguments must be positive.
+	 * The integral is evaluated by either a power series or
+	 * continued fraction expansion, depending on the relative
+	 * values of a and x.
+	 *
+	 * ACCURACY:
+	 *
+	 * Tested at random a, x.
+	 *                a         x                      Relative error:
+	 * arithmetic   domain   domain     # trials      peak         rms
+	 *    IEEE     0.5,100   0,100      200000       1.9e-14     1.7e-15
+	 *    IEEE     0.01,0.5  0,100      200000       1.4e-13     1.6e-15
+	 */
+	
+	/*
+	Cephes Math Library Release 2.0:  April, 1987
+	Copyright 1985, 1987 by Stephen L. Moshier
+	Direct inquiries to 30 Frost Street, Cambridge, MA 02140
+	*/
+
+	private double[] igamc(double  a, double x )
+	{
+	double ans, ax, c, yc, r, t, y, z;
+	double pk, pkm1, pkm2, qk, qkm1, qkm2;
+
+	if( (x <= 0) || ( a <= 0) ) {
+		result[0] = 1.0;
+		return result;
 	}
+
+	if( (x < 1.0) || (x < a) ) {
+		result[0] = 1.0 - igam(a,x)[0];
+		return result;
+	}
+
+	double ansG[] = new double[1];
+	Gamma gam = new Gamma(a, 0, ansG);
+	gam.run();
+	double lgm = ansG[0];
+	ax = a * Math.log(x) - x - lgm;
+	if( ax < -MAXLOG )
+		{
+		MipavUtil.displayError("igamc UNDERFLOW");
+		result[0] = 0.0;
+		return result;
+		}
+	ax = Math.exp(ax);
+
+	/* continued fraction */
+	y = 1.0 - a;
+	z = x + y + 1.0;
+	c = 0.0;
+	pkm2 = 1.0;
+	qkm2 = x;
+	pkm1 = x + 1.0;
+	qkm1 = z * x;
+	ans = pkm1/qkm1;
+
+	do
+		{
+		c += 1.0;
+		y += 1.0;
+		z += 2.0;
+		yc = y * c;
+		pk = pkm1 * z  -  pkm2 * yc;
+		qk = qkm1 * z  -  qkm2 * yc;
+		if( qk != 0 )
+			{
+			r = pk/qk;
+			t = Math.abs( (ans - r)/r );
+			ans = r;
+			}
+		else
+			t = 1.0;
+		pkm2 = pkm1;
+		pkm1 = pk;
+		qkm2 = qkm1;
+		qkm1 = qk;
+		if( Math.abs(pk) > big )
+			{
+			pkm2 *= biginv;
+			pkm1 *= biginv;
+			qkm2 *= biginv;
+			qkm1 *= biginv;
+			}
+		}
+	while( t > MACHEP );
+     
+	result[0] = ans * ax;
+	return result;
+	}
+
+	/*							igam.c
+	 *
+	 *	Incomplete gamma integral
+	 *
+	 *
+	 *
+	 * SYNOPSIS:
+	 *
+	 * double a, x, y, igam();
+	 *
+	 * y = igam( a, x );
+	 *
+	 * DESCRIPTION:
+	 *
+	 * The function is defined by
+	 *
+	 *                           x
+	 *                            -
+	 *                   1       | |  -t  a-1
+	 *  igam(a,x)  =   -----     |   e   t   dt.
+	 *                  -      | |
+	 *                 | (a)    -
+	 *                           0
+	 *
+	 *
+	 * In this implementation both arguments must be positive.
+	 * The integral is evaluated by either a power series or
+	 * continued fraction expansion, depending on the relative
+	 * values of a and x.
+	 *
+	 * ACCURACY:
+	 *
+	 *                      Relative error:
+	 * arithmetic   domain     # trials      peak         rms
+	 *    IEEE      0,30       200000       3.6e-14     2.9e-15
+	 *    IEEE      0,100      300000       9.9e-14     1.5e-14
+	 */
+	
+	/* left tail of incomplete gamma function:
+	 *
+	 *          inf.      k
+	 *   a  -x   -       x
+	 *  x  e     >   ----------
+	 *           -     -
+	 *          k=0   | (a+k+1)
+	 *
+	 */
+
+	private double[] igam(double a, double x)
+	{
+	double ans, ax, c, r;
+
+	if( (x <= 0) || ( a <= 0) ) {
+		result[0] = 0.0;
+		return result;
+	}
+
+	if( (x > 1.0) && (x > a ) ) {
+		result[0] = 1.0 - igamc(a,x)[0];
+		return result;
+	}
+
+	/* Compute  x**a * exp(-x) / gamma(a)  */
+	double ansG[] = new double[1];
+	Gamma gam = new Gamma(a, 0, ansG);
+	gam.run();
+	double lgm = ansG[0];
+	ax = a * Math.log(x) - x - lgm;
+	if( ax < -MAXLOG )
+		{
+		MipavUtil.displayError( "igam UNDERFLOW");
+		result[0] = 0.0;
+		return result;
+		}
+	ax = Math.exp(ax);
+
+	/* power series */
+	r = a;
+	c = 1.0;
+	ans = 1.0;
+
+	do
+		{
+		r += 1.0;
+		c *= x/r;
+		ans += c;
+		}
+	while( c/ans > MACHEP );
+
+	result[0] = ans * ax/a;
+	return result;
+	}
+
+	
 	
 }
