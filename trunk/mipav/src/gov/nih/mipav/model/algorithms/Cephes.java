94d9d273797b14a1237ac79facb0aa1a2bcc5849
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2022-06-10 08:42:02 +0000

    Test for ndtri(0.5) passed.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@17974 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/Cephes.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/Cephes.java
@@ -59,6 +59,41 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  *  */
 
+/**
+ * Correct values for ndtri are taken from scipy-main/scipy/special/tests/test_ndtr.py under the BSD-3 license:
+ * Copyright (c) 2001-2002 Enthought, Inc. 2003-2022, SciPy Developers.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided
+   with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 public class Cephes {
 	public final static int CHDTRI = 1;
 	public final static int IGAMI = 2;
@@ -167,6 +202,7 @@ public class Cephes {
 		// The test for igmac(2,1) passed
 		// The test for igam(1,2) passed
 		// The test for ndtri(0.6) passed
+		// The test for ndtri(0.5) passed
 
 		result = new double[1];
 	    chdtri(4,0.3);
@@ -218,6 +254,17 @@ public class Cephes {
 	    	System.out.println("Implemented ndtri gave " + result[0]);
 	    	System.out.println("Correct answer is 0.25334710313579972");
 	    }
+	    
+	    ndtri(0.5);
+	    if (result[0] == 0.0) {
+	    	System.out.println("The test for ndtri(0.5) passed");
+	    }
+	    else {
+	    	System.out.println("The test for ndtri(0.5) failed");
+	    	System.out.println("Implemented ndtri gave " + result[0]);
+	    	System.out.println("Correct answer is 0.0");
+	    }
+	    
 	}
 	
 	public Cephes() {
