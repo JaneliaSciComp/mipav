3f986e8b917907590c85974a618e3b7aa7c9b4bf
ilb@NIH.GOV <ilb@NIH.GOV@ba61647d-9d00-f842-95cd-605cb4296b96>
2022-08-16 22:26:25 +0000

    Adding padding and unpadding functions.
    git-svn-id: https://citdcbmipav.cit.nih.gov/repos-pub/mipav@18083 ba61647d-9d00-f842-95cd-605cb4296b96

--- trunk/mipav/src/gov/nih/mipav/model/algorithms/fMRIBlindDeconvolution.java
+++ trunk/mipav/src/gov/nih/mipav/model/algorithms/fMRIBlindDeconvolution.java
@@ -139,4 +139,416 @@ public class fMRIBlindDeconvolution extends AlgorithmBase {
 		}
 		return;
 	}
+	
+	// This module provide the padding functions used with the Fourier
+	// implementation for the HRF operator.
+	public double[] _padd_symetric(double arrays[], int p, double c, String paddtype) {
+	   // Helper function to c-padd in a symetric way arrays.
+
+	    // Parameters
+	    // ----------
+	    // arrays : np.ndarray
+	    // array to padd.
+
+	    // p :  int,
+	    //    length of padding.
+
+	    // c : double,
+	    //    value to padd with.
+
+	    // paddtype : "left", "right", "center"
+	    //    where to place the padding.
+
+	    // Results
+	    // -------
+	    // arrays : np.ndarray
+	    // the padded array
+		int p_left;
+		int p_right;
+		double paddedArray[] = new double[arrays.length + p];
+		int i;
+		
+		if (paddtype.equalsIgnoreCase("center")) {
+            p_left = (int)(p / 2);
+            p_right = (int)(p / 2) + (p % 2);
+            for (i = 0; i < p_left; i++) {
+            	paddedArray[i] = c;
+            }
+            for (i = p_left; i < p_left + arrays.length; i++) {
+            	paddedArray[i] = arrays[i-p_left];
+            }
+            for (i = p_left + arrays.length; i < arrays.length + p; i++) {
+            	paddedArray[i] = c;
+            }
+		}
+		else if (paddtype.equalsIgnoreCase("left")) {
+			for (i = 0; i < p; i++) {
+            	paddedArray[i] = c;
+            }
+            for (i = p; i < p + arrays.length; i++) {
+            	paddedArray[i] = arrays[i-p];
+            }
+		}
+		else if (paddtype.equalsIgnoreCase("right")) {
+			for (i = 0; i < arrays.length; i++) {
+            	paddedArray[i] = arrays[i];
+            }
+            for (i = arrays.length; i < arrays.length + p; i++) {
+            	paddedArray[i] = c;
+            }
+		}
+        else {
+        	System.err.println("In _padd_symmetric paddtype = " + paddtype);
+        	System.err.println("paddtype should be left, center, or right");
+        	System.exit(-1);
+        }
+		return paddedArray;
+	}
+	
+	public double[][] _padd_symetric(double arrays[][], int p, double c, String paddtype) {
+		   // Helper function to c-padd in a symetric way arrays.
+
+		    // Parameters
+		    // ----------
+		    // arrays : list of np.ndarray
+            // list of arrays to padd.
+
+
+		    // p :  int,
+		    //    length of padding.
+
+		    // c : double,
+		    //    value to padd with.
+
+		    // paddtype : "left", "right", "center"
+		    //    where to place the padding.
+
+		    // Results
+		    // -------
+	        // arrays : list of np.ndarray
+            // list of arrays
+
+			int p_left;
+			//int p_right;
+			double paddedArray[][] = new double[arrays.length][];
+			int i,j;
+			for (i = 0; i < paddedArray.length; i++) {
+			    paddedArray[i] = new double[arrays[i].length + p];
+				if (paddtype.equalsIgnoreCase("center")) {
+		            p_left = (int)(p / 2);
+		            //p_right = (int)(p / 2) + (p % 2);
+		            for (j = 0; j < p_left; j++) {
+		            	paddedArray[i][j] = c;
+		            }
+		            for (j = p_left; j < p_left + arrays[i].length; j++) {
+		            	paddedArray[i][j] = arrays[i][j-p_left];
+		            }
+		            for (j = p_left + arrays[i].length; j < arrays[i].length + p; j++) {
+		            	paddedArray[i][j] = c;
+		            }
+				}
+				else if (paddtype.equalsIgnoreCase("left")) {
+					for (j = 0; j < p; j++) {
+		            	paddedArray[i][j] = c;
+		            }
+		            for (j = p; j < p + arrays[i].length; j++) {
+		            	paddedArray[i][j] = arrays[i][j-p];
+		            }
+				}
+				else if (paddtype.equalsIgnoreCase("right")) {
+					for (j = 0; j < arrays[i].length; j++) {
+		            	paddedArray[i][j] = arrays[i][j];
+		            }
+		            for (j = arrays[i].length; j < arrays[i].length + p; j++) {
+		            	paddedArray[i][j] = c;
+		            }
+				}
+		        else {
+		        	System.err.println("In _padd_symmetric paddtype = " + paddtype);
+		        	System.err.println("paddtype should be left, center, or right");
+		        	System.exit(-1);
+		        }
+			}
+			return paddedArray;
+		}
+	
+	public double[] _padd_assymetric(double arrays[], int p[], double c, String paddtype) {
+	   // Helper function to c-padd in an assymetric way arrays.
+
+	   // Parameters
+	   // ----------
+	   // arrays : np.ndarray
+	   // array to padd.
+
+	    // p :  int[2],
+	    // length of padding.
+
+	    // c : double,
+	    //    value to padd with.
+
+	    // paddtype : ['center'],
+	    //    where to place the padding.
+
+	   // Note:
+	   // -----
+	   // Will raise a ValueError if paddtype is not 'center'.
+
+	    // Results
+	    // -------
+	    // arrays : np.ndarray
+	    // the padded array
+		int i;
+	   
+		if (!paddtype.equalsIgnoreCase("center")) {
+			System.err.println("In _padd_asymmetric paddtype = " + paddtype);
+        	System.err.println("paddtype should center");
+        	System.exit(-1);	
+		}
+
+        double paddedArray[] = new double[arrays.length + p[0] + p[1]];
+        for (i = 0; i < p[0]; i++) {
+        	paddedArray[i] = c;
+        }
+        for (i = p[0]; i < p[0] + arrays.length; i++) {
+        	paddedArray[i] = arrays[i-p[0]];
+        }
+        for (i = p[0] + arrays.length; i < arrays.length + p[0] + p[1]; i++) {
+        	paddedArray[i] = c;
+        }
+        return paddedArray;
+	}
+	
+	public double[][] _padd_assymetric(double arrays[][], int p[], double c, String paddtype) {
+		   // Helper function to c-padd in an assymetric way arrays.
+
+		   // Parameters
+		   // ----------
+		   // arrays : np.ndarray or list of np.ndarray
+           // list of arrays to padd.
+
+
+		    // p :  int[2],
+		    // length of padding.
+
+		    // c : double,
+		    //    value to padd with.
+
+		    // paddtype : ['center'],
+		    //    where to place the padding.
+
+		   // Note:
+		   // -----
+		   // Will raise a ValueError if paddtype is not 'center'.
+
+		    // Results
+		    // -------
+		    // arrays : list of np.ndarray
+	        // list of arrays
+
+			int i,j;
+		   
+			if (!paddtype.equalsIgnoreCase("center")) {
+				System.err.println("In _padd_asymmetric paddtype = " + paddtype);
+	        	System.err.println("paddtype should center");
+	        	System.exit(-1);	
+			}
+
+	        double paddedArray[][] = new double[arrays.length][];
+	        for (i = 0; i < paddedArray.length; i++) {
+			    paddedArray[i] = new double[arrays[i].length + p[0] + p[1]];
+		        for (j = 0; j < p[0]; j++) {
+		        	paddedArray[i][j] = c;
+		        }
+		        for (j = p[0]; j < p[0] + arrays[i].length; j++) {
+		        	paddedArray[i][j] = arrays[i][j-p[0]];
+		        }
+		        for (j = p[0] + arrays[i].length; j < arrays[i].length + p[0] + p[1]; j++) {
+		        	paddedArray[i][j] = c;
+		        }
+	        }
+	        return paddedArray;
+		}
+	
+	public double[] _unpadd_symetric(double arrays[], int p, String paddtype) {
+	    // Helper function to unpadd in an assymetric way arrays.
+
+	    // Parameters
+	    // ----------
+	    // arrays : np.ndarray
+	    // array to padd.
+
+	    // p :  int,
+	    //    length of padding.
+
+	    // paddtype : left, right, or center,
+	    //    where to place the padding.
+
+	    // Results
+	    // -------
+	    // arrays : np.ndarray
+	    // the unpadded array.
+		
+		int p_left;
+		int p_right;
+		int i;
+		double unpaddedArray[] = new double[arrays.length-p];
+
+		if (paddtype.equalsIgnoreCase("center")) {
+            p_left = (int)(p / 2);
+            p_right = (int)(p / 2) + (p % 2);
+            for (i = p_left; i < arrays.length - p_right; i++) {
+            	unpaddedArray[i-p_left] = arrays[i];
+            }
+		}
+        else if (paddtype.equalsIgnoreCase("left")) {
+        	for (i = p; i < arrays.length; i++) {
+        		unpaddedArray[i-p] = arrays[i];
+        	}
+        }
+        else if (paddtype.equalsIgnoreCase("right")) {
+        	for (i = 0; i < arrays.length-p; i++) {
+        		unpaddedArray[i] = arrays[i];
+        	}
+        }
+        
+        else {
+        	System.err.println("In _unpadd_symmetric paddtype = " + paddtype);
+        	System.err.println("paddtype should be left, center, or right");
+        	System.exit(-1);
+        }
+        return unpaddedArray;
+	}
+	
+	public double[][] _unpadd_symetric(double arrays[][], int p, String paddtype) {
+	    // Helper function to unpadd in an assymetric way arrays.
+
+	    // Parameters
+	    // ----------
+		// arrays : list of np.ndarray
+        // list of arrays to padd.
+
+
+	    // p :  int,
+	    //    length of padding.
+
+	    // paddtype : left, right, or center,
+	    //    where to place the padding.
+
+	    // Results
+	    // -------
+		// arrays : list of np.ndarray
+        // the list of arrays.
+
+		
+		int p_left;
+		int p_right;
+		int i,j;
+		double unpaddedArray[][] = new double[arrays.length][];
+		for (i = 0; i < arrays.length; i++) {
+            unpaddedArray[i] = new double[arrays[i].length-p];
+			if (paddtype.equalsIgnoreCase("center")) {
+	            p_left = (int)(p / 2);
+	            p_right = (int)(p / 2) + (p % 2);
+	            for (j = p_left; j < arrays[i].length - p_right; j++) {
+	            	unpaddedArray[i][j-p_left] = arrays[i][j];
+	            }
+			}
+	        else if (paddtype.equalsIgnoreCase("left")) {
+	        	for (j = p; j < arrays[i].length; j++) {
+	        		unpaddedArray[i][j-p] = arrays[i][j];
+	        	}
+	        }
+	        else if (paddtype.equalsIgnoreCase("right")) {
+	        	for (j = 0; j < arrays[i].length-p; j++) {
+	        		unpaddedArray[i][j] = arrays[i][j];
+	        	}
+	        }
+	        
+	        else {
+	        	System.err.println("In _unpadd_symmetric paddtype = " + paddtype);
+	        	System.err.println("paddtype should be left, center, or right");
+	        	System.exit(-1);
+	        }
+		}
+        return unpaddedArray;
+	}
+	
+	public double[] _unpadd_assymetric(double arrays[], int p[], String paddtype) {
+	    // Helper function to unpadd in assymetric way arrays.
+
+	    // Parameters
+	    // ----------
+	    // arrays : np.ndarray
+	    // array to padd.
+
+	    // p :  int[2]
+	    //    length of padding.
+
+	    // paddtype : ['center'],
+	    //    where to place the padding.
+
+	    // Note:
+	    // -----
+	    // Will raise a ValueError if paddtype is not 'center'.
+
+	    // Results
+	    // -------
+	    // arrays : np.ndarray
+	    //    the unpadded array.
+		int i;
+		   
+		if (!paddtype.equalsIgnoreCase("center")) {
+			System.err.println("In _unpadd_asymmetric paddtype = " + paddtype);
+        	System.err.println("paddtype should center");
+        	System.exit(-1);	
+		}
+
+		double unpaddedArray[] = new double[arrays.length-p[0]-p[1]];
+		for (i = p[0]; i < p[0] + arrays.length; i++) {
+			unpaddedArray[i-p[0]] = arrays[i];
+		}
+		return unpaddedArray;
+	}
+	
+	public double[][] _unpadd_assymetric(double arrays[][], int p[], String paddtype) {
+	    // Helper function to unpadd in assymetric way arrays.
+
+	    // Parameters
+	    // ----------
+		// arrays : list of np.ndarray
+        // list of arrays to padd.
+
+
+	    // p :  int[2]
+	    //    length of padding.
+
+	    // paddtype : ['center'],
+	    //    where to place the padding.
+
+	    // Note:
+	    // -----
+	    // Will raise a ValueError if paddtype is not 'center'.
+
+	    // Results
+	    // -------
+		// arrays : list of np.ndarray
+        // list of arrays.
+
+		int i,j;
+		   
+		if (!paddtype.equalsIgnoreCase("center")) {
+			System.err.println("In _unpadd_asymmetric paddtype = " + paddtype);
+        	System.err.println("paddtype should center");
+        	System.exit(-1);	
+		}
+
+		double unpaddedArray[][] = new double[arrays.length][];
+		for (i = 0; i < arrays.length; i++) {
+			unpaddedArray[i] = new double[arrays[i].length-p[0]-p[1]];
+			for (j = p[0]; j < p[0] + arrays[i].length; j++) {
+				unpaddedArray[i][j-p[0]] = arrays[i][j];
+			}
+		}
+		return unpaddedArray;
+	}
 }
